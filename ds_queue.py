27b66e4697f37b718d5a60a6bfdab800af44e9cb
bowen0701 <bowen0701@gmail.com>
2017-06-24 23:01:18 +0800

    Complete queue.py

--- ds_queue.py
+++ ds_queue.py
@@ -2,12 +2,17 @@ from __future__ import print_function
 
 
 class Queue(object):
-    pass
+	def __init__(self):
+		self.items = []
 
+	def is_empty(self):
+		return self.items == []
 
-def main():
-    pass
+	def enqueue(self, item):
+		self.items.insert(0, item)
 
-
-if __name__ == '__main__':
-    main()
+	def dequeue(self):
+		return self.items.pop()
+	
+	def size(self):
+		return len(self.items)
