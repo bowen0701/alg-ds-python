a2e6ae28446141e95ba4e2e53e825eba8f42f690
bowen0701 <bowen0701@gmail.com>
2017-06-25 18:08:43 +0800

    Dev simulate_print_tasks()

--- alg_print_tasks.py
+++ alg_print_tasks.py
@@ -4,35 +4,53 @@ from ds_queue import Queue
 
 
 class Printer(object):
+	"""A printer class."""
 	def __init__(self, pages_per_minute):
-		pass
+		self.page_rate = pages_per_minute
+		self.current_task = None
+		self.time_remaining = 0
 
 	def tick(self):
-		pass
+		if self.current_task is not None:
+			self.time_remaining = self.time_remaining - 1
+			if self.time_remaining == 0:
+				self.current_task = None
 
 	def busy(self):
-		pass
+		if self.current_task is not None:
+			return True
+		else:
+			return False
 
 	def start_next(self, new_task):
-		pass
+		self.current_task = new_task
+		self.time_remaining = (
+			new_task.get_pages() / (pages_per_minute / 60))
 
 
 class Task(object):
 	def __init__(self, time):
-		pass
+		self.timestamp = time
+		self.pages = np.random.random_integers(1, 20) 
 
 	def get_stamp(self):
-		pass
+		return self.timestamp
 
 	def get_pages(self):
-		pass
+		return self.pages
 
 	def wait_time(self, current_time):
-		pass
+		return current_time - self.timestamp
 
 
 def simulate_print_tasks(num_seconds, pages_per_minute):
-	pass
+	printer = Printer(pages_per_minute)
+	print_queueu = Queue()
+
+	waiting_times = []
+
+	for current_second in xrange(num_seconds):
+		pass
 
 
 def create_print_task():
