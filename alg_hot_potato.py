1b45f627df83fa8fbc6b0b818db0360b8022b95e
bowen0701 <bowen0701@gmail.com>
2017-06-25 12:36:43 +0800

    Complete alg_hot_potato.py

--- alg_hot_potato.py
+++ alg_hot_potato.py
@@ -1,5 +1,28 @@
 from __future__ import print_function
-from ds_queue import Qeueu
+from ds_queue import Queue
 
 def pass_hot_potato(name_ls, num):
-	pass
+	name_queue = Queue()
+
+	for name in name_ls:
+		name_queue.enqueue(name)
+
+	while name_queue.size() > 1:
+		for i in xrange(num):
+			name_queue.enqueue(name_queue.dequeue())
+
+		name_queue.dequeue()
+
+	return name_queue.dequeue()
+
+
+def main():
+	name_ls = ['Bowen', 'Bowen1', 'Bowen2']
+	num = 5
+	print(name_ls)
+	print('num: {}'.format(num))
+	print(pass_hot_potato(name_ls, num))
+
+
+if __name__ == '__main__':
+	main()
