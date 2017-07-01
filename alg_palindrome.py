d7fb60f7d77cf448fff47bb51e29f86d14c8e405
bowen0701 <bowen0701@gmail.com>
2017-07-01 08:23:17 +0800

    Add alg: palindrome

---
+++ alg_palindrome.py
@@ -0,0 +1,15 @@
+def match_palindrome(s):
+    """Check palindrom by front & rear match with Deque.
+
+    A palindrome is a string that read the same forward and backward.
+    For example: radar, madam.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
