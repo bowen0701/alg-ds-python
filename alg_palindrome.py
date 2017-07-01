810dbd1206412b992ebe6b6e105beb952babf91f
bowen0701 <bowen0701@gmail.com>
2017-07-01 09:07:40 +0800

    Complete alg: palindrome

--- alg_palindrome.py
+++ alg_palindrome.py
@@ -1,14 +1,42 @@
-def match_palindrome(s):
+from __future__ import print_function
+from __future__ import division
+
+import numpy as np
+
+
+def match_palindrome(a_str):
     """Check palindrom by front & rear match with Deque.
 
     A palindrome is a string that read the same forward and backward.
     For example: radar, madam.
     """
-    pass
+    from ds_deque import Deque
+
+    str_deque = Deque()
+
+    for s in a_str:
+        str_deque.add_rear(s)
+
+    still_match = True
+
+    while str_deque.size() > 1 and still_match:
+        first = str_deque.remove_front()
+        last = str_deque.remove_rear()
+        if first != last:
+            still_match = False
+    
+    return still_match
 
 
 def main():
-    pass
+    a_str = 'madam'
+    print('{0}: {1}'.format(a_str, match_palindrome(a_str)))
+    
+    a_str = 'Bowen'
+    print('{0}: {1}'.format(a_str, match_palindrome(a_str)))
+
+    a_str = 'toot'
+    print('{0}: {1}'.format(a_str, match_palindrome(a_str)))
 
 
 if __name__ == '__main__':
