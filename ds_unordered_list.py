117b202a1c28282a2c27a545c3da29df9e5675ec
bowen0701 <bowen0701@gmail.com>
2017-07-02 22:49:18 +0800

    Add node class for unordered list building block

--- ds_unordered_list.py
+++ ds_unordered_list.py
@@ -1,8 +1,39 @@
 from __future__ import print_function
 
 
+class Node(object):
+    """Node class as building block for unordered list."""
+    def __init__(self, init_data):
+        pass
+
+    def get_data(self):
+        pass
+
+    def get_next(self):
+        pass
+
+    def set_data(self, new_data):
+        pass
+
+    def set_next(self, new_next):
+        pass
+
+
 class List(object):
-    """List class."""
+    """Unordered list class.
+
+    Implement unordered list by a linked list.
+    Operations include the following:
+      - add(item)
+      - remove(ite)
+      - search(item)
+      - is_empty()
+      - length()
+      - append(item)
+      - index(item)
+      - insert(item, pos)
+      - pop(pos)
+    """
     def __init__(self):
         pass
 
