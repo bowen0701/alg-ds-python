410c22f91f328d4b7a303ba0e1dc980c3d0abfab
bowen0701 <bowen0701@gmail.com>
2017-07-02 22:25:26 +0800

    Add ds: unordered list

---
+++ ds_unordered_list.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+
+class List(object):
+    """List class."""
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def search(self, item):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def length(self):
+        pass
+
+    def append(self, item):
+        pass
+
+    def index(self, item):
+        pass
+
+    def insert(self, pos, item):
+        pass
+
+    def pop(self, pos):
+        pass
