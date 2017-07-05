38d250fda6ce42ee6de208e79c582a098925b2aa
bowen0701 <bowen0701@gmail.com>
2017-07-06 01:05:37 +0800

    Complete size, search, append

--- ds_unordered_list.py
+++ ds_unordered_list.py
@@ -39,30 +39,109 @@ class UnorderedList(object):
         self.head = None
 
     def is_empty(self):
-        return self.head == None
+        return self.head is None
 
     def add(self, item):
         temp = Node(item)
         temp.set_next(self.head)
         self.head = temp
 
-    def length(self):
-        pass
-
-    def remove(self, item):
-        pass
+    def size(self):
+        current = self.head
+        counter = 0
+        while current is not None:
+            counter += 1
+            current = current.get_next()
+        return counter
 
     def search(self, item):
-        pass
+        current = self.head
+        found_b = False
+        while not found_b and current != None:
+            if current.get_data() == item:
+                found_b = True
+            else:
+                current = current.get_next()
+        return found_b
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found_b = False
+
+        while not found_b and current is not None:
+            if current.get_data() == item:
+                found_b = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if current is None:
+            print('{}: not existed'.format(item))
+        elif previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
 
     def append(self, item):
-        pass
+        temp = Node(item)
+        current = self.head
+        previous = None
+        while current is not None:
+            previous = current
+            current = current.get_next()
+        previous.set_next(temp)
 
     def index(self, item):
-        pass
+        current = self.head
+        found_b = False
+        counter = 0
+        while not found_b and current is not None:
+            if current.get_data() == item:
+                found_b = True
+            else:
+                counter += 1
+                current = current.get_next()
+        # TODO: not yet completed.
+        return counter
 
     def insert(self, pos, item):
         pass
 
     def pop(self, pos):
         pass
+
+
+def main():
+    a_list = UnorderedList()
+    a_list.add(31)
+    a_list.add(77)
+    a_list.add(17)
+    a_list.add(93)
+    a_list.add(26)
+    a_list.add(54)
+    print('Is empty: {}'.format(a_list.is_empty()))
+    print('Size: {}'.format(a_list.size()))
+
+    print('Search existed 31: {}'.format(a_list.search(31)))
+    print('Search non-existed 100: {}'.format(a_list.search(100)))
+
+    print('Remove non-existed 100.')
+    a_list.remove(100)
+    print('Length: {}'.format(a_list.size()))
+    print('Remove existed 31.')
+    a_list.remove(31)
+    print('Length: {}'.format(a_list.size()))
+    print('Search removed 31: {}'.format(a_list.search(31)))
+
+    print('Append 100.')
+    a_list.append(100)
+    print('Length: {}'.format(a_list.size()))
+
+    print('Index 100: {}'.format(a_list.index(100)))
+    print('Index 54: {}'.format(a_list.index(54)))
+
+
+
+if __name__ == '__main__':
+    main()
