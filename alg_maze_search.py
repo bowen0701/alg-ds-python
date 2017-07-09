1b67c8a7b52fb718bbfe48ba84f26b6d9fe82b29
bowen0701 <bowen0701@gmail.com>
2017-07-10 07:52:07 +0800

    Add alg_maze_search.py

---
+++ alg_maze_search.py
@@ -0,0 +1,39 @@
+"""The maze search problem."""
+from __future__ import print_function
+
+def search_from(maze, start_row, start_col):
+	pass
+
+
+class Maze(object):
+	def __init__(self, maze_file):
+		pass
+
+	def draw_maze(self):
+		pass
+
+	def draw_centered_box(self, x, y, color):
+		pass
+
+	def move_turtle(self, x, y):
+		pass
+
+	def drop_breadcrumb(self, color):
+		pass
+
+	def update_position(self, row, col, val=None):
+		pass
+
+	def is_exit(self, row, col):
+		pass
+
+	def __getitem__(self, idx):
+		pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
