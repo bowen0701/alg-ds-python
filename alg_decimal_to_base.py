92beb1e19e109f3cbc5a7241e51741d8d7323f4b
bowen0701 <bowen0701@gmail.com>
2017-07-09 20:15:56 +0800

    Complete convert_decimal_to_base2()

--- alg_decimal_to_base.py
+++ alg_decimal_to_base.py
@@ -1,11 +1,32 @@
+from __future__ import print_function
 from ds_stack import Stack
 
 
 def convert_decimal_to_base2(dec_num):
 	"""Convert decimal number to binary number."""
-	pass
+	rem_stack = Stack()
+	
+	while dec_num > 0:
+		rem = dec_num % 2
+		rem_stack.push(rem)
+		dec_num = dec_num // 2
+
+    bin_str = ''
+    while not rem_stack.is_empty():
+    	bin_str = bin_str + str(rem_stack.pop())
+
+    return bin_str
 
 
 def convert_decimal_to_base(dec_num, base):
 	"""Convert decimal number to any base."""
 	pass
+
+
+def main():
+	dec_num = 1024
+    print(convert_decimal_to_base2(dec_num))
+
+
+if __name__ == '__main__':
+	main()
