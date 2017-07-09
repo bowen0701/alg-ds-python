9ad82a63b20c37aa22b947091bc85993b7655800
bowen0701 <bowen0701@gmail.com>
2017-07-09 16:26:02 +0800

    Complete sum_list() w/ recursion

--- alg_sum_list.py
+++ alg_sum_list.py
@@ -1,3 +1,18 @@
+from __future__ import print_function
+
+
 def sum_list(num_ls):
     """Sum number list by recursion."""
-    pass
+    if len(num_ls) == 1:
+    	return num_ls[0]
+    else:
+    	return num_ls[0] + sum_list(num_ls[1:])
+
+
+def main():
+	num_ls = [0, 1, 2, 3, 4, 5]
+	print('Sum of {}: {}'.format(num_ls, sum_list(num_ls)))
+
+
+if __name__ == '__main__':
+	main()
