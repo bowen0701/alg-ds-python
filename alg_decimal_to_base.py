34f99271bbe3b0b090aacdc14433d056eb84231a
bowen0701 <bowen0701@gmail.com>
2017-07-09 16:54:07 +0800

    Rename to alg_decimal_to_base.py

---
+++ alg_decimal_to_base.py
@@ -0,0 +1,11 @@
+from ds_stack import Stack
+
+
+def convert_decimal_to_base2(dec_num):
+	"""Convert decimal number to binary number."""
+	pass
+
+
+def convert_decimal_to_base(dec_num, base):
+	"""Convert decimal number to any base."""
+	pass
