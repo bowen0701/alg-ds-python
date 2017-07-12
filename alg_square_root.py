e0af94d0e40b74c8e820f1d179a5daf09001ece4
bowen0701 <bowen0701@gmail.com>
2017-07-13 07:53:37 +0800

    Revise doc string.

--- alg_square_root.py
+++ alg_square_root.py
@@ -1,5 +1,5 @@
 def square_root(n, steps=20):
-    """Newton's Method for approximating square root:
+    """Newton's Method for approximating square root.
     
     - Initial guess: old_guess = n / 2
     - Iterations: new_guess = 1/2 * (old_guess + n / old_guess)
