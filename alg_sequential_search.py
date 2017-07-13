a64e20dbec5cec7b48e5289874be6bac959ff7b7
bowen0701 <bowen0701@gmail.com>
2017-07-14 00:07:40 +0800

    Complete alg_sequential_search.py

--- alg_sequential_search.py
+++ alg_sequential_search.py
@@ -2,11 +2,26 @@ from __future__ import print_function
 
 def sequential_search(a_list, item):
     """Sequential Search."""
-    pass
-
+    pos = 0
+    found_flag = False
+    while pos < len(a_list) and not found_flag:
+    	if a_list[pos] == item:
+    		found_flag = True
+    	else:
+    		pos += 1
+    return found_flag
 
 def main():
-    pass
+    a_list = [54, 26, 93, 17,77, 31, 44, 55, 20, 65]
+    print('a_list: {}'.format(a_list))
+
+    item = 65
+    print('Search item {0}: {1}'
+    	  .format(item, sequential_search(a_list, item)))
+
+    item = 100
+    print('Search item {0}: {1}'
+    	  .format(item, sequential_search(a_list, item)))
 
 
 if __name__ == '__main__':
