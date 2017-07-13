26220f02a800e8194b07acb9c3ca04bfdb3ccdf2
bowen0701 <bowen0701@gmail.com>
2017-07-14 00:00:06 +0800

    Add alg: seq search

---
+++ alg_sequential_search.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+def sequential_search(a_list, item):
+    """Sequential Search."""
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
