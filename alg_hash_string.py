bc0c01f5dd7fc64d0f9d45ae6147323aaa54e8b5
bowen0701 <bowen0701@gmail.com>
2017-07-19 07:22:29 +0800

    Complete hash_string() & weighted_hash_string()

--- alg_hash_string.py
+++ alg_hash_string.py
@@ -1,12 +1,37 @@
 from __future__ import print_function
 
 
-def hash_str(a_str):
-	"""Hash a string by the folding method."""
+def hash_str(a_str, table_size):
+	"""Hash a string by the folding method.
+ 
+    - Get ordinal number for each char.
+    - Sum all of the ordinal numbers.
+    - Return the remainder of the sum with table_size. 
+	"""
+	sum = 0
+	for c in a_str:
+		sum += ord(c)
+	return sum % table_size
+
+
+def weighted_hash_str(a_str, table_size):
+	"""Weighted-Hash a string by the folding method.
+
+    - Get ordinal number for each char.
+    - Weighted-sum all of the ordinal numbers.
+    - Return the remainder of the sum with table_size. 
+	"""
+	sum = 0
+	for i, c in enumerate(a_str):
+		sum += (i + 1) * ord(c)
+	return sum % table_size
 
 
 def main():
-	pass
+	a_str = 'cat'
+	print('For hash_str(): {}'.format(hash_str(a_str, 11)))
+	print('For weighted_hash_str(): {}'
+		  .format(weighted_hash_str(a_str, 11)))
 
 
 if __name__ == '__main__':
