b98716bd0e719e7c9fd0c1dc4d3dcd8ea9ca021b
bowen0701 <bowen0701@gmail.com>
2017-08-10 07:46:18 +0800

    Add GCD abbreviation

--- alg_gcd.py
+++ alg_gcd.py
@@ -1,5 +1,5 @@
 def compute_gcd(m, n):
-    """Compute the greatest common divisor by Euclid's Algorithm."""
+    """Compute the greatest common divisor (GCD) by Euclid's Algorithm."""
     while m % n != 0:
         old_m = m
         old_n = n
