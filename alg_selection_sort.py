4f583b069819ba7e398720c9a08004d5f7585e35
bowen0701 <bowen0701@gmail.com>
2017-08-11 08:12:20 +0800

    Add selection sort

---
+++ alg_selection_sort.py
@@ -0,0 +1,15 @@
+def selection_sort(a_list):
+    """Selection Sort algortihm."""
+    pass
+
+
+def main():
+    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('a_list: {}'.format(a_list))
+    print('By selection sort: ')
+    # selection_sort(a_list)
+    # print(a_list)
+
+
+if __name__ == '__main__':
+    main()
