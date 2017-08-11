10739a0a1829b64ad289f808a755aa5cdab59494
bowen0701 <bowen0701@gmail.com>
2017-08-11 08:22:23 +0800

    Complete selection_sort.py

--- alg_selection_sort.py
+++ alg_selection_sort.py
@@ -1,14 +1,22 @@
 def selection_sort(a_list):
     """Selection Sort algortihm."""
-    pass
+    for max_slot in reversed(range(len(a_list))):
+        select_slot = 0
+        for slot in range(1, max_slot + 1):
+            if a_list[slot] > a_list[select_slot]:
+                select_slot = slot
+
+        temp = a_list[max_slot]
+        a_list[max_slot] = a_list[select_slot]
+        a_list[select_slot] = temp
 
 
 def main():
     a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     print('a_list: {}'.format(a_list))
     print('By selection sort: ')
-    # selection_sort(a_list)
-    # print(a_list)
+    selection_sort(a_list)
+    print(a_list)
 
 
 if __name__ == '__main__':
