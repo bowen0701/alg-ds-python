230168f6b968b0deaafc85e7b19ac065efb46618
bowen0701 <bowen0701@gmail.com>
2017-08-12 23:13:37 +0800

    Add new alg_insertion_sort.py

---
+++ alg_insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(a_list):
+    """Insertion Sort algortihm."""
+    pass
+
+
+def main():
+    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('a_list: {}'.format(a_list))
+    print('By insertion sort: ')
+    insertion_sort(a_list)
+    print(a_list)
+
+
+if __name__ == '__main__':
+    main()
