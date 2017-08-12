fbc094c160ccbcc13e12ccd899e4d019af8fc667
bowen0701 <bowen0701@gmail.com>
2017-08-12 23:27:01 +0800

    Complete alg_insertion_sort.py

--- alg_insertion_sort.py
+++ alg_insertion_sort.py
@@ -1,11 +1,19 @@
 def insertion_sort(a_list):
     """Insertion Sort algortihm."""
-    pass
+    for index in range(1, len(a_list)):
+        current_value = a_list[index]
+        position = index
+
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position - 1]
+            position -= 1
+
+        a_list[position] = current_value
 
 
 def main():
     a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    print('a_list: {}'.format(a_list))
+    print('a_list: \n{}'.format(a_list))
     print('By insertion sort: ')
     insertion_sort(a_list)
     print(a_list)
