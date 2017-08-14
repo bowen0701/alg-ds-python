eaa5d331f315600fb8946d7f48d6c39562e51a76
bowen0701 <bowen0701@gmail.com>
2017-08-15 07:58:12 +0800

    Add alg_shell_sort.py

---
+++ alg_shell_sort.py
@@ -0,0 +1,15 @@
+def shell_sort(a_list):
+    """Shell Sort algortihm."""
+    pass
+
+
+def main():
+    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('a_list: \n{}'.format(a_list))
+    print('By Shell Sort: ')
+    shell_sort(a_list)
+    print(a_list)
+
+
+if __name__ == '__main__':
+    main()
