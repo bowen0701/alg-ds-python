c729f3ccfe1164f4d03a8a53e9106aab21538acd
bowen0701 <bowen0701@gmail.com>
2017-08-15 08:08:30 +0800

    Implementing shell_sort()

--- alg_shell_sort.py
+++ alg_shell_sort.py
@@ -1,6 +1,19 @@
+from __future__ import print_function
+from __future__ import division
+
+
+def _gap_insertion_sort(a_list, start, gap):
+    pass
+
+
 def shell_sort(a_list):
     """Shell Sort algortihm."""
-    pass
+    sublist_count = len(a_list) // 2
+    while sublist_count > 0:
+        for start_pos in range(sublist_count):
+            _gap_insertion_sort(a_list, start_pos, sublist_count)
+        print('After increments of size {0}, a_list is {1}'
+              .format(sublist_count, a_list))
 
 
 def main():
