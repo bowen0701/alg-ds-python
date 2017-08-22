ba1df0970a718481b9310076470e304743fc0c4a
bowen0701 <bowen0701@gmail.com>
2017-08-22 23:29:30 +0800

    Add merge_sort.py

---
+++ alg_merge_sort.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from __future__ import division
+
+
+def merge_sort(a_list):
+    """Merge sort algortihm."""
+    pass
+
+
+def main():
+    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('a_list: \n{}'.format(a_list))
+    print('By Merge Sort: ')
+    merge_sort(a_list)
+
+
+if __name__ == '__main__':
+    main()
