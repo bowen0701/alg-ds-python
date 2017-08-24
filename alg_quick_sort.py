041e89c4be73a42a166d3875d4a613756eeece42
bowen0701 <bowen0701@gmail.com>
2017-08-24 23:57:20 +0800

    Add alg_quick_sort.py

---
+++ alg_quick_sort.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from __future__ import division
+
+
+def quick_sort(a_list):
+    """Quick sort algortihm."""
+    pass
+
+
+def main():
+    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('a_list: \n{}'.format(a_list))
+    print('By quick sort: ')
+    merge_sort(a_list)
+
+
+if __name__ == '__main__':
+    main()
