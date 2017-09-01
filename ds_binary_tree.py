073d69e58b6cd35e149dd395e7a98f1ec14fc9c6
bowen0701 <bowen0701@gmail.com>
2017-09-01 17:28:49 +0900

    Add ds_binary_tree at Narita Airport

---
+++ ds_binary_tree.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class BinaryTree(oject):
+    """Binary Tree using class."""
+    def __init__(self, root):
+        self.key = root
+        self.left_tree = None
+        self.right_tree = None
+
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
