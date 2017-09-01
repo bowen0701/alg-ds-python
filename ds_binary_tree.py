85edd0e25a74c9fb144468adc88b3081acef8ce2
bowen0701 <bowen0701@gmail.com>
2017-09-01 18:07:14 +0900

    Implement binary tree at Narity Airportx

--- ds_binary_tree.py
+++ ds_binary_tree.py
@@ -10,7 +10,28 @@ class BinaryTree(oject):
         self.left_tree = None
         self.right_tree = None
 
-    pass
+    def insert_left(self, new_node):
+        if self.left_tree is None:
+            self.left_tree = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.left = self.left_tree
+            self.left_tree = t
+
+    def insert_right(self, new_node):
+        if self.right_tree is None:
+            self.right_tree = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_tree = self.right_tree
+            self.right_tree = t
+
+    def get_root_value(self):
+        return self.key
+
+    def set_root_value(self):
+        # TODO: here
+        pass
 
 
 def main():
