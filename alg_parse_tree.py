51bf1ec9f08d97d7c3f508d28b9544fb78769113
bowen0701 <bowen0701@gmail.com>
2017-09-03 23:21:29 +0800

    Complete alg: parse_tree

--- alg_parse_tree.py
+++ alg_parse_tree.py
@@ -5,12 +5,60 @@ from ds_stack import Stack
 from ds_binary_tree import BinaryTree
 
 
-def build_parse_tree():
-    pass
+def build_parse_tree(fp_exp):
+    fp_ls = fp_exp.split()
+    par_stack = Stack()
+    parse_tree = BinaryTree('')
+    par_stack.push(parse_tree)
+    current_tree = parse_tree
+    for i in fp_ls:
+        if i == '(':
+            current_tree.insert_left('')
+            par_stack.push(current_tree)
+            current_tree = current_tree.get_left_tree()
+        elif i not in '+-*/)':
+            current_tree.set_root_value(eval(i))
+            parent = par_stack.pop()
+            current_tree = parent
+        elif i in '+-*/':
+            current_tree.set_root_value(i)
+            current_tree.insert_right('')
+            par_stack.push(current_tree)
+            current_tree = current_tree.get_right_tree()
+        elif i == ')':
+            current_tree = par_stack.pop()
+        else:
+            raise ValueError('Unknown operator: {}'.format(i))
+    return parse_tree
+
+
+def eval_parse_tree(parse_tree):
+    import operator
+    opers = {'+': operator.add,
+             '-': operator.sub,
+             '*': operator.mul,
+             '/': operator.truediv}
+
+    left_tree = parse_tree.get_left_tree()
+    right_tree = parse_tree.get_right_tree()
+    if left_tree and right_tree:
+        ft = opers[parse_tree.get_root_value()]
+        return ft(eval_parse_tree(left_tree), 
+                  eval_parse_tree(right_tree))
+    else:
+        return parse_tree.get_root_value()
 
 
 def main():
-    pass
+    fp_exp = '( 3 + ( 4 * 5 ) )'
+    parse_tree = build_parse_tree(fp_exp)
+    val_parse_tree = eval_parse_tree(parse_tree)
+    print('{0}: {1}'.format(fp_exp, val_parse_tree))
+
+    fp_exp = '( ( 5 - 3 ) * ( 4 * 2 ) )'
+    parse_tree = build_parse_tree(fp_exp)
+    val_parse_tree = eval_parse_tree(parse_tree)
+    print('{0}: {1}'.format(fp_exp, val_parse_tree))
 
 
 if __name__ == '__main__':
