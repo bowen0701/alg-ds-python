3fc04d0ab56f5c471ce243aef9236d8ff7870e87
bowen0701 <bowen0701@gmail.com>
2017-09-05 22:17:28 +0800

    Add tree traversal functions

--- ds_binary_tree.py
+++ ds_binary_tree.py
@@ -39,27 +39,37 @@ class BinaryTree(object):
         return self.right_tree
 
 
+def preorder_travel(tree):
+    pass
+
+def inorder_travel(tree):
+    pass
+
+def postorder_travel(tree):
+    pass
+
+
 def main():
-    root = BinaryTree(3)
-    print('root: {}'.format(root.get_root_value()))
+    tree = BinaryTree(3)
+    print('tree: {}'.format(tree.get_root_value()))
 
-    root.insert_left(4)
-    print('root.insert_left(4): {}'
-          .format(root.get_left_tree().get_root_value()))
+    tree.insert_left(4)
+    print('tree.insert_left(4): {}'
+          .format(tree.get_left_tree().get_root_value()))
 
-    root.insert_left(5)
-    print('root.insert_left(5): {}'
-          .format(root.get_left_tree().get_root_value()))
+    tree.insert_left(5)
+    print('tree.insert_left(5): {}'
+          .format(tree.get_left_tree().get_root_value()))
 
-    root.insert_right(6)
-    print('root.insert_right(6): {}'
-          .format(root.get_right_tree().get_root_value()))
+    tree.insert_right(6)
+    print('tree.insert_right(6): {}'
+          .format(tree.get_right_tree().get_root_value()))
 
-    root.insert_right(7)
-    print('root.insert_right(7): {}'
-          .format(root.get_right_tree().get_root_value()))
+    tree.insert_right(7)
+    print('tree.insert_right(7): {}'
+          .format(tree.get_right_tree().get_root_value()))
 
-    left = root.get_left_tree()
+    left = tree.get_left_tree()
     print('left: {}'.format(left.get_root_value()))
 
     left.set_root_value(9)
@@ -70,8 +80,8 @@ def main():
     print('left.insert_left(11): {}'
           .format(left.get_left_tree().get_root_value()))
 
-    print('Get right tree of right tree of root:')
-    print(root.get_right_tree()
+    print('Get right tree of right tree of tree:')
+    print(tree.get_right_tree()
               .get_right_tree()
               .get_root_value())
 
