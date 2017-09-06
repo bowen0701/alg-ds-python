c3dbe16776ee92569a4cea050e49cb32b16fb232
bowen0701 <bowen0701@gmail.com>
2017-09-06 23:35:23 +0800

    Revise: root -> tree

--- ds_binary_tree_ft.py
+++ ds_binary_tree_ft.py
@@ -3,80 +3,80 @@ from __future__ import division
 from __future__ import print_function
 
 
-def binary_tree(root):
+def binary_tree(tree):
     """Binary tree using list of list."""
-    return [root, [], []]
+    return [tree, [], []]
 
 
-def insert_left(root, new_branch):
-    left_tree = root.pop(1)
+def insert_left(tree, new_branch):
+    left_tree = tree.pop(1)
     if len(left_tree) > 1:
-        root.insert(1, [new_branch, left_tree, []])
+        tree.insert(1, [new_branch, left_tree, []])
     else:
-        root.insert(1, [new_branch, [], []])
-    return root
+        tree.insert(1, [new_branch, [], []])
+    return tree
 
-def insert_right(root, new_branch):
-    right_tree = root.pop(2)
+def insert_right(tree, new_branch):
+    right_tree = tree.pop(2)
     if len(right_tree) > 1:
-        root.insert(2, [new_branch, [], right_tree])
+        tree.insert(2, [new_branch, [], right_tree])
     else:
-        root.insert(2, [new_branch, [], []])
-    return root
+        tree.insert(2, [new_branch, [], []])
+    return tree
 
 
-def get_root_value(root):
-    return root[0]
+def get_root_value(tree):
+    return tree[0]
 
-def set_root_value(root, new_val):
-    root[0] = new_val
+def set_root_value(tree, new_val):
+    tree[0] = new_val
 
-def get_left_tree(root):
-    return root[1]
+def get_left_tree(tree):
+    return tree[1]
 
-def get_right_tree(root):
-    return root[2]
+def get_right_tree(tree):
+    return tree[2]
 
 
-def preorder_travel(root):
+def preorder_travel(tree):
     pass
 
-def inorder_travel(root):
+def inorder_travel(tree):
     pass
 
-def postorder_travel(root):
+def postorder_travel(tree):
     pass
 
 
 def main():
-    root = binary_tree(3)
-    print('root: {}'.format(root))
+    tree = binary_tree(3)
+    print('tree: {}'.format(tree))
 
-    insert_left(root, 4)
-    print('insert_left(root, 4): {}'.format(root))
+    insert_left(tree, 4)
+    print('insert_left(tree, 4): {}'.format(tree))
 
-    insert_left(root, 5)
-    print('insert_left(root, 5): {}'.format(root))
+    insert_left(tree, 5)
+    print('insert_left(root, 5): {}'.format(tree))
 
-    insert_right(root, 6)
-    print('insert_right(root, 6): {}'.format(root))
+    insert_right(tree, 6)
+    print('insert_right(tree, 6): {}'.format(tree))
 
-    insert_right(root, 7)
-    print('insert_right(root, 7): {}'.format(root))
+    insert_right(tree, 7)
+    print('insert_right(tree, 7): {}'.format(tree))
 
-    left = get_left_tree(root)
-    print('get_left_tree(root): {}'.format(left))
+    left = get_left_tree(tree)
+    print('get_left_tree(tree): {}'.format(left))
 
     set_root_value(left, 9)
     print('set_root_value(left, 9): {}'.format(left))
-    print('root: {}'.format(root))
+    print('tree: {}'.format(tree))
 
     insert_left(left, 11)
     print('insert_left(left, 11): {}'.format(left))
-    print('root: {}'.format(root))
+    print('tree: {}'.format(tree))
 
     print('Get right tree of right tree:')
-    print(get_right_tree(get_right_tree(root)))
+    print(get_right_tree(get_right_tree(tree)))
 
 
 if __name__ == '__main__':
