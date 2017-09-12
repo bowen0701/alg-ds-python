52e51af4b9aaa274ef727ce428f129d042dfc595
bowen0701 <bowen0701@gmail.com>
2017-09-12 23:16:36 +0800

    Complete delete_min() and its helper functions

--- ds_binary_heap.py
+++ ds_binary_heap.py
@@ -22,14 +22,33 @@ class BinaryHeap(object):
         self.current_size += 1
         self._percolate_up(self.current_size)
 
-    def _percolate_down(self, i):
-        pass
-
     def _get_min_child(self, i):
-        pass
+        if (i * 2 + 1) > self.current_size:
+            return i * 2
+        else:
+            if self.heap_ls[i * 2] < self.heap_ls[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def _percolate_down(self, i):
+        while (i * 2) <= self.current_size:
+            min_child = _get_min_child(i)
+            if self.heap_ls[i] > self.heap_ls[min_child]:
+                tmp = self.heap_ls[1]
+                self.heap_ls[1] = self.heap_ls[min_child]
+                self.heap_ls[min_child] = tmp
+            else:
+                pass
+            i = min_child
 
     def delete_min(self):
-        pass
+        val_del = self.heap_ls[1]
+        self.heap_ls[1] = self.heap_ls[self.current_size]
+        self.current_size -= 1
+        self.heap_ls.pop()
+        self._percolate_down(1)
+        return val_del
 
 
 def main():
