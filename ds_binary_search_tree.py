4c64ba59be25c31b3d336b0d29a4920fd0f37633
bowen0701 <bowen0701@gmail.com>
2017-09-13 23:25:02 +0800

    Add ds_binary_search_tree.py

---
+++ ds_binary_search_tree.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+class BinarySearchTree(object):
+    def __init__(self):
+        pass
+
+
+def main():
+    bst = BinarySearchTree()
+    pass
+
+
+if __name__ == '__main__':
+    main()
