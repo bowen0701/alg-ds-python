6929798456c53d5b8eeeb33e5f379b242b4f1748
bowen0701 <bowen0701@gmail.com>
2017-09-29 22:58:45 +0800

    Add ds_graph.py

---
+++ ds_graph.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class Graph(object):
+    """Graph class."""
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
