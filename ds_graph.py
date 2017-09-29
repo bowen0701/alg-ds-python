76709e594bd863476c4111185d98ca2551c460d3
bowen0701 <bowen0701@gmail.com>
2017-09-29 23:23:17 +0800

    Add Vertex class and doc strings for 2 classes

--- ds_graph.py
+++ ds_graph.py
@@ -3,8 +3,18 @@ from __future__ import division
 from __future__ import print_function
 
 
+class Vertex(object):
+	"""Vertex class.
+
+    It uses a dict to keep track of the vertices which it's connected.
+	"""
+
+
 class Graph(object):
-    """Graph class."""
+    """Graph class.
+
+    It contains a dict to map vertex name to vertex objects.
+    """
     pass
 
 
