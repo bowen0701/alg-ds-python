2797797497f4f5ad606764815b334321732bef3b
bowen0701 <bowen0701@gmail.com>
2017-09-30 22:39:13 +0800

    Rename fibonacci() to fibonacci_recur()

--- alg_fibonacci.py
+++ alg_fibonacci.py
@@ -7,25 +7,26 @@
 - Fib(n) = Fib(n - 1) + Fib(n - 2)
 """
 
+from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import division
 
-
-def fibonacci(n):
+def fibonacci_recur(n):
     """Get nth number of Fibonacci series by recursion."""
-    if n == 0:
-        return 0
-    elif n == 1 or n == 2:
-        return 1
+    if n <= 1:
+        return n
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci_recur(n - 1) + fibonacci_recur(n - 2)
 
 
 def main():
     import time
     n = 13
-    print('{}th number of Fibonacci series: {}'
-          .format(n, fibonacci(n)))
-
+    
+    start_time = time.time()
+    print('{}th number of Fibonacci series by recursion: {}'
+          .format(n, fibonacci_recur(n)))
+    print('Time: {}'.format(time.time() - start_time))
 
 if __name__ == '__main__':
     main()
