91162995c6425307cb586e663d4bf0241f68d588
bowen0701 <bowen0701@gmail.com>
2017-09-30 22:45:21 +0800

    Complete fiboncci_dp() by dynamic programming

--- alg_fibonacci.py
+++ alg_fibonacci.py
@@ -19,14 +19,31 @@ def fibonacci_recur(n):
         return fibonacci_recur(n - 1) + fibonacci_recur(n - 2)
 
 
+def fibonacci_dp(n):
+    """Get nth number of Fibonacci series by dynamic programming.
+
+    DP performs much faster than recursion.
+    """
+    a, b = 0, 1
+    for _ in xrange(n):
+        a, b = a+b, a
+    return a
+
+
 def main():
     import time
-    n = 13
+    n = 35
     
     start_time = time.time()
     print('{}th number of Fibonacci series by recursion: {}'
           .format(n, fibonacci_recur(n)))
     print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
+    print('{}th number of Fibonacci series by recursion: {}'
+          .format(n, fibonacci_dp(n)))
+    print('Time: {}'.format(time.time() - start_time))
+
 
 if __name__ == '__main__':
     main()
