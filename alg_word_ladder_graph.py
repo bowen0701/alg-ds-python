aa75cb1144e6c25fbe162d19fb786a5fab4d1c0c
bowen0701 <bowen0701@gmail.com>
2017-10-02 23:19:31 +0800

    Implemented creating vertex_dict

--- alg_word_ladder_graph.py
+++ alg_word_ladder_graph.py
@@ -2,17 +2,34 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+WORDS_FILE = 'four_letter_words.txt'
+
 
 def read_words():
-	pass
+	"""Read words file with producing a generator."""
+	with open(WORDS_FILE) as f:
+		for line in f:
+			yield line[:-1]
 
 
-def build_word_ladder_graph():
+def build_word_ladder_graph(words):
+	vertex_dict = {}
+	graph_dict = {}
+
+	# Create buckets of words that are different by one letter.
+	for word in words:
+		for i in xrange(len(word)):
+			bucket = '{0}_{1}'.format(word[:i], word[i+1:])
+			if bucket in vertex_dict:
+				vertex_dict[bucket].append(word)
+			else:
+				vertex_dict[bucket] = list(word)
+
 	pass
 
 
 def main():
-	pass
+	words = read_words()
 
 
 if __name__ == '__main__':
