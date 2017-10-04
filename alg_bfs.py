51b606287ba39383250782eacd1d2a0c4510000f
bowen0701 <bowen0701@gmail.com>
2017-10-04 20:56:37 +0800

    Complete breadth_first_search()

--- alg_bfs.py
+++ alg_bfs.py
@@ -5,13 +5,24 @@ from __future__ import division
 from ds_queue import Queue
 
 
-def breadth_first_search():
-	pass
+def breadth_first_search(graph_dict, start_vertex):
+	queue = Queue()
+	queue.enqueue(start_vertex)
+
+    visited_set = set()
+
+	while queue:
+		path = queue.dequeue()
+		vertex = path[-1]
+		yield vertex, path
+		for neighbor in graph_dict[vertex] - visited_set:
+			visited_set.add(neighbor)
+			queue.enqueue(neighbor)
+
 
 
 def main():
 	pass
-
 
 if __name__ == '__main__':
 	main()
