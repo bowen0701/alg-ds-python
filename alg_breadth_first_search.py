c058c64b1acb673fdbec386e3902048d0c60afc1
bowen0701 <bowen0701@gmail.com>
2017-10-04 21:13:48 +0800

    Revise indentation to 4 spaces

--- alg_breadth_first_search.py
+++ alg_breadth_first_search.py
@@ -6,48 +6,48 @@ from ds_queue import Queue
 
 
 def bfs(graph_dict, start_vertex):
-	queue = Queue()
-	queue.enqueue(start_vertex)
+    queue = Queue()
+    queue.enqueue(start_vertex)
 
     visited_set = set()
 
-	while queue:
-		path = queue.dequeue()
-		vertex = path[-1]
-		yield vertex, path
-		for neighbor in graph_dict[vertex] - visited_set:
-			visited_set.add(neighbor)
-			queue.enqueue(neighbor)
+    while queue:
+        path = queue.dequeue()
+        vertex = path[-1]
+        yield vertex, path
+        for neighbor in graph_dict[vertex] - visited_set:
+            visited_set.add(neighbor)
+            queue.enqueue(neighbor)
 
 
 def traverse(graph_dict, start_vertex, end_vertex):
-	for vertex, path in bfs(graph_dict, start_vertex):
-		if vertex == end_vertex:
-			print(' -> '.join(path))
+    for vertex, path in bfs(graph_dict, start_vertex):
+        if vertex == end_vertex:
+            print(' -> '.join(path))
 
 
 def main():
-	# Small word ladder graph.
-	graph_dict = {
-	    'fool': {'cool', 'pool', 'foil', 'foul'},
-	    'foul': {'fool', 'foil'},
-	    'foil': {'fool', 'foul', 'fail'},
-	    'cool': {'fool', 'cool'},
-	    'fail': {'foil', 'fall'},
-	    'fall': {'fail', 'pall'},
-	    'pool': {'fool', 'cool', 'poll'},
-	    'poll': {'pool', 'pail'},
-	    'pall': {'fall', 'pale', 'poll'},
-	    'pole': {'poll', 'pope', 'pale'},
-	    'pale': {'pall', 'pole', 'sale', 'page'},
-	    'sale': {'pale', 'sage'},
-	    'page': {'pale', 'sage'},
-	    'sage': {'sale', 'page'} 
-	}
+    # Small word ladder graph.
+    graph_dict = {
+        'fool': {'cool', 'pool', 'foil', 'foul'},
+        'foul': {'fool', 'foil'},
+        'foil': {'fool', 'foul', 'fail'},
+        'cool': {'fool', 'cool'},
+        'fail': {'foil', 'fall'},
+        'fall': {'fail', 'pall'},
+        'pool': {'fool', 'cool', 'poll'},
+        'poll': {'pool', 'pail'},
+        'pall': {'fall', 'pale', 'poll'},
+        'pole': {'poll', 'pope', 'pale'},
+        'pale': {'pall', 'pole', 'sale', 'page'},
+        'sale': {'pale', 'sage'},
+        'page': {'pale', 'sage'},
+        'sage': {'sale', 'page'} 
+    }
 
     start_vertex = 'fool'
     end_vertex = 'sage'
-	traverse(graph_dict, start_vertex, end_vertex)
+    traverse(graph_dict, start_vertex, end_vertex)
 
 if __name__ == '__main__':
-	main()
+    main()
