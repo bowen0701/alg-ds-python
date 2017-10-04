b27fdeca8f976bea4c4dd418b58502a5a131258b
bowen0701 <bowen0701@gmail.com>
2017-10-04 20:27:28 +0800

    Add main() and run

--- ds_queue.py
+++ ds_queue.py
@@ -1,19 +1,39 @@
+from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import division
 
 
 class Queue(object):
-	"""Queue class."""
-	def __init__(self):
-		self.items = []
+    """Queue class."""
+    def __init__(self):
+        self.items = []
 
-	def is_empty(self):
-		return self.items == []
+    def is_empty(self):
+        return self.items == []
 
-	def enqueue(self, item):
-		self.items.insert(0, item)
+    def enqueue(self, item):
+        self.items.insert(0, item)
 
-	def dequeue(self):
-		return self.items.pop()
-	
-	def size(self):
-		return len(self.items)
+    def dequeue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+
+
+def main():
+    queue = Queue()
+
+    queue.is_empty()
+
+    queue.enqueue('dog')
+    queue.enqueue(4)
+    queue.enqueue(8.4)
+
+    print('Queue size: {}'.format(queue.size()))
+
+    print('Dequeue: {}'.format(queue.dequeue()))
+    print('Queue size: {}'.format(queue.size()))
+
+if __name__ == '__main__':
+    main()
