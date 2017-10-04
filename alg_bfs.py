a182633b03f654f9d8f933aab3c0aa99f8deadd2
bowen0701 <bowen0701@gmail.com>
2017-10-04 20:03:56 +0800

    Add alg_bfs.py

---
+++ alg_bfs.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+from ds_queue import Queue
+
+
+def breadth_first_search():
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
