94e8e57ab0a6e6b5f43f7fdd71590e62587757d3
bowen0701 <bowen0701@gmail.com>
2017-10-12 21:36:33 +0800

    Add alg_depth_first_search.py

---
+++ alg_depth_first_search.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+
+def dfs(graph_dict, start_vertex):
+    """Depth First Search algorith."""
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
