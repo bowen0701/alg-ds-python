468b3216d10e42a061b5f88974f8b7c822558782
bowen0701 <bowen0701@gmail.com>
2017-10-14 00:40:49 +0800

    Implement dfs()

--- alg_depth_first_search.py
+++ alg_depth_first_search.py
@@ -2,11 +2,14 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+from ds_stack import Stack
 
 
 def dfs(graph_dict, start_vertex):
     """Depth First Search algorith."""
-    pass
+    ls_stack = Stack()
+    ls_stack.push([start_vertex])
+    # print('ls_stack: {}'.format(ls_stack.show()))
 
 
 def main():
