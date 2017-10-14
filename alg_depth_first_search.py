04665b56fc7163f8a886c60c0855e6553c7f2464
bowen0701 <bowen0701@gmail.com>
2017-10-14 23:51:55 +0800

    Implement dfs_recur()

--- alg_depth_first_search.py
+++ alg_depth_first_search.py
@@ -5,11 +5,49 @@ from __future__ import division
 from ds_stack import Stack
 
 
-def dfs(graph_dict, start_vertex):
-    """Depth First Search algorith."""
-    ls_stack = Stack()
-    ls_stack.push([start_vertex])
-    # print('ls_stack: {}'.format(ls_stack.show()))
+# def dfs(graph_dict, start_vertex):
+#     """Depth First Search algorith."""
+#     ls_stack = Stack()
+#     ls_stack.push([start_vertex])
+#     visited_set = set([start_vertex])
+
+#     while ls_stack.size() > 0:
+#         path_ls = ls_stack.pop()
+#         # Take the lastest vertex as the new starting one.
+#         vertex = path_ls[-1]
+#         print('path_ls: {}'.format(path_ls))
+#         print('vertex: {}'.format(vertex))
+#         yield vertex, path_ls
+#         for vertex in graph_dict[vertex] - set([start_vertex]):
+#             visited_set.add(vertex)
+#             ls_stack.push([vertex])
+#             dfs(graph_dict, vertex)
+
+def dfs_recur(graph_dict, start_vertex, end_vertex, path_ls=None):
+    if path_ls is None:
+        path_ls = [start_vertex]
+    else:
+        path_ls.append(start_vertex)
+    print('path_ls: {}'.format(path_ls))
+
+    if start_vertex == end_vertex:
+        return path_ls
+
+    for vertex in graph_dict[start_vertex]:
+        if vertex not in path_ls:
+            path_ls = dfs_recur(graph_dict, vertex, end_vertex, path_ls=path_ls)
+
+    return path_ls
+
+
+def traverse_dfs(graph_dict, start_vertex, end_vertex):
+    """Traverse the depth first search path.
+
+    Take the end_vertex's path from generator of vertex and path_ls. 
+    """
+    for vertex, visited_set in dfs(graph_dict, start_vertex):
+        if vertex == end_vertex:
+            print(' -> '.join(path_ls))
 
 
 def main():
@@ -31,7 +69,13 @@ def main():
         'page': {'pale', 'sage'},
         'sage': {'sale', 'page'} 
     }
-    pass
+    
+    start_vertex = 'fool'
+    end_vertex = 'sage'
+    path_ls = dfs_recur(graph_dict, start_vertex, end_vertex, path_ls=None)
+    print('path_ls: {}'.format(path_ls))
+    # traverse_dfs(graph_dict, start_vertex, end_vertex)
+
 
 if __name__ == '__main__':
     main()
