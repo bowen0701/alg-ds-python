4831c45b53d53d31a6514d5c3e2d0465283b4076
bowen0701 <bowen0701@gmail.com>
2017-10-20 00:09:55 +0800

    Add topolofical_sort_recur(), 2 functions’s doc strings and DAG

--- topological_sort.py
+++ topological_sort.py
@@ -1,9 +1,22 @@
+def topological_sort_recur():
+	"""Topological Sorting by Recursion."""
+	pass
+
 def topological_sort():
+	"""Topological Sorting for Directed Acyclic Graph (DAG)."""
 	pass
 
 
 def main():
-	pass
+	# DAG.
+	adjacency_dict = {
+	    '0': {},
+	    '1': {},
+	    '2': {'3'},
+	    '3': {'1'},
+	    '4': {'0', '1'},
+	    '5': {'0', '2'}
+	}
 
 if __name__ == '__main__':
 	main()
