7c03b870ec46d3d9414db224b08d0a1676567c33
bowen0701 <bowen0701@gmail.com>
2017-10-22 01:47:45 +0800

    Revise changed lines

--- alg_topological_sort.py
+++ alg_topological_sort.py
@@ -2,13 +2,12 @@ def topological_sort_recur(adjacency_dict, start_vertex,
 	                       visited_set, finish_ls):
 	"""Topological Sorting by Recursion."""
 	visited_set.add(start_vertex)
-	for neighbor_vertex in adjacency_dict[start_vertex] - visited_set:
+	for neighbor_vertex in adjacency_dict[start_vertex]:
+		if neighbor_vertex not in visited_set:
 			topological_sort_recur(
-				adjacency_dict, neighbor_vertex,
-	            visited_set, finish_ls)
+				adjacency_dict, neighbor_vertex, visited_set, finish_ls)
 	finish_ls.insert(0, start_vertex)
-	print(finish_ls)
-
+	# print(finish_ls)
 
 def topological_sort(adjacency_dict):
 	"""Topological Sorting for Directed Acyclic Graph (DAG)."""
@@ -17,8 +16,7 @@ def topological_sort(adjacency_dict):
 	for vertex in adjacency_dict:
 		if vertex not in visited_set:
 			topological_sort_recur(
-				adjacency_dict, vertex,
-	            visited_set, finish_ls)
+				adjacency_dict, vertex, visited_set, finish_ls)
 	print(finish_ls)
 
 
