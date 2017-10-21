e30cc7acf88f5b902403e1f960a15b3e552cf4e0
bowen0701 <bowen0701@gmail.com>
2017-10-22 01:37:48 +0800

    Add alg_strongly_connected_graph.py

---
+++ alg_strongly_connected_graph.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+def strongly_connected_graph():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
