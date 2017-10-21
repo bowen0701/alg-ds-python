4dd6765bc6a4f16463da76832604d4efaea2c147
bowen0701 <bowen0701@gmail.com>
2017-10-22 00:06:23 +0800

    Complete topological_sort() & topological_sort_recur()

--- alg_topological_sort.py
+++ alg_topological_sort.py
@@ -1,23 +1,40 @@
 def topological_sort_recur(adjacency_dict, start_vertex,
 	                       visited_set, finish_ls):
 	"""Topological Sorting by Recursion."""
-	pass
+	visited_set.add(start_vertex)
+	for neighbor_vertex in adjacency_dict[start_vertex]:
+		if neighbor_vertex not in visited_set:
+			topological_sort_recur(
+				adjacency_dict, neighbor_vertex,
+	            visited_set, finish_ls)
+	finish_ls.insert(0, start_vertex)
+	print(finish_ls)
+
 
 def topological_sort(adjacency_dict):
 	"""Topological Sorting for Directed Acyclic Graph (DAG)."""
-	pass
+	visited_set = set()
+	finish_ls = []
+	for vertex in adjacency_dict:
+		if vertex not in visited_set:
+			topological_sort_recur(
+				adjacency_dict, vertex,
+	            visited_set, finish_ls)
+	print(finish_ls)
 
 
 def main():
 	# DAG.
 	adjacency_dict = {
-	    '0': {},
-	    '1': {},
-	    '2': {'3'},
-	    '3': {'1'},
-	    '4': {'0', '1'},
-	    '5': {'0', '2'}
+	    'A': {},
+	    'B': {},
+	    'C': {'D'},
+	    'D': {'B'},
+	    'E': {'A', 'B'},
+	    'F': {'A', 'C'}
 	}
+
+	topological_sort(adjacency_dict)
 
 if __name__ == '__main__':
 	main()
