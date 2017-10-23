4744bb60c5de5d388e859055ed0c14ad56af08ef
bowen0701 <bowen0701@gmail.com>
2017-10-24 00:36:09 +0800

    Add import

--- alg_topological_sort.py
+++ alg_topological_sort.py
@@ -1,40 +1,45 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
 def topological_sort_recur(adjacency_dict, start_vertex,
-	                       visited_set, finish_ls):
-	"""Topological Sorting by Recursion."""
-	visited_set.add(start_vertex)
-	for neighbor_vertex in adjacency_dict[start_vertex]:
-		if neighbor_vertex not in visited_set:
-			topological_sort_recur(
-				adjacency_dict, neighbor_vertex, visited_set, finish_ls)
-	finish_ls.insert(0, start_vertex)
-	# print(finish_ls)
+                           visited_set, finish_ls):
+    """Topological Sorting by Recursion."""
+    visited_set.add(start_vertex)
+    for neighbor_vertex in adjacency_dict[start_vertex]:
+        if neighbor_vertex not in visited_set:
+            topological_sort_recur(
+                adjacency_dict, neighbor_vertex, visited_set, finish_ls)
+    finish_ls.insert(0, start_vertex)
+    # print(finish_ls)
 
 def topological_sort(adjacency_dict):
-	"""Topological Sorting for Directed Acyclic Graph (DAG)."""
-	visited_set = set()
-	finish_ls = []
-	for vertex in adjacency_dict:
-		if vertex not in visited_set:
-			topological_sort_recur(
-				adjacency_dict, vertex, visited_set, finish_ls)
-	print(finish_ls)
+    """Topological Sorting for Directed Acyclic Graph (DAG)."""
+    visited_set = set()
+    finish_ls = []
+    for vertex in adjacency_dict:
+        if vertex not in visited_set:
+            topological_sort_recur(
+                adjacency_dict, vertex, visited_set, finish_ls)
+    print(finish_ls)
 
 
 def main():
-	# DAG.
-	dag_adjacency_dict = {
-	    'A': {'D'},
-	    'B': {'D'},
-	    'C': {'D'},
-	    'D': {'G', 'E'},
-	    'E': {'J'},
-	    'F': {'G'},
-	    'G': {'I'},
-	    'I': {'J'},
-	    'J': set()
-	}
+    # DAG.
+    dag_adjacency_dict = {
+        'A': {'D'},
+        'B': {'D'},
+        'C': {'D'},
+        'D': {'G', 'E'},
+        'E': {'J'},
+        'F': {'G'},
+        'G': {'I'},
+        'I': {'J'},
+        'J': set()
+    }
 
-	topological_sort(dag_adjacency_dict)
+    topological_sort(dag_adjacency_dict)
 
 if __name__ == '__main__':
-	main()
+    main()
