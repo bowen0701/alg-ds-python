6ad596abaf5ea37aa02e7107b6ea32441d8f72bb
bowen0701 <bowen0701@gmail.com>
2017-10-23 15:33:34 +0800

    Revise recursion arguments

--- alg_fibonacci.py
+++ alg_fibonacci.py
@@ -19,7 +19,7 @@ def fibonacci_recur(n):
     if n <= 1:
         return n
     else:
-        return fibonacci_recur(n - 1) + fibonacci_recur(n - 2)
+        return fibonacci_recur(n-1) + fibonacci_recur(n-2)
 
 
 def fibonacci_dp(n):
