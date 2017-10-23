9f47a9614b9dc188225bfe502e9d5788776704e6
bowen0701 <bowen0701@gmail.com>
2017-10-23 15:50:35 +0800

    Revise alg_tower_of_hanoi.py

--- alg_tower_of_hanoi.py
+++ alg_tower_of_hanoi.py
@@ -1,11 +1,32 @@
 """The tower of Hanoi."""
 from __future__ import print_function
 
+
 def move_towers(height, from_pole, to_pole, with_pole):
-    move_towers(height - 1, from_pole, with_pole, to_pole)
-    move_disk(from_pole, to_pole)
-    move_towers(height - 1, with_pole, to_pole, from_pole)
+    if height == 1:
+        print('Moving disk from {0} to {1}'.format(from_pole, to_pole))
+    else:
+        move_towers(height - 1, from_pole, with_pole, to_pole)
+        move_towers(1, from_pole, to_pole, with_pole)
+        move_towers(height - 1, with_pole, to_pole, from_pole)  
 
 
-def move_disk(from_pole, to_pole):
-    print('Moving disk from {0} to {1}'.format(from_pole, to_pole))
+def main():
+    from_pole = 'f' 
+    to_pole = 't'
+    with_pole = 'w'
+
+    height = 1
+    print('height: {}'.format(height))
+    move_towers(height, from_pole, to_pole, with_pole)
+
+    height = 2
+    print('height: {}'.format(height))
+    move_towers(height, from_pole, to_pole, with_pole)
+
+    height = 5 
+    print('height: {}'.format(height))
+    move_towers(height, from_pole, to_pole, with_pole)
+
+if __name__ == '__main__':
+    main()
