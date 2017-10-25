a09a7ee4f8fcd1fee349a0909ffb5c8d0517f3a1
bowen0701 <bowen0701@gmail.com>
2017-10-25 23:31:27 +0800

    Remove useless import for defaultdict

--- alg_depth_first_search.py
+++ alg_depth_first_search.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from collections import defaultdict
-
 from ds_stack import Stack
 
 
