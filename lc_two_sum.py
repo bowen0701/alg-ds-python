429d609a7a3f54d29ec0ae618c7b1c21b7e25835
bowen0701 <bowen0701@gmail.com>
2017-10-29 22:56:58 +0800

    Add lc_two_sum.py

---
+++ lc_two_sum.py
@@ -0,0 +1,37 @@
+"""Leetcode 1. Two Sum
+
+URL: https://leetcode.com/problems/two-sum/description/
+
+Given an array of integers, return indices of the two numbers such that 
+they add up to a specific target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
