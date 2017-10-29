8fbb9e1967f0a24ea8bf4dd96d67050384f52948
bowen0701 <bowen0701@gmail.com>
2017-10-29 22:59:58 +0800

    Add ds_trie.py

---
+++ ds_trie.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class Node(object):
+    pass
+
+
+class Trie(object):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
