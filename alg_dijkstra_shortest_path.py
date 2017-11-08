41003f73542c565b0bcff468fa9acd975aff4725
bowen0701 <bowen0701@gmail.com>
2017-11-09 00:13:06 +0800

    Add weight_graph dict

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -5,9 +5,19 @@ from __future__ import division
 from ds_binary_heap_tuple import BinaryHeap
 
 def dijkstra(graph, vertex_start):
-	pass
+    pass
 
 def main():
+    weight_graph = {
+        'u': {'v': 2, 'w': 5, 'x': 1},
+        'v': {'u': 2, 'w': 3, 'x': 2},
+        'w': {'u': 5, 'v': 3, 'x': 3, 'y': 1, 'z': 5},
+        'x': {'u': 1, 'v': 2, 'w': 3, 'y': 1},
+        'y': {'w': 1, 'x': 1, 'z': 1},
+        'z': {'w': 5, 'y': 1}
+    }
+    print(weight_graph)
+
     bh = BinaryHeap()
 
 if __name__ == '__main__':
