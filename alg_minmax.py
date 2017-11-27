a0151bb7934beac7f5db3c79c60d7335a594d29a
bowen0701 <bowen0701@gmail.com>
2017-11-28 00:22:54 +0800

    Complete find_min_max_dc() with divide and conquer method

--- alg_minmax.py
+++ alg_minmax.py
@@ -12,6 +12,21 @@ def find_min_max_naive(a_ls):
     return [_min, _max]
 
 
+def find_min_max_dc(a_ls):
+    """Find mix & max in a list by divide and conquer method."""
+    if len(a_ls) == 1:
+        return [a_ls[0], a_ls[0]]
+    elif 1 < len(a_ls) < 3:
+        if a_ls[0] < a_ls[1]:
+            return [a_ls[0], a_ls[1]]
+        else:
+            return [a_ls[1], a_ls[0]]
+    else:
+        [_min1, _max1] = find_min_max_dc(a_ls[:len(a_ls)//2])
+        [_min2, _max2] = find_min_max_dc(a_ls[len(a_ls)//2:])
+        return [min(_min1, _min2), max(_max1, _max2)]
+
+
 def main():
     # a_ls = [1, 2, 3, 4, 5, 6, 7, 8]
     a_ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -20,6 +35,9 @@ def main():
 
     print('By naive method: {}'
           .format(find_min_max_naive(a_ls)))
+
+    print('By divide and conquer method: {}'
+          .format(find_min_max_dc(a_ls)))
 
 if __name__ == '__main__':
     main()
