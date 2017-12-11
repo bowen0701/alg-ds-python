c935bfe434d2e8197ffa50cbd7048834a368a979
bowen0701 <bowen0701@gmail.com>
2017-12-11 21:19:41 +0900

    add alg_factorial.oy

---
+++ alg_factorial.py
@@ -0,0 +1,33 @@
+"""Factorial series:
+
+1!, 2!, 3!, ...
+
+- Factorial(1) = 1! = 1
+- Factorial(2) = 2! = 2
+- Factorial(n) = n! = n * (n - 1)! = n * Factorial(n - 1)
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+def factorial_recur(n):
+    """Get the nth number of Fibonacci series, Fn, by recursion."""
+    pass
+
+
+def factorial_memo(n):
+    """Get the nth number of Fibonacci series, Fn, by memorization."""
+    pass
+
+
+def factorial_dp(n):
+    """Get the nth number of Fibonacci series by dynamic programming."""
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
