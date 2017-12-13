dd529ed0a3f418657f7bdc40150d2464d03d9da3
bowen0701 <bowen0701@gmail.com>
2017-12-13 21:12:01 +0900

    Complete merge_sort_dc() with merge_recur()

--- alg_merge_sort.py
+++ alg_merge_sort.py
@@ -40,26 +40,44 @@ def merge_sort_naive(a_list):
     print('Merge {}'.format(a_list))
 
 
-def merge_recur():
-    pass
-
-def merge_sort_dc_recur():
-    pass
-
+def merge_recur(x_list, y_list):
+    if len(x_list) == 0:
+        return y_list
+    if len(y_list) == 0:
+        return x_list
+    if x_list[0] <= y_list[0]:
+        return [x_list[0]] + merge_recur(x_list[1:], y_list)
+    else:
+        return [y_list[0]] + merge_recur(x_list, y_list[1:])
 
 def merge_iter():
     pass
 
-def merge_sort_dc_iter():
-    pass
+def merge_sort_dc(a_list, merge_ft=merge_recur):
+    if len(a_list) > 1:
+        mid = len(a_list) // 2
+        return merge_ft(merge_sort_dc(a_list[:mid]), 
+                        merge_sort_dc(a_list[mid:]))
+    else:
+        return a_list
 
 
 def main():
+    import time
+
+    start_time = time.time()
     a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     print('a_list: \n{}'.format(a_list))
-    print('By Merge Sort: ')
+    print('By merge sort:')
     merge_sort_naive(a_list)
+    print('Run time: {}'.format(time.time() - start_time))
 
+    start_time = time.time()
+    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('a_list: \n{}'.format(a_list))
+    print('By merge sort with divide and conquer algortihm:')
+    print(merge_sort_dc(a_list))
+    print('Run time: {}'.format(time.time() - start_time))
 
 if __name__ == '__main__':
     main()
