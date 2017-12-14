cd0abc4769380a632a7354d8b956dbb25e8271d1
bowen0701 <bowen0701@gmail.com>
2017-12-15 00:10:34 +0900

    Add default import

--- ds_binary_heap.py
+++ ds_binary_heap.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+
 class BinaryMinHeap(object):
     """Binary Min Heap class."""
     def __init__(self):
