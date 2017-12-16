9cf0908eb32b023e062ea6b39792a40bac445bf1
bowen0701 <bowen0701@gmail.com>
2017-12-16 21:18:56 +0800

    Rename to alg_percentile_selection.py

---
+++ alg_percentile_selection.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def select_percentile(a_list, k):
+	"""Select list's kth percentile.
+
+    Just select the kth element, without caring about
+    the relative ordering of the rest of them.
+	"""
+	pass
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
