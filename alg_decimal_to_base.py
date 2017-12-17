aa4851f231e716f29eee226b43c15e8c235e03ed
bowen0701 <bowen0701@gmail.com>
2017-12-17 23:39:45 +0800

    Revise to use built-in divmod()

--- alg_decimal_to_base.py
+++ alg_decimal_to_base.py
@@ -10,13 +10,12 @@ def convert_decimal_to_base2(dec_num):
     rem_stack = Stack()
     
     while dec_num > 0:
-        rem = dec_num % 2
+        dec_num, rem = divmod(dec_num, 2)
         rem_stack.push(rem)
-        dec_num = dec_num // 2
 
     bin_str = ''
     while not rem_stack.is_empty():
-        bin_str = bin_str + str(rem_stack.pop())
+        bin_str += str(rem_stack.pop())
 
     return bin_str
 
@@ -28,13 +27,12 @@ def convert_decimal_to_base(dec_num, base):
     digits = '0123456789ABCDEF'
 
     while dec_num > 0:
-        rem = dec_num % base
+        dec_num, rem = divmod(dec_num, base)
         rem_stack.push(rem)
-        dec_num = dec_num // base
 
     bin_str = ''
     while not rem_stack.is_empty():
-        bin_str = bin_str + digits[rem_stack.pop()]
+        bin_str += digits[rem_stack.pop()]
 
     return bin_str
 
@@ -45,19 +43,19 @@ def _recur_decimal_to_base(dec_num, base, rem_stack):
     if dec_num < base:
         rem_stack.push(digits[dec_num])
     else:
-        rem_stack.push(digits[dec_num % base])
-        _recur_decimal_to_base(
-            dec_num // base, base, rem_stack)
+        dec_num, rem = divmod(dec_num, base)
+        rem_stack.push(digits[rem])
+        _recur_decimal_to_base(dec_num, base, rem_stack)
     
     return None
 
-def convert_decimal_to_base_by_recur(dec_num, base):
+def convert_decimal_to_base_recur(dec_num, base):
     """Convert decimal number to any base by recussion with Stack."""
     rem_stack = Stack()
     _recur_decimal_to_base(dec_num, base, rem_stack)
     bin_str = ''
     while not rem_stack.is_empty():
-        bin_str = bin_str + rem_stack.pop()
+        bin_str += rem_stack.pop()
     return bin_str
 
 
@@ -75,8 +73,7 @@ def main():
           .format(dec_num, convert_decimal_to_base(dec_num, 16)))
 
     print('Convert {} to base 16: {}'
-          .format(dec_num, convert_decimal_to_base_by_recur(dec_num, 16)))
-
+          .format(dec_num, convert_decimal_to_base_recur(dec_num, 16)))
 
 if __name__ == '__main__':
     main()
