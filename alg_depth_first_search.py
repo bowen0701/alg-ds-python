7dd307c1379d4fe073770160a4a8452c9a14b160
bowen0701 <bowen0701@gmail.com>
2017-12-19 22:21:06 +0800

    Revise graph adjacency dict to graph_adj_d

--- alg_depth_first_search.py
+++ alg_depth_first_search.py
@@ -3,19 +3,22 @@ from __future__ import print_function
 from __future__ import division
 
 
-def dfs():
+def dfs_stack():
     """Depth first search by iteration algorithm using stack."""
     pass
 
 
-def dfs_recur():
+def _dfs_explore():
+    pass
+
+def dfs():
     """Depth first search by recursion algorithm."""
     pass
 
 
 def main():
     # Connected graph by adjacency dictionary.
-    graph_adjacency_dict = {
+    graph_adj_d = {
         'A': ['B', 'D'],
         'B': ['C', 'D'],
         'C': [],
@@ -23,7 +26,7 @@ def main():
         'E': ['B', 'F'],
         'F': ['C'],
     }
-    print('Graph:\n{}'.format(graph_adjacency_dict))
+    print('Graph:\n{}'.format(graph_adj_d))
 
 if __name__ == '__main__':
     main()
