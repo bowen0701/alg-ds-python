1215138f19a7623807fdd3249343d32bbf8d7399
bowen0701 <bowen0701@gmail.com>
2017-12-21 23:43:05 +0800

    Move to alg_topological_sort_bak.py

---
+++ alg_topological_sort_bak.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+def topological_sort_recur(adjacency_dict, start_vertex, visited_set, finish_ls):
+    """Topological Sorting by Recursion."""
+    visited_set.add(start_vertex)
+    for neighbor_vertex in adjacency_dict[start_vertex]:
+        if neighbor_vertex not in visited_set:
+            topological_sort_recur(
+                adjacency_dict, neighbor_vertex, visited_set, finish_ls)
+    finish_ls.insert(0, start_vertex)
+    
+
+def topological_sort(adjacency_dict):
+    """Topological Sorting for Directed Acyclic Graph (DAG)."""
+    visited_set = set()
+    finish_ls = []
+    for vertex in adjacency_dict:
+        if vertex not in visited_set:
+            topological_sort_recur(
+                adjacency_dict, vertex, visited_set, finish_ls)
+    print(finish_ls)
+
+
+def main():
+    # DAG.
+    dag_adjacency_dict = {
+        'A': ['D'],
+        'B': ['D'],
+        'C': ['D'],
+        'D': ['E', 'G'],
+        'E': ['J'],
+        'F': ['G'],
+        'G': ['I'],
+        'I': ['J'],
+        'J': []
+    }
+
+    topological_sort(dag_adjacency_dict)
+
+if __name__ == '__main__':
+    main()
