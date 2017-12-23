243523ee5e70a94914de23d8444478425b7bb782
bowen0701 <bowen0701@gmail.com>
2017-12-23 23:57:01 +0800

    Add helper methods and revise doc string

--- alg_topological_sort.py
+++ alg_topological_sort.py
@@ -3,8 +3,21 @@ from __future__ import print_function
 from __future__ import division
 
 
+def _previsit():
+    pass
+
+def _postvisit():
+    pass
+
+def _dfs_explore():
+    pass
+
 def topological_sort():
-    """Topological Sorting for Directed Acyclic Graph (DAG)."""
+    """Topological Sorting for Directed Acyclic Graph (DAG).
+
+    To topologically sort a DAG, we simply do depth first search,
+    then arrange DAG's vertices in decreasing postvisit order.
+    """
     pass
 
 
