c1bc108fbdffeae6e26466f3c2669990b318c2a5
bowen0701 <bowen0701@gmail.com>
2017-12-23 23:59:40 +0800

    Add check dag

---
+++ alg_check_dag.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+def _previsit():
+    pass
+
+def _postvisit():
+    pass
+
+def _dfs_explore():
+    pass
+
+def checl_dag():
+    """Check DAG."""
+    pass
+
+
+def main():
+    # DAG.
+    dag_adjacency_dict = {
+        'A': ['D'],
+        'B': ['D'],
+        'C': ['D'],
+        'D': ['E', 'G'],
+        'E': ['J'],
+        'F': ['G'],
+        'G': ['I'],
+        'I': ['J'],
+        'J': []
+    }
+
+if __name__ == '__main__':
+    main()
