eacfca844e5ab590acfcd193e2ca1fa379e10009
bowen0701 <bowen0701@gmail.com>
2018-01-06 23:48:54 +0800

    Add strongly connected components's methods

--- alg_strongly_connected_components.py
+++ alg_strongly_connected_components.py
@@ -3,6 +3,26 @@ from __future__ import print_function
 from __future__ import division
 
 
+def _previsit():
+    pass
+
+
+def _postvisit():
+    pass
+
+
+def dfs():
+    pass
+
+
+def _transpose_graph():
+    pass
+
+
+def _inverse_postvisit_vertex():
+    pass
+
+
 def strongly_connected_components():
     """Strongly connected components for graph.
 
@@ -11,7 +31,7 @@ def strongly_connected_components():
         compute finish times for each vertex.
       - Compute the transpose graph G^T of graph G.
       - Call DFS on G^T, but in the main loop of DFS,
-        feed the vertex in the decreasing order of finish times.
+        feed the vertex in the decreasing order of postvisit times.
       - Outpu the vertices of each tree in the DFS forest as
         separate strongly connected components.
     """
