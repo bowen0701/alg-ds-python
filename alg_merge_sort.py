8775230361be0b0a3d1bb2d45244585138c32779
bowen0701 <bowen0701@gmail.com>
2018-01-12 00:01:08 +0800

    Revise to internal functions, move merge sort naive to alg_merge_sort_bak.py

--- alg_merge_sort.py
+++ alg_merge_sort.py
@@ -3,61 +3,24 @@ from __future__ import print_function
 from __future__ import division
 
 
-def merge_sort_naive(a_list):
-    """Merge sort."""
-    print('Binary split: {}'.format(a_list))
-    
-    if len(a_list) > 1:
-        # Merge sort by recursion.
-        mid = len(a_list) // 2
-        left_list = a_list[:mid]
-        right_list = a_list[mid:]
-        merge_sort_naive(left_list)
-        merge_sort_naive(right_list)
-
-        # Merge two small sorted lists.
-        i = 0
-        j = 0
-        k = 0
-        while i < len(left_list) and j < len(right_list):
-            if left_list[i] < right_list[j]:
-                a_list[k] = left_list[i]
-                i += 1
-            else:
-                a_list[k] = right_list[j]
-                j += 1
-            k += 1
-
-        while i < len(left_list):
-            a_list[k] = left_list[i]
-            i += 1
-            k += 1
-
-        while j < len(right_list):
-            a_list[k] = right_list[j]
-            j += 1
-            k += 1
-
-    print('Merge {}'.format(a_list))
-
-
-def merge_recur(x_list, y_list):
+def _merge_recur(x_list, y_list):
     """Merge two sorted lists by recusions."""
     if len(x_list) == 0:
         return y_list
     if len(y_list) == 0:
         return x_list
     if x_list[0] <= y_list[0]:
-        return [x_list[0]] + merge_recur(x_list[1:], y_list)
+        return [x_list[0]] + _merge_recur(x_list[1:], y_list)
     else:
-        return [y_list[0]] + merge_recur(x_list, y_list[1:])
+        return [y_list[0]] + _merge_recur(x_list, y_list[1:])
 
-def merge_iter(x_list, y_list):
+
+def _merge_iter(x_list, y_list):
     """Merge two sorted lists by iteration."""
     z_list = []
     x_pos = 0
     y_pos = 0
-    for z_pos in range(len(x_list) + len(y_list)):
+    for z_pos in xrange(len(x_list) + len(y_list)):
         if x_pos < len(x_list) and y_pos < len(y_list):
             if x_list[x_pos] <= y_list[y_pos]:
                 z_list.append(x_list[x_pos])
@@ -75,7 +38,8 @@ def merge_iter(x_list, y_list):
             pass
     return z_list
 
-def merge_sort(a_list, merge_ft=merge_iter):
+
+def merge_sort(a_list, merge_ft=_merge_iter):
     """Merge sort by divide and conquer algorithm with
     two methods for sorted sub-lists; see merge_ft.
 
@@ -101,23 +65,17 @@ def main():
     start_time = time.time()
     a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     print('a_list: \n{}'.format(a_list))
-    print('By merge sort:')
-    merge_sort_naive(a_list)
-    print('Run time: {}'.format(time.time() - start_time))
-
-    start_time = time.time()
-    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    print('a_list: \n{}'.format(a_list))
     print('By merge sort with divide and conquer algortihm:')
-    print(merge_sort(a_list, merge_ft=merge_recur))
+    print(merge_sort(a_list, merge_ft=_merge_recur))
     print('Run time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
     a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     print('a_list: \n{}'.format(a_list))
     print('By merge sort with iterative algortihm:')
-    print(merge_sort(a_list, merge_ft=merge_iter))
+    print(merge_sort(a_list, merge_ft=_merge_iter))
     print('Run time: {}'.format(time.time() - start_time))
+
 
 if __name__ == '__main__':
     main()
