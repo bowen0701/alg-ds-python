d52ed3ceb0ba7906ff3fc6d3a83d4b20afaffe06
bowen0701 <bowen0701@gmail.com>
2018-01-11 23:30:40 +0800

    Add alg_depth_first_search_stack.py

---
+++ alg_depth_first_search_stack.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def dfs_stack():
+    """Depth first search by iteration algorithm using stack."""
+    pass
+
+
+def main():
+	# Connected graph by adjacency dictionary.
+    graph_adj_d = {
+        'A': ['B', 'D'],
+        'B': ['C', 'D'],
+        'C': [],
+        'D': ['E'],
+        'E': ['B', 'F'],
+        'F': ['C'],
+    }
+    print('Graph:\n{}'.format(graph_adj_d))
+
+
+if __name__ == '__main__':
+	main()
