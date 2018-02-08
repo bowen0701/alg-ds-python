974dc63c206f021ce5903a1cf347ddbbae87486d
bowen0701 <bowen0701@gmail.com>
2018-02-09 00:03:38 +0800

    Complete revising weighted graph

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -16,19 +16,16 @@ def main():
     weighted_graph_d = {
         's': {'a': 2, 'b': 6},
         'a': {'b': 3, 'c': 1},
-        'w': {'u': 5, 'v': 3, 'x': 3, 'y': 1, 'z': 5},
-        'x': {'u': 1, 'v': 2, 'w': 3, 'y': 1},
-        'y': {'w': 1, 'x': 1, 'z': 1},
-        'z': {'w': 5, 'y': 1}
+        'b': {'a': 5, 'd': 2},
+        'c': {'b': 1, 'e': 4, 'f': 2},
+        'd': {'c': 3, 'f': 2},
+        'e': {},
+        'f': {'e': 1}
     }
-    start_vertex = 'x'
+    start_vertex = 's'
     print('weighted_graph_d: {}'.format(weighted_graph_d))
     print('Dijkstra shortest path from {}:'.format(start_vertex))
-    shortest_path_d, vertex_lookup_d = dijkstra(
-        weighted_graph_d, start_vertex)
-    print('shortest_path_d: {}'.format(shortest_path_d))
-    print('vertex_lookup_d: {}'.format(vertex_lookup_d))
-
+    
 
 if __name__ == '__main__':
     main()
