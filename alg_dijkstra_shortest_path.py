a76b2fb07417ccd79b0c359654a42edd5c0fb7e0
bowen0701 <bowen0701@gmail.com>
2018-02-08 23:59:53 +0800

    Revise weighted graph

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -14,8 +14,8 @@ def dijkstra(weighted_graph_d, start_vertex):
 
 def main():
     weighted_graph_d = {
-        'u': {'v': 2, 'w': 5, 'x': 1},
-        'v': {'u': 2, 'w': 3, 'x': 2},
+        's': {'a': 2, 'b': 6},
+        'a': {'b': 3, 'c': 1},
         'w': {'u': 5, 'v': 3, 'x': 3, 'y': 1, 'z': 5},
         'x': {'u': 1, 'v': 2, 'w': 3, 'y': 1},
         'y': {'w': 1, 'x': 1, 'z': 1},
