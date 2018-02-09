a352df7a39b9ae9757613086995612f3a923685c
bowen0701 <bowen0701@gmail.com>
2018-02-09 22:45:50 +0800

    Add ds_min_priority_queue.py

---
+++ ds_min_priority_queue.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class MinPriorityQueue(object):
+	"""Min Priority Queue."""
+	def __init__(self):
+		pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
