6a102294ff0e13c93ad8c17ac4c688498d44ee95
bowen0701 <bowen0701@gmail.com>
2018-02-10 23:53:23 +0800

    Init min priority queue

--- ds_min_priority_queue.py
+++ ds_min_priority_queue.py
@@ -6,7 +6,17 @@ from __future__ import print_function
 class MinPriorityQueue(object):
 	"""Min Priority Queue."""
 	def __init__(self):
-		pass
+		self.heap_ls = [0]
+		self.heap_size = 0
+
+	def parent(i):
+		return i // 2
+
+	def left(i):
+		return 2 * i
+
+	def right(i):
+		return 2 * i + 1
 
 
 def main():
