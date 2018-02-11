e48629066e12e977ca3117a110a61ae488519b7a
bowen0701 <bowen0701@gmail.com>
2018-02-11 15:38:03 +0800

    Complete implementing heap_min(), heap_extract_min(), heap_decrease_kep(), min_heap_insert()

--- ds_min_priority_queue.py
+++ ds_min_priority_queue.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
+
 
 class MinPriorityQueue(object):
 	"""Min Priority Queue."""
@@ -29,6 +31,31 @@ class MinPriorityQueue(object):
     		self.heap_ls[i], self.heap_ls[minimum] = (
     			self.heap_ls[minimum], self.heap_ls[i])
     		min_heapify(self, minimum)
+
+    def heap_min(self):
+    	return self.heap_ls[1]
+
+    def heap_extract_min(self):
+    	if self.heap_size < 1:
+    		raise ValueError('Heap underflow.')
+    	minimum = self.heap_ls[1]
+    	self.heap_ls[1] = self.heap_ls[self.heap_size]
+    	min_heapify(self, 1)
+    	return minimum
+
+    def heap_decrease_key(self, i, key):
+    	if key > self.heap_ls[i]:
+    		raise ValueError('New key is larger than current key.')
+    	self.heap_ls[i] = key
+    	while i > 1 and self.heap_ls[parent(i)] > self.heap_ls[i]:
+    		self.heap_ls[i], self.heap_ls[parent(i)] = (
+    			self.heap_ls[parent(i)], self.heap_ls[i])
+    		i = parent(i)
+
+    def min_heap_insert(self, key):
+    	self.heap_size += 1
+    	self.heap_ls[self.heap_size] = np.inf
+    	heap_decrease_key(self, self.heap_size, key)
 
 
 def main():
