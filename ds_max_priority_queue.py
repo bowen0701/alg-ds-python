e01d45e3ee39023814bca75b1344477e42865b0b
bowen0701 <bowen0701@gmail.com>
2018-02-14 23:13:05 +0800

    Add parent(), left() & right()

--- ds_max_priority_queue.py
+++ ds_max_priority_queue.py
@@ -3,6 +3,16 @@ from __future__ import division
 from __future__ import print_function
 
 
+def parent(i):
+    return i // 2
+
+def left(i):
+    return 2 * i
+
+def right(i):
+    return 2 * i + 1
+
+
 class MaxPriorityQueue(object):
 	"""Max Priority Queue."""
 	def __init__(self):
