53b5319c95e9990b8a6c584712aff21cd63f3fb3
bowen0701 <bowen0701@gmail.com>
2018-02-15 22:04:26 +0800

    Implement max_heapify()

--- ds_max_priority_queue.py
+++ ds_max_priority_queue.py
@@ -16,7 +16,24 @@ def right(i):
 class MaxPriorityQueue(object):
 	"""Max Priority Queue."""
 	def __init__(self):
-		pass
+		self.heap_ls = [0]
+		self.heap_size = 0
+
+	def max_heapify(self, i):
+		l = left(i)
+		r = right(r)
+
+		if l <= self.heap_size and self.heap_ls[l] > self.heap_ls[i]:
+			max_i = l
+		if r <= self.heap_size and self.heap_ls[r] > self.heap_ls[max_i]:
+			max_i = r
+		if max_i != i:
+			self.heap_ls[i], self.heap_ls[max_i] = (
+				self.heap_ls[max_i], self.heap_ls[i])
+			self.max_heapify(max_i)
+
+    # TODO: heap_max(), heap_extract_max(), heap_increase_key(), 
+    # and max_heap_insert(). 
 
 
 def main():
