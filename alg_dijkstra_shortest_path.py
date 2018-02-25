4c64abbb36b5e30efc8d646443452a389c604636
bowen0701 <bowen0701@gmail.com>
2018-02-25 23:27:05 +0800

    Implementing Dijkstra's alg

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -4,6 +4,8 @@ from __future__ import division
 
 import numpy as np
 
+from ds_min_priority_queue import MinPriorityQueue
+
 
 def dijkstra(weighted_graph_d, start_vertex):
     """Dijkstra algorithm for "weighted" graph.
@@ -11,11 +13,16 @@ def dijkstra(weighted_graph_d, start_vertex):
     Finds shortest path in a weighted graph from a particular node 
     to all vertices that are reachable from it.
     """
+    min_pq = MinPriorityQueue()
+
     distance_d = {v: np.inf for v in weighted_graph_d.keys()}
     visited_d = {v: False for v in weighted_graph_d.keys()}
+    previous_d = {v: None for v in weighted_graph_d.keys()}
     
+    min_pq.insert(start_vertex)
     distance_d[start_vertex] = 0
-    visited_d[visited_d] = True
+    visited_d[start_vertex] = True
+
     pass
 
 
