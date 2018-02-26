80baf58dc9716e8e9a11dc8f4cf720d690d55929
bowen0701 <bowen0701@gmail.com>
2018-02-27 00:22:55 +0800

    Complete Dijkstra alg, TODO: revise min priority queue to tuple

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -7,7 +7,7 @@ import numpy as np
 from ds_min_priority_queue import MinPriorityQueue
 
 
-def dijkstra(weighted_graph_d, start_vertex):
+def dijkstra(w_graph_d, start_vertex):
     """Dijkstra algorithm for "weighted" graph.
 
     Finds shortest path in a weighted graph from a particular node 
@@ -15,9 +15,9 @@ def dijkstra(weighted_graph_d, start_vertex):
     """
     min_pq = MinPriorityQueue()
 
-    distance_d = {v: np.inf for v in weighted_graph_d.keys()}
-    visited_d = {v: False for v in weighted_graph_d.keys()}
-    previous_d = {v: None for v in weighted_graph_d.keys()}
+    distance_d = {v: np.inf for v in w_graph_d.keys()}
+    visited_d = {v: False for v in w_graph_d.keys()}
+    previous_d = {v: None for v in w_graph_d.keys()}
     
     min_pq.insert(start_vertex)
     distance_d[start_vertex] = 0
@@ -27,15 +27,19 @@ def dijkstra(weighted_graph_d, start_vertex):
         v = min_pq.extract_min()
         visited_d[v] = True
 
-        for v_neighbor in weighted_graph_d[v].keys():
+        for v_neighbor in w_graph_d[v].keys():
             if (not visited_d[v_neighbor] and 
                 distance_d[v_neighbor] > 
-                    distance_d[v] + weighted_graph_d[v][v_neighbor]):
-                pass
+                    distance_d[v] + w_graph_d[v][v_neighbor]):
+                distance_d[v_neighbor] = distance_d[v] + w_graph_d[v][v_neighbor]
+                previous_d[v_neighbor] = v
+                min_pq.insert(v_neighbor)
+
+    return distance_d, previous_d, visited_d
 
 
 def main():
-    weighted_graph_d = {
+    w_graph_d = {
         's': {'a': 2, 'b': 6},
         'a': {'b': 3, 'c': 1},
         'b': {'a': 5, 'd': 2},
@@ -45,9 +49,16 @@ def main():
         'f': {'e': 1}
     }
     start_vertex = 's'
-    print('weighted_graph_d: {}'.format(weighted_graph_d))
+    print('w_graph_d: {}'.format(w_graph_d))
     print('Dijkstra shortest path from {}:'.format(start_vertex))
     
+    distance_d, previous_d, visited_d = (
+        dijkstra(w_graph_d, start_vertex))
+
+    print('distance_d: {}'.format(distance_d))
+    print('previous_d: {}'.format(distance_d))
+    print('visited_d: {}'.format(visited_d))
+   
 
 if __name__ == '__main__':
     main()
