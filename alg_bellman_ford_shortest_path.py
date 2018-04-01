c3685b98572bf783ba95f737d216fec7f34cfab7
bowen0701 <bowen0701@gmail.com>
2018-04-01 22:37:59 +0800

    Add bellman_ford_shortest_path.py

---
+++ alg_bellman_ford_shortest_path.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+def bellman_ford(w_graph_d, start_vertex):
+    """Bellman-Ford algorithm for weighted / negative graph.
+    """
+    pass
+
+
+def main():
+    w_graph_d = {
+        's': {'a': 2, 'b': 6},
+        'a': {'b': 3},
+        'b': {'a': -5}
+    }
+    start_vertex = 's'
+   
+
+if __name__ == '__main__':
+    main()
