085bc060daef833951717eb4d9131397ec056b90
bowen0701 <bowen0701@gmail.com>
2018-04-05 22:39:07 +0800

    Add Prim's minimum spanning tree alg

---
+++ alg_prim_minimum_spanning_tree.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def prim():
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
