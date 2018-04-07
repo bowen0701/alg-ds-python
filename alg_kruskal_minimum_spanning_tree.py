5296280c9841f3c1119a5e421b961ea13cc099a9
bowen0701 <bowen0701@gmail.com>
2018-04-07 21:37:31 +0800

    Add alg_kruskal_minimum_spanning_tree.py

---
+++ alg_kruskal_minimum_spanning_tree.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def kruskal():
+	"""Kruskal's Minimum Spanning Tree in weighted graph."""
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
