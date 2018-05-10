4349851bfeefa4a3d59d96667e56a1d8feac1c03
bowen0701 <bowen0701@gmail.com>
2018-05-11 00:14:43 +0200

    Complete kruskal(), and add its helper methods's doc string

--- alg_kruskal_minimum_spanning_tree.py
+++ alg_kruskal_minimum_spanning_tree.py
@@ -3,24 +3,42 @@ from __future__ import division
 from __future__ import print_function
 
 
+def sort_edges_by_weight(w_graph_d):
+	pass
+
+
 def make_set(v, previous_d, rank_d):
 	previous_d[v] = v
 	rank_d[v] = 0
+	return previous_d, rank_d
 
 
-def link():
+def link(v, u, previous_d, rank_d):
+	"""Link method.
+
+	Changes the parent pointer of one of roots, say v,
+	and makes it point to y. Return the root of composit tree u.
+	"""
 	pass
 
 
-def find():
+def find(v, previous_d):
+	"""Find method. 
+
+	Make each vertices point directly to the root.
+	"""
 	pass
 
 
-def union():
+def union(v, u, previous_d, rank_d):
+	"""Union by rank method.
+    
+    Make the shorter tree nodes to the root of the longer.
+	"""
 	pass
 
 
-def kruskal():
+def kruskal(w_graph_d):
 	"""Kruskal's algorithm for minimum spanning tree 
 	in undirected weighted graph.
 
@@ -29,7 +47,23 @@ def kruskal():
         = O(|E|log(|V|^2)) 
         = O(|E|log(|V|)).
 	"""
-	pass
+	mst_set = set()
+	sorted_edges_d = sort_edges_by_weight(w_graph_d)
+
+	previous_d = {} 
+	rank_d = {}
+
+	for v in w_graph_d.keys():
+		previous_d, rank_d = make_set(v, previous_d, rank_d)
+
+	for (v, u) in sorted_edges_d.keys():
+		find_v, previous_d = find(v, previous_d)
+		find_u, previous_d = find(u, previous_d)
+		if find_v != find_u:
+			mst_set.add((v, u))
+			u, previous_d, rank_d = union(v, u, previous_d, rank_d)
+
+	return mst_set, previous_d, rank_d
 
 
 def main():
