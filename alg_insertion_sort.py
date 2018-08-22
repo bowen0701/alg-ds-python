eb3e565426dab7785f7add868b6ac07a709b0f6d
bowen0701 <bowen0701@gmail.com>
2018-08-23 00:04:16 +0200

    Apply generator & enumerate()

--- alg_insertion_sort.py
+++ alg_insertion_sort.py
@@ -8,21 +8,17 @@ def insertion_sort(a_list):
 
     Time complexity: O(n^2).
     """
-    for index in range(1, len(a_list)):
-        current_value = a_list[index]
-        position = index
-
-        while position > 0 and a_list[position - 1] > current_value:
-            a_list[position] = a_list[position - 1]
-            position -= 1
-
-        a_list[position] = current_value
+    gen = ((i, v) for i, v in enumerate(a_list) if i > 0)
+    for (i, v) in gen:
+        pos = i
+        while pos > 0 and a_list[pos - 1] > v:
+            a_list[pos] = a_list[pos - 1]
+            pos -= 1
+        a_list[pos] = v
 
 
 def main():
     a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    print('a_list: \n{}'.format(a_list))
-    print('By insertion sort: ')
     insertion_sort(a_list)
     print(a_list)
 
