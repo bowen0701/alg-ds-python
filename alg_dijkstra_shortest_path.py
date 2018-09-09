a5f3889110f5f0ba58f5c8f0a75727f994b6a19c
bowen0701 <bowen0701@gmail.com>
2018-09-09 20:55:41 +0200

    Revise to use ds_min_binary_heap with tuple

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import numpy as np
 
-from ds_min_binary_heap_tuple import MinBinaryHeapTuple
+from ds_min_binary_heap import MinBinaryHeap
 
 
 def dijkstra(w_graph_d, start_vertex):
@@ -13,7 +13,7 @@ def dijkstra(w_graph_d, start_vertex):
 
     Time complexity for graph G(V, E): (|V|+|E|)log(|V|).
     """
-    min_pq = MinBinaryHeapTuple()
+    min_pq = MinBinaryHeap()
 
     distance_d = {v: np.inf for v in w_graph_d.keys()}
     visited_d = {v: False for v in w_graph_d.keys()}
