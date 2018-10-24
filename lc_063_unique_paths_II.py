7f2b47bdf28671a39be10127b21afe42b83ea3fa
bowen0701 <bowen0701@gmail.com>
2018-10-24 22:51:52 +0200

    Add lc_063_unique_paths_II.py

---
+++ lc_063_unique_paths_II.py
@@ -0,0 +1,32 @@
+"""Leetcode 63. Unique Paths II.
+
+A robot is located at the top-left corner of a m x n grid 
+(marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. 
+How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
