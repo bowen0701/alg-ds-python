98e3264c1d71bb1e61df9e78f21ed2f2f48f038b
bowen0701 <bowen0701@gmail.com>
2018-10-27 23:42:19 +0200

    Complete matrix row product

---
+++ alg_matrix_row_product.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def row_product(arr):
+	nrow, ncol = len(arr), len(arr[0])
+	rprod = [1 for _ in range(ncol)]
+
+	for j in range(ncol):
+		for i in range(nrow):
+			rprod[j] *= arr[i][j]
+
+	return rprod
+
+
+def main():
+	arr = [[1, 2, 3], [4, 5, 6]]
+
+	print('Row product: {}'.format(row_product(arr)))
+
+
+if __name__ == '__main__':
+	main()
