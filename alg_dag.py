a40ae10ef93ad734818b5c02e1a8320d3866b841
bowen0701 <bowen0701@gmail.com>
2018-11-11 00:20:01 +0100

    Remove print and rename to dag()

--- alg_dag.py
+++ alg_dag.py
@@ -17,20 +17,22 @@ def _dfs_explore(v, graph_adj_d, visited_d,
                  previsited_d, postvisited_d, clock, dag_bool):
     visited_d[v] = True
     previsited_d, clock = _previsit(v, previsited_d, clock)
+
     for v_neighbor in graph_adj_d[v]:
-        print('{0} -> {1}'.format(v, v_neighbor))
         if (visited_d[v_neighbor]):
-            if (previsited_d[v] > previsited_d[v_neighbor] and 
+            if (previsited_d[v_neighbor] < previsited_d[v] and 
                 not v_neighbor in postvisited_d):
                 dag_bool = False
+
         if not visited_d[v_neighbor]:
             previsited_d, postvisited_d, clock, dag_bool = (
                 _dfs_explore(v_neighbor, graph_adj_d, visited_d, 
                              previsited_d, postvisited_d, clock, dag_bool))
+
     postvisited_d, clock = _postvisit(v, postvisited_d, clock)
     return previsited_d, postvisited_d, clock, dag_bool
 
-def check_dag(graph_adj_d):
+def dag(graph_adj_d):
     """Check Directed Acyclic Graph (DAG)."""
     visited_d = {v: False for v in graph_adj_d.keys()}
     previsited_d = {}
@@ -56,7 +58,7 @@ def main():
         'F': []
     }
     print('dag_adj_d: {}'.format(dag_adj_d))
-    print('Is DAG: {}'.format(check_dag(dag_adj_d)))
+    print('Is DAG: {}'.format(dag(dag_adj_d)))
 
     # non-DAG's adjacency dictionary.
     nondag_adj_d = {
@@ -66,7 +68,7 @@ def main():
         'D': ['B', 'C']
     }
     print('nondag_adj_d: {}'.format(nondag_adj_d))
-    print('Is DAG: {}'.format(check_dag(nondag_adj_d)))
+    print('Is DAG: {}'.format(dag(nondag_adj_d)))
 
 if __name__ == '__main__':
     main()
