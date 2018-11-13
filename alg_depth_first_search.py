43460030ca383e5d9779264a4782d40f05ba9484
bowen0701 <bowen0701@gmail.com>
2018-11-13 22:55:54 +0100

    Simplify using array without redundant output return

--- alg_depth_first_search.py
+++ alg_depth_first_search.py
@@ -4,31 +4,27 @@ from __future__ import division
 
 
 def _previsit(v, previsit_d, clock):
-    clock += 1
-    previsit_d[v] = clock
-    return previsit_d, clock
+    clock[0] += 1
+    previsit_d[v] = clock[0]
 
 
 def _postvisit(v, postvisit_d, clock):
-    clock += 1
-    postvisit_d[v] = clock
-    return postvisit_d, clock
+    clock[0] += 1
+    postvisit_d[v] = clock[0]
 
 
 def _dfs_visit(v, graph_adj_d, visited_d, 
                previsit_d, postvisit_d, clock):
     """DFS visit by recursion."""
     visited_d[v] = True
-    previsit_d, clock = _previsit(v, previsit_d, clock)
+    _previsit(v, previsit_d, clock)
 
     for v_neighbor in graph_adj_d[v]:
         if not visited_d[v_neighbor]:
-            visited_d, previsit_d, postvisit_d, clock = (
-                _dfs_visit(v_neighbor, graph_adj_d, visited_d, 
-                           previsit_d, postvisit_d, clock))
+            _dfs_visit(v_neighbor, graph_adj_d, visited_d, 
+                       previsit_d, postvisit_d, clock)
     
-    postvisit_d, clock = _postvisit(v, postvisit_d, clock)
-    return visited_d, previsit_d, postvisit_d, clock
+    _postvisit(v, postvisit_d, clock)
 
 
 def dfs(graph_adj_d):
@@ -37,15 +33,14 @@ def dfs(graph_adj_d):
     Time complexity for graph G(V, E): O(|V|+|E|). 
     """
     visited_d = {v: False for v in graph_adj_d.keys()}
-    clock = 0
+    clock = [0]
     previsit_d = {}
     postvisit_d = {}
 
     for v in graph_adj_d.keys():
         if not visited_d[v]:
-            visited_d, previsit_d, postvisit_d, clock = (
-                _dfs_visit(v, graph_adj_d, visited_d, 
-                           previsit_d, postvisit_d, clock))
+            _dfs_visit(v, graph_adj_d, visited_d, 
+                       previsit_d, postvisit_d, clock)
 
     return previsit_d, postvisit_d
 
