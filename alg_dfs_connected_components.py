a691bd7e9ff40f690ac514224c10dd324345f832
bowen0701 <bowen0701@gmail.com>
2018-11-14 23:06:41 +0100

    Revise to use dynamic var and rename file

--- alg_dfs_connected_components.py
+++ alg_dfs_connected_components.py
@@ -4,34 +4,29 @@ from __future__ import division
 
 
 def _previsit(v, previsit_d, clock, ccid, ccid_d):
-    clock += 1
-    previsit_d[v] = clock
+    clock[0] += 1
+    previsit_d[v] = clock[0]
     ccid_d[v] = ccid
-    return previsit_d, clock, ccid_d
 
 
 def _postvisit(v, postvisit_d, clock):
-    clock += 1
-    postvisit_d[v] = clock
-    return postvisit_d, clock
+    clock[0] += 1
+    postvisit_d[v] = clock[0]
 
 
-def _dfs_explore(v, graph_adj_d, visited_d, 
-                 previsit_d, postvisit_d, clock, ccid, ccid_d):
-    """DFS explore by recursion."""
+def _dfs_visit(v, graph_adj_d, visited_d, 
+               previsit_d, postvisit_d, clock, ccid, ccid_d):
+    """DFS visit by recursion."""
     visited_d[v] = True
-    previsit_d, clock, ccid_d = _previsit(v, previsit_d, clock, ccid, ccid_d)
+    _previsit(v, previsit_d, clock, ccid, ccid_d)
 
     for v_neighbor in graph_adj_d[v]:
         if not visited_d[v_neighbor]:
-            visited_d, previsit_d, postvisit_d, clock, ccid_d = (
-                _dfs_explore(v_neighbor, graph_adj_d, visited_d, 
-                             previsit_d, postvisit_d, clock, ccid, ccid_d))
+            _dfs_visit(v_neighbor, graph_adj_d, visited_d, 
+                       previsit_d, postvisit_d, clock, ccid, ccid_d)
     
-    postvisit_d, clock = _postvisit(v, postvisit_d, clock)
+    _postvisit(v, postvisit_d, clock)
     
-    return visited_d, previsit_d, postvisit_d, clock, ccid_d
-
 
 def connected_components(graph_adj_d):
     """Connected components in undirected graph by the DFS.
@@ -39,7 +34,7 @@ def connected_components(graph_adj_d):
     Time complexity for graph G(V, E): O(|V|+|E|). 
     """
     visited_d = {v: False for v in graph_adj_d.keys()}
-    clock = 0
+    clock = [0]
     previsit_d = {}
     postvisit_d = {}
 
@@ -50,9 +45,8 @@ def connected_components(graph_adj_d):
     for v in graph_adj_d.keys():
         if not visited_d[v]:
             ccid += 1
-            visited_d, previsit_d, postvisit_d, clock, ccid_d = (
-                _dfs_explore(v, graph_adj_d, visited_d, 
-                             previsit_d, postvisit_d, clock, ccid, ccid_d))
+            _dfs_visit(v, graph_adj_d, visited_d, 
+                       previsit_d, postvisit_d, clock, ccid, ccid_d)
 
     return ccid_d, previsit_d, postvisit_d
 
