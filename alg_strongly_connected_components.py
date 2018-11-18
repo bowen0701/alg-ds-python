42487ffeb90eaabf3c7231a7d390ef9ab11a62ed
bowen0701 <bowen0701@gmail.com>
2018-11-18 22:27:19 +0100

    Revise strongly connected components alg

--- alg_strongly_connected_components.py
+++ alg_strongly_connected_components.py
@@ -3,58 +3,55 @@ from __future__ import print_function
 from __future__ import division
 
 
-def _previsit(v, previsited_d, clock, ccid_d, ccid):
-    clock += 1
-    previsited_d[v] = clock
+def _reverse_graph(graph_adj_d):
+    rev_graph_adj_d = {v: [] for v in graph_adj_d.keys()}
+    for v in graph_adj_d.keys():
+        for v_neighbor in graph_adj_d[v]:
+            rev_graph_adj_d[v_neighbor].append(v)
+    return rev_graph_adj_d
+
+
+def _previsit(v, previsited_d, clock, ccid, ccid_d):
+    clock[0] += 1
+    previsited_d[v] = clock[0]
     ccid_d[v] = ccid
-    return previsited_d, clock, ccid_d, ccid
 
 
 def _postvisit(v, postvisited_d, clock):
-    clock += 1
-    postvisited_d[v] = clock
-    return postvisited_d, clock
+    clock[0] += 1
+    postvisited_d[v] = clock[0]
 
 
 def _dfs_explore(v, graph_adj_d, visited_d, 
-                 previsited_d, postvisited_d, clock, ccid_d, ccid):
+                 previsited_d, postvisited_d, clock, ccid, ccid_d):
     visited_d[v] = True
 
-    previsited_d, clock, ccid_d, ccid = _previsit(
-        v, previsited_d, clock, ccid_d, ccid)
+    _previsit(v, previsited_d, clock, ccid, ccid_d)
    
     for v_neighbor in graph_adj_d[v]:
         if not visited_d[v_neighbor]:
-            visited_d, previsited_d, postvisited_d, clock, ccid_d, ccid = (
-                _dfs_explore(v_neighbor, graph_adj_d, visited_d,
-                             previsited_d, postvisited_d, clock, ccid_d, ccid))
+            _dfs_explore(v_neighbor, graph_adj_d, visited_d,
+                         previsited_d, postvisited_d, clock, ccid, ccid_d)
     
-    postvisited_d, clock = _postvisit(v, postvisited_d, clock)
-    
-    return visited_d, previsited_d, postvisited_d, clock, ccid_d, ccid
+    _postvisit(v, postvisited_d, clock)
 
 
 def dfs(graph_adj_d):
     visited_d = {v: False for v in graph_adj_d.keys()}
-    clock = 0
+    clock = [0]
     previsited_d = {}
     postvisited_d = {}
-    ccid = 1
+
+    ccid = 0
     ccid_d = {}
+
     for v in graph_adj_d.keys():
         if not visited_d[v]:
-            visited_d, previsited, postvisited_d, clock, ccid_d, ccid = (
-                _dfs_explore(v, graph_adj_d, visited_d, 
-                             previsited_d, postvisited_d, clock, ccid_d, ccid))
-    return previsited_d, postvisited_d
+            ccid += 1
+            _dfs_explore(v, graph_adj_d, visited_d, 
+                         previsited_d, postvisited_d, clock, ccid, ccid_d)
 
-
-def _transpose_graph(graph_adj_d):
-    tr_graph_adj_d = {v: [] for v in graph_adj_d.keys()}
-    for v in graph_adj_d.keys():
-        for v_neighbor in graph_adj_d[v]:
-            tr_graph_adj_d[v_neighbor].append(v)
-    return tr_graph_adj_d
+    return ccid_d, previsited_d, postvisited_d
 
 
 def _decrease_postvisit_vertices(postvisited_d):
@@ -66,37 +63,34 @@ def _decrease_postvisit_vertices(postvisited_d):
 
 
 def strongly_connected_components(graph_adj_d):
-    """Strongly connected components in directed graph by the DFS.
+    """Strongly Connected Components for Directed Graph by DFS.
 
     Procedure:
-      - Call the DFS on graph G to 
-        compute finish times for each vertex.
-      - Compute the transpose graph G^T of graph G.
-      - Call DFS on G^T, but in the main loop of DFS,
-        feed the vertex in the decreasing order of postvisit times.
-      - Output the vertices of each tree in the DFS forest as
-        separate strongly connected components.
+      - Obtain the reverse graph G^R of graph G.
+      - Call DFS on G^R in the decreasing order of postvisit number found
+        in step 1.
+      - Output the labeled connected components found in step 2.
     """
-    previsited_d, postvisited_d = dfs(graph_adj_d)
-    tr_graph_adj_d = _transpose_graph(graph_adj_d)
-    dec_postvisited_ls = _decrease_postvisit_vertices(postvisited_d)
+    rev_graph_adj_d = _reverse_graph(graph_adj_d)
+    rev_ccid_d, rev_revisited_d, rev_postvisited_d = dfs(rev_graph_adj_d)
+    dec_rev_postvisited_ls = _decrease_postvisit_vertices(rev_postvisited_d)
 
-    tr_visited_d = {v: False for v in tr_graph_adj_d}
-    clock = 0
-    tr_previsited_d = {}
-    tr_postvisited_d = {}
-    sccid = 1
+    visited_d = {v: False for v in graph_adj_d}
+    clock = [0]
+    previsited_d = {}
+    postvisited_d = {}
+    
+    sccid = 0
     sccid_d = {}
-    for v in dec_postvisited_ls:
-        if not tr_visited_d[v]:
-            (tr_visited_d, tr_previsited_d, tr_postvisited_d, 
-             clock, sccid_d, sccid) = _dfs_explore(
-                v, tr_graph_adj_d, tr_visited_d, 
-                tr_previsited_d, tr_postvisited_d, clock, sccid_d, sccid)
-            sccid += 1
 
-    return (sccid_d, previsited_d, postvisited_d, 
-            tr_graph_adj_d, dec_postvisited_ls)
+    # Run DFS in the decreasing order of reverse graph's postvisit number. 
+    for v in dec_rev_postvisited_ls:
+        if not visited_d[v]:
+            sccid += 1
+            _dfs_explore(v, graph_adj_d, visited_d, 
+                         previsited_d, postvisited_d, clock, sccid, sccid_d)
+
+    return (sccid_d, previsited_d, postvisited_d)
 
 
 def main():
@@ -112,18 +106,12 @@ def main():
         'H': ['H']
     }
 
-    (sccid_d, previsited_d, postvisited_d, 
-     tr_graph_adj_d, dec_postvisited_ls) = strongly_connected_components(graph_adj_d)
+    (sccid_d, previsited_d, postvisited_d) = (
+        strongly_connected_components(graph_adj_d))
 
     print('previsited_d: {}'.format(previsited_d))
     print('postvisited_d: {}'.format(postvisited_d))
-    print('tr_graph_adj_d: {}'.format(tr_graph_adj_d))
-    print('dec_postvisited_ls: {}'.format(dec_postvisited_ls))
-
     print('Strongly connect components: {}'.format(sccid_d))
-
-
-
 
 
 if __name__ == '__main__':
