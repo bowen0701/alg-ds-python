0e925e87a7aa0d185fa97f46dab3897c6687ffa9
bowen0701 <bowen0701@gmail.com>
2018-11-19 23:02:04 +0100

    Add strassen_matrix_multiply.py

---
+++ strassen_matrix_multiply.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def strassen_matrix_multiply():
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
