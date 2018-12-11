c3af39e613e6170e34cf307700ebeb3870d8bbe7
bowen0701 <bowen0701@gmail.com>
2018-12-11 23:17:59 +0100

    Rename to ds_hashmap.py, revise init vars from Prague

--- ds_hashmap.py
+++ ds_hashmap.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
 
 
-class HashTable(object):
-    """Create a HashTable class to implement Map data structure 
+class HashMap(object):
+    """Create a HashMap class to implement Map data structure 
     with key-value mappings.
     """
     def __init__(self, size, weighted_bool=False):
         self.size = size
         self.slots = [None] * self.size
-        self.data = [None] * self.size
+        self.maps = [None] * self.size
 
     def hash(self, key, weighted_bool=False):
         """Hash function for integer or string."""
@@ -32,17 +32,17 @@ class HashTable(object):
         """Rehash function using the linear probing method."""
         return (old_hash + 1) % self.size
 
-    def put(self, key, data):
+    def put(self, key, value):
         key_hash = self.hash(key)
 
-        # If key_hash's slot does not exist, set slot & data as key & data.
+        # If key_hash's slot does not exist, set slots & map as key & value.
         if not self.slots[key_hash]:
             self.slots[key_hash] = key
-            self.data[key_hash] = data
+            self.maps[key_hash] = value
         else:
-            # If key_hash's slot is key, update its data.
+            # If key_hash's slot is key, update its value.
             if self.slots[key_hash] == key:
-                self.data[key_hash] = data
+                self.maps[key_hash] = value
             else:
                 # If collision exists for key_hash, keep rehashing till
                 # new key_hash's slot does not exist or is key.
@@ -53,14 +53,14 @@ class HashTable(object):
 
                 if not self.slots[next_slot]:
                     self.slots[next_slot] = key
-                    self.data[next_slot] = data
+                    self.maps[next_slot] = value
                 else:
-                    self.data[next_slot] = data
+                    self.maps[next_slot] = value
 
     def get(self, key):
         start_key_hash = self.hash(key)
 
-        data = None
+        value = None
         stop_bool = False
         found_bool = False
         key_hash = start_key_hash
@@ -68,24 +68,24 @@ class HashTable(object):
                not found_bool and not stop_bool):
             if self.slots[key_hash] == key:
                 found_bool = True
-                data = self.data[key_hash]
+                value = self.maps[key_hash]
             else:
                 key_hash = self.rehash(key_hash)
                 if key_hash == start_key_hash:
                     stop_bool = True
 
-        return data
+        return value
 
-    def __setitem__(self, key, data):
-        self.put(key, data)
+    def __setitem__(self, key, value):
+        self.put(key, value)
 
     def __getitem__(self, key):
         return self.get(key)
 
 
 def main():
-    print('Hash Table with integer keys:')
-    h = HashTable(11)
+    print('HashMap with integer keys:')
+    h = HashMap(11)
     h[54] = 'cat'
     h[26] = 'dog'
     h[93] = 'lion'
@@ -96,49 +96,49 @@ def main():
     h[55] = 'pig'
     h[20] = 'chicken'   
     print('- h.slots: {}'.format(h.slots))
-    print('- h.data: {}'.format(h.data))
+    print('- h.maps: {}'.format(h.maps))
     print('- h[20]: {}'.format(h[20]))
     print('- h[17]: {}'.format(h[17]))
 
     h[20] = 'duck'
     print('- replace h[20]: {}'.format(h[20]))
-    print('- h.data: {}'.format(h.data))
+    print('- h.maps: {}'.format(h.maps))
 
     print('- h[99]: {}'.format(h[99]))
 
-    print('Hash Table with string keys:')
-    h = HashTable(11)
+    print('Hash Map with string keys:')
+    h = HashMap(11)
     h['cat'] = 'c'
     h['dog'] = 'd'
     h['lion'] = 'l'
     h['tiger'] = 't'
     h['bird'] = 'b'   
     print('- h.slots: {}'.format(h.slots))
-    print('- h.data: {}'.format(h.data))
+    print('- h.maps: {}'.format(h.maps))
     print('- h["dog"]: {}'.format(h['dog']))
     print('- h["cat"]: {}'.format(h['cat']))
 
     h['bird'] = 'bd'
     print('- replacee h["bird"]: {}'.format(h['bird']))
-    print('- h.data: {}'.format(h.data))
+    print('- h.maps: {}'.format(h.maps))
 
     print('- h["pig"]: {}'.format(h['pig']))
 
-    print('Hash Table with string keys by weighted folding method:')
-    h = HashTable(11, weighted_bool=True)
+    print('Hash Map with string keys by weighted folding method:')
+    h = HashMap(11, weighted_bool=True)
     h['cat'] = 'c'
     h['dog'] = 'd'
     h['lion'] = 'l'
     h['tiger'] = 't'
     h['bird'] = 'b'   
     print('- h.slots: {}'.format(h.slots))
-    print('- h.data: {}'.format(h.data))
+    print('- h.maps: {}'.format(h.maps))
     print('- h["dog"]: {}'.format(h['dog']))
     print('- h["cat"]: {}'.format(h['cat']))
 
     h['bird'] = 'bd'
     print('- replace h["bird"]: {}'.format(h['bird']))
-    print('- h.data: {}'.format(h.data))
+    print('- h.maps: {}'.format(h.maps))
 
     print('- h["pig"]: {}'.format(h['pig']))
 
