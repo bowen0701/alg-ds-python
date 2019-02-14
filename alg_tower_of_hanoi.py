5bf2f25cb309f38bc7a48d76c2018768117a456a
bowen0701 <bowen0701@gmail.com>
2019-02-14 22:22:26 +0800

    Revise tower of hanoi alg from Yuanlin

--- alg_tower_of_hanoi.py
+++ alg_tower_of_hanoi.py
@@ -1,32 +1,42 @@
 """The tower of Hanoi."""
+from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import division
 
 
-def move_towers(height, from_pole, to_pole, with_pole):
+def tower_of_hanoi(height, from_pole, to_pole, with_pole, counter):
     if height == 1:
-        print('Moving disk from {0} to {1}'.format(from_pole, to_pole))
+        counter[0] += 1
+        print('{0} -> {1}'.format(from_pole, to_pole))
     else:
-        move_towers(height - 1, from_pole, with_pole, to_pole)
-        move_towers(1, from_pole, to_pole, with_pole)
-        move_towers(height - 1, with_pole, to_pole, from_pole)  
+        tower_of_hanoi(height - 1, from_pole, with_pole, to_pole, counter)
+        tower_of_hanoi(1, from_pole, to_pole, with_pole, counter)
+        tower_of_hanoi(height - 1, with_pole, to_pole, from_pole, counter)  
 
 
 def main():
-    from_pole = 'f' 
-    to_pole = 't'
-    with_pole = 'w'
+    from_pole = 'A' 
+    to_pole = 'B'
+    with_pole = 'C'
 
     height = 1
+    counter = [0]
     print('height: {}'.format(height))
-    move_towers(height, from_pole, to_pole, with_pole)
+    tower_of_hanoi(height, from_pole, to_pole, with_pole, counter)
+    print('counter: {}'.format(counter[0]))
 
     height = 2
+    counter = [0]
     print('height: {}'.format(height))
-    move_towers(height, from_pole, to_pole, with_pole)
+    tower_of_hanoi(height, from_pole, to_pole, with_pole, counter)
+    print('counter: {}'.format(counter[0]))
 
-    height = 5 
+    height = 5
+    counter = [0]
     print('height: {}'.format(height))
-    move_towers(height, from_pole, to_pole, with_pole)
+    tower_of_hanoi(height, from_pole, to_pole, with_pole, counter)
+    print('counter: {}'.format(counter[0]))
+
 
 if __name__ == '__main__':
     main()
