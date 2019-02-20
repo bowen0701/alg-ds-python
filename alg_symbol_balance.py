2226e86115b13d2673dfe433c7c1bef09a68cfc8
bowen0701 <bowen0701@gmail.com>
2019-02-20 19:51:50 +0100

    Refactor scripts, add break & add main()

--- alg_symbol_balance.py
+++ alg_symbol_balance.py
@@ -1,42 +1,44 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from ds_stack import Stack
 
-def balance_parentheses(symbol_str):
-    """Balance parentheses in a string.
 
-    I extend balance parentheses '()' to an "arbitrary" string.
-    """
+def balance_parentheses(symbol_str):
+    """Balance parentheses in a string."""
     s = Stack()
     balanced = True
     index = 0
     while index < len(symbol_str) and balanced:
-    	symbol = symbol_str[index]
-    	if symbol == '(':
-    		s.push(symbol)
-    	elif symbol == ')':
-    		if s.is_empty():
-    			balanced = False
-    		else:
-    			s.pop()
-    	else:
-    		pass
-    	index += 1
+        symbol = symbol_str[index]
+        if symbol == '(':
+            s.push(symbol)
+        elif symbol == ')':
+            if s.is_empty():
+                balanced = False
+                break
+            else:
+                s.pop()
+        else:
+            pass
+        index += 1
 
     if balanced and s.is_empty():
-    	return True
+        return True
     else:
-    	return False
+        return False
 
 
-def _match_symbols(open, close):
-    opens = '([{'
+def _match_symbols(opener, closer):
+    openers = '([{'
     closers = ')]}'
-    match_bool = opens.index(open) == closers.index(close)
+    match_bool = openers.index(opener) == closers.index(closer)
     return match_bool
 
-def balance_symbols(symbol_str):
-    """Balance symbols in a string.
 
-    I extend balance symbols to an "arbitrary" string.
+def balance_symbols(symbol_str):
+    """Balance "arbitrary" symbols in a string.
     The symbols here are '()', '[]' and '{}'. 
     """
     s = Stack()
@@ -49,10 +51,12 @@ def balance_symbols(symbol_str):
         elif symbol in ')]}':
             if s.is_empty():
                 balanced = False
+                break
             else:
-                top = s.pop()
-                if not _match_symbols(top, symbol):
+                open_symbol = s.pop()
+                if not _match_symbols(open_symbol, symbol):
                     balanced = False
+                    break
         else:
             pass
         index += 1
@@ -61,3 +65,43 @@ def balance_symbols(symbol_str):
         return True
     else:
         return False
+
+
+def main():
+    import time
+
+    text_match = '(abcd)'
+
+    start_time = time.time()
+    print('By balance_parentheses(): {}'
+          .format(balance_parentheses(text_match)))
+    print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
+    print('By balance_symbols(): {}'
+          .format(balance_symbols(text_match)))
+    print('Time: {}'.format(time.time() - start_time))
+
+    text_match2 = '([abcd]efg)'
+
+    start_time = time.time()
+    print('By balance_symbols(): {}'
+          .format(balance_symbols(text_match2)))
+    print('Time: {}'.format(time.time() - start_time))
+
+    text_unmatch = '(abcd]'
+
+    start_time = time.time()
+    print('By balance_parentheses(): {}'
+          .format(balance_parentheses(text_unmatch)))
+    print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
+    print('By balance_symbols(): {}'
+          .format(balance_symbols(text_unmatch)))
+    print('Time: {}'.format(time.time() - start_time))
+
+
+
+if __name__ == '__main__':
+    main()
