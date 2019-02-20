e4c0b47609adab3e01aeb965e5fbd262133cef3b
bowen0701 <bowen0701@gmail.com>
2019-02-20 21:53:32 +0100

    Rename to sequential_search_ordered()

--- alg_sequential_search.py
+++ alg_sequential_search.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 
 def sequential_search(a_list, item):
-    """Sequential search."""
+    """Sequential search by iteration."""
     pos = 0
     found_bool = False
     while pos < len(a_list) and not found_bool:
@@ -15,8 +15,8 @@ def sequential_search(a_list, item):
     return found_bool
 
 
-def ordered_sequential_search(a_list, item):
-    """Ordered sequential search."""
+def sequential_search_ordered(a_list, item):
+    """Sequential search by ordering first."""
     a_list = sorted(a_list)
     pos = 0
     found_bool = False
@@ -35,7 +35,6 @@ def ordered_sequential_search(a_list, item):
 def main():
     import time
     a_list = [54, 26, 93, 17,77, 31, 44, 55, 20, 65]
-    print('a_list: {}'.format(a_list))
 
     start_time = time.time()
     item = 65
@@ -44,18 +43,19 @@ def main():
     item = 100
     print('Search item {0}: {1}'
           .format(item, sequential_search(a_list, item)))
-    print('Time for 2 sequential sorts: {}'
+    print('Time for sequential sorts: {}'
           .format(time.time() - start_time))
 
     start_time = time.time()
     item = 65
     print('Search item {0}: {1}'
-          .format(item, ordered_sequential_search(a_list, item)))
+          .format(item, sequential_search_ordered(a_list, item)))
     item = 100
     print('Search item {0}: {1}'
-          .format(item, ordered_sequential_search(a_list, item)))
-    print('Time for 2 sequential sorts: {}'
+          .format(item, sequential_search_ordered(a_list, item)))
+    print('Time for sequential sort ordered: {}'
           .format(time.time() - start_time))
+
 
 if __name__ == '__main__':
     main()
