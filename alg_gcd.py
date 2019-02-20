ce6a3a3833d498fa32a5317fd95e206cad9d5a83
bowen0701 <bowen0701@gmail.com>
2019-02-20 21:39:45 +0100

    Add importing from __future__ & revise main()

--- alg_gcd.py
+++ alg_gcd.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
 def gcd(m, n):
     """Greatest Common Divisor (GCD) by Euclid's Algorithm.
 
@@ -9,14 +14,14 @@ def gcd(m, n):
 
 
 def main():
-    print(gcd(4, 2))
-    print(gcd(2, 4))
+    print('gcd(4, 2): {}'.format(gcd(4, 2)))
+    print('gcd(2, 4): {}'.format(gcd(2, 4)))
 
-    print(gcd(10, 4))
-    print(gcd(4, 10))
+    print('gcd(10, 4): {}'.format(gcd(10, 4)))
+    print('gcd(4, 10): {}'.format(gcd(4, 10)))
 
-    print(gcd(10, 1))
-    print(gcd(1, 10))
+    print('gcd(3, 4): {}'.format(gcd(3, 4)))
+    print('gcd(4, 3): {}'.format(gcd(4, 3)))
 
 
 if __name__ == '__main__':
