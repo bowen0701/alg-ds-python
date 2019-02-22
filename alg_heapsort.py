fc3c8cd86be5ab29efae616782d718ef868bca73
bowen0701 <bowen0701@gmail.com>
2019-02-22 23:13:01 +0100

    Refactor to use oop, heapify_down() & heapsort()

--- alg_heapsort.py
+++ alg_heapsort.py
@@ -13,72 +13,85 @@ def right(i):
     return 2 * i + 1
 
 
-def max_heapify(A, heap_size, i):
-    """Max heapify operation.
+class MaxHeap(object):
+    def __init__(self):
+        self.A = [0]
+        self.size = 0
 
-    Complexity: O(log(n)).
-    """
-    l = left(i)
-    r = right(i)
-    if l <= heap_size and A[l] > A[i]:
-        max_i = l
-    else:
-        max_i = i
-    if r <= heap_size and A[r] > A[max_i]:
-        max_i = r
-    if max_i != i:
-        # Swap node i and node max_i.
-        A[i], A[max_i] = A[max_i], A[i]
-        max_heapify(A, heap_size, max_i)
+    def heapify_down(self, i):
+        """Max heapify down.
+
+        Complexity: O(log(n)).
+        """
+        l = left(i)
+        r = right(i)
+
+        # Get max index from node i and its two child nodes.
+        if l <= self.size and self.A[l] > self.A[i]:
+            max_i = l
+        else:
+            max_i = i
+        if r <= self.size and self.A[r] > self.A[max_i]:
+            max_i = r
+
+        if max_i != i:
+            # Swap node i and node max_i.
+            self.A[i], self.A[max_i] = self.A[max_i], self.A[i]
+            self.heapify_down(max_i)
+
+    def build(self, arr):
+        """Build max heap from unordered array.
+
+        Start from the level-1 nodes from leaves back to level-log(n) node.
+        Specifically, node (n/2), node (n/2 - 1), ..., node 1, where
+        n is the number of nodes including the root one.
+
+        Complexity: O(n*log(n)) via simple analysis. Actually O(n).
+        """
+        self.A.extend(arr)
+        self.size = len(arr)
+        for i in reversed(range(1, (self.size + 1) // 2 + 1)):
+            self.heapify_down(i)
 
 
-def build_max_heap(A):
-    """Build max heap from unordered array.
-
-    Start from the level-1 nodes from leaves back to level-log(n) node.
-    Specifically, node (n/2), node (n/2 - 1), ..., node 1, where
-    n is the number of nodes including the root one.
-
-    Complexity: O(n*log(n)) via simple analysis. Actually O(n).
-    """
-    max_pq = [0]
-    max_pq.extend(A)
-    heap_size = len(A)
-    for i in reversed(range(1, (heap_size + 1) // 2 + 1)):
-        max_heapify(max_pq, heap_size, i)
-    return max_pq, heap_size
-
-
-def heapsort(A):
+def heapsort(arr):
     """Heapsort Algorithm.
 
     Notes:
       - Build max heap from unordered list.
-      - Swap node n and node 1.
+      - Swap node 1 (max) and node n (last).
       - Discard node n from heap by decrementing heap_size.
-      - Run max_heapify to fix max heap violation in node 1.
+      - Run heapify_down() to fix max heap violation in node 1.
       - Go to Step 2 unless heap is empty.
       - Output heapsorted A by ignoring redundant root.
 
     Complexity: O(n*log(n)),
       - since after n iterations the heap is empty,
-      - and every iteration involves a swap and a max_heapify operation,
-        hence it takes O(log(n)) time.
+      - and every iteration involves a swap and a heapify_down(),
+        this takes O(log(n)) time.
     """
-    max_pq, heap_size = build_max_heap(A)
+    max_pq = MaxHeap()
+    max_pq.build(arr)
 
-    while heap_size > 0:
-        max_pq[heap_size], max_pq[1] = max_pq[1], max_pq[heap_size]
-        heap_size -= 1
-        max_heapify(max_pq, heap_size, 1)
+    while max_pq.size > 0:
+        # Swap node 1 (max) and node n (last).
+        max_pq.A[max_pq.size], max_pq.A[1] = (
+            max_pq.A[1], max_pq.A[max_pq.size])
 
-    heapsort_A = max_pq[1:]
-    return heapsort_A
+        # Discard node n from heap by decrementing heap_size
+        max_pq.size -= 1
+        
+        max_pq.heapify_down(1)
+
+    heapsort_arr = max_pq.A[1:]
+    return heapsort_arr
 
 
 def main():
-    arr = [5, 7, 3, 1]
-    print('Heapsort for unordered list {}:'.format(arr))
+    import random
+    arr = [a for a in range(100)]
+    random.shuffle(arr)
+    print('Heapsort for unordered list:')
     print(heapsort(arr))
 
 
