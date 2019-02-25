28380ae260d3a145d553f86b05717962798bb42d
bowen0701 <bowen0701@gmail.com>
2019-02-25 23:55:40 +0100

    Implement revser linked list

--- lc206_reverse_linked_list.py
+++ lc206_reverse_linked_list.py
@@ -24,12 +24,40 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        pass
+        if head is None:
+            return None
+        new_head = ListNode(head.val)
+        current = head
+        previous = None
+        # TODO: Revise reversion.
+        while current.next is not None:
+            print current.val
+            new_head = current
+            current = current.next
+            previous = new_head
+            new_head.next = previous
+        return new_head
 
 
 def main():
-    # print Solution().reverseList(head)
-    pass
+    # 1->2->3->4->5->NULL
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+
+    print node1.val
+    node1.next = node2
+    print node2.val
+    node2.next = node3
+    print node3.val
+    node3.next = node4
+    print node4.val
+    node4.next = node5
+    print node5.val
+
+    # print Solution().reverseList(node1).val
 
 
 if __name__ == '__main__':
