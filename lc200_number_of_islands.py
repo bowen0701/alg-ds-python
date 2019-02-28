cd437e89e74c6c923334be59221524dee914a169
bowen0701 <bowen0701@gmail.com>
2019-02-28 01:47:27 +0100

    Revise visited_grid from list of lists to dict

--- lc200_number_of_islands.py
+++ lc200_number_of_islands.py
@@ -26,21 +26,21 @@ Answer: 3
 
 class Solution(object):
     def dfs(self, r, c, grid, visited_grid, n_rows, n_cols):
-        visited_grid[r][c] = True
+        visited_grid[(r, c)] = True
 
         if 0 <= r - 1: # Up.
-            if grid[r - 1][c] == '1' and not visited_grid[r - 1][c]:
+            if grid[r - 1][c] == '1' and not visited_grid.get((r - 1, c)):
                 self.dfs(r - 1, c, grid, visited_grid, n_rows, n_cols)
         if r + 1 < n_rows: # Down.
-            if grid[r + 1][c] == '1' and not visited_grid[r + 1][c]:
+            if grid[r + 1][c] == '1' and not visited_grid.get((r + 1, c)):
                 self.dfs(r + 1, c, grid, visited_grid, n_rows, n_cols)
         if 0 <= c - 1: # Left.
-            if grid[r][c - 1] == '1' and not visited_grid[r][c - 1]:
+            if grid[r][c - 1] == '1' and not visited_grid.get((r, c - 1)):
                 self.dfs(r, c - 1, grid, visited_grid, n_rows, n_cols)
         if c + 1 < n_cols: # Right.
-            if grid[r][c + 1] == '1' and not visited_grid[r][c + 1]:
+            if grid[r][c + 1] == '1' and not visited_grid.get((r, c + 1)):
                 self.dfs(r, c + 1, grid, visited_grid, n_rows, n_cols)
-    
+
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
@@ -58,12 +58,12 @@ class Solution(object):
             return 0
 
         n_rows, n_cols = len(grid), len(grid[0])
-        visited_grid = [[False for c in range(n_cols)] for r in range(n_rows)]
+        visited_grid = {}
         n_islands = 0
 
         for r in range(n_rows):
             for c in range(n_cols):
-                if grid[r][c] == '1' and not visited_grid[r][c]:
+                if grid[r][c] == '1' and not visited_grid.get((r, c)):
                     n_islands += 1
                     self.dfs(r, c, grid, visited_grid, n_rows, n_cols)
 
@@ -71,6 +71,11 @@ class Solution(object):
 
 
 def main():
+    import time
+
+    start_time = time.time()
+
+    # Num of islands = 1.
     grid1 = [['1', '1', '1', '1', '0'],
              ['1', '1', '0', '1', '0'], 
              ['1', '1', '0', '0', '0'],
@@ -78,12 +83,15 @@ def main():
 
     print Solution().numIslands(grid1)
 
+    # Num of islands = 3.
     grid2 = [['1', '1', '0', '0', '0'],
              ['1', '1', '0', '0', '0'], 
              ['0', '0', '1', '0', '0'],
              ['0', '0', '0', '1', '1']]
 
     print Solution().numIslands(grid2)
+
+    print('Time: {}'.format(time.time() - start_time))
 
 
 if __name__ == '__main__':
