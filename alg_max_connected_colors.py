0ba3dff1e150d534e4eda086ebbd53ec3789d82c
bowen0701 <bowen0701@gmail.com>
2019-02-28 01:23:18 +0100

    Add alg_balance_symbols.py

---
+++ alg_max_connected_colors.py
@@ -0,0 +1,13 @@
+def max_connected_colors():
+    pass
+
+
+def main():
+    # A grid of connected colors: 5 (of 2's).
+    grid = [[1, 1, 2, 3],
+            [1, 2, 3, 2],
+            [3, 2, 2, 2]]
+
+
+if __init__ == '__main__':
+    main()
