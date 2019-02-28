fe3a0bb0d502208c668b490513314fef23b08731
bowen0701 <bowen0701@gmail.com>
2019-03-01 00:45:49 +0100

    Revise DFS by iteration w/ previsit/postvisit, & collect recur/iter versions

--- alg_dfs.py
+++ alg_dfs.py
@@ -15,7 +15,7 @@ def _postvisit(v, postvisit_d, clock):
 
 def _dfs_visit(v, graph_adj_d, visited_d, 
                previsit_d, postvisit_d, clock):
-    """DFS visit by recursion."""
+    """DFS helper by recursion."""
     visited_d[v] = True
     _previsit(v, previsit_d, clock)
 
@@ -27,7 +27,7 @@ def _dfs_visit(v, graph_adj_d, visited_d,
     _postvisit(v, postvisit_d, clock)
 
 
-def dfs(graph_adj_d):
+def dfs_recur(graph_adj_d):
     """Depth First Search (DFS) by recursion algorithm.
 
     Time complexity for graph G(V, E): O(|V|+|E|). 
@@ -46,6 +46,52 @@ def dfs(graph_adj_d):
     return previsit_d, postvisit_d
 
 
+def dfs_iter_visit(v, graph_adj_d, visited_d, 
+                   previsit_d, postvisit_d, clock):
+    """DFS helper by iteration using stack."""
+    # visited_ls.append(v)
+    visited_d[v] = True
+
+    visit_stack = []
+    visit_stack.append(v)
+
+    while visit_stack:
+        visited_set = {v for v in visited_d if visited_d[v] is True}
+        if set(graph_adj_d[visit_stack[-1]]) - visited_set:
+            # Travel stack last node's neighbor, if not visited, 
+            # add the neighbor to visited and directly go to next traverse.
+            for v_neighbor in graph_adj_d[visit_stack[-1]]:
+                if not visited_d[v_neighbor]:
+                    _previsit(v_neighbor, previsit_d, clock)
+                    visited_d[v_neighbor] = True
+                    visit_stack.append(v_neighbor)
+                    break
+        else:
+            # When there is no neighbor to travel, pop out stack last node,
+            # and step back for further traverse.
+            v_pop = visit_stack.pop()
+            _postvisit(v_pop, postvisit_d, clock)
+    return previsit_d, postvisit_d
+
+
+def dfs_iter(graph_adj_d):
+    """Depth First Search (DFS) by iteration algorithm by iterative 
+    algorithm using Stack.
+
+    Time complexity for G(V, E): O(|V|+|E|).
+    """
+    visited_d = {v: False for v in graph_adj_d.keys()}
+    clock = [0]
+    previsit_d = {}
+    postvisit_d = {}
+
+    for v in graph_adj_d.keys():
+        if not visited_d[v]:
+            dfs_iter_visit(v, graph_adj_d, visited_d, 
+                           previsit_d, postvisit_d, clock)
+    return previsit_d, postvisit_d
+
+
 def main():
     # Undirected graph by adjacency list:
     graph_adj_d = {
@@ -58,12 +104,16 @@ def main():
         'G': ['A', 'E'],
         'H': ['C']
     }
-    print('Graph:\n{}'.format(graph_adj_d))
 
     print('Start DFS by recursion.')
-    previsit_d, postvisit_d = dfs(graph_adj_d)
+    previsit_d, postvisit_d = dfs_recur(graph_adj_d)
     print('previsit_d: {}'.format(previsit_d))
     print('postvisit_d: {}'.format(postvisit_d))
+
+    print('Start DFS by iteration.')
+    previsit_dd, postvisit_dd = dfs_iter(graph_adj_d)
+    print('previsit_dd: {}'.format(previsit_dd))
+    print('postvisit_dd: {}'.format(postvisit_dd))
 
 
 if __name__ == '__main__':
