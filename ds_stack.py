67a7ea9d6f194ee522d79bb25daead073e31bd34
bowen0701 <bowen0701@gmail.com>
2019-03-02 23:51:14 +0100

    Complete stack using linked list

--- ds_stack.py
+++ ds_stack.py
@@ -4,7 +4,7 @@ from __future__ import division
 
 
 class StackLs(object):
-    """Stack class."""
+    """Stack using list."""
     def __init__(self):
         self.items = []
 
@@ -27,8 +27,66 @@ class StackLs(object):
         return self.items
 
 
+class ListNode(object):
+    """List Node for StackLl class."""
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class StackLl(object):
+    """Stack using linked list.
+
+    Stack: Top->Node2->Node1->None.
+    """
+    def __init__(self):
+        self.top = None
+
+    def is_empty(self):
+        return self.top is None
+
+    def peek(self):
+        if not self.top:
+            return None
+        return self.top.data
+
+    def push(self, data):
+        node = ListNode(data)
+        if not self.top:
+            top = node
+        node.next = self.top
+        self.top = node
+
+    def pop(self):
+        if not self.top:
+            return None
+        data = self.top.data
+        self.top = self.top.next
+        return data
+
+    def size(self):
+        counter = 0
+        top = self.top
+        while top:
+            counter += 1
+            top = top.next
+        return counter
+
+    def show(self):
+        if not self.top:
+            return []
+        stack = []
+        top = self.top
+        while top:
+            stack.insert(0, top.data)
+            top = top.next
+        return stack
+
+
 def main():
-    s = StackLs()
+    # Initiate Stack instance by StackLs() or StackLl().
+    # s = StackLs()
+    s = StackLl()
     print('Is empty: {}'.format(s.is_empty()))
 
     s.push('dog')
