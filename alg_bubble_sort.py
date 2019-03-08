d72c0a3b42c497b1337ea2a3d3ccfd24d3e57840
bowen0701 <bowen0701@gmail.com>
2019-03-08 23:14:11 +0100

    Revise bubble sort, naive ver, docstrings & main()

--- alg_bubble_sort.py
+++ alg_bubble_sort.py
@@ -3,10 +3,13 @@ from __future__ import print_function
 from __future__ import division
 
 
-def bubble_sort(a_list):
+def bubble_sort_naive(a_list):
     """Bubble Sort algortihm.
 
-    Time complexity: O(n^2). 
+    The list is sorted in place.
+
+    Time complexity: O(n^2).
+    Space complexity: O(1).
     """
     for pass_num in reversed(range(len(a_list))):
         for i in range(pass_num):
@@ -14,30 +17,44 @@ def bubble_sort(a_list):
                 a_list[i + 1], a_list[i] = a_list[i], a_list[i + 1] 
 
 
-def bubble_sort_short(a_list):
-    """Bubble Short algorithm with early stop."""
-    exchange_bool = True
-    pass_num = len(a_list) - 1
-    while pass_num > 0 and exchange_bool:
-        exchange_bool = False
-        for i in range(pass_num):
+def bubble_sort(a_list):
+    """Bubble Sort algorithm with early stop.
+
+    The list is sorted in place.
+
+    Time complexity: O(n^2).
+    Space complexity: O(1).
+    """
+    is_sorted = False
+    end = len(a_list) - 1
+    
+    while not is_sorted and end > 0:
+        is_sorted = True
+        for i in range(end):
             if a_list[i] > a_list[i + 1]:
-                exchange_bool = True
+                is_sorted = False
                 a_list[i + 1], a_list[i] = a_list[i], a_list[i + 1]
-        pass_num -= 1
+        end -= 1
 
 
 def main():
-    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    print('a_list: {}'.format(a_list))
-    
+    import time
+    import random
+
+    a_list = range(100)
+    random.shuffle(a_list)
+
+    start_time = time.time()
+    print('By naive bubble sort: ')
+    bubble_sort_naive(a_list)
+    print(a_list)
+    print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
     print('By bubble sort: ')
     bubble_sort(a_list)
     print(a_list)
-
-    print('By short_bubble sort: ')
-    bubble_sort_short(a_list)
-    print(a_list)
+    print('Time: {}'.format(time.time() - start_time))
 
 
 if __name__ == '__main__':
