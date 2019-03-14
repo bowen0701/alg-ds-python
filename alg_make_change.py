e7f054ad507852e4eef53d0ed412e66d776b7bd9
bowen0701 <bowen0701@gmail.com>
2019-03-14 21:05:00 +0100

    Add alg_make_change.py

---
+++ alg_make_change.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+"""Make Change.
+
+Compute how many ways you can make up that amount. 
+If that amount of money cannot be made up by any combination of the 
+coins, return -1.
+
+Assume that you have an infinite number of each kind of coin.
+"""
+
+
+def make_change_recur(amount, coins):
+    pass
+
+
+def make_change_memo(amount, coins):
+    pass
+
+
+def make_change_dp(amount, coins):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
