9192fe92621d6f79b0f99802f50014d27c967d26
bowen0701 <bowen0701@gmail.com>
2019-03-21 21:13:38 +0100

    Add alg_knapsack.py

---
+++ alg_knapsack.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def main():
+	wt_cap = 10
+	wt = [1, 2, 4, 2, 5]
+	val = [5, 3, 5, 3, 2]
+
+
+if __name__ == '__main__':
+	main()
