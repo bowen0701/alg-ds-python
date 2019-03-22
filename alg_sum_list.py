df10e9a7f59bcf4f827ce4b309cebbeee2f95550
bowen0701 <bowen0701@gmail.com>
2019-03-22 09:14:26 +0100

    Revise sum_list_recur/dp() w/o copying list

--- alg_sum_list.py
+++ alg_sum_list.py
@@ -3,28 +3,30 @@ from __future__ import print_function
 from __future__ import division
 
 
-def sum_list_recur(a_list):
+def sum_list_recur(a_list, n):
     """Sum list by recursion.
 
     Time complexity: O(n).
     Space complexity: O(n).
     """
-    if len(a_list) == 1:
+    if n == 0:
         return a_list[0]
     else:
-        return a_list[-1] + sum_list_recur(a_list[:-1])
+        return a_list[n] + sum_list_recur(a_list, n - 1)
 
 
-def sum_list_dp(a_list):
+def sum_list_dp(a_list, n):
     """Sum list by bottom-up dynamic programming.
 
     Time complexity: O(n).
     Space complexity: O(n).
     """
-    s = [None for _ in range(len(a_list))]
+    s = [None for _ in range(n + 1)]
+
     s[0] = a_list[0]
-    for i, x in enumerate(a_list[1:]):
-        s[i + 1] = x + s[i]
+
+    for i in range(1, n + 1):
+        s[i] = a_list[i] + s[i - 1]
     return s[-1]
 
 
@@ -45,13 +47,14 @@ def main():
     import random
 
     a_list = [random.randint(0, 1000) for _ in range(100)]
+    n = len(a_list) - 1
 
     start_time = time.time()
-    print('By recursion: {}'.format(sum_list_recur(a_list)))
+    print('By recursion: {}'.format(sum_list_recur(a_list, n)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('By DP: {}'.format(sum_list_dp(a_list)))
+    print('By DP: {}'.format(sum_list_dp(a_list, n)))
     print('Time: {}'.format(time.time() - start_time))
     
     start_time = time.time()
