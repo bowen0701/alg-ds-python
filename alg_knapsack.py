9fbe3bfdf9d57803d5cde8453c1241dafae854cb
bowen0701 <bowen0701@gmail.com>
2019-03-23 15:07:43 +0100

    Add alg_knapsack01.py

---
+++ alg_knapsack.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+"""Knapsack Problem
+Given weights and values of n "splittable" items, put these items in a knapsack of 
+capacity to get the maximum total value in the knapsack. 
+"""
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
