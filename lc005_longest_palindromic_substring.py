f7d3fa716cd73c5a066aa0e40c337b50880befea
bowen0701 <bowen0701@gmail.com>
2019-03-25 23:43:37 +0100

    Complete naive longest palindromic substring

--- lc005_longest_palindromic_substring.py
+++ lc005_longest_palindromic_substring.py
@@ -14,22 +14,48 @@ Input: "cbbd"
 Output: "bb"
 """
 
-class Solution(object):
-	def longestPalindrome(self, s):
-		"""
-		:type s: str
+class SolutionNaive(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
         :rtype: str
-		"""
-		pass
+
+        Time limit exceeded.
+        """
+        n = len(s)
+        max_len = 1
+        max_i = 0
+        max_j = 0
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                if s[i:(j + 1)] == s[i:(j + 1)][::-1]:
+                    ij_len = j - i + 1
+                    if ij_len > max_len:
+                        max_len = ij_len
+                        max_i = i
+                        max_j = j
+                else:
+                    break
+        return s[max_i:(max_j + 1)]
+
+
+class SolutionDP(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        pass
 
 
 def main():
-	s = 'babad'
-	print(Solution().longestPalindrome(s))
+    s = 'babad'    # Ans: bab.
+    print(SolutionNaive().longestPalindrome(s))
 
-	s = 'cbbd'
-	print(Solution().longestPalindrome(s))
+    s = 'cbbd'     # Ans: bb.
+    print(SolutionNaive().longestPalindrome(s))
 
 
 if __name__ == '__main__':
-	main()
+    main()
