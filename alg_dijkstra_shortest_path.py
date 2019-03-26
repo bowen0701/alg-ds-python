d4fe617d10236c25429254fbbe954a5c90f6ee9f
bowen0701 <bowen0701@gmail.com>
2019-03-26 08:32:51 +0100

    Revise to use MinHeapAttribute class

--- alg_dijkstra_shortest_path.py
+++ alg_dijkstra_shortest_path.py
@@ -4,26 +4,25 @@ from __future__ import print_function
 
 import numpy as np
 
-from ds_min_binary_heap_attribute import MinBinaryHeapAttribute
+from ds_min_heap_attribute import MinHeapAttribute
 
 
-def dijkstra(w_graph_d, start_vertex):
+def dijkstra_shortest_path(w_graph_d, start_vertex):
     """Dijkstra algorithm for singel-source shortest path problem 
     in a "weighted" graph.
 
     Time complexity for graph G(V, E): (|V|+|E|)log(|V|).
     """
-    min_pq = MinBinaryHeapAttribute()
+    min_pq = MinHeapAttribute()
 
     distance_d = {v: np.inf for v in w_graph_d.keys()}
     visited_d = {v: False for v in w_graph_d.keys()}
-    previous_d = {v: None for v in w_graph_d.keys()}
     
-    min_pq.insert([0, start_vertex])
+    min_pq.add([0, start_vertex])
     distance_d[start_vertex] = 0
     visited_d[start_vertex] = True
 
-    while min_pq.heap_size > 0:
+    while min_pq.size > 0:
         k, v = min_pq.extract_min()
         visited_d[v] = True
 
@@ -32,10 +31,9 @@ def dijkstra(w_graph_d, start_vertex):
                 distance_d[v_neighbor] > 
                     distance_d[v] + w_graph_d[v][v_neighbor]):
                 distance_d[v_neighbor] = distance_d[v] + w_graph_d[v][v_neighbor]
-                previous_d[v_neighbor] = v
-                min_pq.insert([distance_d[v_neighbor], v_neighbor])
+                min_pq.add([distance_d[v_neighbor], v_neighbor])
 
-    return distance_d, previous_d, visited_d
+    return distance_d
 
 
 def main():
@@ -49,13 +47,10 @@ def main():
         'f': {'e': 1}
     }
     start_vertex = 's'
-    print('w_graph_d:\n{}'.format(w_graph_d))
     
-    print('Dijkstra shortest path from {}:'.format(start_vertex))
-    distance_d, previous_d, visited_d = dijkstra(w_graph_d, start_vertex)
+    print('Dijkstra shortest path:')
+    distance_d = dijkstra_shortest_path(w_graph_d, start_vertex)
     print('distance_d: {}'.format(distance_d))
-    print('previous_d: {}'.format(previous_d))
-    print('visited_d: {}'.format(visited_d))
    
 
 if __name__ == '__main__':
