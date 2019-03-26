cf297df8d02a345d1efba7f1b15e62b2b53857c5
bowen0701 <bowen0701@gmail.com>
2019-03-26 23:48:20 +0100

    Simplify scripts w/o n_rows, n_cols

--- lc200_number_of_islands.py
+++ lc200_number_of_islands.py
@@ -25,21 +25,21 @@ Answer: 3
 """
 
 class SolutionRecur(object):
-    def dfs(self, r, c, grid, visited_d, n_rows, n_cols):
+    def dfs(self, r, c, grid, visited_d):
         visited_d[(r, c)] = True
 
         if (0 <= r - 1 and grid[r - 1][c] == '1' and 
             not visited_d.get((r - 1, c))):  # Up.
-            self.dfs(r - 1, c, grid, visited_d, n_rows, n_cols)
-        if (r + 1 < n_rows and grid[r + 1][c] == '1' and 
+            self.dfs(r - 1, c, grid, visited_d)
+        if (r + 1 < len(grid) and grid[r + 1][c] == '1' and 
             not visited_d.get((r + 1, c))):  # Down.
-            self.dfs(r + 1, c, grid, visited_d, n_rows, n_cols)
+            self.dfs(r + 1, c, grid, visited_d)
         if (0 <= c - 1 and grid[r][c - 1] == '1' and 
             not visited_d.get((r, c - 1))):  # Left.
-            self.dfs(r, c - 1, grid, visited_d, n_rows, n_cols)
-        if (c + 1 < n_cols and grid[r][c + 1] == '1' and 
+            self.dfs(r, c - 1, grid, visited_d)
+        if (c + 1 < len(grid[0]) and grid[r][c + 1] == '1' and 
             not visited_d.get((r, c + 1))):  # Right.
-            self.dfs(r, c + 1, grid, visited_d, n_rows, n_cols)
+            self.dfs(r, c + 1, grid, visited_d)
 
     def numIslands(self, grid):
         """Number of islands by recursion.
@@ -50,38 +50,37 @@ class SolutionRecur(object):
         Space complexity: O(m * n).
 
         Recursive procedure:
-          - Starting from the top-left corner, run DFS with counter n_islands.
-          - If the grid was not visited before, mark it as visited and 
-            add n_islands by 1; if yes, skip DFS.
+        - Starting from the top-left corner, run DFS with counter n_islands.
+        - If the grid was not visited before, mark it as visited and 
+          add n_islands by 1; if yes, skip DFS.
         """
         if not grid:
             return 0
 
-        n_rows, n_cols = len(grid), len(grid[0])
         visited_d = {}
         n_islands = 0
 
-        for r in range(n_rows):
-            for c in range(n_cols):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == '1' and not visited_d.get((r, c)):
                     n_islands += 1
-                    self.dfs(r, c, grid, visited_d, n_rows, n_cols)
+                    self.dfs(r, c, grid, visited_d)
 
         return n_islands
 
 
 class SolutionIter(object):
-    def get_tovisit_ls(self, v_start, grid, n_rows, n_cols):
+    def get_tovisit_ls(self, v_start, grid):
         (r, c) = v_start
         tovisit_ls = []
 
-        if r - 1 >= 0 and grid[r - 1][c] == '1':  # Up.
+        if r - 1 >= 0 and grid[r - 1][c] == '1':            # Up.
             tovisit_ls.append((r - 1, c))
-        if r + 1 < n_rows and grid[r + 1][c] == '1':  # Down.
+        if r + 1 < len(grid) and grid[r + 1][c] == '1':     # Down.
             tovisit_ls.append((r + 1, c))
-        if c - 1 >= 0 and grid[r][c - 1] == '1':  # Left.
+        if c - 1 >= 0 and grid[r][c - 1] == '1':            # Left.
             tovisit_ls.append((r, c - 1))
-        if c + 1 < n_cols and grid[r][c + 1] == '1':  # Right.
+        if c + 1 < len(grid[0]) and grid[r][c + 1] == '1':  # Right.
                 tovisit_ls.append((r, c + 1))
 
         return tovisit_ls
@@ -89,13 +88,13 @@ class SolutionIter(object):
     def get_visited_ls(self, visited_d):
         return [k for (k, v) in visited_d.items() if v is True]
 
-    def dfs(self, r, c, grid, visited_d, n_rows, n_cols):
+    def dfs(self, r, c, grid, visited_d):
         visited_d[(r, c)] = True
         stack = []
         stack.append((r, c))
 
         while stack:
-            tovisit_ls = self.get_tovisit_ls(stack[-1], grid, n_rows, n_cols)
+            tovisit_ls = self.get_tovisit_ls(stack[-1], grid)
             visited_ls = self.get_visited_ls(visited_d)
 
             if set(tovisit_ls) - set(visited_ls):
@@ -117,15 +116,14 @@ class SolutionIter(object):
         if not grid:
             return 0
 
-        n_rows, n_cols = len(grid), len(grid[0])
         visited_d = {}
         n_islands = 0
 
-        for r in range(n_rows):
-            for c in range(n_cols):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == '1' and not visited_d.get((r, c)):
                     n_islands += 1
-                    self.dfs(r, c, grid, visited_d, n_rows, n_cols)
+                    self.dfs(r, c, grid, visited_d)
 
         return n_islands
 
