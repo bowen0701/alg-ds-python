b3d3cc95a90c164b8276326a92d04260a74d6582
bowen0701 <bowen0701@gmail.com>
2019-03-26 22:59:59 +0100

    Optimize space w/o copying list & add space complexity

--- alg_peak_1D.py
+++ alg_peak_1D.py
@@ -12,10 +12,11 @@ If a[0] >= a[1], then a[0] is a peak.
 If a[n - 1] >= a[n - 2], then a[n - 1] is a peak.  
 """
 
-def peak_1D_iter(arr):
+def peak_1d_iter(arr):
     """Find peak by naive iteration.
 
     Time complexity: O(n).
+    Space complexity: O(1).
     """
     for i in range(len(arr)):
         if i == 0:
@@ -29,19 +30,20 @@ def peak_1D_iter(arr):
                 return arr[i]
 
 
-def peak_1D(arr):
+def peak_1d(arr, start, end):
     """Find peak by divide-and-conquer algorithm.
 
     Time complexity: O(logn).
+    Space complexity: O(1).
     """
-    if len(arr) == 1:
-        return arr[0]
+    if end - start == 0:
+        return arr[start]
     else:
-        mid = len(arr) // 2
-        if arr[mid] <= arr[mid - 1]:
-            return peak_1D(arr[:mid-1])
+        mid = (end - start) // 2
+        if arr[mid - 1] >= arr[mid]:
+            return peak_1d(arr, start, mid - 1)
         elif arr[mid] <= arr[mid + 1]:
-            return peak_1D(arr[mid+1:])
+            return peak_1d(arr, mid + 1, end)
         else:
             return arr[mid]
 
@@ -54,24 +56,26 @@ def main():
     arr = [0, 1, 4, 3, 2]
     
     start_time = time.time()
-    print('Peak: {}'.format(peak_1D_iter(arr)))
-    print('Time for peak_1D_iter(): {}'.format(time.time() - start_time))
+    print('Peak: {}'.format(peak_1d_iter(arr)))
+    print('Time for peak_1d_iter(): {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('Peak: {}'.format(peak_1D(arr)))
-    print('Time for peak_1D(): {}'.format(time.time() - start_time))
+    start, end = 0, len(arr) - 1
+    print('Peak: {}'.format(peak_1d(arr, start, end)))
+    print('Time for peak_1d(): {}'.format(time.time() - start_time))
 
     # Array of long length.
     np.random.seed(71)
     arr = np.random.permutation(1000000)
 
     start_time = time.time()
-    print('Peak: {}'.format(peak_1D_iter(arr)))
-    print('Time for peak_1D_iter(): {}'.format(time.time() - start_time))
+    print('Peak: {}'.format(peak_1d_iter(arr)))
+    print('Time for peak_1d_iter(): {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('Peak: {}'.format(peak_1D(arr)))
-    print('Time for peak_1D(): {}'.format(time.time() - start_time))
+    start, end = 0, len(arr) - 1
+    print('Peak: {}'.format(peak_1d(arr, start, end)))
+    print('Time for peak_1d(): {}'.format(time.time() - start_time))
 
 
 if __name__ == '__main__':
