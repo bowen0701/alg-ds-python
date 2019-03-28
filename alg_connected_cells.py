3fb8c38dc9d7f2481adeb40d5b783eb82e71acf2
bowen0701 <bowen0701@gmail.com>
2019-03-28 08:07:57 +0100

    Add alg_connected_cells.py

---
+++ alg_connected_cells.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def connected_cells():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
