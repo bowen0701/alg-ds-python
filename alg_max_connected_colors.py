6acc488ede55e0159818755a39905b17a2438de2
bowen0701 <bowen0701@gmail.com>
2019-03-28 22:16:54 +0100

    Add recursion w/ return

--- alg_max_connected_colors.py
+++ alg_max_connected_colors.py
@@ -12,7 +12,7 @@ def _dfs_recur(r, c, grid, visited_d, max_num):
         color == grid[r - 1][c]):    # Up.
         _dfs_recur(r - 1, c, grid, visited_d, max_num)
     if (r + 1 <= len(grid) - 1 and not visited_d.get((r + 1, c)) and
-        color == grid[r + 1][c]):    # Down.    
+        color == grid[r + 1][c]):    # Down.
         _dfs_recur(r + 1, c, grid, visited_d, max_num)
     if (c - 1 >= 0 and not visited_d.get((r, c - 1)) and
         color == grid[r][c - 1]):    # Left.
@@ -44,12 +44,52 @@ def max_connected_colors(grid):
     return max_num
 
 
+def _dfs_recur2(r, c, grid, visited_d):
+    if visited_d.get((r, c)):
+        return 0
+
+    visited_d[(r, c)] = True
+    color = grid[r][c]
+    max_num = 1
+
+    if (r - 1 >= 0 and color == grid[r - 1][c]):  # Up.
+        max_num += _dfs_recur2(r - 1, c, grid, visited_d)
+    if (r + 1 <= len(grid) - 1 and color == grid[r + 1][c]):  # Down.
+        max_num += _dfs_recur2(r + 1, c, grid, visited_d)
+    if (c - 1 >= 0 and color == grid[r][c - 1]):  # Left.
+        max_num += _dfs_recur2(r, c - 1, grid, visited_d)
+    if (c + 1 <= len(grid[0]) - 1 and color == grid[r][c + 1]):  # Right.
+        max_num += _dfs_recur2(r, c + 1, grid, visited_d)
+    return max_num
+
+
+def max_connected_colors2(grid):
+    """Maximum number of connected colors.
+
+    Technique: DFS in a double for loops.
+
+    Time complexity: O(m * n).
+    Space complexity: O(m * n).
+    """
+    visited_d = {}
+    max_num = 0
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            max_num_rc = _dfs_recur2(r, c, grid, visited_d)
+            if max_num_rc > max_num:
+                max_num = max_num_rc
+
+    return max_num
+
+
 def main():
     # A grid of connected colors. Ans: 5 (of 2's).
     grid = [[1, 1, 2, 3],
             [1, 2, 3, 2],
             [3, 2, 2, 2]]
     print(max_connected_colors(grid))
+    print(max_connected_colors2(grid))
 
 
 if __name__ == '__main__':
