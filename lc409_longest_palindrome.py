c78fdbcd8e7904e20f988aba79e39f4032c58055
bowen0701 <bowen0701@gmail.com>
2019-04-02 23:38:52 +0200

    Complete lc409_longest_palindrome.py

--- lc409_longest_palindrome.py
+++ lc409_longest_palindrome.py
@@ -24,13 +24,36 @@ class Solution(object):
         """
         :type s: str
         :rtype: int
+
+        Time complexity: O(n), where n is the length of string s.
+        Space complexity: O(n).
         """
-        pass
+        if len(s) <= 1:
+            return len(s)
+
+        letter_d = {}
+        for c in s:
+            if c in letter_d:
+                letter_d[c] += 1
+            else:
+                letter_d[c] = 1
+
+        lp_len, rem = 0, 0
+        for letter in letter_d:
+            div, mod = divmod(letter_d[letter], 2)
+            lp_len += div * 2
+            if mod == 1:
+                rem = 1
+        lp_len += rem
+        return lp_len
 
 
 def main():
-    s = 'abccccdd'
-    pass
+    s = 'abccccdd'  # Ans: 7, since 'dccaccd'.
+    print Solution().longestPalindrome(s)
+
+    s = 'Aa'  # Ans: 1, since 'A'.
+    print Solution().longestPalindrome(s)
 
 
 if __name__ == '__main__':
