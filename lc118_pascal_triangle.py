3601aea1882bfa27f8db82f6403e2d59c63da48d
bowen0701 <bowen0701@gmail.com>
2019-04-04 22:57:39 +0200

    Complete lc118_pascal_triangle.py

--- lc118_pascal_triangle.py
+++ lc118_pascal_triangle.py
@@ -18,16 +18,34 @@ Output:
 """
 
 class Solution(object):
+    def get_num(self, last_row, i):
+        if i < 0 or i >= len(last_row):
+            return 0
+        return last_row[i]
+
     def generate(self, numRows):
         """
         :type numRows: int
         :rtype: List[List[int]]
         """
-        pass
+        if numRows == 0:
+            return []
+
+        triangle = [[1] * (r + 1) for r in range(numRows)]
+        if numRows <= 2:
+            return triangle
+        for r in range(2, numRows):
+            last_row = triangle[r - 1]
+            current_row = triangle[r]
+            for i in range(1, r):
+                current_row[i] = last_row[i - 1] + last_row[i]
+        return triangle
 
 
 def main():
-    pass
+    numRows = 5
+    print('Pascal\'s triangle:\n{}'.format(
+        Solution().generate(numRows)))
 
 
 if __name__ == '__main__':
