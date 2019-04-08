7f14735dbcfca317340e65ef83fd1a3f6df4f3b7
bowen0701 <bowen0701@gmail.com>
2019-04-08 22:59:18 +0200

    Complete lc119_pascal_triangle_ii.py

--- lc119_pascal_triangle_ii.py
+++ lc119_pascal_triangle_ii.py
@@ -1,15 +1,17 @@
 """Leetcode 119. Pascal's Triangle II
 Easy
 
-Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+URL: https://leetcode.com/problems/pascals-triangle-ii/
+
+Given a non-negative index k where k <= 33, return the kth index row of the 
+Pascal's triangle.
 
 Note that the row index starts from 0.
 
-
-In Pascal's triangle, each number is the sum of the two numbers directly above it.
+In Pascal's triangle, each number is the sum of the two numbers directly 
+above it.
 
 Example:
-
 Input: 3
 Output: [1,3,3,1]
 Follow up:
@@ -22,12 +24,26 @@ class Solution(object):
         """
         :type rowIndex: int
         :rtype: List[int]
+
+        Time complexity: O(k^2).
+        Space complexity: O(k).
         """
-        pass
+        if rowIndex <= 1:
+            return [1] * (rowIndex + 1)
+
+        last_row = [1] * 2
+        for r in range(2, rowIndex + 1):
+            row = [1] * (r + 1)
+            for i in range(1, r):
+                row[i] = last_row[i - 1] + last_row[i]
+            last_row = row
+        return row
 
 
 def main():
-    pass
+    rowIndex = 3
+    print 'kth row of the Pascal\'s triangle: {}'.format(
+        Solution().getRow(rowIndex))
 
 
 if __name__ == '__main__':
