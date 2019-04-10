6b7303bf3e9e11635195880647861fdaa1b0f4d1
bowen0701 <bowen0701@gmail.com>
2019-04-10 07:55:45 +0200

    Revise to 'naive' max_connected_colors0()

--- alg_max_connected_colors.py
+++ alg_max_connected_colors.py
@@ -3,27 +3,27 @@ from __future__ import division
 from __future__ import print_function
 
 
-def _dfs_recur(r, c, grid, visited_d, cc):
+def _dfs_recur0(r, c, grid, visited_d, cc):
     visited_d[(r, c)] = True
     color = grid[r][c]
     cc[0] += 1
 
     if (r - 1 >= 0 and not visited_d.get((r - 1, c)) and
         color == grid[r - 1][c]):    # Up.
-        _dfs_recur(r - 1, c, grid, visited_d, cc)
+        _dfs_recur0(r - 1, c, grid, visited_d, cc)
     if (r + 1 <= len(grid) - 1 and not visited_d.get((r + 1, c)) and
         color == grid[r + 1][c]):    # Down.
-        _dfs_recur(r + 1, c, grid, visited_d, cc)
+        _dfs_recur0(r + 1, c, grid, visited_d, cc)
     if (c - 1 >= 0 and not visited_d.get((r, c - 1)) and
         color == grid[r][c - 1]):    # Left.
-        _dfs_recur(r, c - 1, grid, visited_d, cc)
+        _dfs_recur0(r, c - 1, grid, visited_d, cc)
     if (c + 1 <= len(grid[0]) - 1 and not visited_d.get((r, c + 1)) and
         color == grid[r][c + 1]):    # Right.
-        _dfs_recur(r, c + 1, grid, visited_d, cc)
+        _dfs_recur0(r, c + 1, grid, visited_d, cc)
 
 
-def max_connected_colors(grid):
-    """Maximum number of connected colors.
+def max_connected_colors0(grid):
+    """Maximum number of connected colors (naive).
 
     Technique: DFS in a double for loops.
 
@@ -36,13 +36,13 @@ def max_connected_colors(grid):
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             cc = [0]
-            _dfs_recur(r, c, grid, visited_d, cc)
+            _dfs_recur0(r, c, grid, visited_d, cc)
             max_cc = max(cc[0], max_cc)
 
     return max_cc
 
 
-def _dfs_recur2(r, c, grid, color, visited_d):
+def _dfs_recur(r, c, grid, color, visited_d):
     if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
         return 0
 
@@ -53,15 +53,15 @@ def _dfs_recur2(r, c, grid, color, visited_d):
     cc = 1
 
     for r_neighbor in range(r - 1, r + 2, 2):  # Up & down.
-        cc += _dfs_recur2(r_neighbor, c, grid, color, visited_d)
+        cc += _dfs_recur(r_neighbor, c, grid, color, visited_d)
 
     for c_neighbor in range(c - 1, c + 2, 2):  # Left & right.
-        cc += _dfs_recur2(r, c_neighbor, grid, color, visited_d)
+        cc += _dfs_recur(r, c_neighbor, grid, color, visited_d)
 
     return cc
 
 
-def max_connected_colors2(grid):
+def max_connected_colors(grid):
     """Maximum number of connected colors.
 
     Technique: DFS in a double for loops by return.
@@ -75,7 +75,7 @@ def max_connected_colors2(grid):
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             color = grid[r][c]
-            cc = _dfs_recur2(r, c, grid, color, visited_d)
+            cc = _dfs_recur(r, c, grid, color, visited_d)
             max_cc = max(cc, max_cc)
 
     return max_cc
@@ -87,8 +87,8 @@ def main():
             [1, 2, 3, 2],
             [3, 2, 2, 2]]
 
+    print(max_connected_colors0(grid))
     print(max_connected_colors(grid))
-    print(max_connected_colors2(grid))
 
 
 if __name__ == '__main__':
