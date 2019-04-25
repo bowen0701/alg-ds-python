b90e3144d38d1172a307d1b5135c6ea5f5bfe28f
bowen0701 <bowen0701@gmail.com>
2019-04-25 23:20:07 +0200

    Complete naive dailyTemperature class

--- lc739_daily_temperatures.py
+++ lc739_daily_temperatures.py
@@ -13,17 +13,48 @@ Note: The length of temperatures will be in the range [1, 30000]. Each
 temperature will be an integer in the range [30, 100].
 """
 
-class Solution(object):
+class SolutionNaive(object):
     def dailyTemperatures(self, T):
         """
         :type T: List[int]
         :rtype: List[int]
+
+        Time complexity: O(n^2).
+        Space complexity: O(n).
+        """
+        D = [0 for _ in range(len(T))]
+        for i, t in enumerate(T):
+            for j in range(i + 1, len(T)):
+                if T[j] > t:
+                    D[i] = j - i
+                    break
+        return D
+
+
+class Solution(object):
+    def dailyTemperature(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+
+        Time complexity: ??
+        Space complexity: ??
         """
         pass
 
 
 def main():
-    pass
+    import time
+
+    T = [73, 74, 75, 71, 69, 72, 76, 73]
+    # Ans: [1, 1, 4, 2, 1, 1, 0, 0]
+  
+    print SolutionNaive().dailyTemperatures(T)
+
+    T = [73, 73, 75, 71, 69, 70, 76, 77]
+    # Ans: [2, 1, 4, 3, 1, 1, 1, 0]
+
+    print SolutionNaive().dailyTemperatures(T)
 
 
 if __name__ == '__main__':
