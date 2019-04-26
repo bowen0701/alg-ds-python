03671a01cb5ea359c22e954a8381bbfd30bce094
bowen0701 <bowen0701@gmail.com>
2019-04-26 23:39:20 +0200

    Complete naive solution by nested for loops

--- lc560_subarray_sum_equals_k.py
+++ lc560_subarray_sum_equals_k.py
@@ -14,21 +14,36 @@ The range of numbers in the array is [-1000, 1000] and the range of the
 integer k is [-1e7, 1e7].
 """
 
-class Solution(object):
+class SolutionNaive(object):
     def subarraySum(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
+
+        Time complexity: O(n^3).
+        Space complexity: O(n).
         """
-        pass
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if sum(nums[i:(j + 1)]) == k:
+                    count += 1
+        return count
 
 
 def main():
     import time
 
-    nums = [1,1,1]
+    nums = [1, 1, 1]
     k = 2
+
+    print SolutionNaive().subarraySum(nums, k)
+
+
+    nums =  [10, 2, -2, -20, 10]   
+    k = -10 
+    print SolutionNaive().subarraySum(nums, k)
 
 
 if __name__ == '__main__':
