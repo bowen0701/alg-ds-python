65d81cfa8e87285dddc3df1778093ba245a62009
bowen0701 <bowen0701@gmail.com>
2019-04-27 01:10:15 +0200

    Complete faster subarraySum()

--- lc560_subarray_sum_equals_k.py
+++ lc560_subarray_sum_equals_k.py
@@ -24,26 +24,88 @@ class SolutionNaive(object):
         Time complexity: O(n^3).
         Space complexity: O(n).
         """
-        count = 0
+        n_subarrs = 0
         for i in range(len(nums)):
             for j in range(i, len(nums)):
                 if sum(nums[i:(j + 1)]) == k:
-                    count += 1
-        return count
+                    n_subarrs += 1
+        return n_subarrs
 
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+
+        Time complexity: O(n).
+        Space complexity: O(n).
+        """
+        n_subarrs = 0
+        sums_d = {}
+        cusum = 0
+        
+        for n in nums:
+            # Compute cumulative sum.
+            cusum += n
+
+            # If cusum equals k, then increment 1.
+            if cusum == k:
+                n_subarrs += 1
+
+            # If cusum - k exists in sums_d, it means the subarray
+            # from index of (cusum - k) to "current" index equals k.
+            if cusum - k in sums_d:
+                n_subarrs += sums_d[cusum - k]
+
+            # Save cusum to sum dict.
+            if cusum in sums_d:
+                sums_d[cusum] += 1
+            else:
+                sums_d[cusum] = 1
+
+        return n_subarrs
+        
 
 def main():
     import time
 
+    # Ans: 2
     nums = [1, 1, 1]
     k = 2
 
-    print SolutionNaive().subarraySum(nums, k)
+    start_time = time.time()
+    print 'Naive: {}'.format(SolutionNaive().subarraySum(nums, k))
+    print 'Time: {}'.format(time.time() - start_time)
 
+    start_time = time.time()
+    print 'Dict: {}'.format(Solution().subarraySum(nums, k))
+    print 'Time: {}'.format(time.time() - start_time)
 
-    nums =  [10, 2, -2, -20, 10]   
-    k = -10 
-    print SolutionNaive().subarraySum(nums, k)
+    # Ans: 3
+    nums =  [10, 2, -2, -20, 10]
+    k = -10
+
+    start_time = time.time()
+    print 'Naive: {}'.format(SolutionNaive().subarraySum(nums, k))
+    print 'Time: {}'.format(time.time() - start_time)
+
+    start_time = time.time()
+    print 'Dict: {}'.format(Solution().subarraySum(nums, k))
+    print 'Time: {}'.format(time.time() - start_time)
+
+    # Ans: 0
+    nums = [1]
+    k = 0
+
+    start_time = time.time()
+    print 'Naive: {}'.format(SolutionNaive().subarraySum(nums, k))
+    print 'Time: {}'.format(time.time() - start_time)
+
+    start_time = time.time()
+    print 'Dict: {}'.format(Solution().subarraySum(nums, k))
+    print 'Time: {}'.format(time.time() - start_time)
 
 
 if __name__ == '__main__':
