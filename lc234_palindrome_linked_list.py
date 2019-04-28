c431a88315e1dfa03186aba982e6dee25484f073
bowen0701 <bowen0701@gmail.com>
2019-04-28 23:26:53 +0200

    Add lc234_palindrome_linked_list.py

---
+++ lc234_palindrome_linked_list.py
@@ -0,0 +1,41 @@
+"""Leetcode 234. Palindrome Linked List
+Easy
+
+URL: https://leetcode.com/problems/palindrome-linked-list/
+
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+Input: 1->2
+Output: false
+
+Example 2:
+Input: 1->2->2->1
+Output: true
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
