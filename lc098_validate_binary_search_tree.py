57631aa567d8071632532097ad47180813e481ba
bowen0701 <bowen0701@gmail.com>
2019-05-02 00:46:07 +0200

    Implementing isValidBST()

--- lc098_validate_binary_search_tree.py
+++ lc098_validate_binary_search_tree.py
@@ -32,10 +32,41 @@ Explanation: The root node's value is 5 but its right child's value is 4.
 
 # Definition for a binary tree node.
 class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, val):
+        self.val = val
         self.left = None
         self.right = None
+
+# Definition for a binary tree.
+class BinaryTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, a_list):
+        self.root = TreeNode(a_list[0])
+
+        stack = []
+        stack.insert(0, self.root)
+
+        for i in range(1, len(a_list)):
+            # Update current node after inserting left and right nodes.
+            if i % 2 == 1:
+                current = stack.pop()
+
+            # Left node.
+            if i % 2 == 1:
+                if a_list[i]:
+                    current.left = TreeNode(a_list[i])
+                    stack.insert(0, current.left)
+                else:
+                    current.left = None
+            # Right node.
+            elif i % 2 == 0:
+                if a_list[i]:
+                    current.right = TreeNode(a_list[i])
+                    stack.insert(0, current.right)
+                else:
+                    current.right = None
 
 
 class Solution(object):
@@ -44,11 +75,37 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
+        if not root:
+            return True
+
         pass
 
 
 def main():
-    pass
+    # Input: [2,1,3]
+    # Output: true
+    bt = BinaryTree()
+    bt.insert([2, 1, 3])
+    print 'BT:', (bt.root.val, bt.root.left.val, bt.root.right.val)
+    print 'isValidBST:', Solution().isValidBST(bt.root)
+
+    # Input: [5,1,4,null,null,3,6]
+    # Output: false
+    bt = BinaryTree()
+    bt.insert([5, 1, 4, None, None, 3, 6])
+    print 'BT:', (
+        bt.root.val, bt.root.left.val, bt.root.right.val, None, None,
+        bt.root.right.left.val, bt.root.right.right.val)
+    print 'isValidBST:', Solution().isValidBST(bt.root)
+
+    # Input: [10,5,15,null,null,6,20]
+    # Output: false
+    bt = BinaryTree()
+    bt.insert([10, 5, 15, None, None, 6, 20])
+    print 'BT:', (
+        bt.root.val, bt.root.left.val, bt.root.right.val, None, None,
+        bt.root.right.left.val, bt.root.right.right.val)
+    print 'isValidBST:', Solution().isValidBST(bt.root)
     
 
 if __name__ == '__main__':
