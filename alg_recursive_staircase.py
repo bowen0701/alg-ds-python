8164605a3f0100b8cacdbf35fd3cab5bfc90186e
bowen0701 <bowen0701@gmail.com>
2019-05-04 22:47:45 +0200

    Add alg_recursive_staircase.py

---
+++ alg_recursive_staircase.py
@@ -0,0 +1,21 @@
+"""Recursive Staircase.
+
+How many paths up a stair of say 100 steps if the child jumps 1, 2 or 3 steps?
+
+F(n) = F(n - 1) + F(n - 2) + F(n -3)
+- F(0) = 1  # Stay put.
+- F(1) = 1  # Take 1-step leap.
+- F(2) = 2  # Take 2 1-step leaps or 1 2-step leap.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
