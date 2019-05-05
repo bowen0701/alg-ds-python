9c6c10bea328be7d0455cf48c037ad854aaff63d
bowen0701 <bowen0701@gmail.com>
2019-05-05 17:32:08 +0200

    Complete staircase by memo

--- alg_recursive_staircase.py
+++ alg_recursive_staircase.py
@@ -2,7 +2,7 @@
 
 How many paths up a stair of say 100 steps if the child jumps 1, 2 or 3 steps?
 
-F(n) = F(n - 1) + F(n - 2) + F(n -3)
+F(n) = F(n - 1) + F(n - 2) + F(n - 3)
 - F(0) = 1  # Stay put.
 - F(1) = 1  # Take 1-step leap.
 - F(2) = 2  # Take 2 1-step leaps or 1 2-step leap.
@@ -14,7 +14,7 @@ from __future__ import print_function
 
 
 def staircase_recur(steps):
-    """Staircase by recursion.
+    """Staircase by top-down recursion.
 
     Time complexity: O(3^n).
     Space complexity: O(n).
@@ -30,13 +30,43 @@ def staircase_recur(steps):
             staircase_recur(steps - 3))
 
 
+def _staircase_memo(steps, M):
+    if steps < 0:
+        return 0
+
+    if steps == 0:
+        return 1
+
+    if M[steps]:
+        return M[steps]
+
+    M[steps] = (_staircase_memo(steps - 1, M) +
+                _staircase_memo(steps - 2, M) + 
+                _staircase_memo(steps - 3, M))
+    return M[steps]
+
+
+def staircase_memo(steps):
+    """Staircase by top-down memoization.
+
+    Time complexity: O(n).
+    Space complexity: O(n).
+    """
+    M = [0] * (steps + 1)
+    return _staircase_memo(steps, M)
+
+
 def main():
     import time
 
-    steps = 10
+    steps = 20
 
     start_time = time.time()
     print('Recursion: {}'.format(staircase_recur(steps)))
+    print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
+    print('Memo: {}'.format(staircase_memo(steps)))
     print('Time: {}'.format(time.time() - start_time))
 
 
