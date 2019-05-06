400e5c7e01587b2d52b144751704f1a18922feca
bowen0701 <bowen0701@gmail.com>
2019-05-06 22:57:04 +0200

    Add alg_anagram_making.py

---
+++ alg_anagram_making.py
@@ -0,0 +1,20 @@
+"""Anagram making.
+
+Count how many characters we need to remove to make anagram.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def anagram_making():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
