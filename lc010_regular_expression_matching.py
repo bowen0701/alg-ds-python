ca40e5a0b742930f620d872867a40dde66947277
bowen0701 <bowen0701@gmail.com>
2019-05-19 22:49:32 +0200

    Implement regexp match by dp

--- lc010_regular_expression_matching.py
+++ lc010_regular_expression_matching.py
@@ -27,7 +27,8 @@ Input:
 s = "aa"
 p = "a*"
 Output: true
-Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+Explanation: '*' means zero or more of the precedeng element, 'a'. 
+Therefore, by repeating 'a' once, it becomes "aa".
 
 Example 3:
 Input:
@@ -41,7 +42,8 @@ Input:
 s = "aab"
 p = "c*a*b"
 Output: true
-Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+Explanation: c can be repeated 0 times, a can be repeated 1 time. 
+Therefore it matches "aab".
 
 Example 5:
 Input:
@@ -56,8 +58,34 @@ class Solution(object):
         :type s: str
         :type p: str
         :rtype: bool
+
+
+        Regular expression matching by bottom-up dynamic programming.
         """
-        pass
+        T = [[False] * (len(p) + 1) for _ in range((len(s) + 1))]
+
+        # For empty s and p.
+        T[0][0] = True
+
+        # For empty s and p = a*, a*b*, or a*b*c*.
+        for j in range(2, len(p) + 1):
+        	if p[j - 1] == '*':
+        		T[0][j] = T[0][j - 2]
+
+        # Update the table for s's char along the p's char.
+        for i in range(1, len(s) + 1):
+        	for j in range(1, len(p) + 1):
+        		if s[i] == p[j] or p[j] == '.':
+        			# Check match without s[i] & p[j].
+        			T[i][j] = T[i - 1][j - 1]
+        		elif p[j] == '*':
+        			# Check match for p before a*.
+        			T[i][j] = T[i][j - 2]
+        			if s[i] == p[j - 1] or p[j - 1] == '.':
+        				# Further check s[i] and char or '.' before '*'.
+        				T[i][j] |= T[i - 1][j]
+
+        return T[-1][-1]
 
 
 def main():
