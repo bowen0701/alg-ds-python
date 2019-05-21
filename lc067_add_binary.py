4efa3e3d8faf6cc5d2755f1c1d0723b6a597a2a3
bowen0701 <bowen0701@gmail.com>
2019-05-21 21:32:11 +0200

    Complete lc067_add_binary.py

--- lc067_add_binary.py
+++ lc067_add_binary.py
@@ -17,13 +17,36 @@ Output: "10101"
 """
 
 class Solution(object):
+    def normalize(self, a, b):
+        if len(a) < len(b):
+            a = '0' * (len(b) - len(a)) + a
+        elif len(a) > len(b):
+            b = '0' * (len(a) - len(b)) + b
+        return a, b
+
     def addBinary(self, a, b):
         """
         :type a: str
         :type b: str
         :rtype: str
         """
-        pass
+        # Normalize a and b to equal size by padding 0's to shorer one.
+        a, b = self.normalize(a, b)
+
+        # Add numbers in a reversed order with carry.
+        s, carry = '', 0
+        i = len(a) - 1
+        
+        while i >= 0 or carry > 0:
+            if i >= 0: 
+                total = int(a[i]) + int(b[i]) + carry
+            else:
+                total = carry
+            carry, val = total // 2, total % 2
+            s = str(val) + s
+            i -= 1
+        
+        return s
 
 
 def main():
