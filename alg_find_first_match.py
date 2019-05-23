f897e91c48cce684abba5119cb33068c5c259122
bowen0701 <bowen0701@gmail.com>
2019-05-23 21:39:51 +0200

    Complete alg_find_first_match.py

--- alg_find_first_match.py
+++ alg_find_first_match.py
@@ -3,12 +3,31 @@ from __future__ import division
 from __future__ import print_function
 
 
-def find_first_match():
-    pass
+def find_first_match(search, source):
+    """Find first match.
+
+    Find the position in source string with the search string. 
+    
+    Time complexity: O(nk), 
+      where n and k is the length of source and search, respectively.
+    Space complexity: O(k).
+    """
+    for i in range(len(source) - len(search)):
+        if source[i:(i + len(search))] == search:
+            return i
+    return -1
 
 
 def main():
-    pass
+    source = 'abcdyesefgh'
+    search = 'yes'
+    # Output: 5
+    print(find_first_match(search, source))
+
+    source =  'abcdyefg'
+    search = 'yes'
+    # Output = -1
+    print(find_first_match(search, source))
 
 
 if __name__ == '__main__':
