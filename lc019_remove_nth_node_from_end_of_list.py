9cbf05f3ee0520de7ebc4e1bd5c026fb37861d7d
bowen0701 <bowen0701@gmail.com>
2019-05-26 22:58:48 +0200

    Implement LinkedList class

--- lc019_remove_nth_node_from_end_of_list.py
+++ lc019_remove_nth_node_from_end_of_list.py
@@ -19,9 +19,23 @@ Could you do this in one pass?
 
 # Definition for singly-linked list.
 class ListNode(object):
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, val):
+        self.val = val
         self.next = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        if not self.head:
+            self.head = ListNode(val)
+
+        current = self.head
+        if current.next:
+            current = current.next
+        current.next = ListNode(val)
 
 
 class Solution(object):
@@ -35,8 +49,15 @@ class Solution(object):
 
 
 def main():
-	pass
-
+    # Given linked list: 1->2->3->4->5, and n = 2.
+    # After removing the 2nd node from the end: 1->2->3->5.
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+    
 
 if __name__ == '__main__':
-	main()
+    main()
