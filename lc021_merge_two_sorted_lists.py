e0557f1c9a79db1debd6d011c992eae30a0a72b8
bowen0701 <bowen0701@gmail.com>
2019-05-28 23:40:26 +0200

    Add lc021_merge_two_sorted_lists.py

---
+++ lc021_merge_two_sorted_lists.py
@@ -0,0 +1,36 @@
+"""Leetcode 21. Merge Two Sorted Lists
+Easy
+
+URL: https://leetcode.com/problems/merge-two-sorted-lists/
+
+Merge two sorted linked lists and return it as a new list. 
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
