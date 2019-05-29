7d2d2793975d7568fc9ece8785f631cfe7d924f6
bowen0701 <bowen0701@gmail.com>
2019-05-29 20:58:02 +0200

    Add LinkedList class

--- lc021_merge_two_sorted_lists.py
+++ lc021_merge_two_sorted_lists.py
@@ -18,6 +18,29 @@ class ListNode(object):
         self.next = None
 
 
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        if not self.head:
+            self.head = ListNode(val)
+            return None
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = ListNode(val)
+        return None
+
+    def show(self):
+        ls = []
+        current = self.head
+        while current:
+            ls.append(current.val)
+            current = current.next
+        print ls
+
+
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         """
@@ -29,7 +52,19 @@ class Solution(object):
 
 
 def main():
-    pass
+    # Input: 1->2->4, 1->3->4
+    # Output: 1->1->2->3->4->4
+    ls1 = LinkedList()
+    ls1.append(1)
+    ls1.append(2)
+    ls1.append(4)
+    ls1.show()
+
+    ls2 = LinkedList()
+    ls2.append(1)
+    ls2.append(3)
+    ls2.append(4)
+    ls2.show()
 
 
 if __name__ == '__main__':
