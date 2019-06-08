c3b41352f9d2f6c3d2c59b3f69868458af71ed9e
bowen0701 <bowen0701@gmail.com>
2019-06-08 19:16:04 +0800

    Complete productExceptSelf w/ left/right acc lists

--- lc238_product_of_array_except_self.py
+++ lc238_product_of_array_except_self.py
@@ -17,17 +17,56 @@ Could you solve it with constant space complexity?
 space complexity analysis.)
 """
 
+class SolutionLeftRight(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+
+        Time complexity: O(n).
+        Space complexity: O(n).
+        """
+        size = len(nums)
+
+        left_prods = [1] * size
+        left_prods[0] = nums[0]
+        for i in range(1, size):
+            left_prods[i] = left_prods[i - 1] * nums[i]
+
+        right_prods = [1] * size
+        right_prods[size - 1] = nums[size - 1]
+        for i in range(size - 2, -1, -1):
+            right_prods[i] = right_prods[i + 1] * nums[i]
+
+        prods = [1] * size
+        for i, n in enumerate(nums):
+            if i - 1 < 0:
+                prods[i] = right_prods[i + 1]
+            elif i + 1 >= size:
+                prods[i] = left_prods[i - 1]
+            if i - 1 >= 0 and i + 1 < size:
+                prods[i] = left_prods[i - 1] * right_prods[i + 1]
+
+        return prods
+
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        pass
+        size = len(nums)
+
+        
 
 
 def main():
-    pass
+    # Input:  [2, 3, 4, 5]
+    # Output: [60, 40, 30, 24]
+    nums = [2, 3, 4, 5]
+
+    print SolutionLeftRight().productExceptSelf(nums)
 
 
 if __name__ == '__main__':
