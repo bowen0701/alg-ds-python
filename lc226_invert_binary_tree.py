1bc1c62b5d2bd5edd6c375c91540a6597b3c47cc
bowen0701 <bowen0701@gmail.com>
2019-06-09 22:38:44 +0800

    Complete recur w/ time/space complexity

--- lc226_invert_binary_tree.py
+++ lc226_invert_binary_tree.py
@@ -31,17 +31,43 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
+class SolutionRecur(object):
     def invertTree(self, root):
         """
         :type root: TreeNode
         :rtype: TreeNode
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        if not root:
+            return None
+
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+
+        root.left = right
+        root.right = left
+        return root
 
 
 def main():
-    pass
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(9)
+
+    root = SolutionRecur().invertTree(root)
+    print root.val              # Should be 4.
+    print root.left.val         # Should be 7.
+    print root.right.val        # Should be 2.
+    print root.left.left.val    # Should be 9.
+    print root.left.right.val   # Should be 6.
+    print root.right.left.val   # Should be 3.
+    print root.right.right.val  # Should be 1.
 
 
 if __name__ == '__main__':
