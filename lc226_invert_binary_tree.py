58741f0dfddb4b681d96884cfefa8d7b389f9aca
bowen0701 <bowen0701@gmail.com>
2019-06-09 22:21:59 +0800

    Add lc226_invert_binary_tree.py

---
+++ lc226_invert_binary_tree.py
@@ -0,0 +1,49 @@
+"""Leetcode 226. Invert Binary Tree
+Easy
+
+URL: https://leetcode.com/problems/invert-binary-tree/
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
