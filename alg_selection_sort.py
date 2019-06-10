3e4360e831d98dadca3f9346f324f3d17769257f
bowen0701 <bowen0701@gmail.com>
2019-06-10 22:55:10 +0800

    Refactor selection sort w/ adding comments

--- alg_selection_sort.py
+++ alg_selection_sort.py
@@ -3,27 +3,28 @@ from __future__ import print_function
 from __future__ import division
 
 
-def selection_sort(a_list):
+def selection_sort(ls):
     """Selection Sort algortihm.
 
     Time complexity: O(n^2).
+    Space complexity: O(1).
     """
-    for max_slot in reversed(range(len(a_list))):
-        select_slot = 0
-        for slot in range(1, max_slot + 1):
-            if a_list[slot] > a_list[select_slot]:
-                select_slot = slot
-
-        a_list[select_slot], a_list[max_slot] = (
-            a_list[max_slot], a_list[select_slot])
+    # Start from the last elemenet reversely: len(ls) - 1, ..., 0.
+    for i_max in reversed(range(len(ls))):
+        # Select the next max, and interchange it with corresponding element.
+        s = 0
+        for i in range(1, i_max + 1):
+            if ls[i] > ls[s]:
+                s = i
+        ls[s], ls[i_max] = ls[i_max], ls[s]
 
 
 def main():
-    a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    print('a_list: {}'.format(a_list))
+    ls = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print('List: {}'.format(ls))
     print('By selection sort: ')
-    selection_sort(a_list)
-    print(a_list)
+    selection_sort(ls)
+    print(ls)
 
 
 if __name__ == '__main__':
