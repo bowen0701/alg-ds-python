51358cccc8ce960500b56def2090b4b9dc82963d
bowen0701 <bowen0701@gmail.com>
2019-06-26 23:34:43 +0200

    Complete sol by naive sorting

--- lc703_kth_largest_element_in_a_stream.py
+++ lc703_kth_largest_element_in_a_stream.py
@@ -27,20 +27,33 @@ Note:
 You may assume that nums' length >= k-1 and k >= 1.
 """
 
-class KthLargest(object):
+class KthLargestSort(object):
+    """Kth Largest Element in a Stream
+
+    Apply naive sorting to obtain the top k largest elements.
+
+    Time complexity: O(n*logn).
+    Space complexity: O(1).
+    """
     def __init__(self, k, nums):
         """
         :type k: int
         :type nums: List[int]
         """
-        pass
+        self.k = k
+        self.nums = nums
+        self.nums.sort()
+        self.nums = self.nums[-k:]
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
-        pass
+        self.nums.append(val)
+        self.nums.sort()
+        self.nums = self.nums[-self.k:]
+        return self.nums[0]
 
 
 def main():
@@ -49,7 +62,18 @@ def main():
     # param_1 = obj.add(val)
     k = 3
     nums = [4, 5, 8, 2]
-    pass
+    obj = KthLargestSort(k, nums)
+
+    # Adding 3 returns 4
+    print obj.add(3)
+    # Adding 5 returns 5
+    print obj.add(5)
+    # Adding 10 returns 5
+    print obj.add(10)
+    # Adding 9 returns 8
+    print obj.add(9)
+    # Adding 4 returns 8
+    print obj.add(4)
 
 
 if __name__ == '__main__':
