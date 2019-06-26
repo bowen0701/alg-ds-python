8891dfdf7e7ab8c6dc48ce5ba6af429abf74caa2
bowen0701 <bowen0701@gmail.com>
2019-06-27 00:44:50 +0200

    Complete class KthLargestSortAndBinarySearch

--- lc703_kth_largest_element_in_a_stream.py
+++ lc703_kth_largest_element_in_a_stream.py
@@ -27,7 +27,7 @@ Note:
 You may assume that nums' length >= k-1 and k >= 1.
 """
 
-class KthLargestSort(object):
+class KthLargestNaiveSort(object):
     """Kth Largest Element in a Stream
 
     Apply naive sorting to obtain the top k largest elements.
@@ -40,30 +40,80 @@ class KthLargestSort(object):
         :type k: int
         :type nums: List[int]
         """
+        # Sort nums in descending order.
+        nums.sort(reverse=True)
         self.k = k
-        self.nums = nums
-        self.nums.sort()
-        self.nums = self.nums[-k:]
+        self.topk = nums[:k]
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
-        self.nums.append(val)
-        self.nums.sort()
-        self.nums = self.nums[-self.k:]
-        return self.nums[0]
+        self.topk.append(val)
+        self.topk.sort(reverse=True)
+
+        if len(self.topk) > self.k:
+            self.topk.pop()
+
+        return self.topk[-1]
+
+
+class KthLargestSortAndBinarySearch(object):
+    """Kth Largest Element in a Stream
+
+    Apply sorting to obtain the top k largest elements, with
+    inserting new element by binary search in sorted list.
+
+    Time complexity: O(n*logn), where n is the length of the original nums.
+    Space complexity: O(k).
+    """
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        nums.sort(reverse=True)
+        self.k = k
+        self.topk = nums[:k]
+        
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if len(self.topk) == self.k and val < self.topk[-1]:
+            return self.topk[-1]
+
+        # Apply binary search in reversed sorted list to 
+        # decide the position to add new element.
+        left = 0
+        right = self.k - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if self.topk[mid] < val:
+                right = mid
+            elif self.topk[mid] >= val:
+                left = mid + 1
+
+        self.topk.insert(left, val)
+        
+        if len(self.topk) > self.k:
+            self.topk.pop()
+
+        return self.topk[-1]
 
 
 def main():
+    import time
     # Your KthLargest object will be instantiated and called as such:
     # obj = KthLargest(k, nums)
     # param_1 = obj.add(val)
     k = 3
     nums = [4, 5, 8, 2]
-    obj = KthLargestSort(k, nums)
 
+    start_time = time.time()
+    obj = KthLargestNaiveSort(k, nums)
     # Adding 3 returns 4
     print obj.add(3)
     # Adding 5 returns 5
@@ -74,6 +124,22 @@ def main():
     print obj.add(9)
     # Adding 4 returns 8
     print obj.add(4)
+    print 'Time by naive sort: {}'.format(time.time() - start_time)
+
+    start_time = time.time()
+    obj = KthLargestSortAndBinarySearch(k, nums)
+    # Adding 3 returns 4
+    print obj.add(3)
+    # Adding 5 returns 5
+    print obj.add(5)
+    # Adding 10 returns 5
+    print obj.add(10)
+    # Adding 9 returns 8
+    print obj.add(9)
+    # Adding 4 returns 8
+    print obj.add(4)
+    print 'Time by sort + binary search: {}'.format(
+        time.time() - start_time)
 
 
 if __name__ == '__main__':
