cb697034025b8404aac0fd5eabbe9e66c4437ba2
bowen0701 <bowen0701@gmail.com>
2019-06-30 22:39:20 +0200

    Complete KthLargestMinHeap

--- lc703_kth_largest_element_in_a_stream.py
+++ lc703_kth_largest_element_in_a_stream.py
@@ -62,8 +62,8 @@ class KthLargestSort(object):
 class KthLargestSortAndBinarySearch(object):
     """Kth Largest Element in a Stream
 
-    Apply sorting to obtain the top k largest elements, with
-    inserting new element by binary search in sorted list.
+    Apply sorting to obtain the top k largest elements, and
+    insert new element by binary search in sorted list.
     """
     def __init__(self, k, nums):
         """
@@ -79,7 +79,7 @@ class KthLargestSortAndBinarySearch(object):
         :type val: int
         :rtype: int
 
-        Time complexity: O(n*logn), where n is the length of the original nums.
+        Time complexity: O(logn), where n is the length of the original nums.
         Space complexity: O(k).
         """
         if len(self.topk) == self.k and val < self.topk[-1]:
@@ -110,14 +110,74 @@ class KthLargestMinHeap(object):
         :type k: int
         :type nums: List[int]
         """
-        pass
+        nums.sort()
+        self.k = k
+        self.heap = [0]
+        self.size = 0
+
+        self.heap.extend(nums[-k:])
+        self.size += len(nums[-k:])
+
+    def _parent(self, i):
+        return i // 2
+
+    def _left(self, i):
+        return i * 2
+
+    def _right(self, i):
+        return i * 2 + 1
+
+    def heapify_up(self, i):
+        """Min heapify up by iteration.
+
+        Time complexity: O(logk).
+        Space complexity: O(1).
+        """
+        # For node i, check it is "smaller" than parent, if yes, swap them.
+        while i > 1 and self.heap[i] < self.heap[self._parent(i)]:
+            self.heap[i], self.heap[self._parent(i)] = (
+                self.heap[self._parent(i)], self.heap[i])
         
+    def heapify_down(self, i):
+        """Min heapify down by recursion.
+
+        Time complexity: O(log(n)).
+        Space complexity: O(1).
+        """
+        # Get min index from node i and its two child nodes.
+        l, r = self._left(i), self._right(i)
+        if l <= self.size and self.heap[l] < self.heap[i]:
+            min_i = l
+        else:
+            min_i = i
+        if r <= self.size and self.heap[r] < self.heap[min_i]:
+            min_i = r
+
+        # If node i is not min, swap node i and node min_i.
+        if min_i != i:
+            self.heap[i], self.heap[min_i] = self.heap[min_i], self.heap[i]
+            self.heapify_down(min_i)
+
     def add(self, val):
         """
         :type val: int
         :rtype: int
+
+        Time complexity: O(logn).
+        Space complexity: O(1). 
         """
-        pass
+        if self.size < self.k:
+            # If size < k, append new val to heap and run heapify_up(size).
+            self.heap.append(val)
+            self.size += 1
+            self.heapify_up(self.size)
+        elif self.heap[1] < val:
+            # If size = k and min < val, replace it by val and 
+            # run heapify_down(1).
+            self.heap[1] = val
+            self.heapify_down(1)
+
+        return self.heap[1]
 
 
 def main():
@@ -155,6 +215,39 @@ def main():
     # Adding 4 returns 8
     print obj.add(4)
     print 'Time by sort + binary search: {}'.format(
+        time.time() - start_time)
+
+    start_time = time.time()
+    obj = KthLargestMinHeap(k, nums)
+    # Adding 3 returns 4
+    print obj.add(3)
+    # Adding 5 returns 5
+    print obj.add(5)
+    # Adding 10 returns 5
+    print obj.add(10)
+    # Adding 9 returns 8
+    print obj.add(9)
+    # Adding 4 returns 8
+    print obj.add(4)
+    print 'Time by min heap: {}'.format(
+        time.time() - start_time)
+
+    k = 1
+    nums = []
+    
+    start_time = time.time()
+    obj = KthLargestMinHeap(k, nums)
+    # Adding -3 returns -3
+    print obj.add(-3)
+    # Adding -2 returns -2
+    print obj.add(-2)
+    # Adding -4 returns -2
+    print obj.add(-4)
+    # Adding 0 returns 0
+    print obj.add(0)
+    # Adding 4 returns 4
+    print obj.add(4)
+    print 'Time by min heap: {}'.format(
         time.time() - start_time)
 
 
