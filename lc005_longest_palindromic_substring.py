00bfa9ff3e83e3128586c2f4bb8572fed45e9ce4
bowen0701 <bowen0701@gmail.com>
2019-07-01 22:07:11 +0200

    Add URL

--- lc005_longest_palindromic_substring.py
+++ lc005_longest_palindromic_substring.py
@@ -1,6 +1,8 @@
 """Leetcode 5. Longest Palindromic Substring
 Medium
 
+URL: https://leetcode.com/problems/longest-palindromic-substring/
+
 Given a string s, find the longest palindromic substring in s. 
 You may assume that the maximum length of s is 1000.
 
