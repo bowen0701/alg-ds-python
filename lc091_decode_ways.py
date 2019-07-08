c42b648968c10a1ffb5c8105584b0bdd543bd1e8
bowen0701 <bowen0701@gmail.com>
2019-07-08 23:35:28 +0200

    Complete class SolutionRecur

--- lc091_decode_ways.py
+++ lc091_decode_ways.py
@@ -25,18 +25,43 @@ Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 """
 
-class Solution(object):
+class SolutionRecur(object):
     def numDecodings(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        if len(s) == 0:
+            return 1
+        if len(s) == 1:
+            if s[0] == '0':
+                return 0
+            else:
+                return 1
+
+        # For string with length >= 2, mimic Fibonacci series.
+        if s[0] > '0':
+            if '10' <= s[:2] <= '26':
+                return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
+            else:
+                return self.numDecodings(s[1:])
+        else:
+            return 0
 
 
 def main():
-	pass
+    s = '12' # Should be 2 = #{1,2; 12}.
+    print SolutionRecur().numDecodings(s)
+
+    s = '226' # Should be 3 = #{2,2,6; 22,6; 2,26}
+    print SolutionRecur().numDecodings(s)
+
+    s = '27' # Should be 1 = #{2,7}
+    print SolutionRecur().numDecodings(s)
+
+    s = '1111' # Should be 5 = #{1,1,1,1; 1,11,1; 1,1,11; 11,1,1; 11,11}
+    print SolutionRecur().numDecodings(s)
 
 
 if __name__ == '__main__':
-	main()
+    main()
