0fd935252582ef906aabdea2810bbffd75e5ebd3
bowen0701 <bowen0701@gmail.com>
2019-07-15 23:58:18 +0200

    Implementing spiralOrder()

--- lc054_spiral_matrix.py
+++ lc054_spiral_matrix.py
@@ -31,11 +31,44 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        pass
+        if not matrix or not matrix[0]:
+            return []
+
+        spiral = []
+        n_rows, n_cols = len(matrix), len(matrix[0])
+        up, down, left, right = 0, n_rows - 1, 0, n_cols - 1
+
+        while left < right and up < down:
+            # Add elements from top's left to right.
+            spiral.extend([matrix[up][j] for j in range(left, right)])
+            # Add elements from right's up to down.
+            spiral.extend([matrix[i][right] for i in range(up, down)])
+            # Add elements from down's right to left.
+            spiral.extend([matrix[down][j] for j in range(right, left, -1)])
+            # Add elements from left's down to up.
+            spiral.extend([matrix[i][left] for i in range(down, up, -1)])
+
+            # Decrement up, down, left and down.
+            up, down, left, right = up - 1, down + 1, left + 1, right - 1
+
+        if left == right:
+            print 'left == right: ({}, {})'.format(left, right)
+            spiral.extend([matrix[i][right] for i in range(up, down + 1)])
+
+        if up == down:
+            print 'up == down: ({}, {})'.format(up, down)
+            spiral.extend([matrix[down][j] for j in range(left, right + 1)])
+
+        return spiral
 
 
 def main():
-    pass
+    matrix = [
+        [ 1, 2, 3 ],
+        [ 4, 5, 6 ],
+        [ 7, 8, 9 ]
+    ]
+    print Solution().spiralOrder(matrix)
 
 
 if __name__ == '__main__':
