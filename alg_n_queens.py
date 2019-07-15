742cf00b95ee46966725756fe5715b8cd34b6837
bowen0701 <bowen0701@gmail.com>
2019-07-15 23:01:08 +0200

    Add alg_n_queens.py

---
+++ alg_n_queens.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+def n_queens():
+    """N Queens.
+
+    On an N by N board. Get the number of possible arrangements of the board 
+    where N queens can be placed on the board without threatening each other, 
+    i.e. no two queens share the same row, column, or diagonal. 
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
