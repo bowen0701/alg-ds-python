adeeb7e8ebd24129c4588e0ec22f69ae5fb30cf3
bowen0701 <bowen0701@gmail.com>
2019-07-18 23:23:55 +0200

    Remove redundant numpy import

--- ds_trie.py
+++ ds_trie.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-
 
 class Node(object):
     """Node class for Trie class."""
