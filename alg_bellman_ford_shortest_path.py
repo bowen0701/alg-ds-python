1a459c84ae6988719913d3fb83ec5a60ec73abe4
bowen0701 <bowen0701@gmail.com>
2019-07-22 22:43:19 +0200

    Refactor to _check_negative_cycle()

--- alg_bellman_ford_shortest_path.py
+++ alg_bellman_ford_shortest_path.py
@@ -5,14 +5,31 @@ from __future__ import print_function
 import numpy as np
 
 
-def update_distance(v, v_neighbor, 
-                    w_graph_d, distance_d, previous_d):
+def _update_distance(v, v_neighbor, 
+                     w_graph_d, distance_d, previous_d):
+    """Update distance with previous node."""
     if (distance_d[v_neighbor] > 
             distance_d[v] + w_graph_d[v][v_neighbor]):
         distance_d[v_neighbor] = (
             distance_d[v] + w_graph_d[v][v_neighbor])
         previous_d[v_neighbor] = v
     return distance_d, previous_d 
+
+
+def _check_negative_cycle(distance_d, previous_d, w_graph_d):
+    """Check negative cycle.
+
+    Update distance once more and check if any diff.
+    """
+    _distance_d = distance_d.copy()
+    _previous_d = previous_d.copy()
+
+    for v in w_graph_d.keys():
+        for v_neighbor in w_graph_d[v].keys():
+            _distance_d, _previous_d = _update_distance(
+                v, v_neighbor, w_graph_d, _distance_d, _previous_d)
+            if _distance_d != distance_d:
+                raise ValueError('Negative cycle exists.')
 
 
 def bellman_ford(w_graph_d, start_vertex):
@@ -27,25 +44,16 @@ def bellman_ford(w_graph_d, start_vertex):
 
     distance_d[start_vertex] = 0
 
+    # Run through |V| - 1 times, in each iteration update all edges's distances.
     n = len(w_graph_d.keys())
-    # Run through |V| - 1 times.
     for i in xrange(1, n):
-        # Run through all edges.
         for v in w_graph_d.keys():
             for v_neighbor in w_graph_d[v].keys():
-                distance_d, previous_d = update_distance(
+                distance_d, previous_d = _update_distance(
                     v, v_neighbor, w_graph_d, distance_d, previous_d)
     
-    # Check negative cycle by update distance once more and check if any diff.
-    _distance_d = distance_d.copy()
-    _previous_d = previous_d.copy()
-
-    for v in w_graph_d.keys():
-        for v_neighbor in w_graph_d[v].keys():
-            _distance_d, _previous_d = update_distance(
-                v, v_neighbor, w_graph_d, _distance_d, _previous_d)
-            if _distance_d != distance_d:
-                raise ValueError('Negative cycle exists.')
+    # Check negative cycle if any.
+    _check_negative_cycle(distance_d, previous_d, w_graph_d)
 
     return distance_d, previous_d
 
