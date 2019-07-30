2c426f950508368277042189ee509668302f9a8a
bowen0701 <bowen0701@gmail.com>
2019-07-30 20:27:17 +0200

    Move comments

--- lc004_median_of_two_sorted_arrays.py
+++ lc004_median_of_two_sorted_arrays.py
@@ -30,20 +30,21 @@ class Solution(object):
         i1, i2 = len(nums1) // 2, len(nums2) // 2
         n1, n2 = nums1[i1], nums2[i2]
 
-        # When k is smaller than or equal to the sum of nums1 & nums2's 
-        # middle indices. 
+
         if k <= i1 + i2:
-            # When nums1's middle element is bigger than nums2's,
-            # the 2nd half of nums1 does not contain the kth. 
+            # When k is smaller than or equal to the sum of nums1 & nums2's 
+            # middle indices.
             if n1 > n2:
+                # When nums1's middle element is bigger than nums2's,
+                # the 2nd half of nums1 does not contain the kth. 
                 return self._findKth(nums1[:i1], nums2, k)
             else:
                 return self._findKth(nums1, nums2[:i2], k)
-        # When k is bigger than the sum of nums1 & nums2's middle indices.
         else:
-            # When nums1's middle element is bigger than nums2's,
-            # the 1st half of nums2 does not contain the kth.
+            # When k is bigger than the sum of nums1 & nums2's middle indices.
             if n1 > n2:
+                # When nums1's middle element is bigger than nums2's,
+                # the 1st half of nums2 does not contain the kth.
                 return self._findKth(nums1, nums2[(i2 + 1):], k - i2 - 1)
             else:
                 return self._findKth(nums1[(i1 + 1):], nums2, k - i1 - 1)
