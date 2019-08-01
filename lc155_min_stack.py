a4211612d106d49e2b704d85e3d8f6bfddb25d59
bowen0701 <bowen0701@gmail.com>
2019-08-02 00:25:45 +0200

    Complete sol w/ time/space complexity

--- lc155_min_stack.py
+++ lc155_min_stack.py
@@ -31,42 +31,80 @@ param_4 = obj.getMin()
 """
 
 class MinStack(object):
-
     def __init__(self):
         """
         initialize your data structure here.
         """
-        pass
+        self._stack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
-        """
-        pass
 
+        Time complexity: O(1).
+        Space complexity: O(n), where n is the number of elements.
+        """
+        if not self._stack:
+            # If stack is empty, insert (x, min) into the stack, where min = x.
+            self._stack = [(x, x)]
+        else:
+            # If stack is not empty, insert (x, min) where min is updated if needed.
+            minimum = min(x, self._stack[-1][1])
+            self._stack.append([x, minimum])
+
+        return None
+            
     def pop(self):
         """
         :rtype: None
+
+        Time complexity: O(1).
+        Space complexity: O(n), where n is the number of elements.
         """
-        pass
+        if self._stack:
+            return self._stack.pop()[1]
+        else:
+            return None
 
     def top(self):
         """
         :rtype: int
+
+        Time complexity: O(1).
+        Space complexity: O(n), where n is the number of elements.
         """
-        pass
+        if self._stack:
+            return self._stack[-1][0]
+        else:
+            return None
 
     def getMin(self):
         """
         :rtype: int
+
+        Time complexity: O(1).
+        Space complexity: O(n), where n is the number of elements.
         """
-        pass
+        if self._stack:
+            return self._stack[-1][1]
+        else:
+            return None
 
 
 def main():
-    pass
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    # Returns -3.
+    print minStack.getMin()
+    minStack.pop()
+    # Returns 0.
+    print minStack.top()
+    # Returns -2.
+    print minStack.getMin()
 
 
 if __name__ == '__main__':
-    main()
+    min_stack = main()
