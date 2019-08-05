e58e9181f679b2af6232ac41eceb76eb6ad1fa68
bowen0701 <bowen0701@gmail.com>
2019-08-05 23:28:02 +0200

    Complete sol by iteration

--- lc028_implement_strstr.py
+++ lc028_implement_strstr.py
@@ -9,18 +9,16 @@ Return the index of the first occurrence of needle in haystack,
 or -1 if needle is not part of haystack.
 
 Example 1:
-
 Input: haystack = "hello", needle = "ll"
 Output: 2
-Example 2:
 
+Example 2:
 Input: haystack = "aaaaa", needle = "bba"
 Output: -1
-Clarification:
 
+Clarification:
 What should we return when needle is an empty string? 
 This is a great question to ask during an interview.
-
 For the purpose of this problem, we will return 0 when needle is an empty string. 
 This is consistent to C's strstr() and Java's indexOf().
 """
@@ -32,11 +30,26 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        pass
+        if not needle:
+            return 0
+
+        len_haystack, len_needle = len(haystack), len(needle)
+
+        for i in range(len_haystack - len_needle + 1):
+            if haystack[i:(i+len_needle)] == needle:
+                return i
+
+        return -1
 
 
 def main():
-    pass
+    haystack = "hello"
+    needle = "ll"
+    print Solution().strStr(haystack, needle)
+
+    haystack = "aaaaa"
+    needle = "bba"
+    print Solution().strStr(haystack, needle)
 
 
 if __name__ == '__main__':
