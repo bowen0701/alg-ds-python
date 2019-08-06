2cd6a49c268e1c56f819fef5f838b2e0dfafb96b
bowen0701 <bowen0701@gmail.com>
2019-08-06 23:28:31 +0200

    Complete extended binary search sol

--- lc033_search_in_rotated_sorted_array.py
+++ lc033_search_in_rotated_sorted_array.py
@@ -30,11 +30,60 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        pass
+        if not nums:
+            return -1
+
+        # Note that pivot is nums[0].
+        pivot = nums[0]
+
+        first, last = 0, len(nums) - 1
+
+        while first < last:
+            mid = first + (last - first) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Check target and middle are in the same side or splitted.
+            if (target >= pivot) == (nums[mid] >= pivot):
+                split_bool = False
+            else:
+                split_bool = True
+
+            if split_bool:
+                # If splitted, apply binary search based on pivot.
+                if target < pivot:
+                    first = mid + 1
+                else:
+                    last = mid - 1
+            else:
+                # If not splitted, apply normal binary search.
+                if target < nums[mid]:
+                    last = mid - 1
+                else:
+                    first = mid + 1
+
+        if nums[first] == target:
+            return first
+        else:
+            return -1
 
 
 def main():
-    pass
+    # Ans: 4
+    nums = [4,5,6,7,0,1,2]
+    target = 0
+    print Solution().search(nums, target)
+
+    # Ans: -1
+    nums = [4,5,6,7,0,1,2]
+    target = 3
+    print Solution().search(nums, target)
+
+    # Ans: 1
+    nums = [1,3]
+    target = 3
+    print Solution().search(nums, target)
 
 
 if __name__ == '__main__':
