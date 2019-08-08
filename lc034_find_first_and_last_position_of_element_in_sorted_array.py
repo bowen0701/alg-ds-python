4bafa90acca39a3d3fa5df0303d885c810244700
bowen0701 <bowen0701@gmail.com>
2019-08-08 21:51:05 +0200

    Add URL

--- lc034_find_first_and_last_position_of_element_in_sorted_array.py
+++ lc034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,6 +1,8 @@
 """Leetcode 34. Find First and Last Position of Element in Sorted Array
 Medium
 
+URL: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+
 Given an array of integers nums sorted in ascending order, 
 find the starting and ending position of a given target value.
 
