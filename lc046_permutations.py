5715dacd8223a2d6bf5daaeb69634ecd2094f87a
bowen0701 <bowen0701@gmail.com>
2019-08-10 23:01:26 +0200

    Add lc046_permutations.py

---
+++ lc046_permutations.py
@@ -0,0 +1,35 @@
+"""Leetcode 46. Permutations
+Medium
+
+URL: https://leetcode.com/problems/permutations/
+
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
