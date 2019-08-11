9138689cabb57e44319424ccf6cd4b64cd19ada1
bowen0701 <bowen0701@gmail.com>
2019-08-11 22:11:46 +0200

    Add lc078_subsets.py

---
+++ lc078_subsets.py
@@ -0,0 +1,39 @@
+"""Leetcode 78. Subsets
+Medium
+
+URL: https://leetcode.com/problems/subsets/
+
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+"""
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
