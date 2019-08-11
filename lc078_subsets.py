531bc3f318316ed9e7e7925be35595c15e69a16a
bowen0701 <bowen0701@gmail.com>
2019-08-11 23:42:45 +0200

    Complete backtrack sol

--- lc078_subsets.py
+++ lc078_subsets.py
@@ -22,17 +22,31 @@ Output:
 ]
 """
 
-class Solution(object):
+class SolutionBackTrack(object):
+    def _backtrack(self, result, temps, nums, start):
+        result.append(temps[:])
+
+        for i in range(start, len(nums)):
+            temps.append(nums[i])
+            self._backtrack(result, temps, nums, i + 1)
+            temps.pop()
+
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
+
+        Time complexity: O(n * 2^n).
+        Space complexity: O(n * 2^n).
         """
-        pass
+        result = []
+        self._backtrack(result, [], nums, 0)
+        return result
 
 
 def main():
-    pass
+    nums = [1, 2, 3]
+    print Solution().subsets(nums)
 
 
 if __name__ == '__main__':
