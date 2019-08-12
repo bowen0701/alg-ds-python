f8556df62054f8c756b40281f6c67ac3612bdce6
bowen0701 <bowen0701@gmail.com>
2019-08-12 22:55:17 +0200

    Add lc131_palindrome_partitioning.py

---
+++ lc131_palindrome_partitioning.py
@@ -0,0 +1,34 @@
+"""Leetcode 131. Palindrome Partitioning
+Medium
+
+URL: https://leetcode.com/problems/palindrome-partitioning/
+
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+Example:
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+"""
+
+
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
