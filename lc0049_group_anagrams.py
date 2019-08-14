d566af35c1dc7f9f016563a410ce64114cfee382
bowen0701 <bowen0701@gmail.com>
2019-08-14 23:08:40 +0200

    Add lc0049_group_anagrams.py

---
+++ lc0049_group_anagrams.py
@@ -0,0 +1,36 @@
+"""Leetcode 49. Group Anagrams
+Medium
+
+URL: https://leetcode.com/problems/group-anagrams/
+
+Given an array of strings, group anagrams together.
+
+Example:
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+Note:
+- All inputs will be in lowercase.
+- The order of your output does not matter.
+"""
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        pass
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
