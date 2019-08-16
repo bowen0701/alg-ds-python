b14a02336b93832448d51cc0145e1cbedb3f580e
bowen0701 <bowen0701@gmail.com>
2019-08-17 00:23:45 +0200

    Add time/space complexity

--- lc0088_merge_sorted_array.py
+++ lc0088_merge_sorted_array.py
@@ -7,7 +7,6 @@ Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
 one sorted array.
 
 Note:
-
 The number of elements initialized in nums1 and nums2 are m and n respectively.
 You may assume that nums1 has enough space (size that is greater or equal to m + n) 
 to hold additional elements from nums2.
@@ -27,12 +26,36 @@ class Solution(object):
         :type nums2: List[int]
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
+
+        Time complexity: O(m+n).
+        Space complexity: O(1).
         """
-        pass
+        i = m - 1
+        j = n - 1
+        for k in reversed(range(len(nums1))):
+            if i >= 0 and j >= 0:
+                if nums1[i] > nums2[j]:
+                    nums1[k] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[k] = nums2[j]
+                    j -= 1
+            elif i >= 0 and j < 0:
+                nums1[k] = nums1[i]
+                i -= 1
+            elif i < 0 and j >= 0:
+                nums1[k] = nums2[j]
+                j -= 1
 
 
 def main():
-    pass
+    # Ans: [1,2,2,3,5,6]
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    nums2 = [2,5,6]
+    n = 3
+    Solution().merge(nums1, m, nums2, n)
+    print nums1
 
 
 if __name__ == '__main__':
