53bc1efe919bbaf39e88c462d67c6c75ca7214a6
bowen0701 <bowen0701@gmail.com>
2019-08-18 22:23:25 +0200

    Refactor for clarification for n

--- alg_num_coin_changes.py
+++ alg_num_coin_changes.py
@@ -10,26 +10,31 @@ Assume that you have an infinite number of each kind of coin.
 """
 
 
-def num_coin_changes_recur(amount, coins, n):
-    """Number of coin changes by recursion.
-
-    Time complexity: O(2^n), where n is number of coins.
-    Space complexity: O(1).
-    """
+def _num_coin_changes_recur_util(amount, coins, n):
+    """Helper function for num_coin_changes_recur()."""
     if amount < 0:
         return 0
     if amount == 0:
         return 1
 
     # When number of coins is 0 but there is still amount remaining.
-    if n < 0 and amount >= 1:
+    if n <= 0 and amount > 0:
         return 0
 
     # Sum num of ways with coin n included & excluded.
-    n_changes_in = num_coin_changes_recur(amount - coins[n], coins, n)
-    n_changes_ex = num_coin_changes_recur(amount, coins, n - 1)
-    n_changes = n_changes_in + n_changes_ex
+    n_changes = (_num_coin_changes_recur_util(amount - coins[n - 1], coins, n) +
+                 _num_coin_changes_recur_util(amount, coins, n - 1))
     return n_changes
+
+
+def num_coin_changes_recur(amount, coins):
+    """Number of coin changes by recursion.
+
+    Time complexity: O(2^n), where n is number of coins.
+    Space complexity: O(1).
+    """
+    n = len(coins)
+    return _num_coin_changes_recur_util(amount, coins, n)
 
 
 def _num_coin_changes_memo_util(amount, coins, T, n):
@@ -39,7 +44,7 @@ def _num_coin_changes_memo_util(amount, coins, T, n):
     if amount < 0:
         return 0
 
-    if n < 0 and amount >= 1:
+    if n <= 0 and amount > 0:
         return 0
 
     # Apply memoization.
@@ -47,9 +52,8 @@ def _num_coin_changes_memo_util(amount, coins, T, n):
         return T[n][amount]
 
     # Sum num of ways with coin n included & excluded.
-    n_changes_in = _num_coin_changes_memo_util(amount - coins[n], coins, T, n)
-    n_changes_ex = _num_coin_changes_memo_util(amount, coins, T, n - 1)
-    T[n][amount] = n_changes_in + n_changes_ex
+    T[n][amount] = (_num_coin_changes_memo_util(amount - coins[n - 1], coins, T, n) +
+                    _num_coin_changes_memo_util(amount, coins, T, n - 1))
 
     return T[n][amount]
 
@@ -61,11 +65,11 @@ def num_coin_changes_memo(amount, coins):
     Time complexity: O(a*n), where a is amount, and n is number of coins.
     Space complexity: O(a*n).
     """
-    n = len(coins) - 1
+    n = len(coins)
     T = [[0] * (amount + 1) for c in range(n + 1)]
 
     # For amount 0, set num equal 1.
-    for c in range(n + 1):
+    for c in range(1, n + 1):
         T[c][0] = 1
 
     return _num_coin_changes_memo_util(amount, coins, T, n)
@@ -77,21 +81,21 @@ def num_coin_changes_dp(amount, coins):
     Time complexity: O(a*n), where a is amount, and n is number of coins.
     Space complexity: O(a*n).
     """
-    n = len(coins) - 1
+    n = len(coins)
     T = [[0] * (amount + 1) for c in range(n + 1)]
 
     # For amount 0, set num equal 1.
-    for c in range(n):
+    for c in range(1, n + 1):
         T[c][0] = 1
 
-    for c in range(n + 1):
+    for c in range(1, n + 1):
         for a in range(1, amount + 1):
-            if a < coins[c]:
+            if a < coins[c - 1]:
                 # Cannot make a change by coin c.
                 T[c][a] = T[c - 1][a]
             else:
                 # Sum num of ways with coin n included & excluded.
-                T[c][a] = T[c][a - coins[c]] + T[c - 1][a]
+                T[c][a] = T[c][a - coins[c - 1]] + T[c - 1][a]
 
     return T[-1][-1]
 
@@ -99,13 +103,13 @@ def num_coin_changes_dp(amount, coins):
 def main():
     import time
 
+    # Ans = 5.
     amount = 5
-    coins = [1, 2, 3]    # Ans = 5.
-    n = len(coins) - 1
+    coins = [1, 2, 3]
 
     start_time = time.time()
     print('By recursion: {}'
-          .format(num_coin_changes_recur(amount, coins, n)))
+          .format(num_coin_changes_recur(amount, coins)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
