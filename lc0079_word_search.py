8ef3be4794db4ac225c2ebfd251be7b3d97a8687
bowen0701 <bowen0701@gmail.com>
2019-08-19 23:09:19 +0200

    Complete DFS sol

--- lc0079_word_search.py
+++ lc0079_word_search.py
@@ -23,17 +23,80 @@ Given word = "ABCB", return false.
 """
 
 class Solution(object):
+    def _dfs(self, i, j, board, word, pos, visited):
+        # If there are no letters, return True.
+        if pos == len(word):
+            return True
+
+        # If visit is out of boundaries, 
+        # (i, j) is not the 1st letter of word,
+        # or (i, j) was visited.
+        if (i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or 
+            board[i][j] != word[pos] or
+            visited.get((i, j))):
+            return False
+
+        # Mark (i, j) as visited.
+        visited[(i, j)] = True
+
+        # Start DFS visiting, one of DFSs is true, then return True.
+        result = (self._dfs(i - 1, j, board, word, pos + 1, visited) or
+                  self._dfs(i + 1, j, board, word, pos + 1, visited) or
+                  self._dfs(i, j - 1, board, word, pos + 1, visited) or
+                  self._dfs(i, j + 1, board, word, pos + 1, visited))
+
+        # Backtrack.
+        visited[(i, j)] = False
+
+        return result
+
     def exist(self, board, word):
         """
         :type board: List[List[str]]
         :type word: str
         :rtype: bool
         """
-        pass
+        if not board:
+            return False
+
+        m, n = len(board), len(board[0])
+
+        for i in range(m):
+            for j in range(n):
+                visited = {}
+                if self._dfs(i, j, board, word, 0, visited):
+                    return True
+
+        return False
 
 
 def main():
-    pass
+    board = [
+              ['A','B','C','E'],
+              ['S','F','C','S'],
+              ['A','D','E','E']
+            ]
+
+    # Given word = "ABCCED", return true.
+    word = 'ABCCED'
+    print Solution().exist(board, word)
+
+    # Given word = "SEE", return true.
+    word = 'SEE'
+    print Solution().exist(board, word)
+
+    # Given word = "ABCB", return false.
+    word = 'ABCB'
+    print Solution().exist(board, word)
+
+    # Given word = "ABCESEEEFS", return true.
+    board = [
+              ["A","B","C","E"],
+              ["S","F","E","S"],
+              ["A","D","E","E"]
+            ]
+    word = "ABCESEEEFS"
+    print Solution().exist(board, word)
 
 
 if __name__ == '__main__':
