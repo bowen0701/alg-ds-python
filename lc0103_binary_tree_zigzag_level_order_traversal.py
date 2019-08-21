3dd0ccdc1eaae25cd37df18eb1b6b5d99615d6f8
bowen0701 <bowen0701@gmail.com>
2019-08-21 23:05:10 +0200

    Complete BFS sol

--- lc0103_binary_tree_zigzag_level_order_traversal.py
+++ lc0103_binary_tree_zigzag_level_order_traversal.py
@@ -35,11 +35,58 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        pass
+        if not root:
+            return []
+    
+        result = []
+
+        # Apply queue for BFS travesal in levels.
+        queue = [root]
+        level_id = 0
+
+        while queue:
+            level = []
+
+            for i in range(len(queue)):
+                # Get the oldest node from queue.
+                current = queue.pop()
+
+                # Insert its left and right nodes to queue.
+                if current.left:
+                    queue.insert(0, current.left)
+                if current.right:
+                    queue.insert(0, current.right)
+            
+                if level_id % 2 == 0:
+                    # If even level, append current value to level's tail.
+                    level.append(current.val)
+                else:
+                    # If odd, insert current value to level's head.
+                    level.insert(0, current.val)
+
+            level_id += 1
+            result.append(level)
+
+        return result
 
 
 def main():
-    pass
+    # Given binary tree:
+    #      3
+    #    /    \
+    #   9     20
+    #  / \   /  \
+    # 6   8 15   7
+    # Ans: [[3], [20, 9], [6, 8, 15, 7]]
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.left.left = TreeNode(6)
+    root.left.right = TreeNode(8)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    print Solution().zigzagLevelOrder(root)
 
 
 if __name__ == '__main__':
