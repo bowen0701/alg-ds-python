5d66e86f16a8c9b36eff7e345fec057cf51a136d
bowen0701 <bowen0701@gmail.com>
2019-08-23 22:24:10 +0200

    Add lc0125_valid_palindrome.py

---
+++ lc0125_valid_palindrome.py
@@ -0,0 +1,34 @@
+"""Leetcode 125. Valid Palindrome
+Easy
+
+URL: https://leetcode.com/problems/valid-palindrome/
+
+Given a string, determine if it is a palindrome,
+considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
+"""
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
