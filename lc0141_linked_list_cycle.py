094d9037e77b4a81576ff0809dad82e233323a01
bowen0701 <bowen0701@gmail.com>
2019-08-25 00:32:07 +0200

    Complete two pointers method, add edge case

--- lc0141_linked_list_cycle.py
+++ lc0141_linked_list_cycle.py
@@ -42,7 +42,13 @@ class SolutionDict(object):
         """
         :type head: ListNode
         :rtype: bool
+
+        Time complexity: O(n), where n is the lenght of linked list.
+        Space complexity: O(n).
         """
+        if not head:
+            return False
+
         visited_d = {}
         visited_d[head] = True
 
@@ -57,6 +63,33 @@ class SolutionDict(object):
         return False
 
 
+class SolutionTwoPointers(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+
+        Time complexity: O(n), where n is the lenght of linked list.
+        Space complexity: O(1).
+        """
+        if not head:
+            return False
+
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            # Two pointers: slow for 1 step; fast for 2 steps.
+            slow = slow.next
+            fast = fast.next.next
+
+            # If fast cathes slow, it means there is a cycle.
+            if slow == fast:
+                return True
+
+        return False
+
+
 def main():
     # Input: head = [3,2,0,-4], pos = 1. Output: true
     head = ListNode(3)
@@ -65,16 +98,19 @@ def main():
     head.next.next.next = ListNode(-4)
     head.next.next.next.next = head.next
     print SolutionDict().hasCycle(head)
+    print SolutionTwoPointers().hasCycle(head)
 
     # Input: head = [1,2], pos = 0. Output: true
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = head
     print SolutionDict().hasCycle(head)
+    print SolutionTwoPointers().hasCycle(head)
 
     # Input: head = [1], pos = -1. Output: false
     head = ListNode(1)
     print SolutionDict().hasCycle(head)    
+    print SolutionTwoPointers().hasCycle(head)
 
 
 if __name__ == '__main__':
