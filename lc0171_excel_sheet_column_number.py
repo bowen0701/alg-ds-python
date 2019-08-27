4a4bcb06cf5f97bc844291a491f5e6747aae75be
bowen0701 <bowen0701@gmail.com>
2019-08-27 23:53:27 +0200

    Add lc0171_excel_sheet_column_number.py

---
+++ lc0171_excel_sheet_column_number.py
@@ -0,0 +1,46 @@
+"""Leetcode 171. Excel Sheet Column Number
+Easy
+
+URL: https://leetcode.com/problems/excel-sheet-column-number/
+
+Given a column title as appear in an Excel sheet,
+return its corresponding column number.
+
+For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+Example 1:
+Input: "A"
+Output: 1
+
+Example 2:
+Input: "AB"
+Output: 28
+
+Example 3:
+Input: "ZY"
+Output: 701
+"""
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
