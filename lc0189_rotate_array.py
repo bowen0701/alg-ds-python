eb1cd0f760888ef80ba45ebbdc392623b3f679f2
bowen0701 <bowen0701@gmail.com>
2019-08-30 01:24:50 +0200

    Complete copy sol

--- lc0189_rotate_array.py
+++ lc0189_rotate_array.py
@@ -26,18 +26,40 @@ Note:
 - Could you do it in-place with O(1) extra space?
 """
 
-class Solution(object):
+class SolutionCopy(object):
     def rotate(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        pass
+        # If k >= n, truncate k by n.
+        n = len(nums)
+        if k >= n:
+            k -= n
+
+        # Append right with left.
+        nums[:] = nums[-k:] + nums[:-k]
 
 
 def main():
-    pass
+    # Ans: [5,6,7,1,2,3,4]
+    nums = [1,2,3,4,5,6,7]
+    k = 3
+    SolutionCopy().rotate(nums, k)
+    print nums
+
+    # Ans: [3,99,-1,-100]
+    nums = [-1,-100,3,99]
+    k = 2
+    SolutionCopy().rotate(nums, k)
+    print nums
+
+    # Ans: [2,1]
+    nums = [1,2]
+    k = 3
+    SolutionCopy().rotate(nums, k)
+    print nums
 
 
 if __name__ == '__main__':
