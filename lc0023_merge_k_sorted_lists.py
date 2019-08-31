31af6fefec9770e1ca6663fafe397465732fbf4d
bowen0701 <bowen0701@gmail.com>
2019-08-31 23:46:14 +0200

    Complete sort sol w/ time/space complexity

--- lc0023_merge_k_sorted_lists.py
+++ lc0023_merge_k_sorted_lists.py
@@ -23,17 +23,76 @@ class ListNode(object):
         self.next = None
 
 
-class Solution(object):
+class SolutionSort(object):
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
+
+        Time complexity: O(nk*log(nk)), where
+          - n is the number of nodes,
+          - k is the length of lists.
+        Space complexity: O(nk).
         """
-        pass
+        # Collect all nodes from list.
+        nodes = []
+
+        for head in lists:
+            current = head
+
+            while current:
+                nodes.append(current)
+                current = current.next
+
+        # Sort all nodes by their values.
+        sorted_nodes = sorted(nodes, key=lambda x: x.val)
+
+        # Link nodes in sorted_nodes.
+        pre_head = ListNode(None)
+        current = pre_head
+
+        for node in sorted_nodes:
+            current.next = node
+            current = current.next
+
+        return pre_head.next
+
+
+def show(head):
+    ls = []
+
+    current = head
+    while current:
+        ls.append(current.val)
+        current = current.next
+
+    print ls
+
 
 
 def main():
-    pass
+    # Input:
+    # [
+    #   1->4->5,
+    #   1->3->4,
+    #   2->6
+    # ]
+    # Output: 1->1->2->3->4->4->5->6
+    head1 = ListNode(1)
+    head1.next = ListNode(4)
+    head1.next.next = ListNode(5)
+
+    head2 = ListNode(1)
+    head2.next = ListNode(3)
+    head2.next.next = ListNode(4)
+
+    head3 = ListNode(2)
+    head3.next = ListNode(6)
+
+    lists = [head1, head2, head3]
+
+    head = SolutionSort().mergeKLists(lists)
+    show(head)
 
 
 if __name__ == '__main__':
