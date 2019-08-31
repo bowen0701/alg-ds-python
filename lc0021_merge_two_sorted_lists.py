8a7e1b350ba767a1606dead43ad296270cdf45ab
bowen0701 <bowen0701@gmail.com>
2019-08-31 23:17:54 +0200

    Complete recur sol

--- lc0021_merge_two_sorted_lists.py
+++ lc0021_merge_two_sorted_lists.py
@@ -34,10 +34,18 @@ class SolutionRecur(object):
         :type l2: ListNode
         :rtype: ListNode
 
-        Time complexity: O(n1 + n2), where n is the lengths of two lists.
-        Space complexity: O(n1 + n2).
+        Time complexity: O(n1 + n2), where ni is the length of list i.
+        Space complexity: O(1).
         """
-        pass
+        if not l1 or not l2:
+            return l1 or l2
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
 
 class SolutionIter(object):
@@ -47,62 +55,55 @@ class SolutionIter(object):
         :type l2: ListNode
         :rtype: ListNode
 
-        Time complexity: O(n1 + n2), where n is the lengths of two lists.
-        Space complexity: O(n1 + n2).
+        Time complexity: O(n1 + n2), where ni is the length of list i.
+        Space complexity: O(1).
         """
-        if not l1 and not l2:
-            return None
-        
         if l1 and not l2:
             return l1
         elif not l1 and l2:
             return l2
 
-        # Set head by comparing lists's heads.
-        if l1.val < l2.val:
-            head = ListNode(l1.val)
-            l1 = l1.next
-        else:
-            head = ListNode(l2.val)
-            l2 = l2.next
+        pre_head = ListNode(None)
 
-        current = head
+        current = pre_head
 
-        while l1 or l2:
-            if l1 and l2:
-                if l1.val < l2.val:
-                    ll = l1
-                    l1 = l1.next
-                else:
-                    ll = l2
-                    l2 = l2.next
-                current.next = ListNode(ll.val)
-                current = current.next
-            elif l1 and not l2:
-                while l1:
-                    current.next = ListNode(l1.val)
-                    current = current.next
-                    l1 = l1.next
-            elif not l1 and l2:
-                while l2:
-                    current.next = ListNode(l2.val)
-                    current = current.next
-                    l2 = l2.next
-        return head
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        current.next = l1 or l2
+        return pre_head.next
 
 
 def main():
     # Input: 1->2->4, 1->3->4
     # Output: 1->1->2->3->4->4
+    print 'By recur:'
     l1 = ListNode(1)
     l1.next = ListNode(2)
     l1.next.next = ListNode(4)
-    show_linked_list(l1)
 
     l2 = ListNode(1)
     l2.next = ListNode(3)
     l2.next.next = ListNode(4)
-    show_linked_list(l2)
+
+    ls = SolutionRecur().mergeTwoLists(l1, l2)
+    show_linked_list(ls)
+
+    print 'By iter:'
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
+
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
 
     ls = SolutionIter().mergeTwoLists(l1, l2)
     show_linked_list(ls)
