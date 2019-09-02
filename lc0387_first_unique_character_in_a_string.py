47f2baf2f1009a2fe06eed14215d624b0579050c
bowen0701 <bowen0701@gmail.com>
2019-09-02 23:49:02 +0200

    Add lc0387_first_unique_character_in_a_string.py

---
+++ lc0387_first_unique_character_in_a_string.py
@@ -0,0 +1,32 @@
+"""Leetcode 387. First Unique Character in a String
+Easy
+
+URL: https://leetcode.com/problems/first-unique-character-in-a-string/
+
+Given a string, find the first non-repeating character in it and return it's index.
+If it doesn't exist, return -1.
+
+Examples:
+s = "leetcode"
+return 0.
+s = "loveleetcode",
+return 2.
+
+Note: You may assume the string contain only lowercase letters.
+"""
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
