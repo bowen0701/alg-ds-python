c0b269ad535fe6d47342150960f8e49fdbe8aa08
bowen0701 <bowen0701@gmail.com>
2019-09-04 00:26:17 +0200

    Add time/space complexity

--- lc0100_same_tree.py
+++ lc0100_same_tree.py
@@ -12,22 +12,21 @@ Example 1:
 Input:     1         1
           / \       / \
          2   3     2   3
-        [1,2,3],   [1,2,3]
+        [1,2,3],  [1,2,3]
 Output: true
 
 Example 2:
 Input:     1         1
           /           \
          2             2
-        [1,2],     [1,null,2]
+        [1,2],    [1,null,2]
 Output: false
 
 Example 3:
 Input:     1         1
           / \       / \
          2   1     1   2
-
-        [1,2,1],   [1,1,2]
+        [1,2,1],  [1,1,2]
 Output: false
 """
 
@@ -39,18 +38,61 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
+class SolutionPreorderRecur(object):
     def isSameTree(self, p, q):
         """
         :type p: TreeNode
         :type q: TreeNode
         :rtype: bool
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        if not p or not q:
+            return p is q
+        
+        return (p.val == q.val and
+                self.isSameTree(p.left, q.left) and
+                self.isSameTree(p.right, q.right))
 
 
 def main():
-    pass
+    # Input:     1         1
+    #           / \       / \
+    #          2   3     2   3
+    #         [1,2,3],  [1,2,3]
+    # Output: true
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(3)
+    q = TreeNode(1)
+    q.left = TreeNode(2)
+    q.right = TreeNode(3)
+    print SolutionPreorderRecur().isSameTree(p, q)
+
+    # Input:     1         1
+    #           /           \
+    #          2             2
+    #         [1,2],    [1,null,2]
+    # Output: false
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    q = TreeNode(1)
+    q.right = TreeNode(2)
+    print SolutionPreorderRecur().isSameTree(p, q)    
+
+    # Input:     1         1
+    #           / \       / \
+    #          2   1     1   2
+    #         [1,2,1],  [1,1,2]
+    # Output: false
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(1)
+    q = TreeNode(1)
+    q.left = TreeNode(1)
+    q.right = TreeNode(2)
+    print SolutionPreorderRecur().isSameTree(p, q)
 
 
 if __name__ == '__main__':
