dd69f35b623fa93579930e03c3aea8fc8f290136
bowen0701 <bowen0701@gmail.com>
2019-09-04 21:44:28 +0200

    Revise to var n & add space line

--- lc0001_two_sum.py
+++ lc0001_two_sum.py
@@ -11,7 +11,6 @@ and you may not use the same element twice.
 
 Example:
 Given nums = [2, 7, 11, 15], target = 9,
-
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
@@ -27,15 +26,16 @@ class Solution(object):
         Space complexity: O(n).
         """
         num_idx_d = {}
-        for i, num in enumerate(nums):
-            if target - num in num_idx_d:
-                return [num_idx_d[target - num], i]
-            num_idx_d[num] = i
+        for i, n in enumerate(nums):
+            if target - n in num_idx_d:
+                return [num_idx_d[target - n], i]
+            num_idx_d[n] = i
         return []
 
 
 def main():
     print Solution().twoSum([2, 7, 11, 15], 9)
+
 
 if __name__ == '__main__':
     main()
