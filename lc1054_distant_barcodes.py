8b1b4b57a342798918e07d8770ab92d38c97741f
bowen0701 <bowen0701@gmail.com>
2019-09-06 20:44:04 +0200

    Complete sorted dict sol

--- lc1054_distant_barcodes.py
+++ lc1054_distant_barcodes.py
@@ -16,24 +16,54 @@ Output: [2,1,2,1,2,1]
 Example 2:
 Input: [1,1,1,1,2,2,3,3]
 Output: [1,3,1,3,2,1,2,1]
- 
-Note:
 
+Note:
 - 1 <= barcodes.length <= 10000
 - 1 <= barcodes[i] <= 10000
 """
 
-class Solution(object):
+class SolutionDictSort(object):
     def rearrangeBarcodes(self, barcodes):
         """
         :type barcodes: List[int]
         :rtype: List[int]
+
+        Time complexity: O(n*logn).
+        Space complexity: O(n).
         """
-        pass
+        from collections import defaultdict
+
+        # Use dict to store barcode's number.
+        barcode_num_d = defaultdict(int)
+        for b in barcodes:
+            barcode_num_d[b] += 1
+
+        # Starting from pos 0, fill barcodes at even positions.
+        # Then starting from pos 1, odd position.
+        res = [0] * len(barcodes)
+        pos = 0
+
+        for b, n in sorted(barcode_num_d.items(), 
+                           key=lambda x: x[1], reverse=True):
+            while n:
+                res[pos] = b
+                pos += 2
+                n -= 1
+
+                if pos >= len(barcodes):
+                    pos = 1
+
+        return res
 
 
 def main():
-    pass
+    # Output: [2,1,2,1,2,1]
+    barcodes = [1,1,1,2,2,2]
+    print SolutionDictSort().rearrangeBarcodes(barcodes)
+
+    # Output: [1,3,1,3,2,1,2,1]
+    barcodes = [1,1,1,1,2,2,3,3]
+    print SolutionDictSort().rearrangeBarcodes(barcodes)
 
 
 if __name__ == '__main__':
