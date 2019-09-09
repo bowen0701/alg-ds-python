4829c6d07d4e81fe950c38ea7ed852a735975ea3
bowen0701 <bowen0701@gmail.com>
2019-09-09 23:30:05 +0200

    Add lc0204_count_primes.py

---
+++ lc0204_count_primes.py
@@ -0,0 +1,29 @@
+"""Leetcode 204. Count Primes
+Easy
+
+URL: https://leetcode.com/problems/count-primes/
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+"""
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
