40391d9d30166ee59b59e4d85ab2c1327cd7b236
bowen0701 <bowen0701@gmail.com>
2019-09-10 23:04:15 +0200

    Complete iter sol

--- lc0231_power_of_two.py
+++ lc0231_power_of_two.py
@@ -6,32 +6,47 @@ URL: https://leetcode.com/problems/power-of-two/
 Given an integer, write a function to determine if it is a power of two.
 
 Example 1:
-
 Input: 1
 Output: true 
-Explanation: 20 = 1
-Example 2:
+Explanation: 2^0 = 1
 
+Example 2:
 Input: 16
 Output: true
-Explanation: 24 = 16
-Example 3:
+Explanation: 2^4 = 16
 
+Example 3:
 Input: 218
 Output: false
 """
 
-class Solution(object):
+class SolutionIter(object):
     def isPowerOfTwo(self, n):
         """
         :type n: int
         :rtype: bool
+
+        Time complexity: O(logn).
+        Space complexity: O(1).
         """
-        pass
+        i = 1
+        while i < n:
+            i *= 2
+        return i == n
 
 
 def main():
-    pass
+    # Output: True
+    n = 1
+    print SolutionIter().isPowerOfTwo(n)
+
+    # Output: True
+    n = 16
+    print SolutionIter().isPowerOfTwo(n)
+
+    # Output: False
+    n = 218
+    print SolutionIter().isPowerOfTwo(n)
 
 
 if __name__ == '__main__':
