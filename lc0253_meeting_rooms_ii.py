f56e390be0e2cea8e08080029aad756a6ab3c91f
bowen0701 <bowen0701@gmail.com>
2019-09-15 23:52:45 +0200

    Add lc0253_meeting_rooms_ii.py from Copenhagen :)

---
+++ lc0253_meeting_rooms_ii.py
@@ -0,0 +1,23 @@
+"""Leetcode 253. Meeting Rooms II (Premium)
+Medium
+
+URL: https://leetcode.com/problems/meeting-rooms-ii
+
+Given an array of meeting time intervals consisting of start and end times
+[[s1,e1],[s2,e2],...] (si < ei),
+find the minimum number of conference rooms required.
+"""
+
+class Solution2(object):
+    # @param {Interval[]} intervals
+    # @return {integer}
+    def minMeetingRooms(self, intervals):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
