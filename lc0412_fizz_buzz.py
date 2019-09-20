dcdb80f9ed0ed2901e8a1c657e124607d1baa795
bowen0701 <bowen0701@gmail.com>
2019-09-20 22:38:14 +0200

    Complete lc0412_fizz_buzz.py

--- lc0412_fizz_buzz.py
+++ lc0412_fizz_buzz.py
@@ -5,9 +5,9 @@ URL: https://leetcode.com/problems/fizz-buzz/
 
 Write a program that outputs the string representation of numbers from 1 to n.
 
-But for multiples of three it should output “Fizz” instead of the number and
-for the multiples of five output “Buzz”.
-For numbers which are multiples of both three and five output “FizzBuzz”.
+But for multiples of three it should output "Fizz" instead of the number and
+for the multiples of five output "Buzz".
+For numbers which are multiples of both three and five output "FizzBuzz".
 
 Example:
 n = 15,
@@ -37,11 +37,24 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        pass
+        fizzbuzz = [None] * n
+
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                fizzbuzz[i - 1] = "FizzBuzz"
+            elif i % 3 == 0:
+                fizzbuzz[i - 1] = "Fizz"
+            elif i % 5 == 0:
+                fizzbuzz[i - 1] = "Buzz"
+            else:
+                fizzbuzz[i - 1] = str(i)
+
+        return fizzbuzz
 
 
 def main():
-    pass
+    n = 15
+    print Solution().fizzBuzz(n)
 
 
 if __name__ == '__main__':
