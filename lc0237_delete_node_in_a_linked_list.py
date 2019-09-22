7111bf857cbc73290727be83b304ad81091d2be2
bowen0701 <bowen0701@gmail.com>
2019-09-23 00:48:03 +0200

    Complete two pointer sol

--- lc0237_delete_node_in_a_linked_list.py
+++ lc0237_delete_node_in_a_linked_list.py
@@ -35,17 +35,49 @@ class ListNode(object):
         self.next = None
 
 
-class Solution(object):
+class SolutionTwoPointers(object):
     def deleteNode(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
+
+        Time complexity: O(n).
+        Space complexity: O(1).
         """
-        pass
+        previous = None
+        current = node
+
+        while current.next:
+            # Update current's value.
+            current.val = current.next.val
+
+            # Iterate to next node.
+            previous = current
+            current = current.next
+
+        previous.next = None
 
 
 def main():
-    pass
+    # Input: head = [4,5,1,9], node = 5
+    # Output: [4,1,9]
+    head = ListNode(4)
+    head.next = ListNode(5)
+    head.next.next = ListNode(1)
+    head.next.next.next = ListNode(9)
+    node = head.next
+    SolutionTwoPointers().deleteNode(node)
+    print head.val, head.next.val, head.next.next.val, head.next.next.next
+
+    # Input: head = [4,5,1,9], node = 1
+    # Output: [4,5,9]
+    head = ListNode(4)
+    head.next = ListNode(5)
+    head.next.next = ListNode(1)
+    head.next.next.next = ListNode(9)
+    node = head.next.next
+    SolutionTwoPointers().deleteNode(node)
+    print head.val, head.next.val, head.next.next.val, head.next.next.next
 
 
 if __name__ == '__main__':
