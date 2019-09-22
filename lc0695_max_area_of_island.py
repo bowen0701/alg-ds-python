bae10a5bf73fe1876109a2cda3402af3af4519ef
bowen0701 <bowen0701@gmail.com>
2019-09-22 23:55:54 +0200

    Complete dfs iter update sol

--- lc0695_max_area_of_island.py
+++ lc0695_max_area_of_island.py
@@ -73,6 +73,69 @@ class SolutionDFSRecurUpdate(object):
         return max_area
 
 
+class SolutionDFSIterUpdate(object):
+    def _get_tovisit_ls(self, v_start, grid):
+        r, c = v_start
+
+        tovisit_ls = []
+
+        # Visit up, down, left and right.
+        dirs = [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
+        for r_neighbor, c_neighbor in dirs:
+            if (0 <= r_neighbor < len(grid) and 
+                0 <= c_neighbor < len(grid[0]) and
+                grid[r_neighbor][c_neighbor] == 1):
+                tovisit_ls.append((r_neighbor, c_neighbor))
+
+        return tovisit_ls
+
+    def _dfs(self, r, c, grid):
+        grid[r][c] = 0
+
+        # Use stack for DFS.
+        stack = [(r, c)]
+
+        area = 1
+
+        while stack:
+            # Get to-visit nodes from the top of stack.
+            tovisit_ls = self._get_tovisit_ls(stack[-1], grid)
+
+            if tovisit_ls:
+                for v_neighbor in tovisit_ls:
+                    r_neighbor, c_neighbor = v_neighbor
+                    grid[r_neighbor][c_neighbor] = 0
+                    area += 1
+                    stack.append((r_neighbor, c_neighbor))
+                    # Break to continue DFS.
+                    break
+            else:
+                stack.pop()
+
+        return area
+
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+
+        Time complexity: O(m*n).
+        Space complexity: O(m*n).
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        max_area = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    area = self._dfs(r, c, grid)
+                    max_area = max(max_area, area)
+
+        return max_area
+
+
 def main():
     # Output: 6
     grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
@@ -85,9 +148,22 @@ def main():
             [0,0,0,0,0,0,0,1,1,0,0,0,0]]
     print SolutionDFSRecurUpdate().maxAreaOfIsland(grid)
 
+    grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,1,1,0,1,0,0,0,0,0,0,0,0],
+            [0,1,0,0,1,1,0,0,1,0,1,0,0],
+            [0,1,0,0,1,1,0,0,1,1,1,0,0],
+            [0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    print SolutionDFSIterUpdate().maxAreaOfIsland(grid)
+
     # Output: 0.
     grid = [[0,0,0,0,0,0,0,0]]
     print SolutionDFSRecurUpdate().maxAreaOfIsland(grid)
+
+    grid = [[0,0,0,0,0,0,0,0]]
+    print SolutionDFSIterUpdate().maxAreaOfIsland(grid)
 
 
 if __name__ == '__main__':
