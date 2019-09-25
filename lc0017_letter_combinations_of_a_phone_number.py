600827cc9f8e69fddfb7807ac1bbfccf50b6ce95
bowen0701 <bowen0701@gmail.com>
2019-09-25 17:59:14 +0200

    Complete BFS recur sol

--- lc0017_letter_combinations_of_a_phone_number.py
+++ lc0017_letter_combinations_of_a_phone_number.py
@@ -28,7 +28,7 @@ Although the above answer is in lexicographical order,
 your answer could be in any order you want.
 """
 
-class Solution(object):
+class SolutionListComprehension(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
@@ -40,7 +40,7 @@ class Solution(object):
         Space complexity: O(m^n).
         """
         # Store digit->letter-list dict.
-        d2l_d = {
+        dtol_d = {
             '2': ['a', 'b', 'c'],
             '3': ['d', 'e', 'f'],
             '4': ['g', 'h', 'i'],
@@ -51,29 +51,83 @@ class Solution(object):
             '9': ['w', 'x', 'y', 'z']
         }
         
-        # Edge cases handling.
         if not digits:
             return []
         if len(digits) == 1:
-            return d2l_d[digits]
+            return dtol_d[digits]
 
-        # Initialize output lc by the 0th digit's letter-list.
-        lc = d2l_d[digits[0]]
+        # Initialize output letter_com by the 0th digit's letter-list.
+        letter_com = dtol_d[digits[0]]
 
         # Run for loop over digits starting from index i = 1.
-        #   - Get the ith digit's letter list li.
-        #   - Use list comprehension to combine lc and li to replace lc.
+        # - Get the ith digit's letter list lecter_i.
+        # - Use list comprehension to combine letter_com and lecter_i 
+        #   to replace letter_com.
         for i in range(1, len(digits)):
-            li = d2l_d[digits[i]]
-            lc = [m + n for m in lc for n in li]
+            lecter_i = dtol_d[digits[i]]
+            letter_com = [m + n for m in letter_com for n in lecter_i]
         
-        return lc
+        return letter_com
+
+
+class SolutionBFSRecur(object):
+    def bfsRecur(self, letter_com, digits, dtol_d, cur_str, idx):
+        if idx == len(digits):
+            letter_com.append(cur_str)
+            return None
+
+        letters = dtol_d[digits[idx]]
+        for l in letters:
+            # For letters in this BFS level, append letter and increment index.
+            self.bfsRecur(letter_com, digits, dtol_d, cur_str + l, idx + 1)
+
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+ 
+        Time complexity: O(n * m^2)
+          - n is the length of digits,
+          - m is the mean length of digit's letters, basically 3.
+        Space complexity: O(m^n).
+        """
+        # Store digit->letter-list dict.
+        dtol_d = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k' ,'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+
+        if not digits:
+            return []
+        if len(digits) == 1:
+            return dtol_d[digits]
+
+        letter_com = []
+        cur_str = ''
+        idx = 0
+        self.bfsRecur(letter_com, digits, dtol_d, cur_str, idx)
+        return letter_com
 
 
 def main():
-    digits =  "23"
+    import time
+
     # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-    print Solution().letterCombinations(digits)
+    digits =  "23"
+
+    start_time = time.time()
+    print 'By list comp:', SolutionListComprehension().letterCombinations(digits)
+    print 'Time:', time.time() - start_time
+
+    start_time = time.time()
+    print 'By BFS recur', SolutionBFSRecur().letterCombinations(digits)
+    print 'Time:', time.time() - start_time
 
 
 if __name__ == '__main__':
