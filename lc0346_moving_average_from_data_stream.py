db8ff703e29095d40e8373ef7e9760ee8dfe39d5
bowen0701 <bowen0701@gmail.com>
2019-09-25 16:50:01 +0200

    Add lc0346_moving_average_from_data_stream.py

---
+++ lc0346_moving_average_from_data_stream.py
@@ -0,0 +1,29 @@
+"""Leetcode 346. Moving Average from Data Stream (Premium)
+Easy
+
+Given a stream of integers and a window size,
+calculate the moving average of all integers in the sliding window.
+
+For example,
+MovingAverage m = new MovingAverage(3);
+m.next(1) = 1
+m.next(10) = (1 + 10) / 2
+m.next(3) = (1 + 10 + 3) / 3
+m.next(5) = (10 + 3 + 5) / 3
+"""
+
+class MovingAverage(object):
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
