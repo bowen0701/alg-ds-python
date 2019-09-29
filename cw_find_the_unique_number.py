8bdfe2e859f4fd627a4fcf9f25bef2b817d47d40
bowen0701 <bowen0701@gmail.com>
2019-09-29 23:47:40 +0200

    Add cw_find_the_unique_number.py

---
+++ cw_find_the_unique_number.py
@@ -0,0 +1,32 @@
+"""Codewars: Find the unique number
+6 kyu
+
+URL: https://www.codewars.com/kata/find-the-unique-number-1/
+
+There is an array with some numbers. All numbers are equal except for one.
+Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+
+It’s guaranteed that array contains more than 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+- Find the unique number (this kata)
+- Find the unique string
+- Find The Unique
+"""
+
+def find_uniq(arr):
+    # your code here
+    return n   # n: unique integer in the array
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
