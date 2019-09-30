b09ced2bd08cf61d75cf567a8aefdd7fb7b3024c
bowen0701 <bowen0701@gmail.com>
2019-10-01 01:32:36 +0200

    Complete bit sol

--- lc0371_sum_of_two_integers.py
+++ lc0371_sum_of_two_integers.py
@@ -15,18 +15,42 @@ Input: a = -2, b = 3
 Output: 1
 """
 
-class Solution(object):
+class SolutionBit(object):
     def getSum(self, a, b):
         """
         :type a: int
         :type b: int
         :rtype: int
         """
-        pass
+        # 32-bits max integer.
+        MAX = 0x7FFFFFFF
+        # 32-bits min interger.
+        MIN = 0x80000000
+        # Mask to get last 32-bits.
+        MASK = 0xFFFFFFFF
+
+        while b != 0:
+            # ^ gets different bits and & gets double 1s, << moves carry.
+            a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
+
+        # If a is negative, get a's 32 bits complement positive first,
+        # then get 32-bit positive's complement negative in Python.
+        if a <= MAX:
+            return a
+        else:
+            return ~(a ^ MASK)
 
 
 def main():
-    pass
+    # Output: 3
+    a = 1
+    b = 2
+    print SolutionBit().getSum(a, b)
+
+    # Output: 1
+    a = -2
+    b = 3
+    print SolutionBit().getSum(a, b)
 
 
 if __name__ == '__main__':
