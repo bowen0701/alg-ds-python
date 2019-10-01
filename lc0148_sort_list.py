53d969fe13098de222d7b9fc875f584627905c4a
bowen0701 <bowen0701@gmail.com>
2019-10-01 18:45:31 +0200

    Add lc0148_sort_list.py

---
+++ lc0148_sort_list.py
@@ -0,0 +1,38 @@
+"""Leetcode 148. Sort List
+Medium
+
+URL: https://leetcode.com/problems/sort-list/
+
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+Input: 4->2->1->3
+Output: 1->2->3->4
+
+Example 2:
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
