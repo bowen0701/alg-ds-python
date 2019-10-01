42fd3bee0b13ac3c4968370a0a780ba7ff38fe00
bowen0701 <bowen0701@gmail.com>
2019-10-02 00:36:19 +0200

    Complete bottom-up iter merge sort sol

--- lc0148_sort_list.py
+++ lc0148_sort_list.py
@@ -21,7 +21,7 @@ class ListNode(object):
         self.next = None
 
 
-class SolutionRecur(object):
+class SolutionMergeSortRecur(object):
     def _merge_sorted_lists(self, l1, l2):
         if not l1 and not l2:
             return None
@@ -56,8 +56,8 @@ class SolutionRecur(object):
         Time complexity: O(n*logn).
         Space complexity: O(logn), for stake call.
         """
-        # Apply recursive sort list with iterative merge sorted lists.
-        # Create base condition: no or just one node.
+        # Apply recursive merge sort with iterative merging sorted lists.
+
         if not head or not head.next:
             return head
 
@@ -80,6 +80,96 @@ class SolutionRecur(object):
         return self._merge_sorted_lists(l1, l2)
 
 
+class SolutionMergeSortIterBottomUp(object):
+    def _get_length(self, head):
+        """Get list length."""
+        length = 0
+        current = head
+
+        while current:
+            length += 1
+            current = current.next
+
+        return length
+
+    def _split(self, head, step):
+        """Split list with length equal to step, and return next head."""
+        counter = 1
+        current = head
+        while current and counter < step:
+            current = current.next
+            counter += 1
+
+        if not current:
+            return None
+
+        next_head = current.next
+        current.next = None
+        return next_head
+
+    def _merge(self, l1, l2, head):
+        """Merge two lists and return next head."""
+        current = head
+
+        # Merge two soreted lists.
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+
+        # Connect the remaining list node.
+        current.next = l1 or l2
+
+        # Arrive at the end of merged list.
+        while current.next:
+            current = current.next
+
+        next_head = current
+        return next_head
+
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+
+        Time complexity: O(n*logn).
+        Space complexity: O(1).
+        """
+        # Apply iterative bottom-up merge sort with merging sorted lists.
+
+        if not head or not head.next:
+            return head
+
+        # Get the list length.
+        length = self._get_length(head)
+
+        # Iterate over list with step = 1 (and then double step size):
+        # - split into 2 lists, with tracking next head, and 
+        # - merge sorted lists.
+        prev = ListNode(None)
+        prev.next = head     
+        step = 1
+
+        while step < length:
+            current = prev.next
+            next_head = prev
+
+            while current:
+                l1 = current
+                l2 = self._split(l1, step)
+                current = self._split(l2, step)
+                next_head = self._merge(l1, l2, next_head)
+
+            # Double step size.
+            step *= 2
+
+        return prev.next
+
+
 def main():
     # Input: 4->2->1->3
     # Output: 1->2->3->4
@@ -88,7 +178,17 @@ def main():
     head.next.next = ListNode(1)
     head.next.next.next = ListNode(3)
 
-    sorted_head = SolutionRecur().sortList(head)
+    sorted_head = SolutionMergeSortRecur().sortList(head)
+    print (sorted_head.val, sorted_head.next.val,
+           sorted_head.next.next.val, 
+           sorted_head.next.next.next.val)
+
+    head = ListNode(4)
+    head.next = ListNode(2)
+    head.next.next = ListNode(1)
+    head.next.next.next = ListNode(3)
+
+    sorted_head = SolutionMergeSortIterBottomUp().sortList(head)
     print (sorted_head.val, sorted_head.next.val,
            sorted_head.next.next.val, 
            sorted_head.next.next.next.val)
