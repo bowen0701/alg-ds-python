14e685ed82115c3c23adb8ed9c997011accf558e
bowen0701 <bowen0701@gmail.com>
2019-10-01 20:17:59 +0200

    Refactor for clarification

--- alg_merge_sort.py
+++ alg_merge_sort.py
@@ -3,74 +3,75 @@ from __future__ import print_function
 from __future__ import division
 
 
-def merge_sorted_lists_recur(x_list, y_list):
+def merge_sorted_lists_recur(x, y):
     """Merge two sorted lists by recusions."""
-    if len(x_list) == 0:
-        return y_list
-    if len(y_list) == 0:
-        return x_list
+    if len(x) == 0:
+        return y
+    if len(y) == 0:
+        return x
 
-    if x_list[0] <= y_list[0]:
-        return [x_list[0]] + merge_sorted_lists_recur(x_list[1:], y_list)
+    if x[0] <= y[0]:
+        return [x[0]] + merge_sorted_lists_recur(x[1:], y)
     else:
-        return [y_list[0]] + merge_sorted_lists_recur(x_list, y_list[1:])
+        return [y[0]] + merge_sorted_lists_recur(x, y[1:])
 
 
-def merge_sorted_lists_iter(x_list, y_list):
+def merge_sorted_lists_iter(x, y):
     """Merge two sorted lists by iteration."""
-    z_list = []
-
     # Apply two pointer method.
-    x_pos = 0
-    y_pos = 0
+    x_pos, y_pos = 0, 0
 
-    for _ in range(len(x_list) + len(y_list)):
-        if x_pos < len(x_list) and y_pos < len(y_list):
-            if x_list[x_pos] <= y_list[y_pos]:
-                z_list.append(x_list[x_pos])
+    z = []
+
+    for _ in range(len(x) + len(y)):
+        if x_pos < len(x) and y_pos < len(y):
+            if x[x_pos] <= y[y_pos]:
+                z.append(x[x_pos])
                 x_pos += 1
             else:
-                z_list.append(y_list[y_pos])
+                z.append(y[y_pos])
                 y_pos += 1      
-        elif x_pos < len(x_list) and y_pos >= len(y_list):
-            z_list.extend(x_list[x_pos:])
+        elif x_pos < len(x) and y_pos >= len(y):
+            z.extend(x[x_pos:])
             break
-        elif x_pos >= len(x_list) and y_pos < len(y_list):
-            z_list.extend(y_list[y_pos:])
+        elif x_pos >= len(x) and y_pos < len(y):
+            z.extend(y[y_pos:])
             break
 
-    return z_list
+    return z
 
 
-def merge_sort(a_list, merge_sorted_lists):
+def merge_sort(arr, merge):
     """Merge sort algorithm by divide and conquer with recursion.
 
     Time complexity: O(n*logn).
     Space complexity: O(n).
     """
-    if len(a_list) == 1:
-        return a_list
-    else:
-        # Divide and conquer to sort the 1st & 2nd half respectively and merge them.
-        mid = len(a_list) // 2
-        return merge_sorted_lists(
-            merge_sort(a_list[:mid], merge_sorted_lists), 
-            merge_sort(a_list[mid:], merge_sorted_lists))
+    if len(arr) == 1:
+        return arr
+    
+    # Sort the 1st & 2nd halves respectively and merge them.
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid], merge)
+    right = merge_sort(arr[mid:], merge)
+
+    return merge(left, right)
 
 
 def main():
     import time
     import random
 
-    a_list = range(100)
-    random.shuffle(a_list)
+    arr = range(100)
+    random.shuffle(arr)
 
     start_time = time.time()
-    print(merge_sort(a_list, merge_sorted_lists_recur))
+    print(merge_sort(arr, merge_sorted_lists_recur))
     print('By recusion: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print(merge_sort(a_list, merge_sorted_lists_iter))
+    print(merge_sort(arr, merge_sorted_lists_iter))
     print('By iteration: {}'.format(time.time() - start_time))
 
 
