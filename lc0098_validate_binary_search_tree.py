91cacc121882a88e527b33dbd187129ec6d45cd8
bowen0701 <bowen0701@gmail.com>
2019-10-02 20:07:18 +0200

    Correct space complexity

--- lc0098_validate_binary_search_tree.py
+++ lc0098_validate_binary_search_tree.py
@@ -58,7 +58,7 @@ class SolutionMinMaxRecur(object):
         :rtype: bool
 
         Time complexity: O(n).
-        Space complexity: O(n).
+        Space complexity: O(logn).
         """
         min_val, max_val = -float('inf'), float('inf')
         return self._isValidBSTUtil(root, min_val, max_val)
@@ -71,7 +71,7 @@ class SolutionMinMaxIter(object):
         :rtype: bool
 
         Time complexity: O(n).
-        Space complexity: O(n).
+        Space complexity: O(logn).
         """
         if not root:
             return True
@@ -126,7 +126,7 @@ class SolutionInorderRecur(object):
         :rtype: bool
 
         Time complexity: O(n).
-        Space complexity: O(n).
+        Space complexity: O(logn).
         """
         self.previous = None
         return self._isValidBSTUtil(root)
@@ -139,7 +139,7 @@ class SolutionInorderIter(object):
         :rtype: bool
 
         Time complexity: O(n).
-        Space complexity: O(n).
+        Space complexity: O(logn).
         """
         if not root:
             return True
