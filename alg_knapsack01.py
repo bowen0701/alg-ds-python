fc1f670d14fb9aef2eba47602fa9b3ad0bf4a4e5
bowen0701 <bowen0701@gmail.com>
2019-10-02 15:55:41 +0200

    Refactor without index n in func signature

--- alg_knapsack01.py
+++ alg_knapsack01.py
@@ -8,7 +8,7 @@ Given weights and values of n "non-splittable" items, put these items in a
 knapsack of capacity to get the maximum total value in the knapsack. 
 """
 
-def knapsack01_recur(val, wt, wt_cap, n):
+def knapsack01_recur_util(val, wt, wt_cap, n):
     """0-1 Knapsack Problem by naive recursion.
 
     Time complexity: O(2^n), where n is the number of items.
@@ -18,15 +18,25 @@ def knapsack01_recur(val, wt, wt_cap, n):
         return 0
     
     if wt[n] > wt_cap:
-        max_val = knapsack01_recur(val, wt, wt_cap, n - 1)
+        max_val = knapsack01_recur_util(val, wt, wt_cap, n - 1)
     else:
-        val_in = val[n] + knapsack01_recur(val, wt, wt_cap - wt[n], n - 1)
-        val_ex = knapsack01_recur(val, wt, wt_cap, n - 1)
+        val_in = val[n] + knapsack01_recur_util(val, wt, wt_cap - wt[n], n - 1)
+        val_ex = knapsack01_recur_util(val, wt, wt_cap, n - 1)
         max_val = max(val_in, val_ex)
     return max_val
 
 
-def _knapsack01_memo(val, wt, wt_cap, M, n):
+def knapsack01_recur(val, wt, wt_cap):
+    """0-1 Knapsack Problem by naive recursion.
+
+    Time complexity: O(2^n), where n is the number of items.
+    Space complexity: O(n).
+    """
+    n = len(wt) - 1
+    return knapsack01_recur_util(val, wt, wt_cap, n)
+
+
+def _knapsack01_memo_util(val, wt, wt_cap, M, n):
     if n < 0 or wt_cap == 0:
         return 0
 
@@ -34,16 +44,16 @@ def _knapsack01_memo(val, wt, wt_cap, M, n):
         return M[n][wt_cap]
 
     if wt[n] > wt_cap:
-        max_val = _knapsack01_memo(val, wt, wt_cap, M, n - 1)
+        max_val = _knapsack01_memo_util(val, wt, wt_cap, M, n - 1)
     else:
-        val_in = val[n] + _knapsack01_memo(val, wt, wt_cap - wt[n], M, n - 1)
-        val_ex = _knapsack01_memo(val, wt, wt_cap, M, n - 1)
+        val_in = val[n] + _knapsack01_memo_util(val, wt, wt_cap - wt[n], M, n - 1)
+        val_ex = _knapsack01_memo_util(val, wt, wt_cap, M, n - 1)
         max_val = max(val_in, val_ex)
     M[n][wt_cap] = max_val
     return max_val
 
 
-def knapsack01_memo(val, wt, wt_cap, n):
+def knapsack01_memo(val, wt, wt_cap):
     """0-1 Knapsack Problem by top-down dynamic programming w/ memoization.
 
     Time complexity: O(nC), where 
@@ -51,11 +61,14 @@ def knapsack01_memo(val, wt, wt_cap, n):
       - C is the weight capacity.
     Space complexity: O(nC).
     """
+    n = len(wt) - 1
     M = [[None for j in range(wt_cap + 1)] for i in range(n + 1)]
+
     for i in range(n + 1):
+        # For empty items.
         M[i][0] = 0
 
-    return _knapsack01_memo(val, wt, wt_cap, M, n)
+    return _knapsack01_memo_util(val, wt, wt_cap, M, n)
 
 
 def knapsack_dp(val, wt, wt_cap):
@@ -69,22 +82,24 @@ def knapsack_dp(val, wt, wt_cap):
     M = [[None for j in range(wt_cap + 1)] for i in range(len(wt))]
     
     for i in range(len(wt)):
+        # For empty items.
         M[i][0] = 0
     
     for j in range(1, wt_cap + 1):
-        if wt[0] > j:
-            M[0][j] = 0
-        else:
+        # For 1s item only.
+        if wt[0] <= j:
             M[0][j] = val[0]
+        else:
+            M[0][j] = 0
 
     for i in range(1, len(wt)):
         for j in range(1, wt_cap + 1):
-            if wt[i] > j:
-                M[i][j] = M[i - 1][j]
+            if wt[i] <= j:
+                M[i][j] = max(M[i-1][j], val[i] + M[i-1][j-wt[i]])
             else:
-                M[i][j] = max(M[i - 1][j], val[i] + M[i - 1][j - wt[i]])
+                M[i][j] = M[i-1][j]
 
-    return M[-1][-1], M
+    return M
 
 
 def item_list(M, wt, wt_cap):
@@ -107,21 +122,20 @@ def main():
     val = [6, 3, 5, 4, 6]
     wt = [2, 5, 4, 2, 3]
     wt_cap = 10
-    n = len(wt) - 1
-    # Ans: 17
+    # Output: 17
 
     start_time = time.time()
-    print(knapsack01_recur(val, wt, wt_cap, n))
-    print('Time by recursion: {}'.format(time.time() - start_time))
+    print('By recur: {}'.format(knapsack01_recur(val, wt, wt_cap)))
+    print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print(knapsack01_memo(val, wt, wt_cap, n))
+    print('By memo: {}'.format(knapsack01_memo(val, wt, wt_cap)))
     print('Time by memo: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    max_val, M = knapsack_dp(val, wt, wt_cap)
-    print(max_val)
-    print('Time by DP: {}'.format(time.time() - start_time))
+    M = knapsack_dp(val, wt, wt_cap)
+    print('By DP: {}'.format(M[-1][-1]))
+    print('Time: {}'.format(time.time() - start_time))
     print('Items: {}'.format(item_list(M, wt, wt_cap)))
 
 
