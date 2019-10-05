31d599bad808822c3bee79c50dcfac36747fd6ce
bowen0701 <bowen0701@gmail.com>
2019-10-05 22:27:05 +0200

    Complete recur sol

--- lc0590_n-ary_tree_postorder_traversal.py
+++ lc0590_n-ary_tree_postorder_traversal.py
@@ -24,17 +24,51 @@ class Node(object):
         self.children = children
 
 
-class Solution(object):
+class SolutionRecur(object):
+    def _postorderRrecur(self, root, vals):
+        if not root:
+            return None
+
+        # Collect children nodes's vals first.
+        for child in root.children:
+            self._postorderRrecur(child, vals)
+
+        # Collect root node's val.
+        vals.append(root.val)
+
     def postorder(self, root):
         """
         :type root: Node
         :rtype: List[int]
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        # Apply recursive postorder traversal.
+        if not root:
+            return []
+
+        vals = []
+        self._postorderRrecur(root, vals)
+        return vals
+
 
 
 def main():
-    pass
+    # For example, given a 3-ary tree:
+    #       1
+    #     / | \
+    #    3  2  4
+    #   / \
+    #  5   6
+    # Return its postorder traversal as: [5,6,3,2,4,1].
+    root = Node(1, [])
+    root.children.append(Node(3, []))
+    root.children.append(Node(2, []))
+    root.children.append(Node(4, []))
+    root.children[0].children.append(Node(5, []))
+    root.children[0].children.append(Node(6, []))
+    print SolutionRecur().postorder(root)
 
 
 if __name__ == '__main__':
