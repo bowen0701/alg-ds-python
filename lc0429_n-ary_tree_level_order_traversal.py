f8a078fb6ebaf4f64263c3f6cd7333f0576e45e6
bowen0701 <bowen0701@gmail.com>
2019-10-05 23:10:07 +0200

    Complete bfs sol

--- lc0429_n-ary_tree_level_order_traversal.py
+++ lc0429_n-ary_tree_level_order_traversal.py
@@ -7,7 +7,11 @@ Given an n-ary tree, return the level order traversal of its nodes' values.
 (ie, from left to right, level by level).
 
 For example, given a 3-ary tree:
-
+      1
+    / | \
+   3  2  4
+  / \
+ 5   6
 We should return its level order traversal:
 [
      [1],
@@ -27,17 +31,56 @@ class Node(object):
         self.children = children
 
 
-class Solution(object):
+class SolutionBFS(object):
     def levelOrder(self, root):
         """
         :type root: Node
         :rtype: List[List[int]]
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        # Apply BFS with queue.
+        if not root:
+            return []
+
+        vals = []
+        queue = [root]
+
+        while queue:
+            # Collect values in level.
+            level = []
+
+            # Iterate over nodes in a level.
+            for i in range(len(queue)):
+                current = queue.pop()
+                level.append(current.val)
+
+                # Insert current's children into queue for further BFS.
+                for child in current.children:
+                    queue.insert(0, child)
+
+            vals.append(level)
+
+        return vals
 
 
 def main():
-    pass
+    # For example, given a 3-ary tree:
+    #       1
+    #     / | \
+    #    3  2  4
+    #   / \
+    #  5   6
+    # Return its level-order traversal as: [[1], [3,2,4], [5,6]].
+    root = Node(1, [])
+    root.children.append(Node(3, []))
+    root.children.append(Node(2, []))
+    root.children.append(Node(4, []))
+    root.children[0].children.append(Node(5, []))
+    root.children[0].children.append(Node(6, []))
+
+    print SolutionBFS().levelOrder(root)
 
 
 if __name__ == '__main__':
