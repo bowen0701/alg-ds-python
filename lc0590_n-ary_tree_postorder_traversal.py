0f96cc8d0c66b4ea9816218c81f5f27f4c187cdc
bowen0701 <bowen0701@gmail.com>
2019-10-05 22:15:13 +0200

    Add lc0590_n-ary_tree_postorder_traversal.py

---
+++ lc0590_n-ary_tree_postorder_traversal.py
@@ -0,0 +1,41 @@
+"""Leetcode 590. N-ary Tree Postorder Traversal
+Easy
+
+URL: https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+For example, given a 3-ary tree:
+      1
+    / | \
+   3  2  4
+  / \
+ 5   6
+Return its postorder traversal as: [5,6,3,2,4,1].
+
+Note:
+Recursive solution is trivial, could you do it iteratively?
+"""
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+
+
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
