e7d1f7c0fcef1cdfb43bc31fea575872ddefb8af
bowen0701 <bowen0701@gmail.com>
2019-10-14 22:55:17 +0200

    Complete two pointer sol

--- lc0704_binary_search.py
+++ lc0704_binary_search.py
@@ -30,11 +30,34 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        pass
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        if nums[left] == target:
+            return left
+        else:
+            return -1
 
 
 def main():
-    pass
+    # Output: 4
+    nums = [-1,0,3,5,9,12]
+    target = 9
+    print Solution().search(nums, target)
+
+    # Output: -1
+    nums = [-1,0,3,5,9,12]
+    target = 2
+    print Solution().search(nums, target)    
 
 
 if __name__ == '__main__':
