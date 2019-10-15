0295a1cbad1dfa2443e6b8e8d639b7d845adaebf
bowen0701 <bowen0701@gmail.com>
2019-10-15 23:19:44 +0200

    Add lc0225_implement_stack_using_queues.py

---
+++ lc0225_implement_stack_using_queues.py
@@ -0,0 +1,77 @@
+"""Leetcode 225. Implement Stack using Queues
+Easy
+
+URL: https://leetcode.com/problems/implement-stack-using-queues/
+
+Implement the following operations of a stack using queues.
+- push(x) -- Push element x onto stack.
+- pop() -- Removes the element on top of the stack.
+- top() -- Get the top element.
+- empty() -- Return whether the stack is empty.
+
+Example:
+MyStack stack = new MyStack();
+stack.push(1);
+stack.push(2);  
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+
+Notes:
+- You must use only standard operations of a queue -- which means only 
+  push to back, peek/pop from front, size, and is empty operations are valid.
+- Depending on your language, queue may not be supported natively. You may simulate
+  a queue by using a list or deque (double-ended queue), as long as you use only
+  standard operations of a queue.
+- You may assume that all operations are valid (for example, no pop or top
+  operations will be called on an empty stack).
+"""
+
+class MyStack(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        pass
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: None
+        """
+        pass
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        pass
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        pass
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        pass
+
+
+def main():
+    # obj = MyStack()
+    # obj.push(x)
+    # param_2 = obj.pop()
+    # param_3 = obj.top()
+    # param_4 = obj.empty()
+    pass
+
+
+if __name__ == '__main__':
+    main()
