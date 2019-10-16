2931607fe1067b20cb677acc4cbc8f7ea06f320d
bowen0701 <bowen0701@gmail.com>
2019-10-16 23:25:42 +0200

    Add lc0113_path_sum_ii.py

---
+++ lc0113_path_sum_ii.py
@@ -0,0 +1,50 @@
+"""Leetcode 113. Path Sum II
+Medium
+
+URL: https://leetcode.com/problems/path-sum-ii/
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum
+equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+Given the below binary tree and sum = 22,
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+Return:
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
