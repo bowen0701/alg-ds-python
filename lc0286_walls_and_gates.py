f5ec4bcd047da40f29dbb3d222acc63c208c05a9
bowen0701 <bowen0701@gmail.com>
2019-10-18 23:38:02 +0200

    Complete BFS sol

--- lc0286_walls_and_gates.py
+++ lc0286_walls_and_gates.py
@@ -25,17 +25,58 @@ After running your function, the 2D grid should be:
 0  -1   3   4
 """
 
-class Solution(object):
+class SolutionBFS(object):
     def wallsAndGates(self, rooms):
         """
         :type rooms: List[List[int]]
         :rtype: void Do not return anything, modify rooms in-place instead.
+
+        Apply BFS starting from all gates.
+
+        Time complexity: O(kmn), where
+          - k: number of gates
+          - m: number of rows
+          - n: number of columns
+        Space complexity: O(mn).
         """
-        pass
+        nrows, ncols = len(rooms), len(rooms[0])
+
+        # Collect all gates in queue.
+        gates = []
+        for i in range(nrows):
+            for j in range(ncols):
+                if rooms[i][j] == 0:
+                    gates.append((i, j))
+
+        # For each gate, start BFS to update neighbors's shorter distances.
+        while gates:
+            queue = [gates.pop()]
+
+            while queue:
+                r, c = queue.pop()
+
+                dirs = [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
+                for r_next, c_next in dirs:
+                    if (0 <= r_next < nrows and 0 <= c_next < ncols and
+                        rooms[r_next][c_next] > rooms[r][c] + 1):
+                        rooms[r_next][c_next] = rooms[r][c] + 1
+                        queue.insert(0, (r_next, c_next))
 
 
 def main():
-    pass
+    # After running your function, the 2D grid should be:
+    # 3  -1   0   1
+    # 2   2   1  -1
+    # 1  -1   2  -1
+    # 0  -1   3   4
+    rooms = [
+        [float('inf'), -1, 0, float('inf')],
+        [float('inf'), float('inf'), float('inf'), -1],
+        [float('inf'), -1, float('inf'), -1],
+        [0, -1, float('inf'), float('inf')]
+    ]
+    SolutionBFS().wallsAndGates(rooms)
+    print rooms
 
 
 if __name__ == '__main__':
