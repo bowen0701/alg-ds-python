2e023d9fa99319d4cfcacae2c77e83832464e5a7
bowen0701 <bowen0701@gmail.com>
2019-10-27 22:42:08 +0100

    Complete backtracking sol

--- lc0090_subsets_ii.py
+++ lc0090_subsets_ii.py
@@ -21,17 +21,39 @@ Output:
 ]
 """
 
-class Solution(object):
+class SolutionBacktrack(object):
+    def _backtrack(self, result, temp, start, nums):
+        # Use shallow copy.
+        result.append(temp[:])
+
+        for i in range(start, len(nums)):
+            # Avoid duplicate by checking the previous num.
+            if i == start or nums[i] != nums[i - 1]:
+                temp.append(nums[i])
+                self._backtrack(result, temp, i + 1, nums)
+
+                # Pop for backtracking.
+                temp.pop()
+
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        pass
+        # Sort nums to avoid duplicates.
+        nums.sort()
+
+        result = []
+        temp = []
+        start = 0
+        self._backtrack(result, temp, start, nums)
+        return result
 
 
 def main():
-    pass
+    # Output = [[2],[1],[1,2,2],[2,2],[1,2],[]]
+    nums = [1,2,2]
+    print SolutionBacktrack().subsetsWithDup(nums)
 
 
 if __name__ == '__main__':
