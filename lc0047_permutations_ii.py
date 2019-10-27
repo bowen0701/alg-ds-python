76d3ce8cda80ab91dde0fde3312eb02e1cc03810
bowen0701 <bowen0701@gmail.com>
2019-10-27 22:57:21 +0100

    Add lc0047_permutations_ii.py

---
+++ lc0047_permutations_ii.py
@@ -0,0 +1,33 @@
+"""Leetcode 47. Permutations II
+Medium
+
+URL: https://leetcode.com/problems/permutations-ii/
+
+Given a collection of numbers that might contain duplicates,
+return all possible unique permutations.
+
+Example:
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+"""
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
