ed0d94a1f05f93ba59cb69a181b5665793e4d8d4
bowen0701 <bowen0701@gmail.com>
2019-10-27 23:21:11 +0100

    Complete backtracking sol

--- lc0047_permutations_ii.py
+++ lc0047_permutations_ii.py
@@ -16,17 +16,53 @@ Output:
 ]
 """
 
-class Solution(object):
+class SolutionBacktrack(object):
+    def _backtrack(self, result, temp, is_used, nums):
+        if len(temp) == len(nums):
+            # Once a permutation is completed, shallow copy it to result.
+            result.append(temp[:])
+            return None
+
+        for i in range(len(nums)):
+            if (is_used[i] or 
+                (i > 0 and nums[i] == nums[i - 1] and is_used[i - 1])):
+                continue
+
+            is_used[i] = True
+            temp.append(nums[i])
+            self._backtrack(result, temp, is_used, nums)
+
+            # Backtracking.
+            is_used[i] = False
+            temp.pop()
+
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
+
+        Time complexity: O(n*n!), where
+          - n is the length of nums for copying temp,
+          - n! is for permutation.
+        Space complexity: O(n*n!).
         """
-        pass
+        # Sort to further avoid duplicates.
+        nums.sort()
+
+        result = []
+        temp = []
+
+        # Use used array to memorize usage in backtracking.
+        is_used = [False] * len(nums)
+
+        self._backtrack(result, temp, is_used, nums)
+        return result
 
 
 def main():
-    pass
+    # Output: [[1,1,2],[1,2,1],[2,1,1]]
+    nums = [1,1,2,4]
+    print SolutionBacktrack().permuteUnique(nums)
 
 
 if __name__ == '__main__':
