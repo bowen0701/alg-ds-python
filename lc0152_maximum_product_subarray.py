2b070018e55be974ad00dbd90723faaeefcd0f69
bowen0701 <bowen0701@gmail.com>
2019-11-06 23:42:02 +0100

    Complete DP sol

--- lc0152_maximum_product_subarray.py
+++ lc0152_maximum_product_subarray.py
@@ -17,17 +17,50 @@ Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 """
 
-class Solution(object):
+class SolutionDP(object):
     def maxProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        Time complexity: O(n), where n is the nums length.
+        Space complexity: O(n).
         """
-        pass
+        cur_max_prod = [0] * len(nums)
+        cur_min_prod = [0] * len(nums)
+        cur_max_prod[0] = nums[0]
+        cur_min_prod[0] = nums[0]
+
+        global_max_prod = nums[0]
+
+        for i in range(1, len(nums)):
+            # Compute current max/min product before pos i:
+            cur_max_prod[i] = max(cur_max_prod[i - 1] * nums[i],
+                                  cur_min_prod[i - 1] * nums[i],
+                                  nums[i])
+            cur_min_prod[i] = min(cur_max_prod[i - 1] * nums[i],
+                                  cur_min_prod[i - 1] * nums[i],
+                                  nums[i])
+
+            # Update global max prod before pos i.
+            global_max_prod = max(cur_max_prod[i], global_max_prod)
+
+        return global_max_prod
 
 
 def main():
-    pass
+    # Output: 6
+    nums = [2,3,-2,4]
+    print SolutionDP().maxProduct(nums)
+
+    # Output: 0
+    nums = [-2,0,-1]
+    print SolutionDP().maxProduct(nums)
+
+    # Output: 24
+    nums = [-2,3,-4]
+    print SolutionDP().maxProduct(nums)
+
 
 
 if __name__ == '__main__':
