c5fd4b3aaa41a7fc41be441a3ff9e5486aed0f17
bowen0701 <bowen0701@gmail.com>
2019-11-06 23:41:49 +0100

    Remove redundant comments

--- lc0053_maximum_subarray.py
+++ lc0053_maximum_subarray.py
@@ -33,7 +33,6 @@ class SolutionDp(object):
 
         for i in range(1, len(nums)):
         	# Compute current max subarray sum before pos i.
-            # T[i] = max(T[i-1] + nums[i], nums[i])
         	cur_max_sums[i] = max(cur_max_sums[i - 1] + nums[i], nums[i])
 
         	# Update global max sum before pos i.
