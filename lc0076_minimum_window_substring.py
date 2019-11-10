bafc72f4851c1fdb14ec26c168b016a6c594ddd4
bowen0701 <bowen0701@gmail.com>
2019-11-11 00:53:26 +0100

    Complete dict two pointers sol

--- lc0076_minimum_window_substring.py
+++ lc0076_minimum_window_substring.py
@@ -17,18 +17,68 @@ Note:
   unique minimum window in S.
 """
 
-class Solution(object):
+class SolutionDictTwoPointers(object):
     def minWindow(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: str
+
+        Time complexity: O(m+n), where
+          - m: lenght of s,
+          - n: lenght of t.
+        Space complexity: O(m+n).
         """
-        pass
+        from collections import defaultdict
+
+        s_len, t_len = len(s), len(t)
+
+        # Use dict to collect char counts of t.
+        t_char_counts = defaultdict(int)
+        for c in t:
+            t_char_counts[c] += 1
+
+        # Track min left & right, and counter.
+        min_left, min_len = 0, float('inf')
+        counter = t_len
+
+        # Apply two pointers method with left & right from head as a window.
+        left, right = 0, 0
+
+        # In s, move right to increase window to satify t.
+        while right < s_len:
+            # If s[right] exits in t, decrement counter.
+            if t_char_counts[s[right]] > 0:
+                counter -= 1
+
+            # Decrement t_char_counts[s[right]], which will be < 0,
+            # if s[right] does not exist in t.
+            t_char_counts[s[right]] -= 1
+            right += 1
+
+            # While we found valid window, move left to shorten it.
+            while counter == 0:
+                if right - left < min_len:
+                    min_len = right - left
+                    min_left = left
+
+                t_char_counts[s[left]] += 1
+                if t_char_counts[s[left]] > 0:
+                    counter += 1
+
+                left += 1
+
+
+        if min_len < float('inf'):
+            return s[min_left:(min_left + min_len)]
+        else:
+            return ''
 
 
 def main():
-    pass
+    # Output: "BANC"
+    s, t = "ADOBECODEBANC", "ABC"
+    print SolutionDictTwoPointers().minWindow(s, t)
 
 
 if __name__ == '__main__':
