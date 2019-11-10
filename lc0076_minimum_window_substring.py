18dda928313a0cd7af06b706b7d2d87435b480e4
bowen0701 <bowen0701@gmail.com>
2019-11-10 23:14:52 +0100

    Add lc0076_minimum_window_substring.py

---
+++ lc0076_minimum_window_substring.py
@@ -0,0 +1,35 @@
+"""Leetcode 76. Minimum Window Substring
+Hard
+
+URL: https://leetcode.com/problems/minimum-window-substring/
+
+Given a string S and a string T, find the minimum window in S which will contain
+all the characters in T in complexity O(n).
+
+Example:
+Input: S = "ADOBECODEBANC", T = "ABC"
+Output: "BANC"
+
+Note:
+- If there is no such window in S that covers all characters in T, return the
+  empty string "".
+- If there is such window, you are guaranteed that there will always be only one
+  unique minimum window in S.
+"""
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
