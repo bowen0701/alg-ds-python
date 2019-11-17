9ae99b4850a5e6d08690321e361e0ce3e02ad9fb
bowen0701 <bowen0701@gmail.com>
2019-11-17 14:25:57 +0100

    Complete two lists sol

--- lc0086_partition_list.py
+++ lc0086_partition_list.py
@@ -21,18 +21,51 @@ class ListNode(object):
         self.next = None
 
 
-class Solution(object):
+class SolutionTwoLists(object):
     def partition(self, head, x):
         """
         :type head: ListNode
         :type x: int
         :rtype: ListNode
         """
-        pass
+        prev_head1 = ListNode(None)
+        prev_head2 = ListNode(None)
+        current1 = prev_head1
+        current2 = prev_head2
+
+        current = head
+        while current:
+            if current.val < x:
+                current1.next = current
+                current1 = current1.next
+            else:
+                current2.next = current
+                current2 = current2.next
+
+            current = current.next
+
+        current1.next = prev_head2.next
+        current2.next = None
+        return prev_head1.next
 
 
 def main():
-    pass
+    # Input: head = 1->4->3->2->5->2, x = 3
+    # Output: 1->2->2->4->3->5
+    head = ListNode(1)
+    head.next = ListNode(4)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next = ListNode(2)
+    x = 3
+    new_head = SolutionTwoLists().partition(head, x)
+    print new_head.val
+    print new_head.next.val
+    print new_head.next.next.val
+    print new_head.next.next.next.val
+    print new_head.next.next.next.next.val
+    print new_head.next.next.next.next.next.val
 
 
 if __name__ == '__main__':
