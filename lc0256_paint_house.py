f7dc48d1cf3e2b7685774a279cfd95264caaedb7
bowen0701 <bowen0701@gmail.com>
2019-11-21 07:29:33 +0100

    Revise array selection style

--- lc0256_paint_house.py
+++ lc0256_paint_house.py
@@ -38,9 +38,9 @@ class SolutionDp(object):
 
         for i in range(1, n_houses):
             # Accumulate painting costs of the other two houses.
-            costs[i][0] += min(costs[i-1][1], costs[i-1][2])
-            costs[i][1] += min(costs[i-1][0], costs[i-1][2])
-            costs[i][2] += min(costs[i-1][0], costs[i-1][1])
+            costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
+            costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
+            costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
 
         return min(costs[-1])
 
