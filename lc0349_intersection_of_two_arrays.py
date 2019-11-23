204705fdb188e5124b44d015dbd07af52edc534b
bowen0701 <bowen0701@gmail.com>
2019-11-23 13:01:15 +0100

    Complete built-in sol

--- lc0349_intersection_of_two_arrays.py
+++ lc0349_intersection_of_two_arrays.py
@@ -18,18 +18,29 @@ Note:
 - The result can be in any order.
 """
 
-class Solution(object):
+class SolutionBuiltIn(object):
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
+
+        Time complexity: O(n1*n2).
+        Space complexity: O(n1+n2).
         """
-        pass
+        return list(set(nums1).intersection(set(nums2)))
 
 
 def main():
-    pass
+    # Output: [2]
+    nums1 = [1,2,2,1]
+    nums2 = [2,2]
+    print SolutionBuiltIn().intersection(nums1, nums2)
+
+    # Output: [9, 4]
+    nums1 = [4,9,5]
+    nums2 = [9,4,9,8,4]
+    print SolutionBuiltIn().intersection(nums1, nums2)
 
 
 if __name__ == '__main__':
