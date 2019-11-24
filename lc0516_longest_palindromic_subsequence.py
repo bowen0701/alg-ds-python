96c99eee430fb4fd6507a1fbbd4721905f708c69
bowen0701 <bowen0701@gmail.com>
2019-11-24 21:55:27 +0800

    Complete memo sol

--- lc0516_longest_palindromic_subsequence.py
+++ lc0516_longest_palindromic_subsequence.py
@@ -34,7 +34,8 @@ class SolutionRecur(object):
             return self._LPS(s, left + 1, right - 1) + 2
         else:
             # Then check max of LPS's in s[left+1:right-1] and s[left:right].
-            return max(self._LPS(s, left + 1, right), self._LPS(s, left, right - 1))
+            return max(self._LPS(s, left + 1, right), 
+                       self._LPS(s, left, right - 1))
 
     def longestPalindromeSubseq(self, s):
         """
@@ -51,14 +52,52 @@ class SolutionRecur(object):
         return self._LPS(s, left, right)
 
 
+class SolutionMemo(object):
+    def _LPS(self, s, left, right, T):
+        if T[left][right]:
+            return T[left][right]
+
+        if left > right:
+            return 0
+
+        if s[left] == s[right]:
+            # Then check LPS in s[left+1:right].
+            T[left][right] = self._LPS(s, left + 1, right - 1, T) + 2
+        else:
+            # Then check max of LPS's in s[left+1:right-1] and s[left:right].
+            T[left][right] = max(self._LPS(s, left + 1, right, T), 
+                                 self._LPS(s, left, right - 1, T))
+        return T[left][right]
+
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+
+        Time complexity: O(n^2).
+        Space complexity: O(n^2).
+        """
+        # Apply top-down recursion with memoization.
+        n = len(s)
+        T = [[None] * n for _ in range(n)]
+
+        for i in range(n):
+            T[i][i] = 1
+
+        left, right = 0, len(s) - 1
+        return self._LPS(s, left, right, T)
+
+
 def main():
     # Output: 4.
     s = "bbbab"
     print SolutionRecur().longestPalindromeSubseq(s)
+    print SolutionMemo().longestPalindromeSubseq(s)
 
     # Output: 2.
     s = "cbbd"
     print SolutionRecur().longestPalindromeSubseq(s)
+    print SolutionMemo().longestPalindromeSubseq(s)
 
 
 if __name__ == '__main__':
