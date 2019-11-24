7ac628984faf81d5cfbb01011c31b73ebec5cd95
bowen0701 <bowen0701@gmail.com>
2019-11-24 22:36:51 +0800

    Complete dp sol

--- lc0516_longest_palindromic_subsequence.py
+++ lc0516_longest_palindromic_subsequence.py
@@ -30,10 +30,10 @@ class SolutionRecur(object):
             return 1
 
         if s[left] == s[right]:
-            # Then check LPS in s[left+1:right].
+            # Check LPS in subsequence between s[left] and s[right].
             return self._LPS(s, left + 1, right - 1) + 2
         else:
-            # Then check max of LPS's in s[left+1:right-1] and s[left:right].
+            # Check max of LPS's in s[left+1:right-1] and s[left:right].
             return max(self._LPS(s, left + 1, right), 
                        self._LPS(s, left, right - 1))
 
@@ -61,10 +61,10 @@ class SolutionMemo(object):
             return 0
 
         if s[left] == s[right]:
-            # Then check LPS in s[left+1:right].
+            # Check LPS in subsequence between s[left] and s[right].
             T[left][right] = self._LPS(s, left + 1, right - 1, T) + 2
         else:
-            # Then check max of LPS's in s[left+1:right-1] and s[left:right].
+            # Check max of LPS's in s[left+1:right-1] and s[left:right].
             T[left][right] = max(self._LPS(s, left + 1, right, T), 
                                  self._LPS(s, left, right - 1, T))
         return T[left][right]
@@ -88,16 +88,44 @@ class SolutionMemo(object):
         return self._LPS(s, left, right, T)
 
 
+class SolutionDp(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+
+        Time complexity: O(n^2).
+        Space complexity: O(n^2).
+        """
+        # Apply bottom-up dynamic programming.
+        n = len(s)
+        T = [[0] * n for _ in range(n)]
+
+        # Start left from tail for bottom-up.
+        for left in range(n - 1, -1, -1):
+            T[left][left] = 1
+
+            for right in range(left + 1, n):
+                if s[left] == s[right]:
+                    T[left][right] = T[left + 1][right - 1] + 2
+                else:
+                    T[left][right] = max(T[left][right - 1], T[left + 1][right])
+
+        return T[0][-1]
+
+
 def main():
     # Output: 4.
     s = "bbbab"
     print SolutionRecur().longestPalindromeSubseq(s)
     print SolutionMemo().longestPalindromeSubseq(s)
+    print SolutionDp().longestPalindromeSubseq(s)
 
     # Output: 2.
     s = "cbbd"
     print SolutionRecur().longestPalindromeSubseq(s)
     print SolutionMemo().longestPalindromeSubseq(s)
+    print SolutionDp().longestPalindromeSubseq(s)
 
 
 if __name__ == '__main__':
