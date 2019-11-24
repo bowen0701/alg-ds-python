270ba416b6962f11fce81bce987d481e5ad2fd53
bowen0701 <bowen0701@gmail.com>
2019-11-24 19:03:30 +0800

    Complete recur sol

--- lc0516_longest_palindromic_subsequence.py
+++ lc0516_longest_palindromic_subsequence.py
@@ -21,17 +21,39 @@ Output:
 One possible longest palindromic subsequence is "bb".
 """
 
-class Solution(object):
+class SolutionRecur(object):
+    def _LPS(self, s, left, right):
+        if left > right:
+            return 0
+
+        if left == right:
+            return 1
+
+        if s[left] == s[right]:
+            # Then check LPS in s[left+1:right].
+            return self._LPS(s, left + 1, right - 1) + 2
+        else:
+            # Then check max of LPS's in s[left+1:right-1] and s[left:right].
+            return max(self._LPS(s, left + 1, right), self._LPS(s, left, right - 1))
+
     def longestPalindromeSubseq(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        # Apply top-down recursion.
+        left, right = 0, len(s) - 1
+        return self._LPS(s, left, right)
 
 
 def main():
-    pass
+    # Output: 4.
+    s = "bbbab"
+    print SolutionRecur().longestPalindromeSubseq(s)
+
+    # Output: 2.
+    s = "cbbd"
+    print SolutionRecur().longestPalindromeSubseq(s)
 
 
 if __name__ == '__main__':
