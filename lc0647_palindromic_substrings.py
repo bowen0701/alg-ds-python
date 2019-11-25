9fb6c89bcb2c3fdb83a5f3e852d101cc228c9666
bowen0701 <bowen0701@gmail.com>
2019-11-25 15:29:09 +0800

    Complete center expansion sol

--- lc0647_palindromic_substrings.py
+++ lc0647_palindromic_substrings.py
@@ -19,17 +19,39 @@ Output: 6
 Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 """
 
-class Solution(object):
+class SolutionCenterExpansion(object):
     def countSubstrings(self, s):
         """
         :type s: str
         :rtype: int
+
+        Time complexity: O(n^2).
+        Space complexity: O(1).
         """
-        pass
+        n = len(s)
+        n_palindromes = 0
+
+        # Iterate to check palindrome starting from 2*n-1 center expansions.
+        for i in range(2 * n - 1):
+            left = i // 2
+            right = (i + 1) // 2
+            while left >=0 and right < n and s[left] == s[right]:
+                # If palindrome, expand to left/right char to further check.
+                n_palindromes += 1
+                left -= 1
+                right += 1
+
+        return n_palindromes
 
 
 def main():
-    pass
+    # Output: 3.
+    s = "abc"
+    print SolutionCenterExpansion().countSubstrings(s)
+
+    # Output: 6.
+    s = "aaa"
+    print SolutionCenterExpansion().countSubstrings(s)
 
 
 if __name__ == '__main__':
