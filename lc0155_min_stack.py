69b41f5d41306e3b4ab00ee75f779cb14c99188b
bowen0701 <bowen0701@gmail.com>
2019-11-25 15:32:43 +0800

    Remove redundant space line

--- lc0155_min_stack.py
+++ lc0155_min_stack.py
@@ -10,7 +10,6 @@ push(x) -- Push element x onto stack.
 pop() -- Removes the element on top of the stack.
 top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
- 
 
 Example:
 MinStack minStack = new MinStack();
