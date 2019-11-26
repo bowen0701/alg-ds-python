124e99d74b0017de4886662345c513d861a5077a
bowen0701 <bowen0701@gmail.com>
2019-11-26 22:04:46 +0800

    Complete max stack

--- lc0716_max_stack.py
+++ lc0716_max_stack.py
@@ -35,29 +35,100 @@ class MinStack(object):
         """
         initialize your data structure here.
         """
-        pass
+        self._stack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
+
+        Time complexity: O(1).
+        Space complexity: O(n).
         """
-        pass
+        if not self._stack:
+            self._stack.append((x, x))
+        else:
+            maximum = max(self._stack[-1][1], x)
+            self._stack.append((x, maximum))
 
     def pop(self):
         """
         :rtype: None
+
+        Time complexity: O(1).
+        Space complexity: O(n).
         """
-        pass
+        return self._stack.pop()[0]
 
     def top(self):
         """
         :rtype: int
-        """
-        pass
 
-    def getMin(self):
+        Time complexity: O(1).
+        Space complexity: O(n).
+        """
+        if not self._stack:
+            return None
+
+        return self._stack[-1][0]
+
+
+    def peekMax(self):
         """
         :rtype: int
+
+        Time complexity: O(1).
+        Space complexity: O(n).
         """
-        pass
+        if not self._stack:
+            return None
+        
+        return self._stack[-1][1]
+
+    def popMax(self):
+        """
+        :rtype: int
+
+        Time complexity: O(n).
+        Space complexity: O(n).
+        """
+        if not self._stack:
+            return None
+
+        # Pop until found current max, then push back the others.
+        maximum = self._stack[-1][1]
+
+        stack = []
+        while self._stack[-1][0] != maximum:
+            stack.append(self._stack.pop()[0])
+
+        self._stack.pop()
+
+        while stack:
+            self.push(stack.pop())
+
+        return maximum
+
+
+def main():
+    min_stack = MinStack()
+    min_stack.push(5)
+    min_stack.push(1)
+    min_stack.push(5)
+
+    # Output: 5
+    print min_stack.top()
+    # Output: 5
+    print min_stack.popMax()
+    # Output: 1
+    print min_stack.top()
+    # Output: 5
+    print min_stack.peekMax()
+    # Output: 1
+    print min_stack.pop()
+    # Output: 5
+    print min_stack.top()
+
+
+if __name__ == '__main__':
+    main()
