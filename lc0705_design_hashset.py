82906b3af7cc092fbe029a52350beb2745e8530c
bowen0701 <bowen0701@gmail.com>
2019-11-29 16:31:14 +0100

    Implementing hashset

--- lc0705_design_hashset.py
+++ lc0705_design_hashset.py
@@ -36,21 +36,64 @@ class MyHashSet(object):
         """
         Initialize your data structure here.
         """
-        pass
+        # Init hashset using list with load factor 2/3.
+        self.capacity = 8
+        self.size = 0
+        self.set = [None] * self.capacity
+        self.load_factor = 2.0 / 3
+
+    def _hash(self, key):
+        return key % self.capacity
+
+    def _rehash(self, key):
+        return (5 * key + 1) % self.capacity
 
     def add(self, key):
         """
         :type key: int
         :rtype: None
         """
-        pass
+        # If hashset is larger than load factor, double it and copy old to new.
+        if float(self.size) / self.capacity >= self.load_factor:
+            self.capacity <<= 1
+            new_set = [None] * self.capacity
+
+            for h in range(self.capacity >> 1):
+                if new_set[h]:
+                    old_key = self.set[h]
+                    new_h = self._hash(old_key)
+                    while new_set[new_h]:
+                        new_h = self._rehash(new_h)
+                    new_set[new_h] = old_key
+
+            self.set = new_set
+
+        # Add key by Open Addressing with rehashing.
+        hash_key = self._hash(key)
+
+        while self.set[hash_key]:
+            if self.set[hash_key] == key:
+                return None
+            else:
+                hash_key = self._rehash(hash_key)
+
+        self.set[hash_key] = key
+        self.size += 1
 
     def remove(self, key):
         """
         :type key: int
         :rtype: None
         """
-        pass
+        hash_key = self._hash(key)
+
+        while self.set[hash_key]:
+            if self.set[hash_key] == key:
+                self.set[hash_key] = None
+                self.size -= 1
+                return None
+            else:
+                hash_key = self._rehash(hash_key)
 
     def contains(self, key):
         """
@@ -58,11 +101,27 @@ class MyHashSet(object):
         :type key: int
         :rtype: bool
         """
-        pass
+        hash_key = self._hash(key)
+
+        while self.set[hash_key]:
+            if self.set[hash_key] == key:
+                return True
+            else:
+                hash_key = self._rehash(hash_key)
+
+        return False
 
 
 def main():
-    pass
+    hashSet = MyHashSet()
+    hashSet.add(1)
+    hashSet.add(2)
+    print hashSet.contains(1)     # returns true
+    print hashSet.contains(3)     # returns false (not found)
+    hashSet.add(2)
+    print hashSet.contains(2)     # returns true
+    hashSet.remove(2)
+    print hashSet.contains(2)     # returns false (already removed)
 
 
 if __name__ == '__main__':
