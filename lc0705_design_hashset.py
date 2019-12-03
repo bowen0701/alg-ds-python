22af79a0603c8b35df8f5811900c78893df7d307
bowen0701 <bowen0701@gmail.com>
2019-12-03 21:27:48 +0100

    Revise typo: Inew_setert->Insert

--- lc0705_design_hashset.py
+++ lc0705_design_hashset.py
@@ -7,7 +7,7 @@ Design a HashSet without using any built-in hash table libraries.
 
 To be specific, your design should include these functions:
 
-- add(value): Inew_setert a value into the HashSet. 
+- add(value): Insert a value into the HashSet. 
 - contains(value) : Return whether the value exists in the HashSet or not.
 - remove(value): Remove a value in the HashSet. 
 
