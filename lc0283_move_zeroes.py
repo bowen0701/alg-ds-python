7b2def066e4fde751287f8916d095473e07055eb
bowen0701 <bowen0701@gmail.com>
2019-12-06 20:47:19 +0100

    Complete update sol

--- lc0283_move_zeroes.py
+++ lc0283_move_zeroes.py
@@ -14,7 +14,7 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 """
 
-class Solution(object):
+class SolutionPopAppend(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
@@ -33,13 +33,38 @@ class Solution(object):
                 start += 1
 
 
+class SolutionUpdate(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+
+        Time complexity: O(n).
+        Space complexity: O(1).
+        """
+        start = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[start] = nums[i]
+                start += 1
+
+        for i in range(start, len(nums)):
+            nums[i] = 0
+
+
 def main():
     nums1 = [0, 1, 0, 3, 12]
-    Solution().moveZeroes(nums1)
+    SolutionPopAppend().moveZeroes(nums1)
+    print nums1
+    nums1 = [0, 1, 0, 3, 12]
+    SolutionUpdate().moveZeroes(nums1)
     print nums1
 
     nums2 = [0, 0, 3, 12, 1, 0, 5]
-    Solution().moveZeroes(nums2)
+    SolutionPopAppend().moveZeroes(nums2)
+    print nums2
+    nums2 = [0, 0, 3, 12, 1, 0, 5]
+    SolutionUpdate().moveZeroes(nums2)
     print nums2
 
 
