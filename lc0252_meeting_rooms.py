f762a401018e18a0575c3b3e2adfd5c4cce02e0e
bowen0701 <bowen0701@gmail.com>
2019-12-06 21:38:44 +0100

    Add lc0252_meeting_rooms.py

---
+++ lc0252_meeting_rooms.py
@@ -0,0 +1,32 @@
+"""Leetcode 252. Meeting Rooms (Premium)
+Easy
+
+URL: https://leetcode.com/problems/meeting-rooms
+
+Given an array of meeting time intervals consisting of start and end times
+[[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+Example 1:
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+Input: [[7,10],[2,4]]
+Output: true
+"""
+
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
