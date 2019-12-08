e1884c5c5b0f109ff4997a20a0f59e099a966fe0
bowen0701 <bowen0701@gmail.com>
2019-12-08 18:52:52 +0100

    Complete dict sol

--- lc0137_single_number_ii.py
+++ lc0137_single_number_ii.py
@@ -19,17 +19,34 @@ Input: [0,1,0,1,0,1,99]
 Output: 99
 """
 
-class Solution(object):
+class SolutionDict(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        from collections import defaultdict
+        num_counts = defaultdict(int)
+
+        for n in nums:
+            num_counts[n] += 1
+
+        for n, count in num_counts.items():
+            if count == 1:
+                return n
 
 
 def main():
-    pass
+    # Output: 3.
+    nums = [2,2,3,2]
+    print SolutionDict().singleNumber(nums)
+
+    # Output: 99.
+    nums = [0,1,0,1,0,1,99]
+    print SolutionDict().singleNumber(nums)
 
 
 if __name__ == '__main__':
