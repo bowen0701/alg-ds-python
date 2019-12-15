da7bd2564dfa018cee62079716040916c6a61223
bowen0701 <bowen0701@gmail.com>
2019-12-15 22:38:33 +0100

    Revise comments

--- lc0078_subsets.py
+++ lc0078_subsets.py
@@ -24,10 +24,10 @@ Output:
 
 class SolutionBacktrack(object):
     def _backtrack(self, result, temp, start, nums):
-        # Use shallow copy.
+        # Append shallow copy.
         result.append(temp[:])
 
-        # Iterate starting from start, with backtracking.
+        # Iterate from start, with backtracking.
         for i in range(start, len(nums)):
             temp.append(nums[i])
             self._backtrack(result, temp, i + 1, nums)
