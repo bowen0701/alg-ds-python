af467b7a1078541f27594c5dbdb0254bb654b1f9
bowen0701 <bowen0701@gmail.com>
2019-12-18 22:45:22 +0100

    Complete dp update sol

--- lc0064_minimum_path_sum.py
+++ lc0064_minimum_path_sum.py
@@ -16,20 +16,49 @@ Input:
   [4,2,1]
 ]
 Output: 7
-Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+Explanation: Because the path 1->3->1->1->1 minimizes the sum.
 """
 
-class Solution(object):
+class SolutionDpUpdate(object):
     def minPathSum(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
+
+        Time complexity: O(m*n),
+          - m is the length of rows.
+          - n is the lenght of cols.
+        Space complexity: O(1).
         """
-        pass
+        # Base case.
+        if not grid:
+            return 0
+
+        # Apply DP to get min path sum of every entry.
+        n_rows, n_cols = len(grid), len(grid[0])
+        for r in range(n_rows):
+            for c in range(n_cols):
+                # Update grid entry by itself + min(up, left).
+                if r == 0 and c == 0:
+                    continue
+                elif r == 0 and c > 0:
+                    grid[r][c] += grid[r][c - 1]
+                elif r > 0 and c == 0:
+                    grid[r][c] += grid[r - 1][c]
+                else:
+                    grid[r][c] += min(grid[r][c - 1], grid[r - 1][c])
+
+        return grid[-1][-1]
 
 
 def main():
-    pass
+    # Output: 7
+    grid = [
+      [1,3,1],
+      [1,5,1],
+      [4,2,1]
+    ]
+    print SolutionDpUpdate().minPathSum(grid)
 
 
 if __name__ == '__main__':
