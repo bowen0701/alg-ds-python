5eb19e31aafd5def9dd9ac4d6bccfd019260cd9a
bowen0701 <bowen0701@gmail.com>
2019-12-18 22:20:11 +0100

    Add lc1119_remove_vowels_from_a_string.py

---
+++ lc1119_remove_vowels_from_a_string.py
@@ -0,0 +1,36 @@
+"""Leetcode 1119.Remove Vowels from a String (Premium)
+Easy
+
+URL: https://leetcode.com/problems/remove-vowels-from-a-string
+
+Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it,
+and return the new string.
+
+Example 1:
+Input: "leetcodeisacommunityforcoders"
+Output: "ltcdscmmntyfrcdrs"
+
+Example 2:
+Input: "aeiou"
+Output: ""
+
+Note:
+- S consists of lowercase English letters only.
+- 1 <= S.length <= 1000
+"""
+
+class Solution(object):
+    def removeVowels(self, S):
+        """
+        :type S: string
+        :rtype: string
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
