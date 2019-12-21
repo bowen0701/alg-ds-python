92f9d9a446891fa605bc6a9b4770b7619aed07d7
bowen0701 <bowen0701@gmail.com>
2019-12-21 20:34:13 +0100

    Add lc0293_flip_game.py

---
+++ lc0293_flip_game.py
@@ -0,0 +1,39 @@
+"""Leetcode 293. Flip Game (Premium)
+Easy
+
+URL: https://leetcode.com/problems/flip-game
+
+You are playing the following Flip Game with your friend: Given a string that
+contains only these two characters: + and -, you and your friend take turns to
+flip two consecutive "++" into "--". The game ends when a person can no longer
+make a move and therefore the other person will be the winner.
+
+Write a function to compute all possible states of the string after one valid
+move.
+
+Example:
+Input: s = "++++"
+Output: 
+[
+  "--++",
+  "+--+",
+  "++--"
+]
+Note: If there is no valid move, return an empty list [].
+"""
+
+class Solution(object):
+    def generatePossibleNextMoves(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
