221dbe182f335c8c78db64ff015bf7d3a9947eb3
bowen0701 <bowen0701@gmail.com>
2019-12-24 19:49:45 +0100

    Add lc0266_palindrome_permutation.py

---
+++ lc0266_palindrome_permutation.py
@@ -0,0 +1,35 @@
+"""Leetcode 266. Palindrome Permutation (Premium)
+Easy
+
+URL: https://leetcode.com/problems/palindrome-permutation
+
+Given a string, determine if a permutation of the string could form a palindrome.
+
+Example 1:
+Input: "code"
+Output: false
+
+Example 2:
+Input: "aab"
+Output: true
+
+Example 3:
+Input: "carerac"
+Output: true
+"""
+
+class Solution(object):
+    def canPermutePalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
