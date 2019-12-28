1fcc102fc523cc8d818cb15b87b0c96e1e722cd1
bowen0701 <bowen0701@gmail.com>
2019-12-28 22:10:41 +0100

    Optimize comments

--- lc0076_minimum_window_substring.py
+++ lc0076_minimum_window_substring.py
@@ -17,7 +17,7 @@ Note:
   unique minimum window in S.
 """
 
-class SolutionDictTwoPointers(object):
+class SolutionCharCountDictTwoPointers(object):
     def minWindow(self, s, t):
         """
         :type s: str
@@ -38,36 +38,38 @@ class SolutionDictTwoPointers(object):
         for c in t:
             t_char_counts[c] += 1
 
-        # Track min left & right, and counter.
-        min_left, min_len = 0, float('inf')
+        # Track min left & len, and counter.
+        min_left = 0
+        min_len = float('inf')
         counter = t_len
 
+        ### s = "ADOBECODEBANC"; t = "ABC"
         # Apply two pointers method with left & right from head as a window.
         left, right = 0, 0
 
         # In s, move right to increase window to satify t.
         while right < s_len:
-            # If s[right] exits in t, decrement counter.
+            # If the char exits in t, decrement counter.
             if t_char_counts[s[right]] > 0:
                 counter -= 1
 
-            # Decrement t_char_counts[s[right]], which will be < 0,
-            # if s[right] does not exist in t.
+            # Decrement t_char_counts and increment right.
             t_char_counts[s[right]] -= 1
             right += 1
 
             # While we found valid window, move left to shorten it.
             while counter == 0:
+                # Update min_len and min_left if improve min_len.
                 if right - left < min_len:
                     min_len = right - left
                     min_left = left
 
+                # Before increment left, add back t_char_counts & counter.
                 t_char_counts[s[left]] += 1
                 if t_char_counts[s[left]] > 0:
                     counter += 1
 
                 left += 1
-
 
         if min_len < float('inf'):
             return s[min_left:(min_left + min_len)]
@@ -77,8 +79,9 @@ class SolutionDictTwoPointers(object):
 
 def main():
     # Output: "BANC"
-    s, t = "ADOBECODEBANC", "ABC"
-    print SolutionDictTwoPointers().minWindow(s, t)
+    s = "ADOBECODEBANC"
+    t = "ABC"
+    print SolutionCharCountDictTwoPointers().minWindow(s, t)
 
 
 if __name__ == '__main__':
