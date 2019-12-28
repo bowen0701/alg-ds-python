1b433d7f5379c2056d7a7b7f86302fe845d2d76e
bowen0701 <bowen0701@gmail.com>
2019-12-28 08:22:44 +0100

    Revise to dict char_pos & class name

--- lc0003_longest_substring_without_repeating_characters.py
+++ lc0003_longest_substring_without_repeating_characters.py
@@ -24,7 +24,7 @@ Explanation: The answer is "wke", with the length of 3.
   subsequence and not a substring.
 """
 
-class Solution(object):
+class SolutionCharPosDict(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
@@ -35,18 +35,18 @@ class Solution(object):
         """
         start = 0
         max_len = 0
-        char_d = {}
+        char_pos = {}
 
         for i, c in enumerate(s):
-            if c in char_d and start <= char_d[c]:
+            if c in char_pos and start <= char_pos[c]:
                 # When repeating char is visited.
-                start = char_d[c] + 1
+                start = char_pos[c] + 1
             else:
                 # If not, update max length.
                 max_len = max(max_len, i - start + 1)
 
             # Always update char dict for every char in s.
-            char_d[c] = i
+            char_pos[c] = i
 
         return max_len
 
@@ -54,15 +54,15 @@ class Solution(object):
 def main():
     # Example 1: "abcabcbb" -> 3 ("abc")
     s = "abcabcbb"
-    print('For {0}: {1}'.format(s, Solution().lengthOfLongestSubstring(s)))
+    print('For {0}: {1}'.format(s, SolutionCharPosDict().lengthOfLongestSubstring(s)))
 
     # Example 2: "bbbbb" -> 1 ("b")
     s = "bbbbb"
-    print('For {0}: {1}'.format(s, Solution().lengthOfLongestSubstring(s)))
+    print('For {0}: {1}'.format(s, SolutionCharPosDict().lengthOfLongestSubstring(s)))
 
     # Example 3: "pwwkew" -> 3 ("wke")
     s = "pwwkew"
-    print('For {0}: {1}'.format(s, Solution().lengthOfLongestSubstring(s)))
+    print('For {0}: {1}'.format(s, SolutionCharPosDict().lengthOfLongestSubstring(s)))
 
 
 if __name__ == '__main__':
