b50eb231c51e645ab5aae11c6fa592f518484a3b
bowen0701 <bowen0701@gmail.com>
2019-12-28 08:58:31 +0100

    Revise size to n

--- lc0238_product_of_array_except_self.py
+++ lc0238_product_of_array_except_self.py
@@ -26,33 +26,33 @@ class SolutionLeftRightProducts(object):
         Time complexity: O(n).
         Space complexity: O(n).
         """
-        size = len(nums)
+        n = len(nums)
 
         # Compute left_prods as product of left numbers and nums[i].
-        left_prods = [1] * size
+        left_prods = [1] * n
         left_prods[0] = nums[0]
 
-        for i in range(1, size):
+        for i in range(1, n):
             left_prods[i] = left_prods[i - 1] * nums[i]
 
         # Compute right_prods as product of right numbers and nums[i].
-        right_prods = [1] * size
-        right_prods[size - 1] = nums[size - 1]
+        right_prods = [1] * n
+        right_prods[n - 1] = nums[n - 1]
 
-        for i in range(size - 2, -1, -1):
-            right_prods[i] = right_prods[i + 1] * nums[i]
+        for i in range(n - 2, -1, -1):
+            right_prods[i] = nums[i] * right_prods[i + 1]
 
         # Multiply left_prods and right_prods excluding nums[i].
-        prods = [1] * size
+        prods = [1] * n
 
-        for i, n in enumerate(nums):
+        for i in range(n):
             if i == 0:
                 # Leftmost = neighbor's right product.
                 prods[i] = right_prods[i + 1]
-            elif i == size - 1:
+            elif i == n - 1:
                 # Rightmost = neighbor's left product.
                 prods[i] = left_prods[i - 1]
-            if 1 <= i <= size - 2:
+            if 1 <= i <= n - 2:
                 # Middles = product of neighbors's left & right products.
                 prods[i] = left_prods[i - 1] * right_prods[i + 1]
 
@@ -68,18 +68,18 @@ class SolutionLeftRightProducts2(object):
         Time complexity: O(n).
         Space complexity: O(1).
         """
-        size = len(nums)
-        prods = [1] * size
+        n = len(nums)
+        prods = [1] * n
 
         # Compute prods as product of numbers left to nums[i].
         # Say nums: [a, b, c, d] => prods: [1, a, ab, abc].
-        for i in range(1, size):
+        for i in range(1, n):
             prods[i] = prods[i - 1] * nums[i - 1]
 
         # Product prods and product of numbers right to nums[i],
         # by right_prods: bcd <- cd <- d <- 1.
         right_prods = 1
-        for i in range(size - 1, -1, -1):
+        for i in range(n - 1, -1, -1):
             prods[i] *= right_prods
             right_prods *= nums[i]
 
