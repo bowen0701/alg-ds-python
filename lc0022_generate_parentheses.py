27e5e3c5c6dee3af6276e12d7fa26e3da3a3d4b1
bowen0701 <bowen0701@gmail.com>
2019-12-28 22:21:06 +0100

    Simplify vars

--- lc0022_generate_parentheses.py
+++ lc0022_generate_parentheses.py
@@ -17,22 +17,19 @@ For example, given n = 3, a solution set is:
 """
 
 class SolutionRecur(object):
-    def _parenthesisRecur(self, n_opens, n_closes, 
-                          cur_string, parentheses):
+    def _parenthesisRecur(self, n_opens, n_closes, cur_str, result):
         if n_opens == 0 and n_closes == 0:
-            # If there are no left/right parentheses, append cur_string.
-            parentheses.append(cur_string)
-            return
+            # If there are no left/right parentheses, append cur_str.
+            result.append(cur_str)
+            return None
 
         if n_opens > 0:
             # If there is left parentheses, use one left.
-            self._parenthesisRecur(n_opens - 1, n_closes, 
-                                   cur_string + '(', parentheses)
+            self._parenthesisRecur(n_opens - 1, n_closes, cur_str + '(', result)
 
         if n_opens < n_closes:
             # If number of left parentheses is less than of right, use one right.
-            self._parenthesisRecur(n_opens, n_closes - 1, 
-                                   cur_string + ')', parentheses)
+            self._parenthesisRecur(n_opens, n_closes - 1, cur_str + ')', result)
 
     def generateParenthesis(self, n):
         """
@@ -42,11 +39,11 @@ class SolutionRecur(object):
         Time complexity: O((2n)!/(n!*n!)/(1/(1+n))).
         Space complexity: O(n).
         """
-        parentheses = []
+        result = []
         n_opens, n_closes = n, n
-        cur_string = ''
-        self._parenthesisRecur(n_opens, n_closes, cur_string, parentheses)
-        return parentheses
+        cur_str = ''
+        self._parenthesisRecur(n_opens, n_closes, cur_str, result)
+        return result
 
 
 def main():
