667892e47cbe3cac795197f14211807fedfb9b29
bowen0701 <bowen0701@gmail.com>
2019-12-29 21:54:19 +0100

    Add lc0301_remove_invalid_parentheses.py

---
+++ lc0301_remove_invalid_parentheses.py
@@ -0,0 +1,38 @@
+"""Leetcode 301. Remove Invalid Parentheses
+Hard
+
+URL: https://leetcode.com/problems/remove-invalid-parentheses/
+
+Remove the minimum number of invalid parentheses in order to make the input string
+valid. Return all possible results.
+
+Note: The input string may contain letters other than the parentheses ( and ).
+
+Example 1:
+Input: "()())()"
+Output: ["()()()", "(())()"]
+
+Example 2:
+Input: "(a)())()"
+Output: ["(a)()()", "(a())()"]
+
+Example 3:
+Input: ")("
+Output: [""]
+"""
+
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
