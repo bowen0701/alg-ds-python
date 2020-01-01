1a4c95ded7bc5f3aeca94a3e5514907f1e06a996
bowen0701 <bowen0701@gmail.com>
2020-01-01 12:09:04 +0100

    Revise class name

--- lc0007_reverse_integer.py
+++ lc0007_reverse_integer.py
@@ -15,7 +15,7 @@ The input is assumed to be a 32-bit signed integer.
 Your function should return 0 when the reversed integer overflows.
 """
 
-class Solution(object):
+class SolutionNegativeOverflow(object):
     def reverse(self, x):
         """
         :type x: int
@@ -25,10 +25,11 @@ class Solution(object):
         Space complexity: O(1).
         """
         # Since input x is a 32-bit integer, -2^31 <= x <= 2^31 - 1.
+        x_str = str(x)
         if x < 0:
-            x_rev = int(str(x)[::-1][-1] + str(x)[::-1][:-1])
+            x_rev = int(x_str[::-1][-1] + x_str[::-1][:-1])
         else:
-            x_rev = int(str(x)[::-1])
+            x_rev = int(x_str[::-1])
 
         # If reversed integer is overflow.
         if abs(x_rev) > 0x7FFFFFFF:
@@ -38,9 +39,9 @@ class Solution(object):
 
 
 def main():
-    print Solution().reverse(123)
-    print Solution().reverse(-123)
-    print Solution().reverse(-pow(2, 31))
+    print SolutionNegativeOverflow().reverse(123)
+    print SolutionNegativeOverflow().reverse(-123)
+    print SolutionNegativeOverflow().reverse(-pow(2, 31))
 
 
 if __name__ == '__main__':
