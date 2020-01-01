7d1a088ad73ae65e2b5a03748b6fdef31f521eb3
bowen0701 <bowen0701@gmail.com>
2020-01-01 08:15:59 +0100

    Refactor insertion sort

--- alg_insertion_sort.py
+++ alg_insertion_sort.py
@@ -9,13 +9,11 @@ def insertion_sort(nums):
     Time complexity: O(n^2).
     Space complexity: O(1).
     """
-    gen = ((i, val) for i, val in enumerate(nums) if i > 0)
-    for (i, val) in gen:
-        j = i
-        while j > 0 and nums[j - 1] > val:
-            nums[j] = nums[j - 1]
-            j -= 1
-        nums[j] = val
+    # Starting at pos i >= 1, swap (num, last num) if order is not correct. 
+    for i in range(1, len(nums)):
+        for j in range(i, -1, -1):
+            if j > 0 and nums[j - 1] > nums[j]:
+                nums[j - 1], nums[j] = nums[j], nums[j - 1]
 
 
 def main():
