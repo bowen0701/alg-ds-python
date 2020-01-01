2a246c78a4506de5b4fcc55ff0257182142a4436
bowen0701 <bowen0701@gmail.com>
2020-01-01 20:35:39 +0100

    Complete nums & num->pos dict sol w/ time/space complexity

--- lc0380_insert_delete_getrandom_o1.py
+++ lc0380_insert_delete_getrandom_o1.py
@@ -36,46 +36,107 @@ randomSet.insert(2);
 randomSet.getRandom();
 
 Your RandomizedSet object will be instantiated and called as such:
-obj = RandomizedSet()
-param_1 = obj.insert(val)
-param_2 = obj.remove(val)
-param_3 = obj.getRandom()
+randomSet = RandomizedSet()
+randomSet = randomSet.insert(val)
+randomSet = randomSet.remove(val)
+randomSet = randomSet.getRandom()
 """
 
-class RandomizedSet(object):
+class RandomizedSetNumsAndNumPosDict(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        pass
+        from collections import defaultdict
+
+        # Use list to store nums and dict num->pos.
+        self.nums = []
+        self.num_pos = defaultdict()
 
     def insert(self, val):
         """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        Inserts a value to the set. Returns true if the set did not
+        already contain the specified element.
+
         :type val: int
         :rtype: bool
+
+        Time complexity: O(1).
+        Space complexity: O(n).
         """
-        pass
+        if val not in self.num_pos:
+            self.nums.append(val)
+            self.num_pos[val] = len(self.nums) - 1
+            return True
+
+        return False
 
     def remove(self, val):
         """
-        Removes a value from the set. Returns true if the set contained the specified element.
+        Removes a value from the set. Returns true if the set contained
+        the specified element.
+
         :type val: int
         :rtype: bool
+
+        Time complexity: O(1).
+        Space complexity: O(n).
         """
-        pass
+        if val in self.num_pos:
+            # Get val's position.
+            val_pos = self.num_pos[val]
+
+            # Replace val by the last num.
+            last_num = self.nums[-1]
+            self.nums[val_pos] = last_num
+            self.num_pos[last_num] = val_pos
+
+            # Remove val from nums and num_pos.
+            self.nums.pop()
+            del self.num_pos[val]
+
+            return True
+
+        return False
 
     def getRandom(self):
         """
         Get a random element from the set.
+
         :rtype: int
+
+        Time complexity: O(1).
+        Space complexity: O(1).
         """
-        pass
+        import random
+        return random.choice(self.nums)
 
 
 def main():
-    pass
+    randomSet = RandomizedSetNumsAndNumPosDict()
+
+    # Inserts 1 to the set. Returns true as 1 was inserted successfully.
+    print randomSet.insert(1);
+
+    # Returns false as 2 does not exist in the set.
+    print randomSet.remove(2);
+
+    # Inserts 2 to the set, returns true. Set now contains [1,2].
+    print randomSet.insert(2);
+
+    # getRandom should return either 1 or 2 randomly.
+    print randomSet.getRandom();
+
+    # Removes 1 from the set, returns true. Set now contains [2].
+    print randomSet.remove(1);
+
+    # 2 was already in the set, so return false.
+    print randomSet.insert(2);
+
+    # Since 2 is the only number in the set, getRandom always return 2.
+    print randomSet.getRandom();
+    print randomSet.getRandom();
 
 
 if __name__ == '__main__':
