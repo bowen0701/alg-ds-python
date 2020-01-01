077575c7e31a9557b6e4c16f98c285319725ccf2
bowen0701 <bowen0701@gmail.com>
2020-01-01 08:25:29 +0100

    Refactor selection sort

--- alg_selection_sort.py
+++ alg_selection_sort.py
@@ -9,19 +9,19 @@ def selection_sort(nums):
     Time complexity: O(n^2).
     Space complexity: O(1).
     """
-    # Start from the last position reversely: len(nums) - 1, ..., 0.
+    # Start from the last num, iteratively select next max num to swap them.  
     for i in reversed(range(len(nums))):
-        # Select "next" max element, and swap it and ith element.
-        m = 0
+        max_i = 0
         for j in range(1, i + 1):
-            if nums[j] > nums[m]:
-                m = j
-        nums[m], nums[i] = nums[i], nums[m]
+            # Update max pos max_i to get max num in loop i.
+            if nums[j] > nums[max_i]:
+                max_i = j
+
+        nums[max_i], nums[i] = nums[i], nums[max_i]
 
 
 def main():
     nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    print('List: {}'.format(nums))
     print('By selection sort: ')
     selection_sort(nums)
     print(nums)
