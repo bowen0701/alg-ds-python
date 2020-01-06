ce4bcc19e61518273e054553494288364ab4f677
bowen0701 <bowen0701@gmail.com>
2020-01-06 08:41:18 +0100

    Add lc085_maximal_rectangle.py

---
+++ lc085_maximal_rectangle.py
@@ -0,0 +1,34 @@
+"""Leetcode 85. Maximal Rectangle
+Hard
+
+URL: https://leetcode.com/problems/maximal-rectangle/
+
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle
+containing only 1's and return its area.
+
+Example:
+Input:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+Output: 6
+"""
+
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
