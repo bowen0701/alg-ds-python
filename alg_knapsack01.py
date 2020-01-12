eaeaeec32f9999058a2ff29e3e3e095145d8fd43
bowen0701 <bowen0701@gmail.com>
2020-01-12 20:44:31 +0100

    Simplify helper funs & add comments

--- alg_knapsack01.py
+++ alg_knapsack01.py
@@ -8,7 +8,7 @@ Given weights and values of n "non-splittable" items, put these items in a
 knapsack of capacity to get the maximum total value in the knapsack. 
 """
 
-def knapsack01_recur_util(val, wt, wt_cap, n):
+def _knapsack01_recur(val, wt, wt_cap, n):
     """0-1 Knapsack Problem by naive recursion.
 
     Time complexity: O(2^n), where n is the number of items.
@@ -18,10 +18,10 @@ def knapsack01_recur_util(val, wt, wt_cap, n):
         return 0
     
     if wt[n] > wt_cap:
-        max_val = knapsack01_recur_util(val, wt, wt_cap, n - 1)
+        max_val = _knapsack01_recur(val, wt, wt_cap, n - 1)
     else:
-        val_in = val[n] + knapsack01_recur_util(val, wt, wt_cap - wt[n], n - 1)
-        val_ex = knapsack01_recur_util(val, wt, wt_cap, n - 1)
+        val_in = val[n] + _knapsack01_recur(val, wt, wt_cap - wt[n], n - 1)
+        val_ex = _knapsack01_recur(val, wt, wt_cap, n - 1)
         max_val = max(val_in, val_ex)
     return max_val
 
@@ -33,23 +33,23 @@ def knapsack01_recur(val, wt, wt_cap):
     Space complexity: O(n).
     """
     n = len(wt) - 1
-    return knapsack01_recur_util(val, wt, wt_cap, n)
+    return _knapsack01_recur(val, wt, wt_cap, n)
 
 
-def _knapsack01_memo_util(val, wt, wt_cap, M, n):
+def _knapsack01_memo(val, wt, wt_cap, T, n):
     if n < 0 or wt_cap == 0:
         return 0
 
-    if M[n][wt_cap]:
-        return M[n][wt_cap]
+    if T[n][wt_cap]:
+        return T[n][wt_cap]
 
     if wt[n] > wt_cap:
-        max_val = _knapsack01_memo_util(val, wt, wt_cap, M, n - 1)
+        max_val = _knapsack01_memo(val, wt, wt_cap, T, n - 1)
     else:
-        val_in = val[n] + _knapsack01_memo_util(val, wt, wt_cap - wt[n], M, n - 1)
-        val_ex = _knapsack01_memo_util(val, wt, wt_cap, M, n - 1)
+        val_in = val[n] + _knapsack01_memo(val, wt, wt_cap - wt[n], T, n - 1)
+        val_ex = _knapsack01_memo(val, wt, wt_cap, T, n - 1)
         max_val = max(val_in, val_ex)
-    M[n][wt_cap] = max_val
+    T[n][wt_cap] = max_val
     return max_val
 
 
@@ -62,13 +62,15 @@ def knapsack01_memo(val, wt, wt_cap):
     Space complexity: O(nC).
     """
     n = len(wt) - 1
-    M = [[None for j in range(wt_cap + 1)] for i in range(n + 1)]
 
+    # Create tabular T of (n+1) x (wt_cap+1).
+    T = [[None for j in range(wt_cap + 1)] for i in range(n + 1)]
+
+    # For empty cap, no value can be added.
     for i in range(n + 1):
-        # For empty items.
-        M[i][0] = 0
+        T[i][0] = 0
 
-    return _knapsack01_memo_util(val, wt, wt_cap, M, n)
+    return _knapsack01_memo(val, wt, wt_cap, T, n)
 
 
 def knapsack_dp(val, wt, wt_cap):
@@ -79,38 +81,42 @@ def knapsack_dp(val, wt, wt_cap):
       - C is the weight capacity.
     Space complexity: O(nC).
     """
-    M = [[None for j in range(wt_cap + 1)] for i in range(len(wt))]
-    
-    for i in range(len(wt)):
-        # For empty items.
-        M[i][0] = 0
-    
-    for j in range(1, wt_cap + 1):
-        # For 1s item only.
-        if wt[0] <= j:
-            M[0][j] = val[0]
-        else:
-            M[0][j] = 0
+    n = len(wt)
 
-    for i in range(1, len(wt)):
+    # Create tabular T of n x (wt_cap+1).
+    T = [[None for j in range(wt_cap + 1)] for i in range(n)]
+ 
+    # For empty cap, no value can be added.   
+    for i in range(n):
+        T[i][0] = 0
+    
+    # For 1s item only.
+    for j in range(1, wt_cap + 1):
+        if wt[0] <= j:
+            T[0][j] = val[0]
+        else:
+            T[0][j] = 0
+
+    for i in range(1, n):
         for j in range(1, wt_cap + 1):
             if wt[i] <= j:
-                M[i][j] = max(M[i-1][j], val[i] + M[i-1][j-wt[i]])
+                T[i][j] = max(T[i-1][j], val[i] + T[i-1][j-wt[i]])
             else:
-                M[i][j] = M[i-1][j]
+                T[i][j] = T[i-1][j]
 
-    return M
+    return T
 
 
-def item_list(M, wt, wt_cap):
-    items = [0 for _ in range(len(wt))]
+def item_list(T, wt, wt_cap):
+    n = len(wt)
+    items = [0 for _ in range(n)]
 
     j = wt_cap
-    for i in range(len(wt) - 1, -1, -1):
-        if i >= 1 and M[i][j] > M[i - 1][j]:
+    for i in range(n - 1, -1, -1):
+        if i >= 1 and T[i][j] > T[i - 1][j]:
             items[i] = 1
             j -= wt[i]
-        elif i == 0 and M[i][j] != 0:
+        elif i == 0 and T[i][j] != 0:
             items[i] = 1
 
     return items
