7ee2e0c28087e780dfd392afacae25c8dbcd0815
bowen0701 <bowen0701@gmail.com>
2020-01-15 22:17:42 +0100

    Complete char count sliding window sol w/ time/space complexity

--- lc0438_find_all_anagrams_in_a_string.py
+++ lc0438_find_all_anagrams_in_a_string.py
@@ -31,7 +31,7 @@ The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 """
 
-class Solution(object):
+class SolutionCharCountListSlidingWindow(object):
     def findAnagrams(self, s, p):
         """
         :type s: str
@@ -41,19 +41,52 @@ class Solution(object):
         Time complexity: O(m*n), where m & n is length of s & p.
         Space complexity: O(1).
         """
-        pass
+        # Edge case.
+        if not s or len(s) < len(p):
+            return []
+
+        result = []
+
+        len_p = len(p)
+        len_s = len(s)
+
+        # Create array of length 26 to aggregate char's count for p.
+        p_char_counts = [0] * 26
+        for i in range(len_p):
+            p_char_counts[ord(p[i]) - ord('a')] += 1
+
+        # Apply sliding window for s of length p to get char's count.
+        s_char_counts = [0] * 26
+        
+        # Initalize 1st sliding window's prefix for s w/o last char of p.
+        for i in range(len_p - 1):
+            s_char_counts[ord(s[i]) - ord('a')] += 1
+
+        for i in range(len_p - 1, len_s):
+            # Increment by RHS char of sliding window of s.
+            s_char_counts[ord(s[i]) - ord('a')] += 1
+
+            # Decrement by LHS char of sliding window of s.
+            if i - len_p >= 0:
+                s_char_counts[ord(s[i - len_p]) - ord('a')] -= 1
+
+            if s_char_counts == p_char_counts:
+                # Append start index of sliding window.
+                result.append(i - len_p + 1)
+
+        return result
 
 
 def main():
     # Output: [0, 6]
     s = "cbaebabacd"
     p = "abc"
-    print Solution().findAnagrams(s, p)
+    print SolutionCharCountListSlidingWindow().findAnagrams(s, p)
 
     # Output: [0, 1, 2]
     s = "abab"
     p = "ab"
-    print Solution().findAnagrams(s, p)
+    print SolutionCharCountListSlidingWindow().findAnagrams(s, p)
 
 
 if __name__ == '__main__':
