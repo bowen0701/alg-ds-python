5503e1f54298a5b6121e35794d43c6642b3af6e0
bowen0701 <bowen0701@gmail.com>
2020-01-17 12:25:56 +0100

    Add lc0340_longest_substring_with_at_most_k_distinct_characters.py

---
+++ lc0340_longest_substring_with_at_most_k_distinct_characters.py
@@ -0,0 +1,36 @@
+"""Leetcode 340. Longest Substring with At Most K Distinct Characters
+Hard
+
+URL: https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+
+Given a string, find the length of the longest substring T that contains at most k
+distinct characters.
+
+Example 1:
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: T is "ece" which its length is 3.
+
+Example 2:
+Input: s = "aa", k = 1
+Output: 2
+Explanation: T is "aa" which its length is 2.
+"""
+
+
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
