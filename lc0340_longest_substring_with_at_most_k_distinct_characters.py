e99c63af5c0143c95f9af10e3a79083235bb7da4
bowen0701 <bowen0701@gmail.com>
2020-01-17 20:16:17 +0100

    Complete two pointers char count dict sol w/ time/space complexity

--- lc0340_longest_substring_with_at_most_k_distinct_characters.py
+++ lc0340_longest_substring_with_at_most_k_distinct_characters.py
@@ -18,18 +18,52 @@ Explanation: T is "aa" which its length is 2.
 """
 
 
-class Solution(object):
+class SolutionTwoPointerCharCountDictIter(object):
     def lengthOfLongestSubstringKDistinct(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: int
+
+        Time complexity: O(n).
+        Space complexity: O(k).
         """
-        pass
+        from collections import defaultdict
+
+        # Apply sliding window with two pointers to increment dict:char->count.
+        char_count_d = defaultdict(int)
+        
+        max_len = 0
+        i = 0
+
+        # Move right pointer j iteratively.
+        for j in range(len(s)):
+            # Increment char count for s[j].
+            char_count_d[s[j]] += 1
+
+            if len(char_count_d) > k:
+                # If distinct char number > k, decrement char count for s[i].
+                char_count_d[s[i]] -= 1
+                if char_count_d[s[i]] == 0:
+                    del char_count_d[s[i]]
+
+                i += 1
+
+            max_len = max(max_len, j - i + 1)
+
+        return max_len
 
 
 def main():
-    pass
+    # Output: 3
+    s = "eceba"
+    k = 2
+    print SolutionTwoPointerCharCountDictIter().lengthOfLongestSubstringKDistinct(s, k)
+
+    # Output: 2
+    s = "aa"
+    k = 1
+    print SolutionTwoPointerCharCountDictIter().lengthOfLongestSubstringKDistinct(s, k)
 
 
 if __name__ == '__main__':
