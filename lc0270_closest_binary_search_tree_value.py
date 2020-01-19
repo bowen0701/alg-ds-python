125bf200c2c03507d3c705d6dd8f72de3144af15
bowen0701 <bowen0701@gmail.com>
2020-01-19 17:19:01 +0100

    Complete val/min_diff bst sol w/ time/space complexity

--- lc0270_closest_binary_search_tree_value.py
+++ lc0270_closest_binary_search_tree_value.py
@@ -30,14 +30,50 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
+class SolutionValMinDiffBST(object):
     def closestValue(self, root, target):
         """
         :type root: TreeNode
         :type target: float
         :rtype: int
+
+        Time complexity: O(logn).
+        Space complexity: O(1).
         """
-        pass
+        # Traverse through BST to update currrent value & min_diff
+        val = 0
+        min_diff = float('inf')
+
+        current = root
+        while current:
+            if abs(current.val - target) < min_diff:
+                val = current.val
+                min_diff = abs(current.val - target)
+
+            if target < current.val:
+                current = current.left
+            else:
+                current = current.right
+
+        return val
+
+
+def main():
+    # target: 3.714286
+    # BST:
+    #     4
+    #    / \
+    #   2   5
+    #  / \
+    # 1   3
+    # Output: 4
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(5)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)    
+    target = 3.714286
+    print SolutionValMinDiffBST().closestValue(root, target)
 
 
 if __name__ == '__main__':
