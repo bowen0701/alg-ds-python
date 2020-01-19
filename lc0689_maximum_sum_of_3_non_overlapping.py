6992db831eec24d0b0ed6f359c0a6c56b0ede2b2
bowen0701 <bowen0701@gmail.com>
2020-01-19 13:44:55 +0100

    Comlete left/right max sum mid iter sol w/ time/space complexity

--- lc0689_maximum_sum_of_3_non_overlapping.py
+++ lc0689_maximum_sum_of_3_non_overlapping.py
@@ -28,18 +28,76 @@ Note:
 """
 
 
-class Solution(object):
+class SolutionLeftRightMaxSumMidIter(object):
     def maxSumOfThreeSubarrays(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        n = len(nums)
+        max_sum = 0
+        result = [0, 0, 0]
+
+        # Compute cumsum before pos i: [0, n1, n1+n2, ...]
+        cumsum = [0]
+
+        for num in nums:
+            cumsum.append(cumsum[-1] + num)
+
+        # Compute left max sum's starting pos in range [0, i], with initial 0.
+        # Interate through left intervals's right pos r.
+        left_pos = [0] * n
+        left_sum = cumsum[k] - cumsum[0]
+
+        for r in range(k, n):
+            if cumsum[r + 1] - cumsum[r + 1 - k] > left_sum:
+                left_pos[r] = r + 1 - k
+                left_sum = cumsum[r + 1] - cumsum[r + 1 - k]
+            else:
+                left_pos[r] = left_pos[r - 1]
+
+        # Compute right max sum's starting pos in range [i, n-1], with initial n - k.
+        # Interate through right intervals's left pos l.
+        right_pos = [n - k] * n
+        right_sum = cumsum[n] - cumsum[n - k]
+
+        for l in range(n - k - 1, -1, -1):
+            if cumsum[l + k] - cumsum[l] >= right_sum:
+                right_pos[l] = l
+                right_sum = cumsum[l + k] - cumsum[l]
+            else:
+                right_pos[l] = right_pos[l + 1]
+
+        # Check every possible middle interval and its left/right ones.
+        # Interate through left pos l of middle intervals to update max sum.
+        for m in range(k, n - 2 * k + 1):
+            l = left_pos[m - 1]
+            r = right_pos[m + k]
+
+            total_sum = ((cumsum[l + k] - cumsum[l]) +
+                       (cumsum[m + k] - cumsum[m]) +
+                       (cumsum[r + k] - cumsum[r]))
+            if total_sum > max_sum:
+                max_sum = total_sum
+                result = [l, m, r]
+
+        return result
 
 
 def main():
-    pass
+    # Output: [0, 3, 5]
+    nums = [1,2,1,2,6,7,5,1]
+    k = 2
+    print SolutionLeftRightMaxSumMidIter().maxSumOfThreeSubarrays(nums, k)
+
+    # Output: [0,2,4]
+    nums = [9,8,7,6,2,2,2,2]
+    k = 2
+    print SolutionLeftRightMaxSumMidIter().maxSumOfThreeSubarrays(nums, k)
 
 
 if __name__ == '__main__':
