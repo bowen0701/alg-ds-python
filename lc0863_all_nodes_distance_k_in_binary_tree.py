48a161edf671a33e5e159a0ce069adacad2b79ef
bowen0701 <bowen0701@gmail.com>
2020-01-20 23:39:10 +0100

    Complete undirected parent children graph BFS sol w/ time/space complexity

--- lc0863_all_nodes_distance_k_in_binary_tree.py
+++ lc0863_all_nodes_distance_k_in_binary_tree.py
@@ -33,19 +33,63 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
+class SolutionUndirectedParentChildrenGraphBFS(object):
     def distanceK(self, root, target, K):
         """
         :type root: TreeNode
         :type target: TreeNode
         :type K: int
         :rtype: List[int]
+
+        Time complexity: O(n + |V|+|E|) = O(n+n+n) = O(n).
+        Space complexity: O(n + |V|) = O(n).
         """
-        pass
+        from collections import defaultdict
+        from collections import deque
+
+        # Apply preorder traversal to build undirected graph: parent<->childrens.
+        parent_children_d = defaultdict(list)
+        stack = [(None, root)]
+        while stack:
+            parent, current = stack.pop()
+            if parent and current:
+                parent_children_d[parent.val].append(current.val)
+                parent_children_d[current.val].append(parent.val)
+
+            if current.right:
+                stack.append((current, current.right))
+            if current.left:
+                stack.append((current, current.left))
+
+        # Start from target, iterate BFS K times to collect node with distance K.
+        queue = deque([target.val])
+        visited = set([target.val])
+        for i in range(K):
+            for j in range(len(queue)):
+                current = queue.pop()
+                for neighbor in parent_children_d[current]:
+                    if neighbor not in visited:
+                        queue.appendleft(neighbor)
+                        visited.add(neighbor)
+
+        return list(queue)
 
 
 def main():
-    pass
+    # Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+    # Output: [7,4,1]
+    root = TreeNode(3)
+    root.left = TreeNode(5)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(6)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(0)
+    root.right.right = TreeNode(8)
+    root.left.right.left = TreeNode(7)
+    root.left.right.right = TreeNode(4)
+    target = root.left
+    K = 2
+    print SolutionUndirectedParentChildrenGraphBFS().distanceK(root, target, K)
 
 
 if __name__ == '__main__':
