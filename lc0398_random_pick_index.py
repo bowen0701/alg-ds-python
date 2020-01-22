e28ce178d8594bba067ad6615f2b0968b95155c7
bowen0701 <bowen0701@gmail.com>
2020-01-22 20:01:03 +0100

    Complete num pos dict sol w/ time/space complexity

--- lc0398_random_pick_index.py
+++ lc0398_random_pick_index.py
@@ -14,29 +14,57 @@ not pass the judge.
 Example:
 int[] nums = new int[] {1,2,3,3,3};
 Solution solution = new Solution(nums);
-// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should
+// have equal probability of returning.
 solution.pick(3);
 // pick(1) should return 0. Since in the array only nums[0] is equal to 1.
 solution.pick(1);
 """
 
-class Solution(object):
+class SolutionNumPosDictRandomSample(object):
     def __init__(self, nums):
         """
         :type nums: List[int]
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        from collections import defaultdict
+
+        # Store in dict: num->list(pos).
+        num_pos_d = defaultdict(list)
+        for pos, num in enumerate(nums):
+            num_pos_d[num].append(pos)
+
+        self.num_pos_d = num_pos_d
 
     def pick(self, target):
         """
         :type target: int
         :rtype: int
+
+        Time complexity: O(1).
+        Space complexity: O(n).
         """
-        pass
+        import random
+
+        # Get target's positions.
+        pos_list = self.num_pos_d[target]
+
+        # Randomly sample a pos.
+        n = len(pos_list)
+        if n == 1:
+            return pos_list[0]
+        else:
+            u = random.uniform(0, 1)
+            return pos_list[int(n * u)]
 
 
 def main():
-    pass
+    nums = [1,2,3,3,3]
+    obj = SolutionNumPosDictRandomSample(nums)
+    print obj.pick(3)
+    print obj.pick(1)
 
 
 if __name__ == '__main__':
