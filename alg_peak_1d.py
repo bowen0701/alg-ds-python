b9976b010f7b663c3f8269f694a131dfcee233d6
bowen0701 <bowen0701@gmail.com>
2020-03-03 20:56:12 +0100

    Refactor 3 sols

--- alg_peak_1d.py
+++ alg_peak_1d.py
@@ -1,11 +1,11 @@
 """
 Find a peak position in 1D array.
 
-Support a is an array of length n.
-If a is an array of length 1, a[0] is a peak.
-In general, a[k] is a peak iff a[k] > a[k - 1] and a[k] > a[k + 1].
-If a[0] > a[1], then a[0] is a peak.
-If a[n - 1] > a[n - 2], then a[n - 1] is a peak.  
+Support nums is an array of length n.
+In general, nums[k] is a peak iff 
+nums[k] > nums[k - 1] and nums[k] > nums[k + 1].
+If nums[0] > nums[1], then nums[0] is a peak.
+If nums[n - 2] < nums[n - 1], then nums[n - 1] is a peak.  
 """
 
 from __future__ import absolute_import
@@ -13,60 +13,60 @@ from __future__ import division
 from __future__ import print_function
 
 
-def peak_1d_iter(arr):
+def peak_1d_brute_force(nums):
     """Find peak by naive iteration.
 
     Time complexity: O(n).
     Space complexity: O(1).
     """
-    # Iterate to check element is greater than its left & right neighbors.
-    for i in range(len(arr)):
-        if ((i == 0 or arr[i - 1] <= arr[i]) and 
-            (i == len(arr) - 1 or arr[i] >= arr[i + 1])):
+    # Iterate to check element is greater than its neighbors.
+    for i in range(len(nums)):
+        if ((i == 0 or nums[i] >= nums[i - 1]) and 
+            (i == len(nums) - 1 or nums[i] >= nums[i + 1])):
             return i
 
 
-def _binary_search_recur_helper(arr, left, right):
+def _binary_search_recur(nums, left, right):
     """Helper function for peak_1d_binary_search_recur()."""
     if right - left == 0:
         return left
     else:
-        mid = (left + right + 1) // 2
+        mid = left + (right - left) // 2
 
-        if arr[mid - 1] > arr[mid]:
-            # If mid's left > mid, search left part.
-            return _binary_search_recur_helper(arr, left, mid - 1)
+        if nums[mid] < nums[mid + 1]:
+            # If mid < mid's right, search right part.
+            return _binary_search_recur(nums, mid + 1, right)
         else:
-            # Otherwise, search right part.
-            return _binary_search_recur_helper(arr, mid, right)
+            # Otherwise, search left part.
+            return _binary_search_recur(nums, left, mid)
 
 
-def peak_1d_binary_search_recur(arr):
+def peak_1d_binary_search_recur(nums):
     """Find peak by recursive binary search algorithm.
 
     Time complexity: O(logn).
     Space complexity: O(1).
     """
-    return _binary_search_recur_helper(arr, 0, len(arr) - 1)
+    return _binary_search_recur(nums, 0, len(nums) - 1)
 
 
-def peak_1d_binary_search_iter(arr):
+def peak_1d_binary_search_iter(nums):
     """Find peak by iterative binary search algorithm.
 
     Time complexity: O(logn).
     Space complexity: O(1).
     """
-    left, right = 0, len(arr) - 1
+    left, right = 0, len(nums) - 1
 
     while left < right:
-        mid = (left + right + 1) // 2
+        mid = left + (right - left) // 2
 
-        if arr[mid - 1] > arr[mid]:
-            # If mid's left > mid, search left part.
-            right = mid - 1
+        if nums[mid] < nums[mid + 1]:
+            # If mid < mid's right, search right part.
+            left = mid + 1
         else:
-            # Otherwise, search right part.
-            left = mid
+            # Otherwise, search left part.
+            right = mid
 
     # For left = right.
     return left
@@ -76,40 +76,40 @@ def main():
     import time
     import numpy as np
 
-    # Array of length 5 with peak at 3.
-    arr = [0, 1, 2, 4, 3]
-    print('arr', arr)
+    # numsay of length 5 with peak at 3.
+    nums = [0, 1, 2, 4, 3]
+    print('nums', nums)
     
     start_time = time.time()
-    print('By peak_1d_iter(): {}'.format(peak_1d_iter(arr)))
+    print('By brute force: {}'.format(peak_1d_brute_force(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('By peak_1d_binary_search_recur(): {}'
-          .format(peak_1d_binary_search_recur(arr)))
+    print('By recur binary search: {}'
+          .format(peak_1d_binary_search_recur(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('By peak_1d_binary_search_iter(): {}'
-          .format(peak_1d_binary_search_iter(arr)))
+    print('By iter binary search: {}'
+          .format(peak_1d_binary_search_iter(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
     np.random.seed(71)
-    arr = np.random.permutation(10)
-    print('arr', arr)
+    nums = np.random.permutation(10)
+    print('nums', nums)
 
     start_time = time.time()
-    print('By peak_1d_iter(): {}'.format(peak_1d_iter(arr)))
+    print('By brute force: {}'.format(peak_1d_brute_force(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('By peak_1d_binary_search_recur(): {}'
-          .format(peak_1d_binary_search_recur(arr)))
+    print('By recur binary search: {}'
+          .format(peak_1d_binary_search_recur(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('By peak_1d_binary_search_iter(): {}'
-          .format(peak_1d_binary_search_iter(arr)))
+    print('By iter binary search: {}'
+          .format(peak_1d_binary_search_iter(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
 
