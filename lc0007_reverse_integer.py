800fcf2564f68ceb7137cade57e6649036903d5f
bowen0701 <bowen0701@gmail.com>
2020-03-05 23:29:00 +0100

    Complete mod-int div sol w/ time/space complexity

--- lc0007_reverse_integer.py
+++ lc0007_reverse_integer.py
@@ -21,10 +21,10 @@ class SolutionNegativeOverflow(object):
         :type x: int
         :rtype: int
 
-        Time complexity: O(1).
-        Space complexity: O(1).
+        Time complexity: O(n), where n is length of digits.
+        Space complexity: O(n).
         """
-        # Since input x is a 32-bit integer, -2^31 <= x <= 2^31 - 1.
+        # Since x is 32-bit integer, -2^31 <= x <= 2^31 - 1.
         x_str = str(x)
         if x < 0:
             x_rev = int(x_str[::-1][-1] + x_str[::-1][:-1])
@@ -39,10 +39,47 @@ class SolutionNegativeOverflow(object):
         return x_rev
 
 
+class SolutionModIntegerDivIter(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+
+        Time complexity: O(n), where n is length of digits.
+        Space complexity: O(1).
+        """
+        # Check if x is negative; if yes, convert x to positive.
+        if x < 0:
+            is_neg = True
+            x = -x
+        else:
+            is_neg = False
+
+        # Create reversed integer.
+        x_rev = 0
+        while x:
+            x_rev = x_rev * 10 + x % 10
+            x = x // 10
+
+        # If reversed integer is overflow.
+        if x_rev > 0x7FFFFFFF:
+            x_rev = 0
+
+        if is_neg:
+            return -x_rev
+        else:
+            return x_rev
+
+
 def main():
     print SolutionNegativeOverflow().reverse(123)
+    print SolutionModIntegerDivIter().reverse(123)
+
     print SolutionNegativeOverflow().reverse(-123)
+    print SolutionModIntegerDivIter().reverse(-123)
+
     print SolutionNegativeOverflow().reverse(-pow(2, 31))
+    print SolutionModIntegerDivIter().reverse(-pow(2, 31))
 
 
 if __name__ == '__main__':
