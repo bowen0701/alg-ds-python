d9d09d75caf2d8bfd80fca50350c6b096b7d4fa9
bowen0701 <bowen0701@gmail.com>
2020-04-07 23:43:30 +0200

    Complete two pointers sol

--- lc0283_move_zeroes.py
+++ lc0283_move_zeroes.py
@@ -53,20 +53,61 @@ class SolutionUpdate(object):
             nums[i] = 0
 
 
+class SolutionTwoPointers(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+
+        Time complexity: O(n).
+        Space complexity: O(1).
+        """
+        n = len(nums)
+
+        # Find the 1st zero.
+        i = 0
+        while nums[i] != 0 and i + 1 < n:
+            i += 1
+
+        # Iterate to find the 1st non-zero at the right of i, switch i & j.
+        j = i + 1
+        while j < n:
+            while nums[j] == 0 and j + 1 < n:
+                j += 1
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j += 1
+
+
 def main():
+    import time
+
+    start_time = time.time()
     nums1 = [0, 1, 0, 3, 12]
     SolutionPopAppend().moveZeroes(nums1)
     print nums1
-    nums1 = [0, 1, 0, 3, 12]
-    SolutionUpdate().moveZeroes(nums1)
-    print nums1
-
     nums2 = [0, 0, 3, 12, 1, 0, 5]
     SolutionPopAppend().moveZeroes(nums2)
     print nums2
+    print 'Time for pop/append: {}'.format(time.time() - start_time)
+
+    start_time = time.time()
+    nums1 = [0, 1, 0, 3, 12]
+    SolutionUpdate().moveZeroes(nums1)
+    print nums1
     nums2 = [0, 0, 3, 12, 1, 0, 5]
     SolutionUpdate().moveZeroes(nums2)
     print nums2
+    print 'Time for update: {}'.format(time.time() - start_time)
+
+    start_time = time.time()
+    nums1 = [0, 1, 0, 3, 12]
+    SolutionTwoPointers().moveZeroes(nums1)
+    print nums1
+    nums2 = [0, 0, 3, 12, 1, 0, 5]
+    SolutionTwoPointers().moveZeroes(nums2)
+    print nums2
+    print 'Time for update: {}'.format(time.time() - start_time)
 
 
 if __name__ == '__main__':
