d6ed0e5925e0793bf4fc84b09e709b3a0d907f58
bowen0701 <bowen0701@gmail.com>
2020-04-13 22:53:31 +0200

    Complete diffsum idx dict sol w/ time/space complexity

--- lc0525_contiguous_array.py
+++ lc0525_contiguous_array.py
@@ -22,17 +22,45 @@ Note: The length of the given binary array will not exceed 50,000.
 """
 
 
-class Solution(object):
+class SolutionDiffsumIdxDict(object):
     def findMaxLength(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        Time complexity: O(n).
+        Space complexity: O(n).
         """
-        pass
+        # Edge case.
+        if len(nums) <= 1:
+            return 0
+
+        max_len = 0
+
+        # Create a dict:diffsum->idx.
+        diffsum_idx_d = {0: -1}
+        diffsum = 0
+        for idx, num in enumerate(nums):
+            # Get diff by converting 0 to -1 and 1 to 1.
+            diffsum += 2 * num - 1
+
+            # Check if diffsum exists, update max_len.
+            if diffsum in diffsum_idx_d:
+                max_len = max(max_len, idx - diffsum_idx_d[diffsum])
+            else:
+                diffsum_idx_d[diffsum] = idx
+
+        return max_len
 
 
 def main():
-    pass
+    # Output: 2
+    nums = [0,1]
+    print SolutionDiffsumIdxDict().findMaxLength(nums)
+
+    # Output: 2
+    nums = [0,1,0]
+    print SolutionDiffsumIdxDict().findMaxLength(nums)
 
 
 if __name__ == '__main__':
