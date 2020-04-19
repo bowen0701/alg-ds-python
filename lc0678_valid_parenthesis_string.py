7f14f39526f6615f6b2478a10bab8c61d9e8c188
bowen0701 <bowen0701@gmail.com>
2020-04-19 23:06:58 +0200

    Complete brute force sol w/ time/space complexity

--- lc0678_valid_parenthesis_string.py
+++ lc0678_valid_parenthesis_string.py
@@ -16,9 +16,11 @@ validity of a string by these rules:
 Example 1:
 Input: "()"
 Output: True
+
 Example 2:
 Input: "(*)"
 Output: True
+
 Example 3:
 Input: "(*))"
 Output: True
@@ -27,17 +29,88 @@ Note:
 The string size will be in the range [1, 100].
 """
 
-class Solution(object):
+class SolutionBruteForce(object):
+    def _enumerate(self, s):
+        """Enumerate s by replacing '*' by '(', '', & ')'."""
+        s_set = set()
+        queue = [s]
+        while queue:
+            for i in range(len(queue)):
+                si = list(queue.pop())
+                if si.count('*'):
+                    for j, c in enumerate(si):
+                        if c == '*':
+                            for r in ['(', 'a', ')']:
+                                si[j] = r
+                                if not si.count('*'):
+                                    s_set.add(''.join(si))
+                                else:
+                                    queue.append(''.join(si))
+                else:
+                    s_set.add(''.join(si))
+        return list(s_set)
+
+    def _is_valid(self, s):
+        """Check s is valid with parentheses."""
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append('(')
+            elif c == ')':
+                if not stack:
+                    return False
+                stack.pop()
+        return not stack
+
     def checkValidString(self, s):
         """
         :type s: str
         :rtype: bool
+
+        Note: Time Limit Exceeded.
+
+        Time complexity: O(3^k), where k is the number of '*'s.
+        Space complexity: O(3^k).
         """
-        pass
+        # Edge case.
+        if len(s) == 1:
+            return s == '*'
+
+        # Enumerate all strings with '*' replaced by, '(', '', ')'.
+        s_lst = self._enumerate(s)
+        print(s_lst)
+
+        # Iterate through list to check if there is one valid.
+        for si in s_lst:
+            if self._is_valid(si):
+                return True
+        return False
 
 
 def main():
-    pass
+    # Output: True
+    s = "()"
+    print(SolutionBruteForce().checkValidString(s))
+
+    # Output: True
+    s = "(*)"
+    print(SolutionBruteForce().checkValidString(s))
+
+    # Output: True
+    s = "(*))"
+    print(SolutionBruteForce().checkValidString(s))
+
+    # Output: False
+    s = "*(("
+    print(SolutionBruteForce().checkValidString(s))
+
+    # Output: False
+    s = "**(("
+    print(SolutionBruteForce().checkValidString(s))
+
+    # Output: False
+    # s = "()(()(*(())()*)(*)))()))*)((()(*(((()())()))()()*)((*)))()))(*)(()()(((()*()()((()))((*((*)()"
+    # print(SolutionBruteForce().checkValidString(s))
 
 
 if __name__ == '__main__':
