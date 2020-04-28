bce6bc91779fe35d5194d224508294387c417b1b
bowen0701 <bowen0701@gmail.com>
2020-04-28 23:46:58 +0200

    Complete common prefix bit sol

--- lc0201_bitwise_and_of_numbers_range.py
+++ lc0201_bitwise_and_of_numbers_range.py
@@ -28,23 +28,52 @@ class SolutionBruteForce(object):
         Time complexity: O(n-m).
         Space complexity: O(1).
         """
+        # Edge case when m = 0.
         if m == 0:
             return 0
 
+        # Apply brute force method.
         result = m
         for i in range(m + 1, n + 1):
             result &= i
         return result
 
 
+class SolutionCommonPrefixBit(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+
+        Time complexity: O(logn).
+        Space complexity: O(1).
+        """
+        # Edge case.
+        if m == 0:
+            return 0
+
+        # Find common prefix binary code by right-shifting m & n.
+        n_shifts = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            n_shifts += 1
+
+        # Left-shift m by n_shifts digits.
+        return m <<= n_shifts
+
+
 def main():
     # Output: 4
     m, n = 5, 7
     print SolutionBruteForce().rangeBitwiseAnd(m, n)
+    print SolutionCommonPrefixBit().rangeBitwiseAnd(m, n)
 
     # Output: 0
     m, n = 0, 1
     print SolutionBruteForce().rangeBitwiseAnd(m, n)
+    print SolutionCommonPrefixBit().rangeBitwiseAnd(m, n)
 
 
 if __name__ == '__main__':
