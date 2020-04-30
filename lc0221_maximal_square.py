b55c61c43edc769ee702223b5451fac1f1d9be56
bowen0701 <bowen0701@gmail.com>
2020-05-01 00:05:31 +0200

    Complete DP sol

--- lc0221_maximal_square.py
+++ lc0221_maximal_square.py
@@ -16,17 +16,46 @@ Output: 4
 """
 
 
-class Solution(object):
+class SolutionDPSquare(object):
     def maximalSquare(self, matrix):
         """
         :type matrix: List[List[str]]
         :rtype: int
+
+        Time complexity: O(m*n), where
+          - m: number of rows
+          - n: number of cols
+        Space complexity: O(m*n).
         """
-        pass
+        # Edge case.
+        if not matrix or not matrix[0]:
+            return 0
+
+        # Create DP table T[i][j] as maximal square with bottom-right at (i, j).
+        nrows, ncols = len(matrix), len(matrix[0])
+        T = [[0] * (ncols + 1) for _ in range(nrows + 1)]
+
+        # Update max size.
+        size = 0
+
+        for r in range(1, nrows + 1):
+            for c in range(1, ncols + 1):
+                if matrix[r - 1][c - 1] == '1':
+                    # If (r, c) is 1, set as min(left, top-left, left) + 1.
+                    T[r][c] = min(T[r - 1][c], T[r - 1][c - 1], T[r][c - 1]) + 1
+                    size = max(size, T[r][c])
+        return size * size
 
 
 def main():
-    pass
+    # Output: 4
+    matrix = [
+        ['1', '0', '1', '0', '0'],
+        ['1', '0', '1', '1', '1'],
+        ['1', '1', '1', '1', '1'],
+        ['1', '0', '0', '1', '0']
+    ]
+    print SolutionDPSquare().maximalSquare(matrix)
 
 
 if __name__ == '__main__':
