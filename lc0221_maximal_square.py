7c495964702a890846284c90eb9c5cf4e868aaea
bowen0701 <bowen0701@gmail.com>
2020-04-30 22:56:42 +0200

    Add lc0221_maximal_square.py

---
+++ lc0221_maximal_square.py
@@ -0,0 +1,33 @@
+"""Leetcode 221. Maximal Square
+Medium
+
+URL: https://leetcode.com/problems/maximal-square/
+
+Given a 2D binary matrix filled with 0's and 1's, find the largest
+square containing only 1's and return its area.
+
+Example:
+Input: 
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Output: 4
+"""
+
+
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
