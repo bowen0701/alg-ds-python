e2703cab398af71aa082a3062caae7ad9683b9cb
bowen0701 <bowen0701@gmail.com>
2020-05-01 23:51:45 +0200

    Add cw_difference_of_2.py

---
+++ cw_difference_of_2.py
@@ -0,0 +1,31 @@
+"""Codewars: Difference of 2
+6 kyu
+
+URL: https://www.codewars.com/kata/5340298112fa30e786000688/
+
+The objective is to return all pairs of integers from a given collection
+of integers that have a difference of 2.
+
+The result should be sorted in ascending order.
+
+The input will consist of unique values. The order of the integers in the
+input collection should not matter.
+
+Examples
+[1, 2, 3, 4]      -->  [[1, 3], [2, 4]]
+[4, 1, 2, 3]      -->  [[1, 3], [2, 4]]
+[1, 23, 3, 4, 7]  -->  [[1, 3]]
+[4, 3, 1, 5, 6]   -->  [[1, 3], [3, 5], [4, 6]]
+"""
+
+
+def twos_difference(lst): 
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
