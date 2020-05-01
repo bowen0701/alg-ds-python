aa2d71601f45ee16f397232dc6be5ead10c6eabc
bowen0701 <bowen0701@gmail.com>
2020-05-02 00:00:50 +0200

    Complete cw_difference_of_2.py

--- cw_difference_of_2.py
+++ cw_difference_of_2.py
@@ -19,12 +19,26 @@ Examples
 """
 
 
-def twos_difference(lst): 
-    pass
+def twos_difference(lst):
+    """
+    Time complexity: O(nlogn).
+    Space complexity: O(n).
+    """
+    lst_set = set(lst)
+    diff2 = [(i, i + 2) for i in lst if i + 2 in lst_set]
+    return sorted(diff2)
 
 
 def main():
-    pass
+    assert twos_difference([1, 2, 3, 4]) == [(1, 3), (2, 4)]
+    assert twos_difference([1, 3, 4, 6]) == [(1, 3), (4, 6)]
+    assert twos_difference([0, 3, 1, 4]) == [(1, 3)]
+    assert twos_difference([4, 1, 2, 3]) == [(1, 3), (2, 4)]
+    assert twos_difference([6, 3, 4, 1, 5]) == [(1, 3), (3, 5), (4, 6)]
+    assert twos_difference([3, 1, 6, 4]) == [(1, 3), (4, 6)]
+    assert twos_difference([1, 3, 5, 6, 8, 10, 15, 32, 12, 14, 56]) == [(1, 3), (3, 5), (6, 8), (8, 10), (10, 12), (12, 14)]
+    assert twos_difference([1, 4, 7, 10]) == []
+    assert twos_difference([]) == []
 
 
 if __name__ == '__main__':
