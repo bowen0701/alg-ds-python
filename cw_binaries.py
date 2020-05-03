28eaa6f834a5e8cf8eadc05722c8d4c8929c26d9
bowen0701 <bowen0701@gmail.com>
2020-05-03 11:01:12 +0200

    Complete cw_binaries.py

--- cw_binaries.py
+++ cw_binaries.py
@@ -11,7 +11,7 @@ will code each.
 
 Coding process to code a number n expressed in base 10:
 a) Let k be the number of bits of n
-b) Put k-1 times the digit 0 followed by the digit 
+b) Put k-1 times the digit 0 followed by the digit 1
 c) Put number n in binary
 d) Concat the result of b) and c)
 
@@ -34,21 +34,73 @@ code("77338")  --> "0011110011110111011100011000"
 code("0011121314") --> "1010111111011011011111001100"
 
 decode("001111001111011101110001100000011000001101001101") -> "77338855"
-decode("0011110011110111011100011000") -> "77338"
+decode("001111 001111 0111011100011000") ->  "77338"
 decode("1010111111011011011111001100") -> "0011121314"
 """
 
 
+def _encode(c):
+    binary = bin(int(c))[2:]
+    k = len(binary)
+    code01 = '0' * (k - 1) + '1'
+    return code01 + binary
+
+
 def code(strng):
-    pass
+    """Code decimal digits to special binary codes.
+
+    Time complexity: O(n), where n is the number of decimal digits.
+    Space complexity: O(n).
+    """
+    # Edge case.
+    if not strng:
+        return ''
+
+    # Iterate through char in strng, encode it and append to encode_ls.
+    encode_ls = []
+    for c in strng:
+        code = _encode(c)
+        encode_ls.append(code)
+    return ''.join(encode_ls)
 
     
 def decode(strng):
-    pass
+    """Decode special binary codes back to decimal digits.
+
+    Time complexity: O(n), where n is the number of decimal digits.
+    Space complexity: O(n).
+    """
+    n = len(strng)
+    
+    # Create a dict:digit->code01.
+    digit_code_d = {d: _encode(str(d)) for d in range(10)}
+
+    # Apply max match with digits in descending order, 
+    # with one pointer as the start of the next code.
+    decode_ls = []
+    i = 0
+    while i < n - 1:
+        for d in reversed(range(10)):
+            code = digit_code_d[d]
+            if strng[i:i+len(code)] == code:
+                decode_ls.append(str(d))
+                i += len(code)
+                break
+    return ''.join(decode_ls)
 
 
 def main():
-    pass
+    assert code("62") == "0011100110"
+    assert code("55337700") == "001101001101011101110011110011111010"
+    assert code("1119441933000055") == "1111110001100100110000110011000110010111011110101010001101001101"
+    assert code("69") == "00111000011001"
+    assert code("86") == "00011000001110"
+
+    assert decode("10001111") == "07"
+    assert decode("001100001100001100001110001110001110011101110111001110001110001110001111001111001111001100001100001100") == "444666333666777444"
+    assert decode("01110111110001100100011000000110000011110011110111011100110000110001100110") == "33198877334422"
+    assert decode("0011010011010011011010101111110011000011000011000011100011100011100011100011100011100001100100011001000110011100011001001111001111001111001111001111001111") == "55500011144466666699919777777"
+    assert decode("01110111011111000110010011110011110011110011110011110011110111011101110110011001100110011001101111111010101100011001000110000001100000011000") == "3331977777733322222211100019888"
 
 
 if __name__ == '__main__':
