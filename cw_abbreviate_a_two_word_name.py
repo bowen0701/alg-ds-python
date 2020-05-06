8767fa65cff39278a548d24317d4c27b72c75aee
bowen0701 <bowen0701@gmail.com>
2020-05-06 07:44:35 +0200

    Complete cw_abbreviate_a_two_word_name.py

--- cw_abbreviate_a_two_word_name.py
+++ cw_abbreviate_a_two_word_name.py
@@ -14,11 +14,19 @@ Patrick Feeney => P.F
 
 
 def abbrevName(name):
-    pass
+    """
+    Time complexity: O(n), where n is the number of words.
+    Space complexity: O(n).
+    """
+    return '.'.join([w[0].upper() for w in name.split()])
 
 
 def main():
-    pass
+    assert abbrevName("Sam Harris") == "S.H"
+    assert abbrevName("Patrick Feenan") == "P.F"
+    assert abbrevName("Evan Cole") == "E.C"
+    assert abbrevName("P Favuzzi") == "P.F"
+    assert abbrevName("David Mendieta") == "D.M"
 
 
 if __name__ == '__main__':
