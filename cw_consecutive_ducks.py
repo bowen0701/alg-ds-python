7984e47e37a45d7ef844de0fef20ed6853c29a86
bowen0701 <bowen0701@gmail.com>
2020-05-06 07:52:34 +0200

    Add cw_consecutive_ducks.py

---
+++ cw_consecutive_ducks.py
@@ -0,0 +1,34 @@
+"""Codewars: Consecutive Ducks
+7 kyu
+
+URL: https://www.codewars.com/kata/5dae2599a8f7d90025d2f15f/
+
+Positive integers have so many gorgeous features. Some of them could be
+expressed as a sum of two or more consecutive positive numbers.
+
+Consider an Example :
+10 , could be expressed as a sum of 1 + 2 + 3 + 4.
+Task
+Given Positive integer, N , Return true if it could be expressed as a sum
+of two or more consecutive positive numbers , OtherWise return false .
+
+Notes
+Guaranteed constraint : 2 ≤ N ≤ (2^32) -1 .
+Input >> Output Examples:
+* consecutiveDucks(9)  ==>  return (true)  //  9 , could be expressed as
+  a sum of ( 2 + 3 + 4 ) or ( 4 + 5 ) .
+* consecutiveDucks(64)  ==>  return (false)
+* consecutiveDucks(42)  ==>  return (true) //  42 , could be expressed
+"""
+
+
+def consecutive_ducks(n):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
