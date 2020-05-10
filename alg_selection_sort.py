71d42d763bdb2d0c1bd8474a4da99695d5b77f91
bowen0701 <bowen0701@gmail.com>
2020-05-10 16:40:42 +0200

    Revise to i_max and enhance comments

--- alg_selection_sort.py
+++ alg_selection_sort.py
@@ -9,15 +9,13 @@ def selection_sort(nums):
     Time complexity: O(n^2).
     Space complexity: O(1).
     """
-    # Start from the last num, iteratively select next max num to swap them.  
+    # Start from the last num, iteratively select max num to swap.  
     for i in reversed(range(len(nums))):
-        max_i = 0
+        i_max = 0
         for j in range(1, i + 1):
-            # Update max pos max_i to get max num in loop i.
-            if nums[j] > nums[max_i]:
-                max_i = j
-
-        nums[max_i], nums[i] = nums[i], nums[max_i]
+            if nums[j] > nums[i_max]:
+                i_max = j
+        nums[i_max], nums[i] = nums[i], nums[i_max]
 
 
 def main():
