d7720d07d080835d8269fd3b17a7bb7d8d34e246
bowen0701 <bowen0701@gmail.com>
2020-05-22 23:59:51 +0200

    Implementing replace sol

--- lc0013_roman_to_integer.py
+++ lc0013_roman_to_integer.py
@@ -137,9 +137,13 @@ class SolutionReplace(object):
         :rtype: int
 
         Time complexity: O(n), where n is the length of s.
-        Space complexity: O(1), which is the size of roman to integer dict.
+        Space complexity: O(n), which is the size of roman to integer dict.
         """
-        pass
+        # - I can be placed before V (5) and X (10) to make 4 and 9. 
+        # - X can be placed before L (50) and C (100) to make 40 and 90. 
+        # - C can be placed before D (500) and M (1000) to make 400 and 900.
+        s = (s.replace('IV', 'IIII').replace('IX', 'IIIII'))
+
 
 
 def main():
