a8c70e2f470714dc4365e551c4ba266ff14ec0bd
bowen0701 <bowen0701@gmail.com>
2020-06-13 10:04:12 +0200

    Revise problem docstring

--- lc0983_minimum_cost_for_tickets.py
+++ lc0983_minimum_cost_for_tickets.py
@@ -33,7 +33,8 @@ Input: days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15]
 Output: 17
 Explanation: 
 For example, here is one way to buy passes that lets you travel your travel plan:
-On day 1, you bought a 30-day pass for costs[2] = $15 which covered days 1, 2, ..., 30.
+On day 1, you bought a 30-day pass for costs[2] = $15 which covered days
+1, 2, ..., 30.
 On day 31, you bought a 1-day pass for costs[0] = $2 which covered day 31.
 In total you spent $17 and covered all the days of your travel.
 
