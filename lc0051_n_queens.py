978abbe9dce3650e7427afe461dc7e2acfcc5093
bowen0701 <bowen0701@gmail.com>
2020-11-21 23:31:46 +0000

    Remove redundant kwarg for dfs, refactor vars & enhance comments

--- lc0051_n_queens.py
+++ lc0051_n_queens.py
@@ -9,7 +9,7 @@ such that no two queens attack each other.
 Given an integer n, return all distinct solutions to the n-queens puzzle.
 
 Each solution contains a distinct board configuration of the n-queens' placement,
-where 'Q' and '.' both indicate a queen and an empty space respectively.
+where 'Q' and '.' both indicate a queen and an empty space resultpectively.
 
 Example:
 Input: 4
@@ -31,21 +31,24 @@ as shown above.
 class Solution(object):
     def _is_valid(self, queens):
         """Check current queen position is valid among previous queens."""
-        current_row, current_col = len(queens) - 1, queens[-1]
+        # Start from the latest queen.
+        cur_row, cur_col = len(queens) - 1, queens[-1]
 
         # Check any queens can attack the current queen.
         for row, col in enumerate(queens[:-1]):
-            col_diff = abs(current_col - col)
-            row_diff = abs(current_row - row)
+            col_diff = abs(cur_col - col)
+            row_diff = abs(cur_row - row)
+
+            # Check two queens are in the same column or diagonal.
             if col_diff == 0 or col_diff == row_diff:
                 return False
 
         return True
 
-    def _dfs(self, n, res=[], queens=[]):
+    def _dfs(self, n, result, queens):
         """DFS for putting queens in suitable position."""
         if n == len(queens):
-            res.append(queens[:])
+            result.append(queens[:])
             return None
 
         for col in range(n):
@@ -54,7 +57,7 @@ class Solution(object):
 
             if self._is_valid(queens):
                 # If current queen's position is valid, search the next level.
-                self._dfs(n, res, queens)
+                self._dfs(n, result, queens)
 
             # Backtrack by poping out current queen.
             queens.pop()
@@ -67,17 +70,18 @@ class Solution(object):
         Time complexity: O(n!).
         Space complexity: O(n).
         """
-        # res to collect multiple solutions for n_queens.
-        res = []
-        # queens is an 1-D array to store the column ids of queens.
+        # Collect multiple solutions for n_queens.
+        result = []
+
+        # Use 1-D array to store the column ids of queens in rows.
         queens = []
 
-        self._dfs(n, res, queens)
+        self._dfs(n, result, queens)
 
         # Make solution configs.
-        sol = [['.'*j + 'Q' + '.'*(n - j - 1) for j in queens] 
-               for queens in res]
-        return sol
+        sols = [['.' * j + 'Q' + '.' * (n - j - 1) for j in queens] 
+                for queens in result]
+        return sols
 
 
 def main():
