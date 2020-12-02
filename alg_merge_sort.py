ca36931297832a889e9f6c7dc46b1e01850538c9
bowen0701 <bowen0701@gmail.com>
2020-12-02 00:40:36 +0000

    Refactor to clearer recur/iter methods

--- alg_merge_sort.py
+++ alg_merge_sort.py
@@ -3,77 +3,92 @@ from __future__ import print_function
 from __future__ import division
 
 
-def _merge_sorted_lists_recur(arr1, arr2):
+def _merge_sorted_lists_recur(nums1, nums2):
     """Merge two sorted lists by recusion."""
-    if not arr1 or not arr2:
-        return arr1 or arr2
+    if not nums1 or not nums2:
+        return nums1 or nums2
 
     # Merge two lists one element by one element.
-    if arr1[0] <= arr2[0]:
-        return [arr1[0]] + _merge_sorted_lists_recur(arr1[1:], arr2)
+    if nums1[0] <= nums2[0]:
+        return [nums1[0]] + _merge_sorted_lists_recur(nums1[1:], nums2)
     else:
-        return [arr2[0]] + _merge_sorted_lists_recur(arr1, arr2[1:])
+        return [nums2[0]] + _merge_sorted_lists_recur(nums1, nums2[1:])
 
 
-def _merge_sorted_lists_iter(arr1, arr2):
-    """Merge two sorted lists by iteration."""
-    # Apply two pointer method.
-    i, j = 0, 0
-
-    merged = []
-
-    for _ in range(len(arr1) + len(arr2)):
-        if i < len(arr1) and j < len(arr2):
-            if arr1[i] <= arr2[j]:
-                merged.append(arr1[i])
-                i += 1
-            else:
-                merged.append(arr2[j])
-                j += 1      
-        elif i < len(arr1) and j >= len(arr2):
-            merged.extend(arr1[i:])
-            break
-        elif i >= len(arr1) and j < len(arr2):
-            merged.extend(arr2[j:])
-            break
-
-    return merged
-
-
-def merge_sort(arr, merge):
+def merge_sort_recur(nums):
     """Merge sort algorithm by divide and conquer.
 
-    Two merge sorted list methods:
-    - Merge by recursion.
-    - Merge by iteration with two pointers.
+    Merge sorted list by recursion.
 
     Time complexity: O(n*logn).
     Space complexity: O(n).
     """
-    if len(arr) == 1:
-        return arr
+    if len(nums) == 1:
+        return nums
     
     # Sort the 1st & 2nd halves respectively and merge them.
-    mid = len(arr) // 2
+    mid = len(nums) // 2
 
-    arr1 = merge_sort(arr[:mid], merge)
-    arr2 = merge_sort(arr[mid:], merge)
-    return merge(arr1, arr2)
+    nums1 = merge_sort_recur(nums[:mid])
+    nums2 = merge_sort_recur(nums[mid:])
+    return _merge_sorted_lists_recur(nums1, nums2)
 
+
+def _merge_sorted_lists_iter(nums1, nums2):
+    """Merge two sorted lists by iteration."""
+    # Apply two pointer method.
+    i, j = 0, 0
+    result = []
+
+    for _ in range(len(nums1) + len(nums2)):
+        if i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1      
+        elif i < len(nums1) and j >= len(nums2):
+            result.extend(nums1[i:])
+            break
+        elif i >= len(nums1) and j < len(nums2):
+            result.extend(nums2[j:])
+            break
+
+    return result
+
+
+def merge_sort_iter(nums):
+    """Merge sort algorithm by divide and conquer.
+
+    Merge sorted list by iteration with two pointers.
+
+    Time complexity: O(n*logn).
+    Space complexity: O(n).
+    """
+    if len(nums) == 1:
+        return nums
+    
+    # Sort the 1st & 2nd halves respectively and merge them.
+    mid = len(nums) // 2
+
+    nums1 = merge_sort_iter(nums[:mid])
+    nums2 = merge_sort_iter(nums[mid:])
+    return _merge_sorted_lists_iter(nums1, nums2)
 
 def main():
     import time
     import random
 
-    arr = range(100)
-    random.shuffle(arr)
+    nums = range(100)
+    random.shuffle(nums)
 
     start_time = time.time()
-    print('By recur: {}'.format(merge_sort(arr, _merge_sorted_lists_recur)))
+    print('By recur: {}'.format(merge_sort_recur(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print('By iter: {}'.format(merge_sort(arr, _merge_sorted_lists_iter)))
+    print('By iter: {}'.format(merge_sort_iter(nums)))
     print('Time: {}'.format(time.time() - start_time))
 
 
