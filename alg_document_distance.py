450be0b2e8c0286b9afa16a89d38e703fedb4804
bowen0701 <bowen0701@gmail.com>
2021-04-10 23:53:15 +0100

    Correct indentation

--- alg_document_distance.py
+++ alg_document_distance.py
@@ -4,24 +4,24 @@ from __future__ import print_function
 
 
 class DocumentDistance():
-	"""Document Distance Algorithm.
+    """Document Distance Algorithm.
 
-	Procedure:
-	- Step 1: Split doc into words; 
-	  non-alphanumeric characters are treated as blanks, and case is not significant.
+    Procedure:
+    - Step 1: Split doc into words; 
+      non-alphanumeric characters are treated as blanks, and case is not significant.
     - Step 2: Compute word frequencies.
     - Step 3: Compute cosine similarity: inner_product(d1, d1) / (|d1| * |d2|)
-	"""
-	def __init__(self, filename1, filename2):
-		self._filename1 = filename1
-		self._filename2 = filename2
+    """
+    def __init__(self, filename1, filename2):
+        self._filename1 = filename1
+        self._filename2 = filename2
 
-	pass
+    pass
 
 
 def main():
-	pass
+    pass
 
 
 if __name__ == '__main__':
-	main()
+    main()
