9cf22c87dadf3fce4881217401b88fce29e81168
bowen0701 <bowen0701@gmail.com>
2021-07-24 22:10:55 +0100

    Refactor, enhance comments & migrate to Python3

--- lc0238_product_of_array_except_self.py
+++ lc0238_product_of_array_except_self.py
@@ -17,8 +17,11 @@ Could you solve it with constant space complexity?
 space complexity analysis.)
 """
 
+from typing import List
+
+
 class SolutionLeftRightProducts(object):
-    def productExceptSelf(self, nums):
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
         :type nums: List[int]
         :rtype: List[int]
@@ -28,35 +31,35 @@ class SolutionLeftRightProducts(object):
         """
         n = len(nums)
 
-        # Compute left products: nums=[a,b,c] => lprods=[a,ab,abc]
+        # Compute left products: [a,b,c] => lprods=[a,ab,abc]
         lprods = [1] * n
         lprods[0] = nums[0]
         for i in range(1, n):
             lprods[i] = lprods[i - 1] * nums[i]
 
-        # Compute right products: rprods=[abc,bc,c]
+        # Compute right products: [a,b,c] => rprods=[abc,bc,c]
         rprods = [1] * n
         rprods[-1] = nums[-1]
         for i in range(n - 2, -1, -1):
             rprods[i] = nums[i] * rprods[i + 1]
 
-        prods = [1] * n
+        result = [1] * n
         for i in range(n):
             if i == 0:
                 # Leftmost = neighbor's right product.
-                prods[i] = rprods[1]
+                result[i] = rprods[1]
             elif i == n - 1:
                 # Rightmost = neighbor's left product.
-                prods[i] = lprods[n - 2]
+                result[i] = lprods[n - 2]
             else:
                 # Middles: multiply left & right products excluding nums[i].
-                prods[i] = lprods[i - 1] * rprods[i + 1]
+                result[i] = lprods[i - 1] * rprods[i + 1]
 
-        return prods
+        return result
 
 
 class SolutionLeftRightProductsOptim(object):
-    def productExceptSelf(self, nums):
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
         :type nums: List[int]
         :rtype: List[int]
@@ -66,25 +69,25 @@ class SolutionLeftRightProductsOptim(object):
         """
         n = len(nums)
 
-        # Compute prods (excluding nums[i]): nums=[a,b,c] => prods=[1,a,ab].
-        prods = [1] * n
+        # Compute "shift-left" prods (exclude nums[i]): [a,b,c] => [1,a,ab].
+        result = [1] * n
         for i in range(1, n):
-            prods[i] = prods[i - 1] * nums[i - 1]
+            result[i] = result[i - 1] * nums[i - 1]
 
-        # Compute prods by rprods (excluding nums[i]): bc<-c<-1.
+        # Compute "shift-right" prods (exclude nums[i]): [a,b,c] => [bc, c, 1].
         rprods = 1
         for i in range(n - 1, -1, -1):
-            prods[i] *= rprods
+            result[i] *= rprods
             rprods *= nums[i]
 
-        return prods
+        return result
 
 
 def main():
     # Output: [60, 40, 30, 24]
     nums = [2, 3, 4, 5]
-    print SolutionLeftRightProducts().productExceptSelf(nums)
-    print SolutionLeftRightProductsOptim().productExceptSelf(nums)
+    print(SolutionLeftRightProducts().productExceptSelf(nums))
+    print(SolutionLeftRightProductsOptim().productExceptSelf(nums))
 
 
 if __name__ == '__main__':
