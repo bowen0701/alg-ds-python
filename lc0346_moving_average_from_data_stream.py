4b42dfe22e39ed47c8cdb4cfa7e7d0a658f7d75a
bowen0701 <bowen0701@gmail.com>
2021-08-03 22:43:37 +0100

    Refactor to python3

--- lc0346_moving_average_from_data_stream.py
+++ lc0346_moving_average_from_data_stream.py
@@ -1,6 +1,8 @@
 """Leetcode 346. Moving Average from Data Stream (Premium)
 Easy
 
+URL: https://leetcode.com/problems/moving-average-from-data-stream/
+
 Given a stream of integers and a window size,
 calculate the moving average of all integers in the sliding window.
 
@@ -13,40 +15,37 @@ m.next(5) = (10 + 3 + 5) / 3
 """
 
 class MovingAverageQueue(object):
-    def __init__(self, size):
+    def __init__(self, size: int):
         from collections import deque
 
         self.size = size
 
-        # Apply queue to collect stream of specific length.
+        # Apply queue to collect stream integers of max length.
         self.queue = deque()
         self.sum = 0
 
-    def next(self, val):
+    def next(self, val: int) -> float:
         """
-        :type val: int
-        :rtype: float
-
         Time complexity: O(1).
         Space complexity: O(1).
         """
-        # First check if queue is full, if yes, pop the first-in.
+        # First check if queue is full, if yes, pop the first.
         if len(self.queue) == self.size:
             self.sum -= self.queue.popleft()
 
         self.queue.append(val)
         self.sum += val
 
-        # Note queue len is dynamic for first few elements.
-        return self.sum / float(len(self.queue))
+        # Queue len is dynamic for first few elements.
+        return self.sum / len(self.queue)
 
 
 def main():
     moving_avg = MovingAverageQueue(3)
-    print moving_avg.next(1)
-    print moving_avg.next(10)
-    print moving_avg.next(3)
-    print moving_avg.next(5)
+    print(moving_avg.next(1))
+    print(moving_avg.next(10))
+    print(moving_avg.next(3))
+    print(moving_avg.next(5))
 
 
 if __name__ == '__main__':
