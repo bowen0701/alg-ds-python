183215e783bddfef1c9ba43a00c3efe36cb6abaf
bowen0701 <bowen0701@gmail.com>
2021-08-22 21:59:20 +0100

    Refactor postorder traversal sol

--- lc0226_invert_binary_tree.py
+++ lc0226_invert_binary_tree.py
@@ -31,35 +31,28 @@ class TreeNode(object):
         self.right = None
 
 
-class SolutionRecur(object):
-    def invertTree(self, root):
+class SolutionPostorderRecur(object):
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         """
-        :type root: TreeNode
-        :rtype: TreeNode
-
         Time complexity: O(n).
         Space complexity: O(logn) for balanced tree; O(n) for single sided.
         """
-        # Apply postorder traversal to invert tree.
+        # Edge case.
         if not root:
             return None
 
-        # Recursively invert left subtree and right subtree.
+        # Postorder traversal: left->right->root.
         left = self.invertTree(root.left)
         right = self.invertTree(root.right)
 
-        # Invert tree by interchanging left and right trees.
         root.left = right
         root.right = left
         return root
 
 
 class SolutionBFS(object):
-    def invertTree(self, root):
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         """
-        :type root: TreeNode
-        :rtype: TreeNode
-
         Time complexity: O(n).
         Space complexity: O(logn) for balanced tree; O(n) for single sided.
         """
@@ -94,15 +87,31 @@ def main():
     root.right.left = TreeNode(6)
     root.right.right = TreeNode(9)
 
-    # root = SolutionRecur().invertTree(root)
+    root = SolutionPostorderRecur().invertTree(root)
+    print(root.val)              # Should be 4.
+    print(root.left.val)         # Should be 7.
+    print(root.right.val)        # Should be 2.
+    print(root.left.left.val)    # Should be 9.
+    print(root.left.right.val)   # Should be 6.
+    print(root.right.left.val)   # Should be 3.
+    print(root.right.right.val)  # Should be 1.
+    
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(9)
+
     root = SolutionBFS().invertTree(root)
-    print root.val              # Should be 4.
-    print root.left.val         # Should be 7.
-    print root.right.val        # Should be 2.
-    print root.left.left.val    # Should be 9.
-    print root.left.right.val   # Should be 6.
-    print root.right.left.val   # Should be 3.
-    print root.right.right.val  # Should be 1.
+    print(root.val)              # Should be 4.
+    print(root.left.val)         # Should be 7.
+    print(root.right.val)        # Should be 2.
+    print(root.left.left.val)    # Should be 9.
+    print(root.left.right.val)   # Should be 6.
+    print(root.right.left.val)   # Should be 3.
+    print(root.right.right.val)  # Should be 1.
 
 
 if __name__ == '__main__':
