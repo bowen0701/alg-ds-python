4c8a90e096ce3a487728984ca975d04a36a517aa
bowen0701 <bowen0701@gmail.com>
2021-08-22 23:05:41 +0100

    Complete SolutionSortBisect

--- lc0480_sliding_window_median.py
+++ lc0480_sliding_window_median.py
@@ -36,11 +36,14 @@ You may assume k is always valid, ie:
 k is always smaller than input array's size for non-empty array.
 """
 
+from typing import List
+
 import random
+import bisect
 
 
 class SolutionSelection(object):
-    def _select_mth_smallest(self, sub_nums, m):
+    def _select_mth_smallest(self, sub_nums: List[int], k: int) -> int:
         # Randomly select a num in sub array as pivot.
         pivot_idx = random.choice(range(len(sub_nums)))
         pivot = sub_nums[pivot_idx]
@@ -53,29 +56,25 @@ class SolutionSelection(object):
         n_small = len(small_idx)
         n_mid = len(mid_idx)
 
-        if m <= n_small:
-            # Select the m from small nums.
+        if k <= n_small:
+            # Select the k from small nums.
             small_nums = [sub_nums[idx] for idx in small_idx]
-            return self._select_mth_smallest(small_nums, m)
-        elif n_small < m <= n_small + n_mid:
-            # Select pivot as the m.
+            return self._select_mth_smallest(small_nums, k)
+        elif n_small < k <= n_small + n_mid:
+            # Select pivot as the k.
             return pivot
-        elif m > n_small + n_mid:
-            # Select the m from large nums.
+        elif k > n_small + n_mid:
+            # Select the k from large nums.
             large_nums = [sub_nums[idx] for idx in large_idx]
-            return self._select_mth_smallest(large_nums, m - n_small - n_mid)
+            return self._select_mth_smallest(large_nums, k - n_small - n_mid)
 
-    def medianSlidingWindow(self, nums, k):
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[float]
-
         Time complexity: O(n*k), where n is the length of nums.
         Space complexity: O(n).
         """
         n = len(nums)
-        medians = []
+        result = []
 
         for i in range(n - k + 1):
             # Create a sub nums.
@@ -91,25 +90,23 @@ class SolutionSelection(object):
                 med2 = self._select_mth_smallest(sub_nums, k // 2 + 1)
                 med = (med1 + med2) / 2.0
 
-            medians.append(med)
+            result.append(med)
 
-        return medians
+        return result
 
 
 class SolutionSortAndBinarySearch(object):
-    def _binary_search(self, window, k, element):
-        # Apply binary search to
-        # - remove old element from sorted window.
-        # - insert new element into sorted window.
+    def _binary_search(self, sorted_window: List[int], k: int, x: int):
+        # Apply binary search to get the left index.
         left = 0
         right = k - 1
 
         while left < right:
             mid = left + (right - left) // 2
 
-            if window[mid] == element:
+            if sorted_window[mid] == x:
                 return mid
-            elif window[mid] < element:
+            elif sorted_window[mid] < x:
                 left = mid + 1
             else:
                 # Note: not right = mid - 1.
@@ -117,34 +114,53 @@ class SolutionSortAndBinarySearch(object):
 
         return left
 
-    def medianSlidingWindow(self, nums, k):
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[float]
-
         Time complexity: O(k*logk + n*(logk+k)) = O(n*k).
         Space complexity: O(k).
         """
-        medians = []
+        result = []
 
         # Keep the window as sorted list.
-        window = sorted(nums[:k])
+        sorted_window = sorted(nums[:k])
 
         # Apply two pointers method with to-be-removed & to-be-added elements.
         # The last zippped pair is to add the last median only.
-        for old, new in zip(nums, nums[k:] + [None]):
-            medians.append((window[k // 2] + window[~(k // 2)]) / 2.0)
+        for old, new in zip(nums, nums[k:] + [0]):
+            result.append((sorted_window[k // 2] + sorted_window[~(k // 2)]) / 2)
 
             # Apply binary search to remove old element from sorted window.
-            old_pos = self._binary_search(window, k, old)
-            window.pop(old_pos)
+            old_pos = self._binary_search(sorted_window, k, old)
+            sorted_window.pop(old_pos)
             
             # Apply binary search to add new element to sorted window.
-            left_pos = self._binary_search(window, k, new)
-            window.insert(left_pos, new)
+            left_pos = self._binary_search(sorted_window, k, new)
+            sorted_window.insert(left_pos, new)
 
-        return medians
+        return result
+
+
+class SolutionSortBisect(object):
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        """
+        Time complexity: O(k*logk + n*(logk+k)) = O(n*k).
+        Space complexity: O(k).
+        """
+        result = []
+
+        # Keep the window sorted.
+        sorted_window = sorted(nums[:k])
+
+        # Apply two-pointer method with to-be-removed & to-be-added elements.
+        for old, new in zip(nums, nums[k:] + [0]):
+            result.append((sorted_window[k // 2] + sorted_window[~(k // 2)]) / 2)
+
+            old_pos = bisect.bisect_left(sorted_window, old)
+            sorted_window.pop(old_pos)
+
+            bisect.insort(sorted_window, new)
+
+        return result
 
 
 def main():
@@ -155,14 +171,19 @@ def main():
     k = 3
 
     start_time = time.time()
-    print 'By kth smallest selection method:'
-    print SolutionSelection().medianSlidingWindow(nums, k)
-    print 'Time: {}'.format(time.time() - start_time)
+    print('By kth smallest selection method:')
+    print(SolutionSelection().medianSlidingWindow(nums, k))
+    print('Time: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    print 'By sorted window with binary search:'
-    print SolutionSortAndBinarySearch().medianSlidingWindow(nums, k)
-    print 'Time: {}'.format(time.time() - start_time)
+    print('By sorted window with binary search:')
+    print(SolutionSortAndBinarySearch().medianSlidingWindow(nums, k))
+    print('Time: {}'.format(time.time() - start_time))
+
+    start_time = time.time()
+    print('By sorted window with bisect:')
+    print(SolutionSortBisect().medianSlidingWindow(nums, k))
+    print('Time: {}'.format(time.time() - start_time))
 
 
 if __name__ == '__main__':
