c255e86e4c812aa25fa0adad7f059673957655e6
bowen0701 <bowen0701@gmail.com>
2021-11-15 21:34:48 +0000

    Complete preorder traversal recursion sol

--- lc0314_binary_tree_vertical_order_traversal.py
+++ lc0314_binary_tree_vertical_order_traversal.py
@@ -68,7 +68,7 @@ Output:
 ]
 """
 
-from typing import List
+from typing import List, Dict
 
 
 # Definition for a binary tree node.
@@ -79,7 +79,47 @@ class TreeNode(object):
         self.right = None
 
 
-class SolutionBFSQueueOrderValsDict(object):
+class SolutionPreorderRecurOrderValsDict:
+    def _preorder_recur(self, root: TreeNode, vorder_vals_d: Dict[int, List], vorder: int):
+        # Edge case.
+        if not root:
+            return
+
+        self.min_vorder = min(self.min_vorder, vorder)
+        self.max_vorder = max(self.max_vorder, vorder)
+
+        # Apply preorder traversal: root -> left -> right.
+        vorder_vals_d[vorder].append(root.val)
+        self._preorder_recur(root.left, vorder_vals_d, vorder - 1)
+        self._preorder_recur(root.right, vorder_vals_d, vorder + 1)
+
+    def verticalOrder(self, root: TreeNode) -> List[List[int]]:
+        """
+        Time complexity: O(n).
+        Space complexity: O(n).
+        """
+        from collections import defaultdict
+
+        # Edge case.
+        if not root:
+            return []
+
+        # Create dict: vertical order->list(values).
+        vorder_vals_d = defaultdict(list)
+
+        # Apply recursion to visit left/right relative to middle root.
+        vorder, self.min_vorder, self.max_vorder = 0, float('inf'), -float('inf')
+        self._preorder_recur(root, vorder_vals_d, vorder)
+
+        # Return list(node values) based on vertical order.
+        result = []
+        for v in range(int(self.min_vorder), int(self.max_vorder) + 1):
+            result.append(vorder_vals_d[v])
+
+        return result
+
+
+class SolutionBFSQueueOrderValsDict:
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
         """
         Time complexity: O(n).
@@ -88,16 +128,17 @@ class SolutionBFSQueueOrderValsDict(object):
         from collections import defaultdict
         from collections import deque
 
+        # Edge case.
         if not root:
             return []
 
-        # Create dict: vertical order->list(node values).
+        # Create dict: vertical order->list(values).
         vorder_vals_d = defaultdict(list)
 
         # Use queue to add root and left/right with their orders to dict.
-        min_order, max_order = float('inf'), -float('inf')
-
         queue = deque([(root, 0)])
+
+        min_order, max_order = float('inf'), -float('inf')
 
         while queue:
             current, vorder = queue.pop()
@@ -111,10 +152,10 @@ class SolutionBFSQueueOrderValsDict(object):
             if current.right:
                 queue.appendleft((current.right, vorder + 1))
 
-        # Return sorted list(node values) based on vertical order.
+        # Return list(node values) based on vertical order.
         result = []
-        for i in range(int(min_order), int(max_order) + 1):
-            result.append(vorder_vals_d[i])
+        for v in range(int(min_order), int(max_order) + 1):
+            result.append(vorder_vals_d[v])
 
         return result
 
@@ -139,7 +180,9 @@ def main():
     root.right = TreeNode(20)
     root.right.left = TreeNode(15)
     root.right.right = TreeNode(7)
+    print(SolutionPreorderRecurOrderValsDict().verticalOrder(root))
     print(SolutionBFSQueueOrderValsDict().verticalOrder(root))
+
 
     #      3
     #     /\
@@ -163,6 +206,7 @@ def main():
     root.left.right = TreeNode(0)
     root.right.left = TreeNode(1)
     root.right.right = TreeNode(7)
+    print(SolutionPreorderRecurOrderValsDict().verticalOrder(root))
     print(SolutionBFSQueueOrderValsDict().verticalOrder(root))
 
     #     3
@@ -192,6 +236,7 @@ def main():
     root.right.right = TreeNode(7)
     root.left.right.right = TreeNode(2)
     root.right.left.left = TreeNode(5)
+    print(SolutionPreorderRecurOrderValsDict().verticalOrder(root))
     print(SolutionBFSQueueOrderValsDict().verticalOrder(root))
 
 
