ac468bc69078e65187dc643e427e1668ea3692b7
bowen0701 <bowen0701@gmail.com>
2022-01-01 17:43:10 +0000

    Refactor codes to enhance readibility

--- alg_longest_common_subsequence.py
+++ alg_longest_common_subsequence.py
@@ -5,19 +5,19 @@ from __future__ import print_function
 
 def _lcs_recur(s1, s2, n1, n2):
     """Helper function for longest_common_subsequence_recur()."""
-    if n1 <= 0 or n2 <= 0:  # Base case.
+    if n1 == 0 or n2 == 0:  # Base case.
         return 0
     
     if s1[n1 - 1] == s2[n2 - 1]:
         # If the current chars are the same, LCS is the last LCS plus 1.
-        lcs = _lcs_recur(s1, s2, n1 - 1, n2 - 1) + 1
+        result = _lcs_recur(s1, s2, n1 - 1, n2 - 1) + 1
     else:
         # If not, LCS is the max of LCS's w/o current char of text1 or text2.
         lcs1 = _lcs_recur(s1, s2, n1 - 1, n2)
         lcs2 = _lcs_recur(s1, s2, n1, n2 - 1)
-        lcs = max(lcs1, lcs2)
+        result = max(lcs1, lcs2)
     
-    return lcs
+    return result
 
 
 def longest_common_subsequence_recur(s1, s2):
@@ -30,25 +30,25 @@ def longest_common_subsequence_recur(s1, s2):
     return _lcs_recur(s1, s2, n1, n2)
 
 
-def _lcs_memo(s1, s2, n1, n2, M):
+def _lcs_memo(s1, s2, n1, n2, T):
     # Helper function for longest_common_subsequence_memo().
-    if n1 <= 0 or n2 <= 0:
+    if n1 == 0 or n2 == 0:
         return 0
     
-    if M[n1][n2]:
-        return M[n1][n2]
+    if T[n1][n2]:
+        return T[n1][n2]
 
     if s1[n1 - 1] == s2[n2 - 1]:
         # If the current chars are the same, LCS is the last LCS plus 1.
-        lcs = _lcs_memo(s1, s2, n1 - 1, n2 - 1, M) + 1
+        result = _lcs_memo(s1, s2, n1 - 1, n2 - 1, T) + 1
     else:
         # If not, LCS is the max of LCS's w/o current char of text1 or text2.
-        lcs1 = _lcs_memo(s1, s2, n1 - 1, n2 - 1, M)
-        lcs2 = _lcs_memo(s1, s2, n1, n2 - 1, M)
-        lcs = max(lcs1, lcs2)
+        lcs1 = _lcs_memo(s1, s2, n1 - 1, n2 - 1, T)
+        lcs2 = _lcs_memo(s1, s2, n1, n2 - 1, T)
+        result = max(lcs1, lcs2)
 
-    M[n1][n2] = lcs
-    return lcs
+    T[n1][n2] = result
+    return result
 
 
 def longest_common_subsequence_memo(s1, s2):
@@ -58,8 +58,8 @@ def longest_common_subsequence_memo(s1, s2):
     Space complexity: O(n1*n2).
     """
     n1, n2 = len(s1), len(s2)
-    M = [[0] * (n2 + 1) for _ in range(n1 + 1)]
-    return _lcs_memo(s1, s2, n1, n2, M)
+    T = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+    return _lcs_memo(s1, s2, n1, n2, T)
 
 
 def longest_common_subsequence_dp(s1, s2):
@@ -70,18 +70,18 @@ def longest_common_subsequence_dp(s1, s2):
     """
     n1, n2 = len(s1), len(s2)
 
-    M = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+    T = [[0] * (n2 + 1) for _ in range(n1 + 1)]
 
     for r in range(1, n1 + 1):
         for c in range(1, n2 + 1):
             if s1[r - 1] == s2[c - 1]:
                 # If the current chars are the same, LCS is the last LCS plus 1.
-                M[r][c] = M[r - 1][c - 1] + 1
+                T[r][c] = T[r - 1][c - 1] + 1
             else:
                 # If not, LCS is the max of LCS's w/o current char of text1 or text2.
-                M[r][c] = max(M[r - 1][c], M[r][c - 1])
+                T[r][c] = max(T[r - 1][c], T[r][c - 1])
 
-    return M[-1][-1]
+    return T[-1][-1]
 
 
 def main():
