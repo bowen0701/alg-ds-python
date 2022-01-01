17a355d7c48ef3530b4ada167b0f7d1148ede514
bowen0701 <bowen0701@gmail.com>
2022-01-01 16:49:01 +0000

    Refactor knapsack01 codes/comments

--- alg_knapsack01.py
+++ alg_knapsack01.py
@@ -18,14 +18,14 @@ def _knapsack01_recur(val, wt, wt_cap, n):
         return 0
     
     if wt[n] > wt_cap:
-        # Cannot be put.
-        max_val = _knapsack01_recur(val, wt, wt_cap, n - 1)
+        # Cannot be added.
+        result = _knapsack01_recur(val, wt, wt_cap, n - 1)
     else:
-        # Can be put: to put or not to put. 
+        # Can be added: to add or not to add. 
         val_in = val[n] + _knapsack01_recur(val, wt, wt_cap - wt[n], n - 1)
         val_ex = _knapsack01_recur(val, wt, wt_cap, n - 1)
-        max_val = max(val_in, val_ex)
-    return max_val
+        result = max(val_in, val_ex)
+    return result
 
 
 def knapsack01_recur(val, wt, wt_cap):
@@ -46,29 +46,29 @@ def _knapsack01_memo(val, wt, wt_cap, T, n):
         return T[n][wt_cap]
 
     if wt[n] > wt_cap:
-        # Cannot be put.
-        max_val = _knapsack01_memo(val, wt, wt_cap, T, n - 1)
+        # Cannot be added.
+        result = _knapsack01_memo(val, wt, wt_cap, T, n - 1)
     else:
-        # Can be put: to put or not to put. 
+        # Can be added: to add or not to add. 
         val_in = val[n] + _knapsack01_memo(val, wt, wt_cap - wt[n], T, n - 1)
         val_ex = _knapsack01_memo(val, wt, wt_cap, T, n - 1)
-        max_val = max(val_in, val_ex)
+        result = max(val_in, val_ex)
 
-    T[n][wt_cap] = max_val
-    return max_val
+    T[n][wt_cap] = result
+    return result
 
 
 def knapsack01_memo(val, wt, wt_cap):
     """0-1 Knapsack Problem by top-down dynamic programming w/ memoization.
 
-    Time complexity: O(nC), where 
+    Time complexity: O(n*c), where 
       - n is the number of items, and 
-      - C is the weight capacity.
-    Space complexity: O(nC).
+      - c is the weight capacity.
+    Space complexity: O(n*c).
     """
     n = len(wt) - 1
 
-    # Create tabular T of (n+1)x(wt_cap+1).
+    # Memoization table T of (n+1)*(wt_cap+1).
     T = [[None] * (wt_cap + 1) for i in range(n + 1)]
 
     # For empty cap, no value can be added.
@@ -78,17 +78,17 @@ def knapsack01_memo(val, wt, wt_cap):
     return _knapsack01_memo(val, wt, wt_cap, T, n)
 
 
-def knapsack_dp(val, wt, wt_cap):
+def knapsack01_dp(val, wt, wt_cap):
     """0-1 Knapsack Problem by bottom-up dynamic programming.
 
-    Time complexity: O(nC), where 
+    Time complexity: O(n*c), where 
       - n is the number of items, and 
-      - C is the weight capacity.
-    Space complexity: O(nC).
+      - c is the weight capacity.
+    Space complexity: O(n*c).
     """
     n = len(wt)
 
-    # Create tabular T of n x (wt_cap+1).
+    # Memoization table T of n x (wt_cap+1).
     T = [[None] * (wt_cap + 1) for i in range(n)]
  
     # For empty cap, no value can be added.   
@@ -105,27 +105,27 @@ def knapsack_dp(val, wt, wt_cap):
     for i in range(1, n):
         for j in range(1, wt_cap + 1):
             if wt[i] <= j:
-                # Can be put: to put or not to put.
+                # Can be added: to add or not to add.
                 T[i][j] = max(val[i] + T[i - 1][j - wt[i]], T[i - 1][j])
             else:
-                # Cannot be put.
+                # Cannot be added.
                 T[i][j] = T[i-1][j]
 
     return T
 
 
-def item_list(T, wt, wt_cap):
+def get_items(T, wt, wt_cap):
     n = len(wt)
     items = [0] * n
 
     w = wt_cap
     for i in range(n - 1, -1, -1):
         if i >= 1 and T[i][w] > T[i - 1][w]:
-            # Item i, i >= 1, is put.
+            # Item i, i >= 1, is added.
             items[i] = 1
             w -= wt[i]
         elif i == 0 and T[i][w] != 0:
-            # Item 0 is put.
+            # Item 0 is added.
             items[i] = 1
 
     return items
@@ -148,10 +148,10 @@ def main():
     print('Time by memo: {}'.format(time.time() - start_time))
 
     start_time = time.time()
-    T = knapsack_dp(val, wt, wt_cap)
+    T = knapsack01_dp(val, wt, wt_cap)
     print('By DP: {}'.format(T[-1][-1]))
     print('Time: {}'.format(time.time() - start_time))
-    print('Items: {}'.format(item_list(T, wt, wt_cap)))
+    print('Items: {}'.format(get_items(T, wt, wt_cap)))
 
 
 if __name__ == '__main__':
