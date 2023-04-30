2dcb0aab5fa1ba1b7127a483ef1d5aeeb27b0d2c
bowen0701 <bowen0701@gmail.com>
2023-04-30 22:33:44 +0100

    Complete class BitsDropLowestSetBit

--- alg_parity_bits.py
+++ alg_parity_bits.py
@@ -11,6 +11,10 @@ from __future__ import division
 
 class BitsIter:
     def parity_bits(self, n: int) -> int:
+        """
+        Time complexity: O(64) = O(1).
+        Space complexity: O(1).
+        """
         result = 0
         while n:
             result ^= n & 1
@@ -18,14 +22,31 @@ class BitsIter:
         return result
 
 
+class BitsDropLowestSetBit:
+    def parity_bits(self, n: int) -> int:
+        """
+        Time complexity: O(k) = O(1), where k is the number of bits.
+        Space complexity: O(1).
+        """
+        result = 0
+        while n:
+            result ^= 1
+            # Drop the lowest set bit.
+            n &= (n - 1)
+        return result 
+
+
+
 def main():
     # Output: 1
     n = 0b1011
     print(BitsIter().parity_bits(n))
+    print(BitsDropLowestSetBit().parity_bits(n))
 
     # Output: 0
     n = 0b10001000
     print(BitsIter().parity_bits(n))
+    print(BitsDropLowestSetBit().parity_bits(n))
 
 
 if __name__ == "__main__":
