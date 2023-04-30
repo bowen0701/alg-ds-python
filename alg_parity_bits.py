fe55ed3ad449fa3d62bf9ae96e6147a85d96cb03
bowen0701 <bowen0701@gmail.com>
2023-04-30 22:29:20 +0100

    Complete class BitsIter

--- alg_parity_bits.py
+++ alg_parity_bits.py
@@ -11,11 +11,20 @@ from __future__ import division
 
 class BitsIter:
     def parity_bits(self, n: int) -> int:
-        pass
+        result = 0
+        while n:
+            result ^= n & 1
+            n >>= 1
+        return result
 
 
 def main():
+    # Output: 1
     n = 0b1011
+    print(BitsIter().parity_bits(n))
+
+    # Output: 0
+    n = 0b10001000
     print(BitsIter().parity_bits(n))
 
 
