d7438893a6656c71478876a14cc1713baa8cb6fc
bowen0701 <bowen0701@gmail.com>
2023-04-30 23:23:56 +0100

    Complete class BitsCache

--- alg_bits_parity.py
+++ alg_bits_parity.py
@@ -38,16 +38,35 @@ class BitsDropLowestSetBit:
 
 class BitsCache:
     def __init__(self, cache_size: int = 16):
-        pass
+        # Cache parity for all numbers between 0 - 2^cache_size.
+        self.precomputed_parity = dict()
+        for n in range(2 ** cache_size):
+            self.precomputed_parity[n] = self._precompute_bits_parity(n)
 
-    def bits_parity(self, n: int) -> int:
+    def _precompute_bits_parity(self, n: int) -> int:
+        # Helper function for precomputing bits parity.
+        result = 0
+        while n:
+            result ^= 1
+            # Drop the lowest set bit.
+            n &= (n - 1)
+        return result
+
+    def bits_parity(self, n: int, cache_size: int = 16) -> int:
         """
         Time complexity: O(n/l) = O(1), where 
           - n is the binary word size; and 
           - l is the width of the words for cache.
         Space complexity: O(2^l).
         """
-        pass
+        # Actually bit_mask = 2 ** cache_size - 1.
+        bit_mask = 0xFFFF
+        return (
+            self.precomputed_parity[n >> (3 * cache_size)] ^
+            self.precomputed_parity[(n >> (2 * cache_size)) & bit_mask] ^
+            self.precomputed_parity[(n >> cache_size) & bit_mask] ^
+            self.precomputed_parity[n & bit_mask]
+        )
 
 
 def main():
@@ -55,11 +74,13 @@ def main():
     n = 0b1011
     print(BitsIter().bits_parity(n))
     print(BitsDropLowestSetBit().bits_parity(n))
+    print(BitsCache().bits_parity(n))
 
     # Output: 0
     n = 0b10001000
     print(BitsIter().bits_parity(n))
     print(BitsDropLowestSetBit().bits_parity(n))
+    print(BitsCache().bits_parity(n))
 
 
 if __name__ == "__main__":
