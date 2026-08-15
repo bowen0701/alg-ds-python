17f7adce0633df5d38dcfc56cb0cafda2a6d246c
bowen0701 <bowen0701@gmail.com>
2026-08-15 19:20:54 +0100

    Update lc0200 Number of Islands problem docstring to modern Leetcode format

--- lc0200_number_of_islands.py
+++ lc0200_number_of_islands.py
@@ -1,27 +1,38 @@
 """Leetcode 200. Number of Islands
 Medium
 
-URL: https://leetcode.com/problems/number-of-islands/description/
+URL: https://leetcode.com/problems/number-of-islands/
 
-Given a 2d grid map of '1's (land) and '0's (water), 
-count the number of islands. 
-An island is surrounded by water and is formed by connecting 
-adjacent lands horizontally or vertically. 
-You may assume all four edges of the grid are all surrounded by water.
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+'0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands
+horizontally or vertically. You may assume all four edges of the grid are all
+surrounded by water.
 
 Example 1:
-11110
-11010
-11000
-00000
-Answer: 1
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
 
 Example 2:
-11000
-11000
-00100
-00011
-Answer: 3
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+Constraints:
+- m == grid.length
+- n == grid[i].length
+- 1 <= m, n <= 300
+- grid[i][j] is '0' or '1'.
 """
 
 from typing import List, Tuple, Dict
