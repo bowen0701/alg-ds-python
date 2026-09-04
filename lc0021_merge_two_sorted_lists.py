725b7c5098c9c5e8fcf3fd3ad53ddb747832c804
bowen0701 <bowen0701@gmail.com>
2026-09-04 14:55:14 +0100

    Update lc0021_merge_two_sorted_lists.py problem docstring

--- lc0021_merge_two_sorted_lists.py
+++ lc0021_merge_two_sorted_lists.py
@@ -3,12 +3,29 @@ Easy
 
 URL: https://leetcode.com/problems/merge-two-sorted-lists/
 
-Merge two sorted linked lists and return it as a new list. 
-The new list should be made by splicing together the nodes of the first two lists.
+You are given the heads of two sorted linked lists list1 and list2.
 
-Example:
-Input: 1->2->4, 1->3->4
-Output: 1->1->2->3->4->4
+Merge the two lists into one sorted list. The list should be made by splicing
+together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+Constraints:
+- The number of nodes in both lists is in the range [0, 50].
+- -100 <= Node.val <= 100
+- Both list1 and list2 are sorted in non-decreasing order.
 """
 
 # Definition for singly-linked list.
