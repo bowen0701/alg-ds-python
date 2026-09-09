6c1105bd3f262eceb1676f4b3ad198cd28d66418
bowen0701 <bowen0701@gmail.com>
2026-09-09 23:18:02 +0100

    Update lc0300 problem docstring to current LC format

--- lc0300_longest_increasing_subsequence.py
+++ lc0300_longest_increasing_subsequence.py
@@ -3,20 +3,29 @@ Medium
 
 URL: https://leetcode.com/problems/longest-increasing-subsequence/
 
-Given an unsorted array of integers, 
-find the length of longest increasing subsequence.
+Given an integer array nums, return the length of the longest strictly
+increasing subsequence.
 
-Example:
-Input: [10,9,2,5,3,7,101,18]
-Output: 4 
-Explanation: The longest increasing subsequence is [2,3,7,101], 
-therefore the length is 4. 
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101],
+therefore the length is 4.
 
-Note:
-There may be more than one LIS combination, 
-it is only necessary for you to return the length.
-Your algorithm should run in O(n^2) complexity.
-Follow up: Could you improve it to O(n*logn) time complexity?
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+Constraints:
+- 1 <= nums.length <= 2500
+- -10^4 <= nums[i] <= 10^4
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n))
+time complexity?
 """
 
 from typing import List
