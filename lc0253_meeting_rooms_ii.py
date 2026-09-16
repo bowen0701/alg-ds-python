d7408b27e678714ad9f464fefb6d238a29476805
bowen0701 <bowen0701@gmail.com>
2026-09-16 23:00:54 +0100

    Update lc0253 problem docstring to current LC format

--- lc0253_meeting_rooms_ii.py
+++ lc0253_meeting_rooms_ii.py
@@ -1,25 +1,23 @@
 """Leetcode 253. Meeting Rooms II (Premium)
 Medium
 
-URL: https://leetcode.com/problems/meeting-rooms-ii
+URL: https://leetcode.com/problems/meeting-rooms-ii/
 
-Given an array of meeting time intervals consisting of start and end times
-[[s1,e1],[s2,e2],...] (si < ei),
-find the minimum number of conference rooms required.
+Given an array of meeting time intervals intervals where
+intervals[i] = [starti, endi], return the minimum number of conference
+rooms required.
 
-Example1
+Example 1:
 Input: intervals = [[0,30],[5,10],[15,20]]
 Output: 2
-Explanation:
-We need two meeting rooms
-room1: (0,30)
-room2: (5,10),(15,20)
 
-Example2
-Input: intervals = [[7, 10], [2, 4]]
+Example 2:
+Input: intervals = [[7,10],[2,4]]
 Output: 1
-Explanation: 
-Only need one meeting room
+
+Constraints:
+- 1 <= intervals.length <= 10^4
+- 0 <= starti < endi <= 10^6
 """
 
 class SolutionSortEndMinHeapEnd:
